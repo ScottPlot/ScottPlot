61dc3c76b94309c7d398b4cfb63934a164012b66
Scott W Harden <swharden@gmail.com>
2018-01-29 22:55:58 -0500

    improved test sequence

--- doc/testOutput/GitTests.bat
+++
@@ -1,8 +0,0 @@
-del *.png
-del *.html
-del *.md
-
-cd "C:\Users\scott\Documents\GitHub\ScottPlot\src\Examples\console\ConsoleApp1\bin\Debug"
-cp output.html ../../../../../../doc/testOutput/
-cp readme.md ../../../../../../doc/testOutput/
-cp *.png ../../../../../../doc/testOutput/
