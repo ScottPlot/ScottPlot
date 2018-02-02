8b399b265bedc4aee10a02bc9f60a230dda348b8
Scott W Harden <swharden@gmail.com>
2018-02-01 20:30:21 -0500

    resizable demo

---
+++ src/Examples/GUI/Resizable/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Resizable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 253);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "ScottPlot Stretchy Demo";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ src/Examples/GUI/Resizable/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ScottPlot;
+
+namespace Resizable
+{
+    public partial class Form1 : Form
+    {
+
+        Figure fig;
+        double[] Xs, Ys;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // create the figure and apply styling
+            fig = new Figure(pictureBox1.Width, pictureBox1.Height);
+            fig.styleForm();
+            fig.title = "Awesome Data";
+            fig.yLabel = "Random Walk";
+            fig.xLabel = "Sample Number";
+            
+            // synthesize data
+            int pointCount = 123;
+            Xs = fig.gen.Sequence(pointCount);
+            Ys = fig.gen.RandomWalk(pointCount);
+            fig.ResizeToData(Xs, Ys, .9, .9);
+        }
+        
+        private void Form1_Shown(object sender, EventArgs e) {ResizeAndRedraw();}
+        private void Form1_Resize(object sender, EventArgs e) { ResizeAndRedraw(); }
+
+        public void ResizeAndRedraw()
+        {
+            if (fig == null) return;
+            fig.Resize(pictureBox1.Width, pictureBox1.Height);
+            fig.RedrawFrame();
+            fig.PlotLines(Xs, Ys, 1, Color.Red);
+            fig.PlotScatter(Xs, Ys, 5, Color.Blue);
+            pictureBox1.Image = fig.Render();
+        }
+
+    }
+}
