5b2eb4ec5cbd9dedc66aaca2e58bbc58f53eb344
Scott W Harden <swharden@gmail.com>
2019-04-17 13:57:05 -0400

    overlayed plotting
    If a scottplot user control's "plt2" object is populated with a scottplot object, it will attempt to render the second plot and overlay it on the primary plot every time the primary plot is updated.

--- src/ScottPlot/ScottPlotUC.cs
+++ src/ScottPlot/ScottPlotUC.cs
@@ -51,23 +51,48 @@ namespace ScottPlotDev2
             }
         }
 
+        public ScottPlot.Plot plt2;
         public void Render(bool resizeToo = false)
         {
             if (resizeToo)
+            {
                 plt.settings.Resize(pb.Width, pb.Height);
-            pb.Image = plt.figure.GetBitmap();
+                if (plt2 != null)
+                {
+                    plt2.settings.Resize(plt.settings.width, plt.settings.height);
+                }
+            }
+
+            if (plt2 == null)
+            {
+                pb.Image = plt.figure.GetBitmap();
+            }
+            else
+            {
+                // if plt2 contains a ScottPlot, match its X axis to the user control then overlay it
+                plt2.settings.axisX.Set(plt.settings.axisX.x1, plt.settings.axisX.x2);
+                Bitmap bmp1 = plt.figure.GetBitmap();
+                Bitmap bmp2 = plt2.figure.GetBitmap();
+                Bitmap bmpMerged = new Bitmap(bmp1);
+                using (Graphics gfx = Graphics.FromImage(bmpMerged))
+                    gfx.DrawImage(bmp2, new Rectangle(0, 0, bmp2.Width, bmp2.Height));
+                pb.Image = bmpMerged;
+            }
             Application.DoEvents();
         }
 
         #region mouse tracking for pan and zoom
 
+        public bool disableMouse = false;
         private void pb_MouseDown(object sender, MouseEventArgs e)
         {
+            if (disableMouse) return;
             plt.settings.mouse.Down();
         }
 
         private void pb_MouseUp(object sender, MouseEventArgs e)
         {
+            if (disableMouse) return;
             plt.settings.mouse.Up();
             Render();
         }
@@ -75,6 +100,7 @@ namespace ScottPlotDev2
         private bool currentlyProcessingMoveEvent = false;
         private void pb_MouseMove(object sender, MouseEventArgs e)
         {
+            if (disableMouse) return;
             if (currentlyProcessingMoveEvent)
             {
                 return;
@@ -90,6 +116,7 @@ namespace ScottPlotDev2
 
         private void pb_MouseClick(object sender, MouseEventArgs e)
         {
+            if (disableMouse) return;
             if (e.Button == MouseButtons.Middle)
             {
                 plt.settings.AxisFit();
