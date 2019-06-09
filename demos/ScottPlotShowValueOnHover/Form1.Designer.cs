7a8043838b3f5b896e2435dd66cb266476163f1b
Scott W Harden <swharden@gmail.com>
2019-06-09 16:35:21 -0400

    add demo to highlight point on hover

---
+++ demos/ScottPlotShowValueOnHover/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ScottPlotShowValueOnHover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 341);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(623, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(118, 17);
+            this.lblStatus.Text = "toolStripStatusLabel1";
+            // 
+            // scottPlotUC1
+            // 
+            this.scottPlotUC1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scottPlotUC1.Location = new System.Drawing.Point(0, 0);
+            this.scottPlotUC1.Name = "scottPlotUC1";
+            this.scottPlotUC1.Size = new System.Drawing.Size(623, 341);
+            this.scottPlotUC1.TabIndex = 2;
+            this.scottPlotUC1.MouseMoved += new System.EventHandler(this.ScottPlotUC1_MouseMoved);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 363);
+            this.Controls.Add(this.scottPlotUC1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private ScottPlot.ScottPlotUC scottPlotUC1;
+    }
+}
+
---
+++ demos/ScottPlotShowValueOnHover/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlotShowValueOnHover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int pointCount = 50;
+            double[] Ys = ScottPlot.DataGen.Sin(pointCount, 2);
+            double[] Xs = ScottPlot.DataGen.Consecutive(pointCount);
+            scottPlotUC1.plt.PlotScatter(Xs, Ys, color: Color.Blue);
+            scottPlotUC1.plt.PlotPoint(Xs[10], Ys[10], markerSize: 10, color: Color.Red);
+            scottPlotUC1.plt.PlotText("asdf", Xs[10], Ys[10], fontSize: 10, color: Color.Black);
+            scottPlotUC1.plt.AxisAuto();
+            scottPlotUC1.Render();
+        }
+
+        private void UpdateHover()
+        {
+            var plottables = scottPlotUC1.plt.GetPlottables();
+            var scatterPlot = (ScottPlot.PlottableScatter)plottables[0];
+            var highlightScatter = (ScottPlot.PlottableScatter)plottables[1];
+            var highlightText = (ScottPlot.PlottableText)plottables[2];
+
+            // get mouse position on the screen
+            Point mouseLoc = new Point(Cursor.Position.X, Cursor.Position.Y);
+
+            // modify it to be mouse position on the ScottPlot
+            mouseLoc.X -= this.PointToScreen(scottPlotUC1.Location).X;
+            mouseLoc.Y -= this.PointToScreen(scottPlotUC1.Location).Y;
+
+            // determine the position on the screen
+            //PointF mousePos = scottPlotUC1.plt.CoordinateFromPixel(mouseLoc);
+
+            // determine which scatter point is closest to the mouse
+            int closestIndex = 0;
+            double closestDistance = double.PositiveInfinity;
+            for (int i = 0; i < scatterPlot.ys.Length; i++)
+            {
+                double dx = mouseLoc.X - scottPlotUC1.plt.CoordinateToPixel(scatterPlot.xs[i], 0).X;
+                double dy = mouseLoc.Y - scottPlotUC1.plt.CoordinateToPixel(0, scatterPlot.ys[i]).Y;
+                double distance = Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2));
+                if (closestIndex < 0)
+                {
+                    closestDistance = distance;
+                }
+                else if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestIndex = i;
+                }
+            }
+
+            highlightText.x = scatterPlot.xs[closestIndex];
+            highlightText.y = scatterPlot.ys[closestIndex];
+            highlightScatter.xs[0] = scatterPlot.xs[closestIndex];
+            highlightScatter.ys[0] = scatterPlot.ys[closestIndex];
+
+            if (closestDistance < 20)
+            {
+                highlightText.text = string.Format(
+                        "   ({0}, {1})",
+                        Math.Round(scatterPlot.xs[closestIndex], 3),
+                        Math.Round(scatterPlot.ys[closestIndex], 3)
+                    );
+                highlightScatter.markerSize = 10;
+            }
+            else
+            {
+                highlightText.text = "";
+                highlightScatter.markerSize = 0;
+            }
+            scottPlotUC1.Render();
+        }
+
+        private void ScottPlotUC1_MouseMoved(object sender, EventArgs e)
+        {
+            UpdateHover();
+        }
+    }
+}
