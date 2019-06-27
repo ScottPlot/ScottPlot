a523de699017f90900fa1fbbd46970593f4c6302
Scott W Harden <swharden@gmail.com>
2019-06-26 21:45:26 -0400

    improve log scale demo

--- src/ScottPlotDemoLogScale/Form1.cs
+++ src/ScottPlotDemoLogScale/Form1.cs
@@ -19,16 +19,25 @@ namespace ScottPlotDemoLogScale
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Random rand = new Random();
             double[] xs = ScottPlot.DataGen.Consecutive(100, 0.1, 0.0);
             double[] ys = new double[xs.Length];
             for (int i = 0; i < ys.Length; i++)
                 ys[i] = xs[i] * xs[i];
 
+            Random rand = new Random();
+            double[] xs2 = ScottPlot.DataGen.Random(rand, 100, 10);
+            double[] ys2 = ScottPlot.DataGen.Random(rand, 100, 100);
+
             scottPlotUC1.plt.Title("Log Scale Test");
             scottPlotUC1.plt.PlotScatter(xs, ys);
+            scottPlotUC1.plt.PlotScatter(xs2, ys2, lineWidth: 0);
             scottPlotUC1.plt.AxisAuto();
             scottPlotUC1.Render();
+        }
+
+        private void CbLogScale_CheckedChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
