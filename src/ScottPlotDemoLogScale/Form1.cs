05fe8cb7f8cfaa7aa6682883eeb67f7d65836ed1
Scott W Harden <swharden@gmail.com>
2019-06-26 21:30:03 -0400

    start log scale demo program

---
+++ src/ScottPlotDemoLogScale/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlotDemoLogScale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            double[] xs = ScottPlot.DataGen.Consecutive(100, 0.1, 0.0);
+            double[] ys = new double[xs.Length];
+            for (int i = 0; i < ys.Length; i++)
+                ys[i] = xs[i] * xs[i];
+
+            scottPlotUC1.plt.Title("Log Scale Test");
+            scottPlotUC1.plt.PlotScatter(xs, ys);
+            scottPlotUC1.plt.AxisAuto();
+            scottPlotUC1.Render();
+        }
+    }
+}
