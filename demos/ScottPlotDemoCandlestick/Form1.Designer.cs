c17f1995d059d45bd46ddaffd3143b1a78a52ec1
Scott W Harden <swharden@gmail.com>
2019-06-30 17:42:09 -0400

    start a candlestick demo

---
+++ demos/ScottPlotDemoCandlestick/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ScottPlotDemoCandlestick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
+            this.button1 = new System.Windows.Forms.Button();
+            this.scottPlotUC2 = new ScottPlot.ScottPlotUC();
+            this.SuspendLayout();
+            // 
+            // scottPlotUC1
+            // 
+            this.scottPlotUC1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scottPlotUC1.Location = new System.Drawing.Point(12, 12);
+            this.scottPlotUC1.Name = "scottPlotUC1";
+            this.scottPlotUC1.Size = new System.Drawing.Size(785, 311);
+            this.scottPlotUC1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Randomize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // scottPlotUC2
+            // 
+            this.scottPlotUC2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scottPlotUC2.Location = new System.Drawing.Point(12, 329);
+            this.scottPlotUC2.Name = "scottPlotUC2";
+            this.scottPlotUC2.Size = new System.Drawing.Size(785, 116);
+            this.scottPlotUC2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 457);
+            this.Controls.Add(this.scottPlotUC2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.scottPlotUC1);
+            this.Name = "Form1";
+            this.Text = "ScottPlot Candlestick Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScottPlot.ScottPlotUC scottPlotUC1;
+        private System.Windows.Forms.Button button1;
+        private ScottPlot.ScottPlotUC scottPlotUC2;
+    }
+}
+
---
+++ demos/ScottPlotDemoCandlestick/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlotDemoCandlestick
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Button1_Click(null, null);
+        }
+
+        private void GenerateNewData()
+        {
+            Random rand = new Random();
+            int pointCount = 100;
+            double[] xs = ScottPlot.DataGen.Consecutive(pointCount);
+            double[] volume = ScottPlot.DataGen.RandomNormal(rand, pointCount, 100_000, 20_000);
+            double[] priceMean = ScottPlot.DataGen.RandomWalk(rand, pointCount, 20, 75);
+            double[] priceVar = ScottPlot.DataGen.RandomNormal(rand, pointCount, 5, 2);
+
+            scottPlotUC1.plt.Clear();
+            scottPlotUC1.plt.YLabel("Share Price", fontSize: 10);
+            scottPlotUC1.plt.Title("ScottPlot Candlestick Demo");
+            scottPlotUC1.plt.PlotScatter(xs, priceMean, lineWidth: 0, errorY: priceVar, errorCapSize: 0);
+            scottPlotUC1.plt.AxisAuto();
+            scottPlotUC1.Render();
+
+            scottPlotUC2.plt.Clear();
+            scottPlotUC2.plt.YLabel("Volume", fontSize: 10);
+            scottPlotUC2.plt.PlotBar(xs, volume, barWidth: .5);
+            scottPlotUC2.plt.AxisAuto(.01, .1);
+            scottPlotUC2.plt.Axis(null, null, 0, null);
+            scottPlotUC2.Render();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            GenerateNewData();
+        }
+    }
+}
