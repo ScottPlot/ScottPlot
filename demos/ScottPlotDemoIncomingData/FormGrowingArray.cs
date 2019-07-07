387657ecdc833dcf50ff8a435c7492def29cf19c
Scott W Harden <swharden@gmail.com>
2019-07-07 12:17:27 -0400

    create incoming data demos

---
+++ demos/ScottPlotDemoIncomingData/FormGrowingArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlotDemoIncomingData
+{
+    public partial class FormGrowingArray : Form
+    {
+        Random rand = new Random();
+        double[] dataYs = new double[] { 0 }; // start with a point so it's not empty
+
+        public FormGrowingArray()
+        {
+            InitializeComponent();
+            scottPlotUC1.plt.YLabel("Value");
+            scottPlotUC1.plt.XLabel("Data Point");
+            scottPlotUC1.plt.Title("Plotting a Growing Array");
+        }
+        
+        private void CbIncoming_CheckedChanged(object sender, EventArgs e)
+        {
+            timerNewData.Enabled = cbIncoming.Checked;
+        }
+
+        private void TimerNewData_Tick(object sender, EventArgs e)
+        {
+            // simulate an array containing new incoming data
+            int newPoints = 10;
+            double[] newData = ScottPlot.DataGen.RandomWalk(rand, newPoints, 1, dataYs.Last());
+
+            // create a new full-size array to hold the old data and new data
+            double[] newArray = new double[dataYs.Length + newData.Length];
+
+            // copy the old data into the new full-size array
+            Array.Copy(dataYs, 0, newArray, 0, dataYs.Length);
+
+            // copy the new data into the new full-size array
+            Array.Copy(newData, 0, newArray, dataYs.Length, newData.Length);
+
+            // assign the new full-size array to a class-level variable so it is remembered
+            dataYs = newArray;
+
+            // let the plot timer know we need to replot
+            dataHasBeenUpdated = true;
+        }
+
+        bool dataHasBeenUpdated = false;
+        private void TimerUpdatePlot_Tick(object sender, EventArgs e)
+        {
+            if (dataHasBeenUpdated)
+            {
+                dataHasBeenUpdated = false;
+                scottPlotUC1.plt.Clear();
+                scottPlotUC1.plt.PlotSignal(dataYs);
+                scottPlotUC1.plt.AxisAuto();
+                scottPlotUC1.Render();
+            }
+        }
+    }
+}
---
+++ demos/ScottPlotDemoIncomingData/FormRollingBuffer.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ScottPlotDemoIncomingData
+{
+    partial class FormRollingBuffer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbIncoming = new System.Windows.Forms.CheckBox();
+            this.timerNewData = new System.Windows.Forms.Timer(this.components);
+            this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
+            this.SuspendLayout();
+            // 
+            // cbIncoming
+            // 
+            this.cbIncoming.AutoSize = true;
+            this.cbIncoming.Checked = true;
+            this.cbIncoming.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbIncoming.Location = new System.Drawing.Point(12, 12);
+            this.cbIncoming.Name = "cbIncoming";
+            this.cbIncoming.Size = new System.Drawing.Size(69, 17);
+            this.cbIncoming.TabIndex = 1;
+            this.cbIncoming.Text = "Incoming";
+            this.cbIncoming.UseVisualStyleBackColor = true;
+            this.cbIncoming.CheckedChanged += new System.EventHandler(this.CbIncoming_CheckedChanged);
+            // 
+            // timerNewData
+            // 
+            this.timerNewData.Enabled = true;
+            this.timerNewData.Interval = 20;
+            this.timerNewData.Tick += new System.EventHandler(this.TimerNewData_Tick);
+            // 
+            // scottPlotUC1
+            // 
+            this.scottPlotUC1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scottPlotUC1.Location = new System.Drawing.Point(0, 0);
+            this.scottPlotUC1.Name = "scottPlotUC1";
+            this.scottPlotUC1.Size = new System.Drawing.Size(800, 450);
+            this.scottPlotUC1.TabIndex = 0;
+            // 
+            // FormRollingBuffer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbIncoming);
+            this.Controls.Add(this.scottPlotUC1);
+            this.Name = "FormRollingBuffer";
+            this.Text = "FormRollingBuffer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScottPlot.ScottPlotUC scottPlotUC1;
+        private System.Windows.Forms.CheckBox cbIncoming;
+        private System.Windows.Forms.Timer timerNewData;
+    }
+}
