8aae1a24313e94925f00cf3f36668ae638e29812
Scott W Harden <swharden@gmail.com>
2019-07-22 17:22:00 -0400

    simplify core demo
    I'll give legend its own demo application

--- demos/ScottPlotDemo/Form1.Designer.cs
+++ demos/ScottPlotDemo/Form1.Designer.cs
@@ -44,17 +44,6 @@
             this.cbAntiAliasData = new System.Windows.Forms.CheckBox();
             this.cbAntiAliasFigure = new System.Windows.Forms.CheckBox();
             this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.rbMiddleRight = new System.Windows.Forms.RadioButton();
-            this.rbBottomRight = new System.Windows.Forms.RadioButton();
-            this.rbTopRight = new System.Windows.Forms.RadioButton();
-            this.rbBottomCenter = new System.Windows.Forms.RadioButton();
-            this.rbMiddleLeft = new System.Windows.Forms.RadioButton();
-            this.rbNone = new System.Windows.Forms.RadioButton();
-            this.rbBottomLeft = new System.Windows.Forms.RadioButton();
-            this.rbTopCenter = new System.Windows.Forms.RadioButton();
-            this.rbTopLeft = new System.Windows.Forms.RadioButton();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnClear
@@ -217,129 +206,19 @@
             // 
             // scottPlotUC1
             // 
-            this.scottPlotUC1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            this.scottPlotUC1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.scottPlotUC1.Location = new System.Drawing.Point(12, 70);
             this.scottPlotUC1.Name = "scottPlotUC1";
-            this.scottPlotUC1.Size = new System.Drawing.Size(831, 417);
+            this.scottPlotUC1.Size = new System.Drawing.Size(836, 389);
             this.scottPlotUC1.TabIndex = 0;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.rbMiddleRight);
-            this.groupBox1.Controls.Add(this.rbBottomRight);
-            this.groupBox1.Controls.Add(this.rbTopRight);
-            this.groupBox1.Controls.Add(this.rbBottomCenter);
-            this.groupBox1.Controls.Add(this.rbMiddleLeft);
-            this.groupBox1.Controls.Add(this.rbNone);
-            this.groupBox1.Controls.Add(this.rbBottomLeft);
-            this.groupBox1.Controls.Add(this.rbTopCenter);
-            this.groupBox1.Controls.Add(this.rbTopLeft);
-            this.groupBox1.Location = new System.Drawing.Point(835, 102);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(161, 132);
-            this.groupBox1.TabIndex = 17;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Legend";
-            // 
-            // rbMiddleRight
-            // 
-            this.rbMiddleRight.AutoSize = true;
-            this.rbMiddleRight.Location = new System.Drawing.Point(103, 42);
-            this.rbMiddleRight.Name = "rbMiddleRight";
-            this.rbMiddleRight.Size = new System.Drawing.Size(42, 17);
-            this.rbMiddleRight.TabIndex = 0;
-            this.rbMiddleRight.Text = "MR";
-            this.rbMiddleRight.UseVisualStyleBackColor = true;
-            // 
-            // rbBottomRight
-            // 
-            this.rbBottomRight.AutoSize = true;
-            this.rbBottomRight.Location = new System.Drawing.Point(103, 65);
-            this.rbBottomRight.Name = "rbBottomRight";
-            this.rbBottomRight.Size = new System.Drawing.Size(40, 17);
-            this.rbBottomRight.TabIndex = 0;
-            this.rbBottomRight.Text = "BR";
-            this.rbBottomRight.UseVisualStyleBackColor = true;
-            // 
-            // rbTopRight
-            // 
-            this.rbTopRight.AutoSize = true;
-            this.rbTopRight.Location = new System.Drawing.Point(103, 19);
-            this.rbTopRight.Name = "rbTopRight";
-            this.rbTopRight.Size = new System.Drawing.Size(40, 17);
-            this.rbTopRight.TabIndex = 0;
-            this.rbTopRight.Text = "TR";
-            this.rbTopRight.UseVisualStyleBackColor = true;
-            // 
-            // rbBottomCenter
-            // 
-            this.rbBottomCenter.AutoSize = true;
-            this.rbBottomCenter.Location = new System.Drawing.Point(59, 66);
-            this.rbBottomCenter.Name = "rbBottomCenter";
-            this.rbBottomCenter.Size = new System.Drawing.Size(39, 17);
-            this.rbBottomCenter.TabIndex = 0;
-            this.rbBottomCenter.Text = "BC";
-            this.rbBottomCenter.UseVisualStyleBackColor = true;
-            // 
-            // rbMiddleLeft
-            // 
-            this.rbMiddleLeft.AutoSize = true;
-            this.rbMiddleLeft.Location = new System.Drawing.Point(15, 43);
-            this.rbMiddleLeft.Name = "rbMiddleLeft";
-            this.rbMiddleLeft.Size = new System.Drawing.Size(40, 17);
-            this.rbMiddleLeft.TabIndex = 0;
-            this.rbMiddleLeft.Text = "ML";
-            this.rbMiddleLeft.UseVisualStyleBackColor = true;
-            // 
-            // rbNone
-            // 
-            this.rbNone.AutoSize = true;
-            this.rbNone.Checked = true;
-            this.rbNone.Location = new System.Drawing.Point(15, 94);
-            this.rbNone.Name = "rbNone";
-            this.rbNone.Size = new System.Drawing.Size(51, 17);
-            this.rbNone.TabIndex = 0;
-            this.rbNone.TabStop = true;
-            this.rbNone.Text = "None";
-            this.rbNone.UseVisualStyleBackColor = true;
-            // 
-            // rbBottomLeft
-            // 
-            this.rbBottomLeft.AutoSize = true;
-            this.rbBottomLeft.Location = new System.Drawing.Point(15, 66);
-            this.rbBottomLeft.Name = "rbBottomLeft";
-            this.rbBottomLeft.Size = new System.Drawing.Size(38, 17);
-            this.rbBottomLeft.TabIndex = 0;
-            this.rbBottomLeft.Text = "BL";
-            this.rbBottomLeft.UseVisualStyleBackColor = true;
-            // 
-            // rbTopCenter
-            // 
-            this.rbTopCenter.AutoSize = true;
-            this.rbTopCenter.Location = new System.Drawing.Point(59, 20);
-            this.rbTopCenter.Name = "rbTopCenter";
-            this.rbTopCenter.Size = new System.Drawing.Size(39, 17);
-            this.rbTopCenter.TabIndex = 0;
-            this.rbTopCenter.Text = "TC";
-            this.rbTopCenter.UseVisualStyleBackColor = true;
-            // 
-            // rbTopLeft
-            // 
-            this.rbTopLeft.AutoSize = true;
-            this.rbTopLeft.Location = new System.Drawing.Point(15, 20);
-            this.rbTopLeft.Name = "rbTopLeft";
-            this.rbTopLeft.Size = new System.Drawing.Size(38, 17);
-            this.rbTopLeft.TabIndex = 0;
-            this.rbTopLeft.Text = "TL";
-            this.rbTopLeft.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 499);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(860, 471);
             this.Controls.Add(this.cbAntiAliasFigure);
             this.Controls.Add(this.cbAntiAliasData);
             this.Controls.Add(this.btnText);
@@ -359,8 +238,6 @@
             this.Name = "Form1";
             this.Text = "ScottPlot Demo";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -384,16 +261,6 @@
         private System.Windows.Forms.Button btnText;
         private System.Windows.Forms.CheckBox cbAntiAliasData;
         private System.Windows.Forms.CheckBox cbAntiAliasFigure;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton rbMiddleRight;
-        private System.Windows.Forms.RadioButton rbBottomRight;
-        private System.Windows.Forms.RadioButton rbTopRight;
-        private System.Windows.Forms.RadioButton rbBottomCenter;
-        private System.Windows.Forms.RadioButton rbMiddleLeft;
-        private System.Windows.Forms.RadioButton rbNone;
-        private System.Windows.Forms.RadioButton rbBottomLeft;
-        private System.Windows.Forms.RadioButton rbTopCenter;
-        private System.Windows.Forms.RadioButton rbTopLeft;
     }
 }
 
