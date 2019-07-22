97c47e5b7cf8690bf9e2441c786821ef839d7ee8
Scott W Harden <swharden@gmail.com>
2019-07-22 18:06:59 -0400

    create standalone legend demo

---
+++ demos/ScottPlotLegend/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Legend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbLocations = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbLocations
+            // 
+            this.cbLocations.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbLocations.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLocations.FormattingEnabled = true;
+            this.cbLocations.Location = new System.Drawing.Point(6, 19);
+            this.cbLocations.Name = "cbLocations";
+            this.cbLocations.Size = new System.Drawing.Size(188, 21);
+            this.cbLocations.TabIndex = 0;
+            this.cbLocations.SelectedIndexChanged += new System.EventHandler(this.CbLocations_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbLocations);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 47);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Legend Location";
+            // 
+            // scottPlotUC1
+            // 
+            this.scottPlotUC1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scottPlotUC1.Location = new System.Drawing.Point(12, 65);
+            this.scottPlotUC1.Name = "scottPlotUC1";
+            this.scottPlotUC1.Size = new System.Drawing.Size(610, 354);
+            this.scottPlotUC1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 431);
+            this.Controls.Add(this.scottPlotUC1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "ScottPlot Legend Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbLocations;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private ScottPlot.ScottPlotUC scottPlotUC1;
+    }
+}
+
---
+++ demos/ScottPlotLegend/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Legend
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] legendLocationStrings = Enum.GetNames(typeof(ScottPlot.legendLocation));
+            cbLocations.Items.AddRange(legendLocationStrings);
+            cbLocations.SelectedItem = cbLocations.Items[0];
+
+            Random rand = new Random();
+            int pointCount = 100;
+            double[] xs = ScottPlot.DataGen.Consecutive(pointCount);
+            double[] ys1 = ScottPlot.DataGen.RandomWalk(rand, pointCount);
+            double[] ys2 = ScottPlot.DataGen.RandomWalk(rand, pointCount);
+            double[] ys3 = ScottPlot.DataGen.RandomWalk(rand, pointCount);
+            scottPlotUC1.plt.PlotScatter(xs, ys1, label: "one");
+            scottPlotUC1.plt.PlotScatter(xs, ys2, label: "two");
+            scottPlotUC1.plt.PlotScatter(xs, ys3, label: "three");
+            scottPlotUC1.Render();
+        }
+
+        private void CbLocations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string locationString = cbLocations.SelectedItem.ToString();
+            ScottPlot.legendLocation location = (ScottPlot.legendLocation)Enum.Parse(typeof(ScottPlot.legendLocation), locationString);
+            Console.WriteLine($"legend location: {location}");
+            scottPlotUC1.plt.Legend(location: location);
+            scottPlotUC1.Render();
+        }
+    }
+}
