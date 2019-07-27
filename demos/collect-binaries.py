3bd048fb52ea85869fa6bb19cb6c949dc70d86ec
Scott W Harden <swharden@gmail.com>
2019-07-27 13:08:29 -0400

    move demos

---
+++ demos/collect-binaries.py
@@ -0,0 +1,43 @@
+"""
+The purpose of this script is to find all binaries in the demos src folder and
+copy them into the bin folder.
+"""
+
+import os
+import glob
+import shutil
+
+PATH_HERE = os.path.abspath(os.path.dirname(__file__))
+
+def cleanFolder():
+    for path in glob.glob(f"{PATH_HERE}/bin/*"):
+        print(f"deleting {os.path.basename(path)}")
+        os.remove(path)
+
+def copyExes():
+    for pathIn in glob.glob(f"{PATH_HERE}/src/*/bin/Debug/*.exe"):
+        fileName = os.path.basename(pathIn)
+        pathOut = os.path.join(f"{PATH_HERE}/bin/{fileName}")
+        print(f"copying {fileName}")
+        shutil.copy(pathIn, pathOut)
+
+
+def copyDll():
+    pathIn = os.path.abspath(f"{PATH_HERE}/../src/ScottPlot/bin/Debug/")
+    pathIn = os.path.join(pathIn, "ScottPlot.dll")
+    pathOut = os.path.abspath(os.path.join(f"{PATH_HERE}/bin/ScottPlot.dll"))
+    print(f"copying ScottPlot.dll")
+    shutil.copy(pathIn, pathOut)
+
+def createZip():
+    zipFilePath = os.path.join(PATH_HERE, "ScottPlot-Demos")
+    binFolderPath = os.path.join(PATH_HERE, "bin")
+    shutil.make_archive(zipFilePath, 'zip', binFolderPath)
+    print("creating zip file")
+
+if __name__ == "__main__":
+    cleanFolder()
+    copyExes()
+    copyDll()
+    createZip()
+    print("DONE")
