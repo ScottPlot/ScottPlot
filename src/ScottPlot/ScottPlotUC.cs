f5ad2f80e24d8c4b7dadc2121cc62aae272a5600
Scott W Harden <swharden@gmail.com>
2019-07-29 21:22:27 -0400

    refactored user control

--- src/ScottPlot/ScottPlotUC.cs
+++ src/ScottPlot/ScottPlotUC.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 
@@ -15,18 +9,15 @@ namespace ScottPlot
     {
         public Plot plt = new Plot();
         private bool currentlyRendering = false;
-        private ContextMenuStrip rightClickMenu;
+        private readonly ContextMenuStrip rightClickMenu = UserControlTools.GetRightClickMenu();
 
         public ScottPlotUC()
         {
             InitializeComponent();
-
             plt.Style(ScottPlot.Style.Control);
             pbPlot.MouseWheel += PbPlot_MouseWheel;
-            RightClickMenuSetup();
             if (Process.GetCurrentProcess().ProcessName == "devenv")
                 ScottPlot.UserControlTools.DesignerModeDemoPlot(plt);
-
             PbPlot_SizeChanged(null, null);
         }
 
@@ -45,10 +36,8 @@ namespace ScottPlot
         private void PbPlot_SizeChanged(object sender, EventArgs e)
         {
             plt.Resize(Width, Height);
-            Render();
+            Render(skipIfCurrentlyRendering: false);
         }
-
-        #region mouse events
 
         private void PbPlot_MouseDown(object sender, MouseEventArgs e)
         {
@@ -87,25 +76,22 @@ namespace ScottPlot
             }
         }
 
-
         private void PbPlot_MouseUp(object sender, MouseEventArgs e)
         {
             plt.mouseTracker.MouseUp(e.Location);
-            Render(true);
+            Render(skipIfCurrentlyRendering: false);
             if (plt.mouseTracker.PlottableUnderCursor(e.Location) != null)
                 OnMouseDropPlottable(EventArgs.Empty);
         }
 
         private void PbPlot_MouseClick(object sender, MouseEventArgs e)
         {
-            double mouseDownMsec = plt.mouseTracker.mouseDownStopwatch.ElapsedMilliseconds;
             if (e.Button == MouseButtons.Middle)
             {
                 plt.AxisAuto();
-                OnMouseDragged(EventArgs.Empty);
-                Render();
+                Render(skipIfCurrentlyRendering: false);
             }
-            else if (e.Button == MouseButtons.Right && mouseDownMsec < 100)
+            else if (e.Button == MouseButtons.Right && plt.mouseTracker.mouseDownStopwatch.ElapsedMilliseconds < 100)
             {
                 rightClickMenu.Show(pbPlot, PointToClient(Cursor.Position));
                 rightClickMenu.ItemClicked += new ToolStripItemClickedEventHandler(RightClickMenuItemClicked);
@@ -115,7 +101,7 @@ namespace ScottPlot
         private void PbPlot_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             plt.Benchmark(toggle: true);
-            Render();
+            Render(skipIfCurrentlyRendering: false);
         }
 
         private void PbPlot_MouseWheel(object sender, MouseEventArgs e)
@@ -126,108 +112,24 @@ namespace ScottPlot
                 plt.AxisZoom(1 + zoomAmount, 1 + zoomAmount, zoomCenter);
             else
                 plt.AxisZoom(1 - zoomAmount, 1 - zoomAmount, zoomCenter);
-            Render();
-        }
-
-        #endregion
-
-        #region right click menu
-
-        private void RightClickMenuSetup()
-        {
-            rightClickMenu = new ContextMenuStrip();
-            rightClickMenu.Items.Add("Save Image");
-            rightClickMenu.Items.Add("Auto-Axis");
-            rightClickMenu.Items.Add("Clear");
-            rightClickMenu.Items.Add(new ToolStripSeparator());
-            rightClickMenu.Items.Add("Help");
-            int i = rightClickMenu.Items.Count - 1;
-            (rightClickMenu.Items[i] as ToolStripMenuItem).DropDownItems.Add("left-click-drag to pan");
-            (rightClickMenu.Items[i] as ToolStripMenuItem).DropDownItems.Add("right-click-drag to zoom");
-            (rightClickMenu.Items[i] as ToolStripMenuItem).DropDownItems.Add("middle-click for auto-axis");
-            (rightClickMenu.Items[i] as ToolStripMenuItem).DropDownItems.Add("double-click to toggle benchmark");
-            (rightClickMenu.Items[i] as ToolStripMenuItem).DropDownItems[0].Enabled = false;
-            (rightClickMenu.Items[i] as ToolStripMenuItem).DropDownItems[1].Enabled = false;
-            (rightClickMenu.Items[i] as ToolStripMenuItem).DropDownItems[2].Enabled = false;
-            (rightClickMenu.Items[i] as ToolStripMenuItem).DropDownItems[3].Enabled = false;
-            rightClickMenu.Items.Add(new ToolStripSeparator());
-            rightClickMenu.Items.Add("About ScottPlot");
+            Render(skipIfCurrentlyRendering: false);
         }
 
         private void RightClickMenuItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
-            ToolStripItem item = e.ClickedItem;
-            switch (item.ToString())
-            {
-                case "Save Image":
-                    rightClickMenu.Hide();
-                    SaveFileDialog savefile = new SaveFileDialog();
-                    savefile.FileName = "ScottPlot.png";
-                    savefile.Filter = "PNG Files (*.png)|*.png|All files (*.*)|*.*";
-                    if (savefile.ShowDialog() == DialogResult.OK)
-                        plt.SaveFig(savefile.FileName);
-                    break;
-                case "Auto-Axis":
-                    rightClickMenu.Hide();
-                    plt.AxisAuto();
-                    OnMouseDragged(EventArgs.Empty);
-                    Render();
-                    break;
-                case "Clear":
-                    rightClickMenu.Hide();
-                    plt.Clear();
-                    Render();
-                    break;
-                case "About ScottPlot":
-                    rightClickMenu.Hide();
-                    System.Diagnostics.Process.Start("https://github.com/swharden/ScottPlot");
-                    break;
-            }
+            UserControlTools.RightClickMenuItemClicked(e.ClickedItem, rightClickMenu, plt);
+            Render(skipIfCurrentlyRendering: false);
         }
-
-        #endregion
-
-        #region custom events
 
         public event EventHandler MouseDownOnPlottable;
-        protected virtual void OnMouseDownOnPlottable(EventArgs e)
-        {
-            var handler = MouseDownOnPlottable;
-            if (handler != null)
-                handler(this, e);
-        }
-
         public event EventHandler MouseDragPlottable;
-        protected virtual void OnMouseDragPlottable(EventArgs e)
-        {
-            var handler = MouseDragPlottable;
-            if (handler != null)
-                handler(this, e);
-        }
-
         public event EventHandler MouseMoved;
-        protected virtual void OnMouseMoved(EventArgs e)
-        {
-            var handler = MouseMoved;
-            if (handler != null)
-                handler(this, e);
-        }
-
         public event EventHandler MouseDragged;
-        protected virtual void OnMouseDragged(EventArgs e)
-        {
-            var handler = MouseDragged;
-            if (handler != null)
-                handler(this, e);
-        }
-
         public event EventHandler MouseDropPlottable;
-        protected virtual void OnMouseDropPlottable(EventArgs e)
-        {
-            var handler = MouseDropPlottable;
-            if (handler != null)
-                handler(this, e);
-        }
-        #endregion
+        protected virtual void OnMouseDownOnPlottable(EventArgs e) { MouseDownOnPlottable?.Invoke(this, e); }
+        protected virtual void OnMouseDragPlottable(EventArgs e) { MouseDragPlottable?.Invoke(this, e); }
+        protected virtual void OnMouseMoved(EventArgs e) { MouseMoved?.Invoke(this, e); }
+        protected virtual void OnMouseDragged(EventArgs e) { MouseDragged?.Invoke(this, e); }
+        protected virtual void OnMouseDropPlottable(EventArgs e) { MouseDropPlottable?.Invoke(this, e); }
     }
 }
--- src/ScottPlot/UserControlTools.cs
+++ src/ScottPlot/UserControlTools.cs
@@ -23,5 +23,54 @@ namespace ScottPlot
             plt.Title("ScottPlot User Control");
             plt.YLabel("Sample Data");
         }
+
+        public static ContextMenuStrip GetRightClickMenu()
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+            menu.Items.Add("Save Image");
+            menu.Items.Add("Auto-Axis");
+            menu.Items.Add("Clear");
+            menu.Items.Add(new ToolStripSeparator());
+            menu.Items.Add("Help");
+            int i = menu.Items.Count - 1;
+            (menu.Items[i] as ToolStripMenuItem).DropDownItems.Add("left-click-drag to pan");
+            (menu.Items[i] as ToolStripMenuItem).DropDownItems.Add("right-click-drag to zoom");
+            (menu.Items[i] as ToolStripMenuItem).DropDownItems.Add("middle-click for auto-axis");
+            (menu.Items[i] as ToolStripMenuItem).DropDownItems.Add("double-click to toggle benchmark");
+            (menu.Items[i] as ToolStripMenuItem).DropDownItems[0].Enabled = false;
+            (menu.Items[i] as ToolStripMenuItem).DropDownItems[1].Enabled = false;
+            (menu.Items[i] as ToolStripMenuItem).DropDownItems[2].Enabled = false;
+            (menu.Items[i] as ToolStripMenuItem).DropDownItems[3].Enabled = false;
+            menu.Items.Add(new ToolStripSeparator());
+            menu.Items.Add("About ScottPlot");
+            return menu;
+        }
+
+        public static void RightClickMenuItemClicked(ToolStripItem item, ContextMenuStrip rightClickMenu, ScottPlot.Plot plt)
+        {
+            switch (item.ToString())
+            {
+                case "Save Image":
+                    rightClickMenu.Hide();
+                    SaveFileDialog savefile = new SaveFileDialog();
+                    savefile.FileName = "ScottPlot.png";
+                    savefile.Filter = "PNG Files (*.png)|*.png|All files (*.*)|*.*";
+                    if (savefile.ShowDialog() == DialogResult.OK)
+                        plt.SaveFig(savefile.FileName);
+                    break;
+                case "Auto-Axis":
+                    rightClickMenu.Hide();
+                    plt.AxisAuto();
+                    break;
+                case "Clear":
+                    rightClickMenu.Hide();
+                    plt.Clear();
+                    break;
+                case "About ScottPlot":
+                    rightClickMenu.Hide();
+                    System.Diagnostics.Process.Start("https://github.com/swharden/ScottPlot");
+                    break;
+            }
+        }
     }
 }
