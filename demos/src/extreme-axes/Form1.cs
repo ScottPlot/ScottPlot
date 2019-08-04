8b4ca08b03e16221410a6694f3c85399e780cca2
Scott W Harden <swharden@gmail.com>
2019-08-04 14:59:41 -0400

    create demo app to test large and small axes

---
+++ demos/src/extreme-axes/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace extreme_axes
+{
+    public partial class Form1 : Form
+    {
+        double bigNumber = 987654321 * 10e50;
+        double smallNumber = 123456789 * 10e-50;
+
+        public Form1()
+        {
+            InitializeComponent();
+            scottPlotUC1.plt.PlotSignal(ScottPlot.DataGen.Sin(1000), lineWidth: 3);
+            scottPlotUC1.plt.AxisAuto();
+            scottPlotUC1.Render();
+        }
+
+        private void BtnNormal_Click(object sender, EventArgs e)
+        {
+            scottPlotUC1.plt.AxisAuto();
+            scottPlotUC1.Render();
+        }
+
+        private void BtnBig_Click(object sender, EventArgs e)
+        {
+            scottPlotUC1.plt.Axis(-bigNumber, bigNumber, -bigNumber, bigNumber);
+            scottPlotUC1.Render();
+        }
+
+        private void BtnSmall_Click(object sender, EventArgs e)
+        {
+            scottPlotUC1.plt.Axis(-smallNumber, smallNumber, -smallNumber, smallNumber);
+            scottPlotUC1.Render();
+        }
+
+        private void ButtonBigOffset_Click(object sender, EventArgs e)
+        {
+            scottPlotUC1.plt.Axis(bigNumber, bigNumber + bigNumber / 10, bigNumber, bigNumber + bigNumber / 10);
+            scottPlotUC1.Render();
+        }
+
+        private void BtnSmallOffset_Click(object sender, EventArgs e)
+        {
+            scottPlotUC1.plt.Axis(smallNumber, smallNumber + smallNumber / 10, smallNumber, smallNumber + smallNumber / 10);
+            scottPlotUC1.Render();
+        }
+
+        private void BtnWav_Click(object sender, EventArgs e)
+        {
+            // simulate zooming in to a 10-millisecond window at the 5-minute mark of a wav file
+            double time1 = 5 * 60;
+            double time2 = time1 + .01;
+            scottPlotUC1.plt.Axis(time1, time2, -2e16, 2e16);
+            scottPlotUC1.Render();
+        }
+    }
+}
