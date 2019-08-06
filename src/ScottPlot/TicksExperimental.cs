6b0428aaaf54959ea7c64d631a563736b5227f8e
Alessandro Mandelli <mandelli.alessandro@ngi.it>
2019-08-06 11:34:38 +0200

    Update to TicksExperimental for time domains ticks calculation. Update to console application to reflect new methods.

--- src/ScottPlot/TicksExperimental.cs
+++ src/ScottPlot/TicksExperimental.cs
@@ -3,15 +3,27 @@
  */
 
 using System;
+using System.Globalization;
 
 namespace ScottPlot
 {
-    public class TicksExperimental
+    public class IncorrectTime : Exception
+    {
+    }
+        public class TicksExperimental
     {
         private static double[] intervals = { 1.0, 2.0, 2.5, 3.0, 5.0, 10.0 };
+        private static double[] int_intervals = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0 };
+        private static double[] int_12_intervals = { 1.0, 2.0, 3.0, 4.0, 6.0, 12.0 };
+        private static double[] int_60_intervals = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 10.0, 12.0, 15.0, 20.0, 30.0 };
 
-        public static double[] GetTicks(double lo, double hi, int ticks = 5, double basee = 10.0)
+
+
+        public static double[] GetTicks(double lo, double hi, int ticks = 5, double[] intervals = null, double basee = 10.0)
         {
+
+            if (intervals == null)
+                intervals = TicksExperimental.intervals;
 
             if (lo > hi)
                 throw new Exception("Low value greater than high value.");
@@ -24,8 +36,8 @@ namespace ScottPlot
                 delta_x = hi - lo;
                 if (delta_x == 0)
                 {
-                    lo = lo - 0.5;
-                    hi = hi + 0.5;
+                    lo -= 0.5;
+                    hi += 0.5;
                     delta_x = hi - lo;
                 }
             }
@@ -52,7 +64,7 @@ namespace ScottPlot
             {
                 Array.Resize(ref tickPositions, tickPositions.Length + 1);
                 tickPositions[tickPositions.Length - 1] = t;
-                t = t + delta_t;
+                t += delta_t;
             }
 
             return tickPositions;
@@ -74,7 +86,6 @@ namespace ScottPlot
                 return result;
             return intervals[intervals.Length - 1] * z;
         }
-
         private static double GetFloor(double x, double[] intervals, double basee = 10.0)
         {
             if (intervals == null)
@@ -93,7 +104,6 @@ namespace ScottPlot
             }
             return intervals[0] * z;
         }
-
         private static double GetRound(double x, double[] intervals, double basee = 10.0)
         {
             if (intervals == null)
@@ -113,6 +123,220 @@ namespace ScottPlot
             }
             return intervals[intervals.Length - 1] * z;
 
+        }
+
+        private static double[] GetYearTicks(DateTime lo, DateTime hi, int ticks = 5, double basee = 10.0)
+        {
+            int lo_year = lo.Year;
+            int hi_year = hi.Year;
+
+            if (hi > new DateTime(hi_year, 1, 1))
+                hi_year += 1;
+            if (hi_year - lo_year < (ticks - 1))
+                throw new IncorrectTime();
+            double[] result = GetTicks(lo_year, hi_year, ticks, intervals: int_intervals, basee);
+
+            return result;
+        }
+
+        private static double[] GetMonthTicks(DateTime lo, DateTime hi, int ticks = 5, double basee = 10.0)
+        {
+            int lo_year = lo.Year;
+            int hi_year = hi.Year;
+            int lo_month = lo.Month;
+            int hi_month = hi.Month;
+
+            if (hi > new DateTime(hi_year, hi_month, 1))
+                if (hi_month == 11)
+                {
+                    hi_month = 0;
+                    hi_year += 1;
+                }
+                else
+                {
+                    hi_month += 1;
+                }
+            int delta_year = hi_year - lo_year;
+            if (delta_year * 12 + hi_month - lo_month < (ticks - 1))
+                throw new IncorrectTime();
+
+            double[] result = GetTicks(lo_month, delta_year * 12 + hi_month, ticks, intervals: int_12_intervals, basee);
+
+            return result;
+        }
+
+        private static double[] GetWeekTicks(DateTime lo, DateTime hi, int ticks = 5, double basee = 10.0)
+        {
+            Calendar myCalendar = CultureInfo.InvariantCulture.Calendar;
+            int lo_week = myCalendar.GetWeekOfYear(lo, rule: CalendarWeekRule.FirstFourDayWeek, firstDayOfWeek: DayOfWeek.Monday);
+            int hi_week = myCalendar.GetWeekOfYear(hi, rule: CalendarWeekRule.FirstFourDayWeek, firstDayOfWeek: DayOfWeek.Monday);
+
+            int ts_lo = lo.Subtract(new DateTime(1970, 01, 01)).Days;
+            int ts_hi = hi.Subtract(new DateTime(1970, 01, 01)).Days;
+
+            lo_week += ts_lo / 7;
+            hi_week += ts_hi / 7;
+
+            int delta_weeks = hi_week - lo_week;
+            if (delta_weeks < (ticks - 1))
+                throw new IncorrectTime();
+
+            double[] result = GetTicks(0, delta_weeks, ticks, intervals: int_intervals, basee);
+
+            return result;
+        }
+
+        private static double[] GetDayTicks(DateTime lo, DateTime hi, int ticks = 5, double basee = 10.0)
+        {
+            Calendar myCalendar = CultureInfo.InvariantCulture.Calendar;
+            int lo_day = myCalendar.GetDayOfYear(lo);
+            int hi_day = myCalendar.GetDayOfYear(hi);
+
+            if (hi.Day > hi_day)
+                hi_day += 1;
+
+            int delta_days = hi_day - lo_day;
+            if (delta_days < (ticks - 1))
+                throw new IncorrectTime();
+
+            double[] result = GetTicks(0, delta_days, ticks, intervals: int_intervals, basee);
+
+            return result;
+        }
+
+        private static double[] GetHourTicks(DateTime lo, DateTime hi, int ticks = 5, double basee = 10.0)
+        {
+            Calendar myCalendar = CultureInfo.InvariantCulture.Calendar;
+            int lo_hour = myCalendar.GetHour(lo);
+            int hi_hour = myCalendar.GetHour(hi);
+
+            if (hi.Hour > hi_hour)
+                hi_hour += 1;
+
+            int delta_hours = hi_hour - lo_hour;
+            if (delta_hours < (ticks - 1))
+                throw new IncorrectTime();
+
+            double[] result = GetTicks(0, delta_hours, ticks, intervals: int_12_intervals, basee: 24.0);
+
+            return result;
+        }
+
+        private static double[] GetMinuteTicks(DateTime lo, DateTime hi, int ticks = 5, double basee = 60.0)
+        {
+            Calendar myCalendar = CultureInfo.InvariantCulture.Calendar;
+            int lo_minute = myCalendar.GetMinute(lo);
+            int hi_minute = myCalendar.GetMinute(hi);
+
+            if (hi.Minute > hi_minute)
+                hi_minute += 1;
+
+            int delta_minute = hi_minute - lo_minute;
+            if (delta_minute < (ticks - 1))
+                throw new IncorrectTime();
+
+            double[] result = GetTicks(0, delta_minute, ticks, intervals: int_60_intervals, basee: 60.0);
+
+            return result;
+        }
+
+        private static double[] GetSecondTicks(DateTime lo, DateTime hi, int ticks = 5, double basee = 60.0)
+        {
+            Calendar myCalendar = CultureInfo.InvariantCulture.Calendar;
+            int lo_second = myCalendar.GetSecond(lo);
+            int hi_second = myCalendar.GetSecond(hi);
+
+            if (hi.Second > hi_second)
+                hi_second += 1;
+
+            int delta_second = hi_second - lo_second;
+            if (delta_second < (ticks - 1))
+                throw new IncorrectTime();
+
+            double[] result = GetTicks(0, delta_second, ticks, intervals: int_60_intervals, basee: 60.0);
+
+            return result;
+        }
+
+        private static double[] GetMillisecondTicks(DateTime lo, DateTime hi, int ticks = 5, double basee = 10.0)
+        {
+            Calendar myCalendar = CultureInfo.InvariantCulture.Calendar;
+            int lo_millisecond = (int)myCalendar.GetMilliseconds(lo);
+            int hi_millisecond = (int)myCalendar.GetMilliseconds(hi);
+
+            if (hi.Millisecond > hi_millisecond)
+                hi_millisecond += 1;
+
+            int delta_millisecond = hi_millisecond - lo_millisecond;
+            if (delta_millisecond < (ticks - 1))
+                throw new ArgumentException();
+
+            double[] result = GetTicks(0, delta_millisecond, ticks, intervals: int_intervals, basee: 10.0);
+
+            return result;
+        }
+
+
+
+        public static double[] GetTicksForTime(DateTime lo, DateTime hi, int ticks = 5, double basee = 10.0)
+        {
+            try
+            {
+                return GetYearTicks(lo, hi, ticks, basee);
+            }
+            catch (IncorrectTime)
+            {
+                try
+                {
+                    return GetMonthTicks(lo, hi, ticks, basee);
+                }
+                catch (IncorrectTime)
+                {
+                    try
+                    {
+                        return GetWeekTicks(lo, hi, ticks, basee);
+                    }
+                    catch (IncorrectTime)
+                    {
+                        try
+                        {
+                            return GetDayTicks(lo, hi, ticks, basee);
+                        }
+                        catch (IncorrectTime)
+                        {
+                            try
+                            {
+                                return GetHourTicks(lo, hi, ticks, basee);
+                            }
+                            catch (IncorrectTime)
+                            {
+                                try
+                                {
+                                    return GetMinuteTicks(lo, hi, ticks, basee);
+                                }
+                                catch (IncorrectTime)
+                                {
+                                    try
+                                    {
+                                        return GetSecondTicks(lo, hi, ticks, basee);
+                                    }
+                                    catch (IncorrectTime)
+                                    {
+                                        try
+                                        {
+                                            return GetMillisecondTicks(lo, hi, ticks, basee);
+                                        }
+                                        catch (ArgumentException)
+                                        {
+                                            throw new ArgumentException();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
     }
