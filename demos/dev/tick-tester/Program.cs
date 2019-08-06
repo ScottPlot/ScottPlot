4fb1a3cef0913943528ef002082b1a02332fc637
Alessandro Mandelli <mandelli.alessandro@ngi.it>
2019-08-06 11:34:38 +0200

    Update to TicksExperimental for time domains ticks calculation. Update to console application to reflect new methods.

--- demos/dev/tick-tester/Program.cs
+++ demos/dev/tick-tester/Program.cs
@@ -9,6 +9,8 @@ namespace tick_tester
     class Program
     {
         static void Main(string[] args)
+        //all comments to be stripped out when done
+        //right click on output window and uncheck "Exception messages" to clean output
         {
             DisplayTicksForRange(-10, 10);
             DisplayTicksForRange(-50, 50);
@@ -16,6 +18,16 @@ namespace tick_tester
             DisplayTicksForRange(-12345678, -12345678+1);
             DisplayTicksForRange(-1234567812345678, -1234567812345678 + 1);
             DisplayTicksForRange(.00000010, .00000011);
+            DisplayTicksForTime(new DateTime(1492, 10, 12), new DateTime(1776, 10, 3)); //years
+            DisplayTicksForTime(new DateTime(1915, 1, 1), new DateTime(1918, 1, 1));  //months
+            DisplayTicksForTime(new DateTime(2019, 6, 1, 6, 0, 0), new DateTime(2019, 6, 1, 11, 12, 15)); //hours
+            DisplayTicksForTime(new DateTime(2019, 6, 1, 6, 0, 0), new DateTime(2019, 6, 1, 6, 12, 15)); //minutes
+            DisplayTicksForTime(new DateTime(2019, 6, 1, 6, 12, 0), new DateTime(2019, 6, 1, 6, 12, 15)); //seconds
+            DisplayTicksForTime(DateTime.Now, DateTime.Now); //this returns the milliseconds between 
+                                                             //two samples of Datetime.Now
+                                                             //6ms on average on my PC
+            Console.WriteLine();
+
         }
 
         static void DisplayTicksForRange(double low, double high)
@@ -26,5 +38,15 @@ namespace tick_tester
                 Console.Write(tickPosition + " ");
             Console.WriteLine();
         }
+        static void DisplayTicksForTime(DateTime low, DateTime high)
+        {
+
+            double[] tickPositions = ScottPlot.TicksExperimental.GetTicksForTime(low, high);
+            Console.Write($"{tickPositions.Length} ticks in [{low} - {high}]: ");
+            foreach (double tickPosition in tickPositions)
+                Console.Write(tickPosition + " ");
+            Console.WriteLine();
+        }
+
     }
 }
