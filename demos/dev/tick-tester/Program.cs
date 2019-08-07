be1d2866f014b5fc1a20b9e72d5496ec80ce5f76
Alessandro Mandelli <mandelli.alessandro@ngi.it>
2019-08-07 12:35:55 +0200

    Calculation of exponent, unicode characters for superscript, autocalculation of offset and exponent of offset.

--- demos/dev/tick-tester/Program.cs
+++ demos/dev/tick-tester/Program.cs
@@ -18,24 +18,39 @@ namespace tick_tester
             DisplayTicksForRange(-12345678, -12345678+1);
             DisplayTicksForRange(-1234567812345678, -1234567812345678 + 1);
             DisplayTicksForRange(.00000010, .00000011);
+            DisplayTicksForRange(.0000001000001, .0000001000002); //does not render correctly
+            DisplayTicksForRange(100000001, 200000001);
+
             DisplayTicksForTime(new DateTime(1492, 10, 12), new DateTime(1776, 10, 3)); //years
             DisplayTicksForTime(new DateTime(1915, 1, 1), new DateTime(1918, 1, 1));  //months
             DisplayTicksForTime(new DateTime(2019, 6, 1, 6, 0, 0), new DateTime(2019, 6, 1, 11, 12, 15)); //hours
             DisplayTicksForTime(new DateTime(2019, 6, 1, 6, 0, 0), new DateTime(2019, 6, 1, 6, 12, 15)); //minutes
             DisplayTicksForTime(new DateTime(2019, 6, 1, 6, 12, 0), new DateTime(2019, 6, 1, 6, 12, 15)); //seconds
-            DisplayTicksForTime(DateTime.Now, DateTime.Now); //this returns the milliseconds between 
-                                                             //two samples of Datetime.Now
-                                                             //6ms on average on my PC
+            DisplayTicksForTime(DateTime.Now, DateTime.Now); 
+            //this returns the milliseconds between 
+            //two samples of Datetime.Now
+            //6ms on average on my PC
             Console.WriteLine();
 
         }
 
         static void DisplayTicksForRange(double low, double high)
         {
-            double[] tickPositions = ScottPlot.TicksExperimental.GetTicks(low, high);
-            Console.Write($"{tickPositions.Length} ticks in [{low} - {high}]: ");
-            foreach (double tickPosition in tickPositions)
-                Console.Write(tickPosition + " ");
+            double[] tickPositions = new double[] { };
+
+            double[] tickPositionsMantissas;
+            int tickPositionsExponent;
+            double offset;
+
+            tickPositions = ScottPlot.TicksExperimental.GetTicks(low, high);
+            ScottPlot.TicksExperimental.GetMantissasExponentOffset(tickPositions, out tickPositionsMantissas, out tickPositionsExponent, out offset);
+            string multiplierString = ScottPlot.TicksExperimental.GetMultiplierString(offset, tickPositionsExponent);
+
+            Console.Write($"{tickPositionsMantissas.Length} ticks in [{low} - {high}]: ");
+            foreach (double tickPositionsMantissa in tickPositionsMantissas)
+                Console.Write(tickPositionsMantissa + " ");
+             if (multiplierString != "")
+                Console.Write($". Multiplier is {multiplierString}");  //does not render in console because of unicode characters
             Console.WriteLine();
         }
         static void DisplayTicksForTime(DateTime low, DateTime high)
