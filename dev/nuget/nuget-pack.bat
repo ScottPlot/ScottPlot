43e9c6a49b8121aded5cc54c5945f882fae72f4e
Scott W Harden <swharden@gmail.com>
2019-08-11 16:13:06 -0400

    add nuspec for dotNetCore

--- dev/nuget/nuget-pack.bat
+++ dev/nuget/nuget-pack.bat
@@ -1,3 +1,4 @@
 cd ..\..\src\ScottPlot 
-..\..\dev\nuget\nuget.exe pack -OutputDirectory ..\..\dev\nuget
+..\..\dev\nuget\nuget.exe pack scottplot.nuspec -OutputDirectory ..\..\dev\nuget\DotNet
+..\..\dev\nuget\nuget.exe pack ScottPlot-dotNetCore.nuspec -OutputDirectory ..\..\dev\nuget\DotNetCore
 pause
