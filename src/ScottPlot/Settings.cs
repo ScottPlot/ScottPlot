83d87c55197f2e1adf084fe61c7ff5400e497a37
Stend <StendGit@gmail.com>
2019-08-17 19:52:33 +0300

    Fix crash on attemp to take generic type on nongeneric class

--- src/ScottPlot/Settings.cs
+++ src/ScottPlot/Settings.cs
@@ -457,7 +457,7 @@ namespace ScottPlot
                     indicesToDelete.Add(i);
                 else if (plottables[i] is PlottableSignal && signals)                
                     indicesToDelete.Add(i);
-                else if (plottables[i].GetType().GetGenericTypeDefinition() == typeof(PlottableSignalConst<>) && signals)
+                else if (plottables[i].GetType().IsGenericType && plottables[i].GetType().GetGenericTypeDefinition() == typeof(PlottableSignalConst<>) && signals)
                     indicesToDelete.Add(i);
                 else if (plottables[i] is PlottableText && text)
                     indicesToDelete.Add(i);
