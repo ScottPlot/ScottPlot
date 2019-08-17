af938a0432a58747af39eb215f33fb0771ba5c62
Stend <StendGit@gmail.com>
2019-08-17 19:28:55 +0300

    SignalConst and Signal separated classes now

--- src/ScottPlot/plottables/PlottableSignalConst.cs
+++ src/ScottPlot/plottables/PlottableSignalConst.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ScottPlot
@@ -14,10 +15,20 @@ namespace ScottPlot
     // - in x64 mode limit can be up to maximum array size (2G points) with special solution and 64 GB RAM (not tested)
     // - if source array is changed UpdateTrees() must be called
     // - source array can be change by call updateData(), updating by ranges much faster.
-    public class PlottableSignalConst<T> : PlottableSignal<T> where T : struct, IComparable
-    {
+    public class PlottableSignalConst<T> : Plottable where T : struct, IComparable
+    {    
+        // Any changes must be sync with PlottableSignal
+        public T[] ys;
+        public double sampleRate;
+        public double samplePeriod;
+        public float markerSize;
+        public double xOffset;
+        public double yOffset;
+        public Pen pen;
+        public Brush brush;
+
         // using 2 x signal memory in best case: ys.Length is Pow2 
-        // using 4 x signal memory in worst case: ys.Length is (Pow2 +1);        
+        // using 4 x signal memory in worst case: ys.Length is (Pow2 +1);
         T[] TreeMin;
         T[] TreeMax;
         private int n = 0; // size of each Tree
@@ -28,6 +39,8 @@ namespace ScottPlot
         private static Func<T, T, bool> EqualExp;
         private static Func<T> MaxValue;
         private static Func<T> MinValue;
+        private static Func<T, T, bool> LessThanExp;
+        private static Func<T, T, bool> GreaterThanExp;
 
         private void InitExp()
         {
@@ -39,15 +52,39 @@ namespace ScottPlot
             BinaryExpression bodyEqual = Expression.Equal(paramA, paramB);
             MemberExpression bodyMaxValue = Expression.MakeMemberAccess(null, typeof(T).GetField("MaxValue"));
             MemberExpression bodyMinValue = Expression.MakeMemberAccess(null, typeof(T).GetField("MinValue"));
+            BinaryExpression bodyLessThan = Expression.LessThan(paramA, paramB);
+            BinaryExpression bodyGreaterThan = Expression.GreaterThan(paramA, paramB);
             // compile it
             MinExp = Expression.Lambda<Func<T, T, T>>(bodyMin, paramA, paramB).Compile();
             MaxExp = Expression.Lambda<Func<T, T, T>>(bodyMax, paramA, paramB).Compile();
             EqualExp = Expression.Lambda<Func<T, T, bool>>(bodyEqual, paramA, paramB).Compile();
             MaxValue = Expression.Lambda<Func<T>>(bodyMaxValue).Compile();
             MinValue = Expression.Lambda<Func<T>>(bodyMinValue).Compile();
+            LessThanExp = Expression.Lambda<Func<T, T, bool>>(bodyLessThan, paramA, paramB).Compile();
+            GreaterThanExp = Expression.Lambda<Func<T, T, bool>>(bodyGreaterThan, paramA, paramB).Compile();
         }
-        public PlottableSignalConst(T[] ys, double sampleRate, double xOffset, double yOffset, Color color, double lineWidth, double markerSize, string label, bool useParallel) : base(ys, sampleRate, xOffset, yOffset, color, lineWidth, markerSize, label, useParallel)
+        public PlottableSignalConst(T[] ys, double sampleRate, double xOffset, double yOffset, Color color, double lineWidth, double markerSize, string label, bool useParallel)
         {
+            if (ys == null)
+                throw new Exception("Y data cannot be null");
+            this.ys = ys;
+            this.sampleRate = sampleRate;
+            this.samplePeriod = 1.0 / sampleRate;
+            this.markerSize = (float)markerSize;
+            this.xOffset = xOffset;
+            this.label = label;
+            this.color = color;
+            this.yOffset = yOffset;
+            this.useParallel = useParallel;
+            pointCount = ys.Length;
+            brush = new SolidBrush(color);
+            pen = new Pen(color, (float)lineWidth)
+            {
+                // this prevents sharp corners
+                StartCap = System.Drawing.Drawing2D.LineCap.Round,
+                EndCap = System.Drawing.Drawing2D.LineCap.Round,
+                LineJoin = System.Drawing.Drawing2D.LineJoin.Round
+            };
             try // runtime check
             {               
                 Convert.ToDouble(new T());
@@ -56,7 +93,7 @@ namespace ScottPlot
             {
                 throw new ArgumentOutOfRangeException("Unsupported data type, provide convertable to double data types");
             }
-            InitExp();
+            InitExp();         
             if (useParallel)
                 UpdateTreesInBackground();
             else
@@ -234,18 +271,30 @@ namespace ScottPlot
         }
 
         //  O(log(n)) for each range min/max query
-        protected override void MinMaxRangeQuery(int l, int r, out double lowestValue, out double highestValue)
+        protected void MinMaxRangeQuery(int l, int r, out double lowestValue, out double highestValue)
         {
+            T lowestValueT;
+            T highestValueT;
             // if the tree calculation isn't finished or if it crashed
             if (!TreesReady)
             {
                 // use the original (slower) min/max calculated method
-                base.MinMaxRangeQuery(l, r, out lowestValue, out highestValue);
+                lowestValueT = ys[l];
+                highestValueT = ys[l];
+                for (int i = l; i < r; i++)
+                {
+                    if (LessThanExp(ys[i], lowestValueT))
+                        lowestValueT = ys[i];
+                    if (GreaterThanExp(ys[i], highestValueT))
+                        highestValueT = ys[i];
+                }
+                lowestValue = Convert.ToDouble(lowestValueT);
+                highestValue = Convert.ToDouble(highestValueT);
                 return;
             }
 
-            T lowestValueT = MaxValue();
-            T highestValueT = MinValue();
+            lowestValueT = MaxValue();
+            highestValueT = MinValue();
             if (l == r)
             {
                 lowestValue = highestValue = Convert.ToDouble(ys[l]);
@@ -297,9 +346,170 @@ namespace ScottPlot
             return limits;
         }
 
+        private void RenderSingleLine(Settings settings)
+        {
+            // this function is for when the graph is zoomed so far out its entire display is a single vertical pixel column
+            double yMin, yMax;
+            MinMaxRangeQuery(0, ys.Length - 1, out yMin, out yMax);            
+            PointF point1 = settings.GetPixel(xOffset, yMin + yOffset);
+            PointF point2 = settings.GetPixel(xOffset, yMax + yOffset);
+            settings.gfxData.DrawLine(pen, point1, point2);
+        }
+
+        private void RenderLowDensity(Settings settings, int visibleIndex1, int visibleIndex2)
+        {
+            // this function is for when the graph is zoomed in so individual data points can be seen
+
+            List<PointF> linePoints = new List<PointF>(visibleIndex2 - visibleIndex1 + 2);
+            if (visibleIndex2 > ys.Length - 2)
+                visibleIndex2 = ys.Length - 2;
+            if (visibleIndex1 < 0)
+                visibleIndex1 = 0;
+            for (int i = visibleIndex1; i <= visibleIndex2 + 1; i++)
+                linePoints.Add(settings.GetPixel(samplePeriod * i + xOffset, Convert.ToDouble(ys[i]) + yOffset));
+
+            if (linePoints.Count > 1)
+            {
+                settings.gfxData.DrawLines(pen, linePoints.ToArray());
+                foreach (PointF point in linePoints)
+                    settings.gfxData.FillEllipse(brush, point.X - markerSize / 2, point.Y - markerSize / 2, markerSize, markerSize);
+            }
+        }
+
+        private void RenderHighDensityParallel(Settings settings, double offsetPoints, double columnPointCount)
+        {
+            int xPxStart = (int)Math.Ceiling((-1 - offsetPoints) / columnPointCount - 1);
+            int xPxEnd = (int)Math.Ceiling((ys.Length - offsetPoints) / columnPointCount);
+            xPxStart = Math.Max(0, xPxStart);
+            xPxEnd = Math.Min(settings.dataSize.Width, xPxEnd);
+            if (xPxStart >= xPxEnd)
+                return;
+            PointF[] linePoints = new PointF[(xPxEnd - xPxStart) * 2];
+            Parallel.For(xPxStart, xPxEnd, xPx =>
+            {
+                // determine data indexes for this pixel column
+                int index1 = (int)(offsetPoints + columnPointCount * xPx);
+                int index2 = (int)(offsetPoints + columnPointCount * (xPx + 1));
+
+                if (index1 < 0)
+                    index1 = 0;
+                if (index2 > ys.Length - 1)
+                    index2 = ys.Length - 1;
+
+                // get the min and max value for this column                
+                double lowestValue, highestValue;
+                MinMaxRangeQuery(index1, index2, out lowestValue, out highestValue);
+                float yPxHigh = settings.GetPixel(0, lowestValue + yOffset).Y;
+                float yPxLow = settings.GetPixel(0, highestValue + yOffset).Y;
+
+                linePoints[(xPx - xPxStart) * 2] = new PointF(xPx, yPxLow);
+                linePoints[(xPx - xPxStart) * 2 + 1] = new PointF(xPx, yPxHigh);
+            });
+
+            // adjust order of points to enhance anti-aliasing
+            PointF buf;
+            for (int i = 1; i < linePoints.Length / 2; i++)
+            {
+                if (linePoints[i * 2].Y >= linePoints[i * 2 - 1].Y)
+                {
+                    buf = linePoints[i * 2];
+                    linePoints[i * 2] = linePoints[i * 2 + 1];
+                    linePoints[i * 2 + 1] = buf;
+                }
+            }
+
+            settings.gfxData.DrawLines(pen, linePoints);
+        }
+
+        private void RenderHighDensity(Settings settings, double offsetPoints, double columnPointCount)
+        {
+            // this function is for when the graph is zoomed out so each pixel column represents the vertical span of multiple data points
+            int xPxStart = (int)Math.Ceiling((-1 - offsetPoints) / columnPointCount - 1);
+            int xPxEnd = (int)Math.Ceiling((ys.Length - offsetPoints) / columnPointCount);
+            xPxStart = Math.Max(0, xPxStart);
+            xPxEnd = Math.Min(settings.dataSize.Width, xPxEnd);
+            if (xPxStart >= xPxEnd)
+                return;
+            List<PointF> linePoints = new List<PointF>((xPxEnd - xPxStart) * 2 + 1);
+            for (int xPx = xPxStart; xPx < xPxEnd; xPx++)
+            {
+                // determine data indexes for this pixel column
+                int index1 = (int)(offsetPoints + columnPointCount * xPx);
+                int index2 = (int)(offsetPoints + columnPointCount * (xPx + 1));
+
+                if (index1 < 0)
+                    index1 = 0;
+                if (index2 > ys.Length - 1)
+                    index2 = ys.Length - 1;
+
+                // get the min and max value for this column                
+                double lowestValue, highestValue;                
+                MinMaxRangeQuery(index1, index2, out lowestValue, out highestValue);
+                float yPxHigh = settings.GetPixel(0, lowestValue + yOffset).Y;
+                float yPxLow = settings.GetPixel(0, highestValue + yOffset).Y;
+
+                // adjust order of points to enhance anti-aliasing
+                if ((linePoints.Count < 2) || (yPxLow < linePoints[linePoints.Count - 1].Y))
+                {
+                    linePoints.Add(new PointF(xPx, yPxLow));
+                    linePoints.Add(new PointF(xPx, yPxHigh));
+                }
+                else
+                {
+                    linePoints.Add(new PointF(xPx, yPxHigh));
+                    linePoints.Add(new PointF(xPx, yPxLow));
+                }
+            }
+
+            if (linePoints.Count > 0)
+                settings.gfxData.DrawLines(pen, linePoints.ToArray());
+        }
+
+        public override void Render(Settings settings)
+        {
+            double dataSpanUnits = ys.Length * samplePeriod;
+            double columnSpanUnits = settings.xAxisSpan / settings.dataSize.Width;
+            double columnPointCount = (columnSpanUnits / dataSpanUnits) * ys.Length;
+            double offsetUnits = settings.axis[0] - xOffset;
+            double offsetPoints = offsetUnits / samplePeriod;
+            int visibleIndex1 = (int)(offsetPoints);
+            int visibleIndex2 = (int)(offsetPoints + columnPointCount * (settings.dataSize.Width + 1));
+            int visiblePointCount = visibleIndex2 - visibleIndex1;
+            double pointsPerPixelColumn = visiblePointCount / settings.dataSize.Width;
+
+            PointF firstPoint = settings.GetPixel(xOffset, Convert.ToDouble(ys[0]) + yOffset);
+            PointF lastPoint = settings.GetPixel(samplePeriod * (ys.Length - 1) + xOffset, Convert.ToDouble(ys[ys.Length - 1]) + yOffset);
+            double dataWidthPx = lastPoint.X - firstPoint.X;
+
+            // use different rendering methods based on how dense the data is on screen
+            if (dataWidthPx <= 1)
+            {
+                RenderSingleLine(settings);
+            }
+            else if (pointsPerPixelColumn > 1)
+            {
+                if (useParallel)
+                    RenderHighDensityParallel(settings, offsetPoints, columnPointCount);
+                else
+                    RenderHighDensity(settings, offsetPoints, columnPointCount);
+            }
+            else
+            {
+                RenderLowDensity(settings, visibleIndex1, visibleIndex2);
+            }
+        }
+
         public override string ToString()
         {
             return $"PlottableSignalConst with {pointCount} points ({typeof(T).Name}), trees {(TreesReady ? "" : "not")} calculated";
+        }
+
+        public override void SaveCSV(string filePath)
+        {
+            StringBuilder csv = new StringBuilder();
+            for (int i = 0; i < ys.Length; i++)
+                csv.AppendFormat("{0}, {1}\n", xOffset + i * samplePeriod, Convert.ToDouble(ys[i]));
+            System.IO.File.WriteAllText(filePath, csv.ToString());
         }
     }
 }
