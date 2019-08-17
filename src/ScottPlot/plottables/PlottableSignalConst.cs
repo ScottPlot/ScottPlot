d85797b2a16435f165bd7721e96d75e6973541a6
Stend <StendGit@gmail.com>
2019-08-17 14:11:42 +0300

    Fix mistakes

--- src/ScottPlot/plottables/PlottableSignalConst.cs
+++ src/ScottPlot/plottables/PlottableSignalConst.cs
@@ -9,7 +9,8 @@ namespace ScottPlot
 {
     // Variation of PlottableSignal that uses a segmented tree for faster min/max range queries
     // - frequent min/max lookups are a bottleneck displaying large signals
-    // - limited to 60M points (250M in x64 mode) due to memory (tree uses from 2X to 4X memory)
+    // - double[] limited to 60M points (250M in x64 mode) due to memory (tree uses from 2X to 4X memory)
+    // - smaller data types have higher points count limits
     // - in x64 mode limit can be up to maximum array size (2G points) with special solution and 64 GB RAM (not tested)
     // - if source array is changed UpdateTrees() must be called
     // - source array can be change by call updateData(), updating by ranges much faster.
