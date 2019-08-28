ca7f9433642167e3e93b110c3946012d4c8a0d48
Scott W Harden <swharden@gmail.com>
2019-08-27 22:59:22 -0400

    move Tighten() into the Layout module

--- src/ScottPlot/Config/Layout.cs
+++ src/ScottPlot/Config/Layout.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,44 @@ namespace ScottPlot.Config
         public bool[] displayFrameByAxis = new bool[] { true, true, true, true };
 
         public bool tighteningOccurred = false;
+
+        private Graphics gfx = Graphics.FromHwnd(IntPtr.Zero);
+
+        public void Tighten(Ticks ticks, TextLabel title, TextLabel xLabel, TextLabel yLabel)
+        {
+
+            // "tighten" the plot by reducing whitespce between labels, data, and the edge of the figure
+            if (ticks.x == null)
+                return;
+
+            int tickLetterHeight = (int)gfx.MeasureString("test", ticks.font).Height;
+
+            // top
+            paddingBySide[3] = 1;
+            paddingBySide[3] += Math.Max((int)title.height, tickLetterHeight);
+            paddingBySide[3] += padOnAllSides;
+
+            // bottom
+            int xLabelHeight = (int)gfx.MeasureString(xLabel.text, xLabel.font).Height;
+            paddingBySide[2] = Math.Max(xLabelHeight, tickLetterHeight);
+            paddingBySide[2] += tickLetterHeight;
+            paddingBySide[2] += padOnAllSides;
+
+            // left
+            SizeF yLabelSize = gfx.MeasureString(yLabel.text, yLabel.font);
+            paddingBySide[0] = (int)yLabelSize.Height;
+            paddingBySide[0] += (int)ticks.y.maxLabelSize.Width;
+            paddingBySide[0] += padOnAllSides;
+
+            // right
+            paddingBySide[1] = (int)ticks.y.maxLabelSize.Width / 2;
+            paddingBySide[1] += padOnAllSides;
+
+            // override for frameles
+            if (!displayAxisFrames)
+                paddingBySide = new int[] { 0, 0, 0, 0 };
+
+            tighteningOccurred = true;
+        }
     }
 }
