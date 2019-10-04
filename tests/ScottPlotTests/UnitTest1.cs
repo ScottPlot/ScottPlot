2e3cf7c350e2461e50e81a26c2002eba5f2c8552
Scott W Harden <swharden@gmail.com>
2019-10-03 20:34:43 -0400

    start unit tests
    discussed in #147

---
+++ tests/ScottPlotTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScottPlotTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var plt = new ScottPlot.Plot();
+            plt.PlotScatter(
+                xs: new double[] { 2, 4, 6 },
+                ys: new double[] { 4, 16, 36 }
+                );
+            System.Drawing.Bitmap bmp = plt.GetBitmap(); // force a render
+            Console.WriteLine(plt.GetHashCode()); // force a render too
+        }
+    }
+}
