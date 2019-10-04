756380bf9548dda6d7ba0b032d8024281fee2579
morningkyle <morningkyle@gmail.com>
2019-10-04 19:57:35 +0800

    ScottPlotTests: add more tests

--- tests/ScottPlotTests/UnitTest1.cs
+++ tests/ScottPlotTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ScottPlotTests
@@ -7,15 +9,47 @@ namespace ScottPlotTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetBitmap()
         {
-            var plt = new ScottPlot.Plot();
-            plt.PlotScatter(
-                xs: new double[] { 2, 4, 6 },
-                ys: new double[] { 4, 16, 36 }
-                );
-            System.Drawing.Bitmap bmp = plt.GetBitmap(); // force a render
-            Console.WriteLine(plt.GetHashCode()); // force a render too
+            {
+                var plt = new ScottPlot.Plot(1, 1);
+                Bitmap bmp = plt.GetBitmap();
+                Assert.AreEqual(bmp.Width, 1);
+                Assert.AreEqual(bmp.Height, 1);
+            }
+
+            {
+                var plt = new ScottPlot.Plot(1, 1);
+                Bitmap bmp = plt.GetBitmap(false, false);
+                Assert.AreEqual(bmp.Width, 1);
+                Assert.AreEqual(bmp.Height, 1);
+            }
+        }
+
+        [TestMethod]
+        public void TestSaveFig()
+        {
+            {
+                var plt = new ScottPlot.Plot(1, 1);
+                string file = Path.GetTempPath() + "test.bmp";
+                plt.SaveFig(file);
+                Bitmap bmp = new Bitmap(file);
+                Assert.AreEqual(bmp.Width, 1);
+                Assert.AreEqual(bmp.Height, 1);
+            }
+
+            {
+                var plt = new ScottPlot.Plot(1, 1);
+                string file = Path.GetTempPath() + "test.bmp";
+                try
+                {
+                    plt.SaveFig(file, renderFirst: false);
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail("Expected no exception, but got: " + ex.Message);
+                }
+            }
         }
     }
 }
