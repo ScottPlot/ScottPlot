8dc427903996ab02e02aa350a328b29abecd96fb
Scott W Harden <swharden@gmail.com>
2019-10-22 19:30:04 -0400

    Plot.Layout()
    discussed in #156

--- tests/ScottPlotTests/Recipes.cs
+++ tests/ScottPlotTests/Recipes.cs
@@ -512,23 +512,12 @@ namespace ScottPlotTests
             var plt = new ScottPlot.Plot(width, height);
             plt.PlotScatter(dataXs, dataSin);
             plt.PlotScatter(dataXs, dataCos);
-
             plt.Style(figBg: Color.LightBlue);
-            plt.Ticks(rulerModeX: true);
 
-            plt.Title("Very Complicated Data");
-            plt.YLabel("Productivity");
-
-            // customize update the layout as desired (this is a bit of a hack, but it works)
-            plt.TightenLayout();
-            var layout = plt.GetSettings().layout;
-            layout.titleHeight = 123;
-            layout.yScaleWidth = 70;
-            layout.xLabelHeight = 0;
-            layout.Update(width, height);
-
-            // then force a resize (so the ticks recalculate based on the new data area)
-            plt.Resize(width, height);
+            // Layout component sizes are typically auto-calculated by TightenLayout()
+            // Plots without title or axis labels typically extend right to the edge of the image
+            // You can call Layout() to manually define the sizes of plot components
+            plt.Layout(yScaleWidth: 80, titleHeight: 50, xLabelHeight: 20, y2LabelWidth: 20);
 
             if (outputPath != null) plt.SaveFig(fileName); else Console.WriteLine(plt.GetHashCode());
             Console.WriteLine($"Saved: {fileName}");
