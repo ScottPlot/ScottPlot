55062a81e5abb2dee5f9ffdcb2c10761ee6c04bf
Scott W Harden <swharden@gmail.com>
2019-11-24 00:44:20 -0500

    add secondary OHLC constructor
    addresses #182

--- src/ScottPlot/plottables/OHLC.cs
+++ src/ScottPlot/plottables/OHLC.cs
@@ -12,7 +12,7 @@ namespace ScottPlot
         public double high;
         public double low;
         public double close;
-        public double day;
+        public double time;
 
         public double highestOpenClose;
         public double lowestOpenClose;
@@ -24,7 +24,20 @@ namespace ScottPlot
             this.high = high;
             this.low = low;
             this.close = close;
-            day = dateTime.ToOADate();
+            time = dateTime.ToOADate();
+
+            highestOpenClose = Math.Max(open, close);
+            lowestOpenClose = Math.Min(open, close);
+            closedHigher = (close > open) ? true : false;
+        }
+
+        public OHLC(double open, double high, double low, double close, double time)
+        {
+            this.open = open;
+            this.high = high;
+            this.low = low;
+            this.close = close;
+            this.time = time;
 
             highestOpenClose = Math.Max(open, close);
             lowestOpenClose = Math.Min(open, close);
@@ -33,7 +46,7 @@ namespace ScottPlot
 
         public override string ToString()
         {
-            return $"OHLC: open={open}, high={high}, low={low}, close={close}, timestamp={day}";
+            return $"OHLC: open={open}, high={high}, low={low}, close={close}, timestamp={time}";
         }
     }
 }
