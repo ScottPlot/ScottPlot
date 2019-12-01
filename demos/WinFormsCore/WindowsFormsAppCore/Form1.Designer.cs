ed54b7b530a918f2b6c0a3ce9ec12ed841204526
Scott W Harden <swharden@gmail.com>
2019-12-01 12:39:14 -0500

    create .NET Core 3.0 WinForms demo
    discussed in #138

---
+++ demos/WinFormsCore/WindowsFormsAppCore/Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WindowsFormsAppCore
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "WinForms (.NET Core 3.0)";
+
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.formsPlot1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Controls.Add(this.formsPlot1);
+        }
+
+        #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+    }
+}
+
---
+++ demos/WinFormsCore/WindowsFormsAppCore/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppCore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // generate some data
+            int pointCount = 100;
+            double[] xs = new double[pointCount];
+            double[] ys = new double[pointCount];
+            for (int i = 0; i < pointCount; i++)
+            {
+                xs[i] = i;
+                ys[i] = Math.Pow(i, .5);
+            }
+
+            // plot the data
+            formsPlot1.plt.PlotScatter(xs, ys);
+            formsPlot1.Render();
+        }
+    }
+}
