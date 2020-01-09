f79c954bb8453edd5507bb5447eddf4fe5fb104f
Scott W Harden <swharden@gmail.com>
2020-01-08 20:19:44 -0500

    autoformat
    CTRL+K, CTRL+D

--- src/ScottPlot/Statistics/Regression.cs
+++ src/ScottPlot/Statistics/Regression.cs
@@ -5,73 +5,82 @@ using System.Text;
 
 namespace ScottPlot.Statistics
 {
-	public interface IRegressionLine {
-		double[] GetCoefficients();
-		double[] Residual();
-	}
+    public interface IRegressionLine
+    {
+        double[] GetCoefficients();
+        double[] Residual();
+    }
 
-	public class LinearRegressionLine : IRegressionLine {
-		private double[] x;
-		private double[] y;
-		private double[] coefficients;
+    public class LinearRegressionLine : IRegressionLine
+    {
+        private double[] x;
+        private double[] y;
+        private double[] coefficients;
 
-		public LinearRegressionLine(double[] xAxis, double[] yAxis) {
-			if (xAxis.Length != yAxis.Length) {
-				throw new ArgumentException("Axes of mismatched length");
-			}
+        public LinearRegressionLine(double[] xAxis, double[] yAxis)
+        {
+            if (xAxis.Length != yAxis.Length)
+            {
+                throw new ArgumentException("Axes of mismatched length");
+            }
 
-			x = xAxis;
-			y = yAxis;
-			coefficients = GetCoefficients();
-		}
+            x = xAxis;
+            y = yAxis;
+            coefficients = GetCoefficients();
+        }
 
-		private double y_hat(double x) {//Value predicted by regression model
-			//double[] coefficients = GetCoefficients();
+        private double y_hat(double x)
+        {//Value predicted by regression model
+         //double[] coefficients = GetCoefficients();
 
-			return coefficients[0] + coefficients[1] * x;
-		}
+            return coefficients[0] + coefficients[1] * x;
+        }
 
-		public double[] GetCoefficients() {//y_hat = a+bx
-			double a;
-			double b;
+        public double[] GetCoefficients()
+        {//y_hat = a+bx
+            double a;
+            double b;
 
-			double xMean=x.Average();
-			double yMean=y.Average();
+            double xMean = x.Average();
+            double yMean = y.Average();
 
-			double sumXYResidual = 0;
-			double sumXSquareResidual = 0;
+            double sumXYResidual = 0;
+            double sumXSquareResidual = 0;
 
-			for (int i = 0; i < x.Length; i++)
-			{
-				sumXYResidual += (x[i] - xMean) * (y[i] - yMean);
-				sumXSquareResidual += Math.Pow((x[i] - xMean), 2);
-			}
+            for (int i = 0; i < x.Length; i++)
+            {
+                sumXYResidual += (x[i] - xMean) * (y[i] - yMean);
+                sumXSquareResidual += Math.Pow((x[i] - xMean), 2);
+            }
 
-			b = sumXYResidual / (sumXSquareResidual);
+            b = sumXYResidual / (sumXSquareResidual);
 
-			a = yMean - (b * xMean);//LSRL always passes through the point (x̅,y̅)
+            a = yMean - (b * xMean);//LSRL always passes through the point (x̅,y̅)
 
-			return new double[] { a, b };
-		}
+            return new double[] { a, b };
+        }
 
-		public double[] Residual() {
-			//double[] coefficients = GetCoefficients();
+        public double[] Residual()
+        {
+            //double[] coefficients = GetCoefficients();
 
-			double[] residuals = new double[y.Length];
+            double[] residuals = new double[y.Length];
 
-			for (int i = 0; i < y.Length; i++) {
-				residuals[i] = y[i] - y_hat(x[i]);
-			}
+            for (int i = 0; i < y.Length; i++)
+            {
+                residuals[i] = y[i] - y_hat(x[i]);
+            }
 
-			return residuals;
-		}
+            return residuals;
+        }
 
-		public Plot DrawResidual(Plot plot) {
-			double[] residuals = Residual();
+        public Plot DrawResidual(Plot plot)
+        {
+            double[] residuals = Residual();
 
-			plot.PlotScatter(x, residuals, lineWidth: 0);
+            plot.PlotScatter(x, residuals, lineWidth: 0);
 
-			return plot;
-		}
-	}
+            return plot;
+        }
+    }
 }
