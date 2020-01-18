f423d8de448421c04830f284ebed0ec77a4d4041
Scott W Harden <swharden@gmail.com>
2020-01-18 16:37:59 -0500

    improve bitmap tests

---
+++ tests/Bitmap.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace ScottPlotTests
+{
+    public class Bitmap
+    {
+        [Test]
+        public void Test_Plot_GetBitmap()
+        {
+            {
+                var plt = new ScottPlot.Plot(321, 123);
+                System.Drawing.Bitmap bmp = plt.GetBitmap();
+                Assert.AreEqual(bmp.Width, 321);
+                Assert.AreEqual(bmp.Height, 123);
+            }
+
+            {
+                var plt = new ScottPlot.Plot(321, 123);
+                System.Drawing.Bitmap bmp = plt.GetBitmap(false, false);
+                Assert.AreEqual(bmp.Width, 321);
+                Assert.AreEqual(bmp.Height, 123);
+            }
+        }
+
+        [Test]
+        public void Test_Plot_SaveFig()
+        {
+            {
+                var plt = new ScottPlot.Plot(321, 123);
+                string file = Path.GetTempPath() + "test.bmp";
+                plt.SaveFig(file);
+                using (System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(file))
+                {
+                    Assert.AreEqual(bmp.Width, 321);
+                    Assert.AreEqual(bmp.Height, 123);
+                }
+            }
+
+            {
+                var plt = new ScottPlot.Plot(321, 123);
+                string file = Path.GetTempPath() + "test.png";
+                plt.SaveFig(file, renderFirst: false);
+                using (System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(file))
+                {
+                    Assert.AreEqual(bmp.Width, 321);
+                    Assert.AreEqual(bmp.Height, 123);
+                }
+            }
+        }
+    }
+}
