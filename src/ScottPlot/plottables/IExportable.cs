3c852e2fd22ee91e259e097df43b361fde537acc
Scott W Harden <swharden@gmail.com>
2020-01-19 17:30:03 -0500

    Update IExportable.cs

--- src/ScottPlot/plottables/IExportable.cs
+++ src/ScottPlot/plottables/IExportable.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace ScottPlot
 {
+    /// <summary>
+    /// describes Plottable objects whose data can be exported as a text file
+    /// </summary>
     public interface IExportable
     {
         void SaveCSV(string filePath);
