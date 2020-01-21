5c6447bcdd193d9f0482c7542209026da7af18b2
Stend <StendGit@gmail.com>
2020-01-21 19:30:13 +0300

    GetDefaultFontName() accept IEnumerable istead of array. Code clean up.

--- tests/FontsTests.cs
+++ tests/FontsTests.cs
@@ -1,9 +1,5 @@
 ﻿using NUnit.Framework;
 using ScottPlot.Config;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ScottPlotTests
 {
@@ -14,19 +10,15 @@ namespace ScottPlotTests
         public void GetDefaultFontName_SegoeUIFontPresent_ReturnSegoeFont()
         {
             string[] presentFonts = new string[] { "newFont ar", "SOME font", "AnotherOne", "Arial", "Segoe UI", "Comic Sans", "onemorefont" };
-            //presentFonts = presentFonts.Select(x => x.ToUpper()).ToArray();
-
             string expected = "Segoe UI";
             string result = Fonts.GetDefaultFontName(presentFonts);
             Assert.AreEqual(expected, result);
         }
-        
+
         [Test]
         public void GetDefaultFontName_SegoeUIFontNotPresent_ReturnDifferentFont()
         {
             string[] presentFonts = new string[] { "newFont ar", "SOME font", "AnotherOne", "Arial", "Comic Sans", "onemorefont" };
-            //presentFonts = presentFonts.Select(x => x.ToUpper()).ToArray();
-
             string expected = "Segoe UI";
             string result = Fonts.GetDefaultFontName(presentFonts);
             Assert.AreNotEqual(expected, result);
@@ -36,8 +28,6 @@ namespace ScottPlotTests
         public void GetDefaultFontName_NoSegoeUIButSegoeUIBlackPresent_ReturnSegoeUIBlack()
         {
             string[] presentFonts = new string[] { "newFont ar", "SOME font", "AnotherOne", "Segoe UI Black", "Arial", "Comic Sans", "onemorefont" };
-            //presentFonts = presentFonts.Select(x => x.ToUpper()).ToArray();
-
             string expected = "Segoe UI Black";
             string result = Fonts.GetDefaultFontName(presentFonts);
             Assert.AreEqual(expected, result);
@@ -46,9 +36,7 @@ namespace ScottPlotTests
         [Test]
         public void GetDefaultFontName_NoSegoeUIButSomeSansPresent_ReturnSansFont()
         {
-            string[] presentFonts = new string[] { "newFont ar", "SOME font", "AnotherOne","Arial", "Comic Sans", "onemorefont" };
-            //presentFonts = presentFonts.Select(x => x.ToUpper()).ToArray();
-
+            string[] presentFonts = new string[] { "newFont ar", "SOME font", "AnotherOne", "Arial", "Comic Sans", "onemorefont" };
             string expected = "Comic Sans";
             string result = Fonts.GetDefaultFontName(presentFonts);
             Assert.AreEqual(expected, result);
@@ -57,13 +45,10 @@ namespace ScottPlotTests
         [Test]
         public void GetDefaultFontName_NoSegoeUIButSomeSansandDejavuPresent_ReturnDejavuFont()
         {
-            string[] presentFonts = new string[] { "newFont ar", "SOME font", "AnotherOne",  "Arial", "Comic Sans", "onemorefont", "Fictional Dejavu"};
-            //presentFonts = presentFonts.Select(x => x.ToUpper()).ToArray();
-
+            string[] presentFonts = new string[] { "newFont ar", "SOME font", "AnotherOne", "Arial", "Comic Sans", "onemorefont", "Fictional Dejavu" };
             string expected = "Fictional Dejavu";
             string result = Fonts.GetDefaultFontName(presentFonts);
             Assert.AreEqual(expected, result);
         }
-
     }
 }
