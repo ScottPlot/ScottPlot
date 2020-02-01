97a4a0f1fc6d473bc32bc840496b92969ef58ee8
Scott W Harden <swharden@gmail.com>
2020-02-01 18:34:34 -0500

    add axis spans to draggable demo
    #228

--- demos/WinForms/Demos/FormDraggableAxisLines.Designer.cs
+++ demos/WinForms/Demos/FormDraggableAxisLines.Designer.cs
@@ -33,13 +33,16 @@
             this.btnClearLines = new System.Windows.Forms.Button();
             this.formsPlot1 = new ScottPlot.FormsPlot();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnAddHspan = new System.Windows.Forms.Button();
+            this.btnAddVSpan = new System.Windows.Forms.Button();
+            this.btnClearSpans = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAddHline
             // 
             this.btnAddHline.Location = new System.Drawing.Point(12, 12);
             this.btnAddHline.Name = "btnAddHline";
-            this.btnAddHline.Size = new System.Drawing.Size(75, 23);
+            this.btnAddHline.Size = new System.Drawing.Size(107, 23);
             this.btnAddHline.TabIndex = 1;
             this.btnAddHline.Text = "Add HLine";
             this.btnAddHline.UseVisualStyleBackColor = true;
@@ -49,7 +52,7 @@
             // 
             this.btnAddVline.Location = new System.Drawing.Point(12, 41);
             this.btnAddVline.Name = "btnAddVline";
-            this.btnAddVline.Size = new System.Drawing.Size(75, 23);
+            this.btnAddVline.Size = new System.Drawing.Size(107, 23);
             this.btnAddVline.TabIndex = 2;
             this.btnAddVline.Text = "Add VLine";
             this.btnAddVline.UseVisualStyleBackColor = true;
@@ -59,7 +62,7 @@
             // 
             this.btnClearLines.Location = new System.Drawing.Point(12, 70);
             this.btnClearLines.Name = "btnClearLines";
-            this.btnClearLines.Size = new System.Drawing.Size(75, 23);
+            this.btnClearLines.Size = new System.Drawing.Size(107, 23);
             this.btnClearLines.TabIndex = 3;
             this.btnClearLines.Text = "Clear Lines";
             this.btnClearLines.UseVisualStyleBackColor = true;
@@ -70,9 +73,9 @@
             this.formsPlot1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.formsPlot1.Location = new System.Drawing.Point(182, 12);
+            this.formsPlot1.Location = new System.Drawing.Point(242, 12);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(558, 333);
+            this.formsPlot1.Size = new System.Drawing.Size(544, 343);
             this.formsPlot1.TabIndex = 5;
             this.formsPlot1.MouseDownOnPlottable += new System.EventHandler(this.ScottPlotUC1_MouseDownOnPlottable);
             this.formsPlot1.MouseDragPlottable += new System.EventHandler(this.ScottPlotUC1_MouseDragPlottable);
@@ -86,21 +89,54 @@
             this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 99);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(164, 246);
+            this.richTextBox1.Size = new System.Drawing.Size(224, 256);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
             // 
-            // Form1
+            // btnAddHspan
+            // 
+            this.btnAddHspan.Location = new System.Drawing.Point(129, 41);
+            this.btnAddHspan.Name = "btnAddHspan";
+            this.btnAddHspan.Size = new System.Drawing.Size(107, 23);
+            this.btnAddHspan.TabIndex = 6;
+            this.btnAddHspan.Text = "Add HSpan";
+            this.btnAddHspan.UseVisualStyleBackColor = true;
+            this.btnAddHspan.Click += new System.EventHandler(this.BtnAddHspan_Click);
+            // 
+            // btnAddVSpan
+            // 
+            this.btnAddVSpan.Location = new System.Drawing.Point(129, 12);
+            this.btnAddVSpan.Name = "btnAddVSpan";
+            this.btnAddVSpan.Size = new System.Drawing.Size(107, 23);
+            this.btnAddVSpan.TabIndex = 7;
+            this.btnAddVSpan.Text = "Add VSpan";
+            this.btnAddVSpan.UseVisualStyleBackColor = true;
+            this.btnAddVSpan.Click += new System.EventHandler(this.BtnAddVSpan_Click);
+            // 
+            // btnClearSpans
+            // 
+            this.btnClearSpans.Location = new System.Drawing.Point(129, 70);
+            this.btnClearSpans.Name = "btnClearSpans";
+            this.btnClearSpans.Size = new System.Drawing.Size(107, 23);
+            this.btnClearSpans.TabIndex = 8;
+            this.btnClearSpans.Text = "Clear Spans";
+            this.btnClearSpans.UseVisualStyleBackColor = true;
+            this.btnClearSpans.Click += new System.EventHandler(this.BtnClearSpans_Click);
+            // 
+            // FormDraggableAxisLines
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 357);
+            this.ClientSize = new System.Drawing.Size(798, 367);
+            this.Controls.Add(this.btnClearSpans);
+            this.Controls.Add(this.btnAddVSpan);
+            this.Controls.Add(this.btnAddHspan);
             this.Controls.Add(this.formsPlot1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.btnClearLines);
             this.Controls.Add(this.btnAddVline);
             this.Controls.Add(this.btnAddHline);
-            this.Name = "Form1";
+            this.Name = "FormDraggableAxisLines";
             this.Text = "ScottPlot Draggable Markers";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -113,6 +149,9 @@
         private System.Windows.Forms.Button btnClearLines;
         private ScottPlot.FormsPlot formsPlot1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnAddHspan;
+        private System.Windows.Forms.Button btnAddVSpan;
+        private System.Windows.Forms.Button btnClearSpans;
     }
 }
 
