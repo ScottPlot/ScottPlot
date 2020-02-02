d1a8693634f54097112d79d920f74f35c53ba2af
Scott W Harden <swharden@gmail.com>
2020-02-02 13:34:56 -0500

    improve DataGen.Sin() and Cos() symmetry

--- src/ScottPlot/DataGen.cs
+++ src/ScottPlot/DataGen.cs
@@ -19,7 +19,7 @@ namespace ScottPlot
 
         public static double[] Sin(int pointCount, double oscillations = 1, double offset = 0,  double mult = 1, double phase = 0)
         {
-            double sinScale = 2 * Math.PI * oscillations / pointCount;
+            double sinScale = 2 * Math.PI * oscillations / (pointCount - 1);
             double[] ys = new double[pointCount];
             for (int i = 0; i < ys.Length; i++)
                 ys[i] = Math.Sin(i * sinScale + phase * Math.PI * 2) * mult + offset;
@@ -52,7 +52,7 @@ namespace ScottPlot
 
         public static double[] Cos(int pointCount, double oscillations = 1, double offset = 0, double mult = 1, double phase = 0)
         {
-            double sinScale = 2 * Math.PI * oscillations / pointCount;
+            double sinScale = 2 * Math.PI * oscillations / (pointCount - 1);
             double[] ys = new double[pointCount];
             for (int i = 0; i < ys.Length; i++)
                 ys[i] = Math.Cos(i * sinScale + phase * Math.PI * 2) * mult + offset;
