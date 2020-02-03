70246b14082fc4a13990e5dcee8ab140bc9e412d
Stend <StendGit@gmail.com>
2020-02-03 18:18:37 +0300

    Convert IDraggable methods to properties

--- src/ScottPlot.WinForms/FormsPlot.cs
+++ src/ScottPlot.WinForms/FormsPlot.cs
@@ -220,7 +220,7 @@ namespace ScottPlot
         {
             // set the cursor based on what's beneath it
             var draggableUnderCursor = plt.GetDraggableUnderMouse(e.Location.X, e.Location.Y);
-            var spCursor = (draggableUnderCursor is null) ? Config.Cursor.Arrow : draggableUnderCursor.GetDragCursor();
+            var spCursor = (draggableUnderCursor is null) ? Config.Cursor.Arrow : draggableUnderCursor.DragCursor;
             pbPlot.Cursor = GetCursor(spCursor);
         }
 
