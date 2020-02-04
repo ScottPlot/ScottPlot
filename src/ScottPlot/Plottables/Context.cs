c2752da68366387979081e4954d71270294faf87
Scott W Harden <swharden@gmail.com>
2020-02-03 23:21:03 -0500

    Create Context.cs

---
+++ src/ScottPlot/Plottables/Context.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlot.Plottables
+{
+    // package render info into this context, then pass this into a plottable (no settings required)
+    public struct Context
+    {
+
+    }
+}
