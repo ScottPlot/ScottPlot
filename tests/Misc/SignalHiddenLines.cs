97c5bf473f7d0832caefd19999ae17c752a2b83f
Scott W Harden <swharden@gmail.com>
2020-02-19 21:57:50 -0500

    create test to demonstrate #236

---
+++ tests/Misc/SignalHiddenLines.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlotTests.Misc
+{
+    public class SignalHiddenLines
+    {
+        [Test]
+        public void Test_HiddenLines_1()
+        {
+            var mplt = new MultiPlot(800, 300, 1, 2);
+
+            mplt.GetSubplot(0, 0).PlotSignal(DataGen.Sin(10000), markerSize: 10);
+            mplt.GetSubplot(0, 0).Title("Zoomed Out");
+
+            mplt.GetSubplot(0, 1).PlotSignal(DataGen.Sin(10000), markerSize: 10);
+            mplt.GetSubplot(0, 1).Title("Zoomed In");
+            mplt.GetSubplot(0, 1).Axis(0, 20, -.1, .1);
+
+            TestTools.SaveFig(mplt);
+        }
+    }
+}
