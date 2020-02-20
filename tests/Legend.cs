eeaaeaf72234cfea614c3b8557222c172dd64538
Scott W Harden <swharden@gmail.com>
2020-02-19 19:50:13 -0500

    create test to assess legend location

---
+++ tests/Legend.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScottPlotTests
+{
+    public class Legend
+    {
+        [Test]
+        public void Test_Legend_LooksGoodInEveryPosition()
+        {
+
+            var mplt = new MultiPlot(1000, 800, 3, 3);
+
+            legendLocation[] locs = Enum.GetValues(typeof(legendLocation)).Cast<legendLocation>().ToArray();
+            for (int i = 0; i < locs.Length; i++)
+            {
+                var plt = mplt.subplots[i];
+                plt.PlotSignal(DataGen.Sin(100), label: "sin");
+                plt.PlotSignal(DataGen.Cos(100), label: "sin");
+                plt.Legend(location: locs[i]);
+                plt.Title(locs[i].ToString());
+            }
+
+            TestTools.SaveFig(mplt);
+        }
+    }
+}
