279bf0c207833f8c8ab61de1be459ea07af8b181
Scott W Harden <swharden@gmail.com>
2020-02-23 18:18:32 -0500

    create ScottPlot.Demo module
    long-term goal is to create 1 place to organize demos, then the Cookbook, WinForms demo, and WPF demo can all reference it.

---
+++ src/ScottPlot/Demo/PlotTypes/BoxAndWhisker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlot.Demo.PlotTypes
+{
+    public static class BoxAndWhisker
+    {
+        public static Plot Quickstart()
+        {
+            var plt = new Plot();
+
+            var boxAndWiskers = new Statistics.BoxAndWhisker[3];
+            boxAndWiskers[0] = GetBoxAndWhisker(Data.LineLengths.plot, 1);
+            boxAndWiskers[1] = GetBoxAndWhisker(Data.LineLengths.formsPlot, 2);
+            boxAndWiskers[2] = GetBoxAndWhisker(Data.LineLengths.wpfPlot, 3);
+
+            plt.Title("Source Code Line Length");
+            plt.PlotBoxAndWhisker(boxAndWiskers);
+            plt.YLabel("Number of Characters");
+
+            double[] xPositions = { 1, 2, 3 };
+            string[] labels = { "Plot.cs", "FormsPlot.cs", "WpfPlot.cs" };
+            plt.XTicks(xPositions, labels);
+
+            plt.AxisAuto(.3, .2);
+            return plt;
+        }
+
+        public static Plot Quickstart2()
+        {
+            var plt = new Plot();
+
+            double[] xPositions = { 1, 2, 3 };
+            double[][] dataArrays = new double[][] { Data.LineLengths.plot, Data.LineLengths.formsPlot, Data.LineLengths.wpfPlot };
+            plt.PlotBoxAndWhiskerV2(xPositions, dataArrays);
+
+            plt.Title("Source Code Line Length (Benny's Version)");
+            plt.YLabel("Number of Characters");
+
+            string[] labels = { "Plot.cs", "FormsPlot.cs", "WpfPlot.cs" };
+            plt.XTicks(xPositions, labels);
+
+            plt.AxisAuto(.3, .2);
+            return plt;
+        }
+
+        static Statistics.BoxAndWhisker GetBoxAndWhisker(double[] data, double xPosition)
+        {
+            var baw = new Statistics.BoxAndWhisker(xPosition);
+            var stats = new Statistics.PopulationStats(data);
+            baw.midline.position = stats.median;
+            baw.whisker.max = stats.max;
+            baw.whisker.min = stats.min;
+            baw.box.max = stats.mean + stats.stDev;
+            baw.box.min = stats.mean - stats.stDev;
+            return baw;
+        }
+    }
+}
