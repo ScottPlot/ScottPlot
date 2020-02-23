128e872995a8df16223db01c1ed3a12f32d6ac58
Scott W Harden <swharden@gmail.com>
2020-02-23 18:19:07 -0500

    update WPF demo app

--- demos/WPF/MainWindow.xaml.cs
+++ demos/WPF/MainWindow.xaml.cs
@@ -27,5 +27,17 @@ namespace WpfApp
 
         private void LaunchQuickstart(object sender, RoutedEventArgs e) { new WPF.Examples.Quickstart().ShowDialog(); }
         private void LaunchProceduralPlots(object sender, RoutedEventArgs e) { new WPF.Examples.ProceduralPlots().ShowDialog(); }
+
+        private void LaunchBoxAndWhisker(object sender, RoutedEventArgs e)
+        {
+            var viewer = new WPF.PlotViewer(ScottPlot.Demo.PlotTypes.BoxAndWhisker.Quickstart());
+            viewer.ShowDialog();
+        }
+
+        private void LaunchBoxAndWhisker2(object sender, RoutedEventArgs e)
+        {
+            var viewer = new WPF.PlotViewer(ScottPlot.Demo.PlotTypes.BoxAndWhisker.Quickstart2());
+            viewer.ShowDialog();
+        }
     }
 }
