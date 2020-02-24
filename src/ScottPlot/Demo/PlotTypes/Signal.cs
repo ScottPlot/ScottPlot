c1d35734bf045704fc63862fce73c08c975f4840
Scott W Harden <swharden@gmail.com>
2020-02-24 18:31:17 -0500

    Demo.PlotTypes.Signal.BigMarkers() #243

---
+++ src/ScottPlot/Demo/PlotTypes/Signal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlot.Demo.PlotTypes
+{
+    public static class Signal
+    {
+        public static Plot Quickstart()
+        {
+            double[] signalData = DataGen.RandomWalk(null, 100000);
+            double sampleRateHz = 20000;
+
+            var plt = new Plot();
+            plt.Title($"Signal Plot ({signalData.Length.ToString("N0")} points)");
+            plt.PlotSignal(signalData, sampleRateHz);
+            return plt;
+        }
+
+        public static Plot BigMarkers()
+        {
+            Random rand = new Random(0);
+            double[] ys1 = DataGen.RandomWalk(rand, 500);
+            double[] ys2 = DataGen.RandomWalk(rand, 500, offset: 20);
+
+            var plt = new Plot();
+            plt.Title($"Signal Plot with Markers");
+            plt.PlotSignal(ys1, label: "default markers");
+            plt.PlotSignal(ys2, markerSize: 20, label: "big markers");
+            plt.Legend();
+            return plt;
+        }
+    }
+}
