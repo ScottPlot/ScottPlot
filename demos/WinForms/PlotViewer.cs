4d51529e33408ed4f45ec1e8156b96dc8d2ca4af
Scott W Harden <swharden@gmail.com>
2020-02-23 19:24:15 -0500

    create a WinForms PlotViewer

---
+++ demos/WinForms/PlotViewer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlotDemos
+{
+    public partial class PlotViewer : Form
+    {
+        public PlotViewer(ScottPlot.Plot plt)
+        {
+            InitializeComponent();
+            Controls.Add(new ScottPlot.FormsPlot(plt) { Dock = DockStyle.Fill, Name = "formsPlot1" });
+        }
+    }
+}
