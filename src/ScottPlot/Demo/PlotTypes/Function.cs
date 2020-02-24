a113744a7d50961a3379d0536587afb3c34c98dd
Scott W Harden <swharden@gmail.com>
2020-02-24 18:08:50 -0500

    create PlotFunction() quickstart
    #243

---
+++ src/ScottPlot/Demo/PlotTypes/Function.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlot.Demo.PlotTypes
+{
+    public static class Function
+    {
+        public static Plot Quickstart()
+        {
+            var plt = new Plot();
+            var func = new Func<double, double?>((x) => Math.Sin(x) * Math.Sin(10 * x));
+            plt.PlotFunction(func, -10, 10, -1, 1);
+            return plt;
+        }
+    }
+}
