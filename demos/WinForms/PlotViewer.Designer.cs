4e05c2c869589aa18dd986d0f0999db2f023ef4a
Scott W Harden <swharden@gmail.com>
2020-02-23 19:24:15 -0500

    create a WinForms PlotViewer

---
+++ demos/WinForms/PlotViewer.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ScottPlotDemos
+{
+    partial class PlotViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PlotViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "PlotViewer";
+            this.Text = "WinForms Plot Viewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
