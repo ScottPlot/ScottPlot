0ad0869bc7953d67d16e10cf64d300c4a6526a7b
Scott W Harden <swharden@gmail.com>
2020-02-25 23:26:07 -0500

    created ScottPlot.Demo package in /src
    #271

---
+++ src/ScottPlot.Demo/PlotTypes/BoxAndWhisker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlot.Demo.PlotTypes
+{
+    class BoxAndWhisker
+    {
+        public static Plot Quickstart()
+        {
+            var plt = new Plot();
+
+            var boxAndWiskers = new Statistics.BoxAndWhisker[3];
+            boxAndWiskers[0] = Statistics.BoxAndWhiskerFromData.OutlierQuartileMedian(Data.LineLengths.plot, 1);
+            boxAndWiskers[1] = Statistics.BoxAndWhiskerFromData.OutlierQuartileMedian(Data.LineLengths.formsPlot, 2);
+            boxAndWiskers[2] = Statistics.BoxAndWhiskerFromData.OutlierQuartileMedian(Data.LineLengths.wpfPlot, 3);
+
+            plt.Title("Source Code Line Length");
+            plt.PlotBoxAndWhisker(boxAndWiskers);
+            plt.YLabel("Number of Characters");
+
+            double[] xPositions = { 1, 2, 3 };
+            string[] labels = { "Plot.cs", "FormsPlot.cs", "WpfPlot.cs" };
+            plt.XTicks(xPositions, labels);
+
+            plt.AxisAuto(.3, .2);
+            return plt;
+        }
+    }
+}
---
+++ src/ScottPlot.Demo/Plots.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ScottPlot;
+
+namespace ScottPlot.Demo
+{
+    public static class Plots
+    {
+        public static Plot SinAndCos()
+        {
+            var plt = new Plot();
+            int pointCount = 100;
+            plt.PlotScatter(DataGen.Consecutive(pointCount), DataGen.Sin(pointCount));
+            plt.PlotScatter(DataGen.Consecutive(pointCount), DataGen.Cos(pointCount));
+            return plt;
+        }
+    }
+}
