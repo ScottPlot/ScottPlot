03792253459447b0498624986266c2b9cf373e2f
Scott W Harden <swharden@gmail.com>
2020-02-25 23:45:57 -0500

    create WPF and WinForms demo projects
    #271

---
+++ src/ScottPlot.Demo.WinForms/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlot.Demo.WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var plt = ScottPlot.Demo.Plots.SinAndCos();
+            Controls.Add(new ScottPlot.FormsPlot(plt) { Dock = DockStyle.Fill, Name = "formsPlot1" });
+        }
+    }
+}
