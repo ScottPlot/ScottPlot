337c36cbd9d050e16023924b15c41c2cd199ee15
Scott W Harden <swharden@gmail.com>
2020-02-26 23:41:39 -0500

    rough start for central demo library #271

---
+++ src/ScottPlot.Demo/IPlotDemo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlot.Demo
+{
+    public interface IPlotDemo
+    {
+        string name { get; }
+        string description { get; }
+        void Render(Plot plt);
+    }
+}
--- src/ScottPlot.Demo/PlotTypes/BoxAndWhisker.cs
+++ src/ScottPlot.Demo/PlotTypes/BoxAndWhisker.cs
@@ -4,27 +4,48 @@ using System.Text;
 
 namespace ScottPlot.Demo.PlotTypes
 {
-    public static class BoxAndWhisker
+    public class BoxAndWhisker
     {
-        public static Plot Quickstart()
+        public class Quickstart : IPlotDemo
         {
-            var plt = new Plot();
 
-            var boxAndWiskers = new Statistics.BoxAndWhisker[3];
-            boxAndWiskers[0] = Statistics.BoxAndWhiskerFromData.OutlierQuartileMedian(Data.LineLengths.plot, 1);
-            boxAndWiskers[1] = Statistics.BoxAndWhiskerFromData.OutlierQuartileMedian(Data.LineLengths.formsPlot, 2);
-            boxAndWiskers[2] = Statistics.BoxAndWhiskerFromData.OutlierQuartileMedian(Data.LineLengths.wpfPlot, 3);
+            public string name { get; }
+            public string description { get; }
 
-            plt.Title("Source Code Line Length");
-            plt.PlotBoxAndWhisker(boxAndWiskers);
-            plt.YLabel("Number of Characters");
+            public void Render(Plot plt)
+            {
+                Random rand = new Random(0);
+                var baw1 = new Statistics.BoxAndWhisker(0, DataGen.Random(rand, 20));
+                var baw2 = new Statistics.BoxAndWhisker(1, DataGen.Random(rand, 20));
+                var baw3 = new Statistics.BoxAndWhisker(2, DataGen.Random(rand, 20));
+                var baws = new Statistics.BoxAndWhisker[] { baw1 , baw2, baw3 };
+                plt.PlotBoxAndWhisker(baws);
+            }
+        }
 
-            double[] xPositions = { 1, 2, 3 };
-            string[] labels = { "Plot.cs", "FormsPlot.cs", "WpfPlot.cs" };
-            plt.XTicks(xPositions, labels);
+        public class Advanced : IPlotDemo
+        {
 
-            plt.AxisAuto(.3, .2);
-            return plt;
+            public string name { get; }
+            public string description { get; }
+
+            public void Render(Plot plt)
+            {
+                var boxAndWiskers = new Statistics.BoxAndWhisker[3];
+                boxAndWiskers[0] = Statistics.BoxAndWhiskerFromData.OutlierQuartileMedian(Data.LineLengths.plot, 1);
+                boxAndWiskers[1] = Statistics.BoxAndWhiskerFromData.OutlierQuartileMedian(Data.LineLengths.formsPlot, 2);
+                boxAndWiskers[2] = Statistics.BoxAndWhiskerFromData.OutlierQuartileMedian(Data.LineLengths.wpfPlot, 3);
+
+                plt.Title("Source Code Line Length");
+                plt.PlotBoxAndWhisker(boxAndWiskers);
+                plt.YLabel("Number of Characters");
+
+                double[] xPositions = { 1, 2, 3 };
+                string[] labels = { "Plot.cs", "FormsPlot.cs", "WpfPlot.cs" };
+                plt.XTicks(xPositions, labels);
+
+                plt.AxisAuto(.3, .2);
+            }
         }
     }
 }
