0a1f45974356bd966366afbfd8106e112dad2313
Scott W Harden <swharden@gmail.com>
2020-03-05 19:47:37 -0500

    Update Chef.cs

--- src/ScottPlot.Demo/Cookbook/Chef.cs
+++ src/ScottPlot.Demo/Cookbook/Chef.cs
@@ -9,22 +9,52 @@ namespace ScottPlot.Demo.Cookbook
         public static void MakeCookbook(string sourceCodeFolder, string outputFolder = "./output")
         {
             outputFolder = System.IO.Path.GetFullPath(outputFolder);
-            string imageFolder = System.IO.Path.Combine(outputFolder, "images");
-
             Console.WriteLine($"Generating cookbook in: {outputFolder}");
+            ClearFolders(outputFolder);
+            RenderAndSaveImages(outputFolder);
+            CreateReport(sourceCodeFolder, outputFolder);
+        }
 
-            if (System.IO.Directory.Exists(outputFolder))
-                System.IO.Directory.Delete(outputFolder, recursive: true);
-            System.IO.Directory.CreateDirectory(outputFolder);
-            System.IO.Directory.CreateDirectory(imageFolder);
+        private static void ClearFolders(string outputFolder)
+        {
+            if (!System.IO.Directory.Exists(outputFolder))
+                System.IO.Directory.CreateDirectory(outputFolder);
+            foreach (string filePathToDelete in System.IO.Directory.GetFiles(outputFolder, "*.*"))
+                System.IO.File.Delete(filePathToDelete);
 
+            string imageFolder = System.IO.Path.Combine(outputFolder, "images");
+            if (!System.IO.Directory.Exists(imageFolder))
+                System.IO.Directory.CreateDirectory(imageFolder);
+            foreach (string filePathToDelete in System.IO.Directory.GetFiles(imageFolder, "*.*"))
+                System.IO.File.Delete(filePathToDelete);
+        }
+
+        private static void RenderAndSaveImages(string outputFolder)
+        {
             foreach (IPlotDemo recipe in Reflection.GetPlots())
             {
                 var plt = new Plot(600, 400);
                 recipe.Render(plt);
-                plt.SaveFig($"{imageFolder}/{recipe.id}.png");
-                System.IO.File.WriteAllText($"{imageFolder}/{recipe.id}.cs", recipe.GetSourceCode(sourceCodeFolder));
+                plt.SaveFig($"{outputFolder}/images/{recipe.id}.png");
             }
+        }
+
+        private static void CreateReport(string sourceCodeFolder, string outputFolder)
+        {
+            StringBuilder md = new StringBuilder();
+            StringBuilder html = new StringBuilder();
+
+            foreach (IPlotDemo recipe in Reflection.GetPlots())
+            {
+                md.AppendLine($"## {recipe.name}\n\n");
+                md.AppendLine($"**Description:** {recipe.description}\n\n");
+                md.AppendLine($"**Source code:** {recipe.categoryMajor}.{recipe.categoryMinor} ({recipe.categoryClass})\n\n");
+                md.AppendLine($"```cs\n{recipe.GetSourceCode(sourceCodeFolder)}\n```\n\n");
+                md.AppendLine($"![](images/{recipe.id}.png)\n\n");
+            }
+
+            System.IO.File.WriteAllText(outputFolder + "/index.html", html.ToString());
+            System.IO.File.WriteAllText(outputFolder + "/readme.md", md.ToString());
         }
     }
 }
