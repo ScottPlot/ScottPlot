2f96ed4d705a676b4d6b64907db7ba6ebb399924
Scott W Harden <swharden@gmail.com>
2020-03-08 11:45:16 -0400

    support for non-interactive (Bitmap) demo plots #271

---
+++ src/ScottPlot.Demo/IBitmapDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlot.Demo
+{
+    public interface IBitmapDemo
+    {
+        // For plots which cannot be rendered onto an existing plot.
+        System.Drawing.Bitmap Render(int width, int height);
+    }
+}
