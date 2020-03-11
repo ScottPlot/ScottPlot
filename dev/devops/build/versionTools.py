76496267fb357c3ea067ef1b25132cc5dbc2e0e9
Scott W Harden <swharden@gmail.com>
2020-03-10 20:30:36 -0400

    start new build scripts

--- dev/devops/build/versionTools.py
+++ dev/devops/build/versionTools.py
@@ -1,23 +1,6 @@
-"""
-This script reduces friction between code modification and NuGet releases
-by automating the versioning, building, and uploading of NuGet packages.
-"""
-
-import urllib.request
+import urllib
 import json
 import os
-import glob
-import subprocess
-
-PATH_HERE = os.path.abspath(os.path.dirname(__file__))
-PATH_SRC = os.path.dirname(os.path.dirname(PATH_HERE))+"/src/"
-SOLUTION_FILE = PATH_SRC+"/ScottPlotV4.sln"
-PROJECT_FILES = [
-    PATH_SRC+"/ScottPlot/ScottPlot.csproj",
-    PATH_SRC+"/ScottPlot.WinForms/ScottPlot.WinForms.csproj",
-    PATH_SRC+"/ScottPlot.WPF/ScottPlot.WPF.csproj",
-]
-
 
 class Version:
     def __init__(self, versionString):
@@ -75,57 +58,9 @@ def SetProjectVersion(csprojPath, newVersion):
         f.write("\n".join(lines))
 
 
-def IncreasePackageVersions():
+def increasePackageVersions(PROJECT_FILES):
     version = GetOnlineVersion()
     version.increase()
     newVersion = str(version)
     for projectFile in PROJECT_FILES:
         SetProjectVersion(projectFile, newVersion)
-
-
-def BuildNewPackages():
-    for projectFile in PROJECT_FILES:
-        print(f"building {os.path.basename(projectFile)}...")
-
-        projectFolder = os.path.abspath(os.path.dirname(projectFile))
-        releaseFolder = projectFolder+"/bin/Release"
-        for oldPackage in glob.glob(releaseFolder+"/*nupkg"):
-            os.remove(oldPackage)
-
-        args = ["dotnet", "build", projectFile, "--configuration", "Release"]
-        p = subprocess.call(args, stdout=subprocess.DEVNULL)
-        if p != 0:
-            raise Exception("build failed")
-
-
-def UpdateNuget():
-    p = subprocess.call(["nuget", "update", "-self"])
-    if p != 0:
-        raise Exception("nuget update failed")
-
-
-def UploadToNuGet():
-    # requires you to have run "nuget.exe setApiKey [secretKey]" previously
-    UpdateNuget()
-    for projectFile in PROJECT_FILES:
-        releaseFolder = os.path.dirname(projectFile)+"/bin/Release"
-        releaseFolder = os.path.abspath(releaseFolder)
-        packages = glob.glob(releaseFolder+"/*.nupkg")
-        assert len(packages) == 1
-        nupkgPath = packages[0]
-        print(f"uploading {os.path.basename(nupkgPath)}...")
-        args = [
-            "nuget", "push", nupkgPath,
-            "-Source", "https://api.nuget.org/v3/index.json"
-        ]
-        p = subprocess.call(args)
-        if p != 0:
-            raise Exception("nuget update failed")
-
-
-if __name__ == "__main__":
-    print("Build and upload a new NuGet package? (type yes)")
-    if input() == "yes":
-        IncreasePackageVersions()
-        BuildNewPackages()
-        UploadToNuGet()
