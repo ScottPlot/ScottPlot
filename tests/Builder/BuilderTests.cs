a6a68035a930642b0456842fe8373fb846d2dfd6
Scott W Harden <swharden@gmail.com>
2020-03-11 23:52:54 -0400

    start project builder

---
+++ tests/Builder/BuilderTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlotTests.Builder
+{
+    class BuilderTests
+    {
+        [Test]
+        public void Test_Version_ParsesCsprojFile()
+        {
+            string projectFilePath = @"../../../../src/ScottPlot.WinForms/ScottPlot.WinForms.csproj";
+            var ver = new ScottPlotBuilder.ProjectFileVersion(projectFilePath);
+            Console.WriteLine(ver);
+        }
+    }
+}
