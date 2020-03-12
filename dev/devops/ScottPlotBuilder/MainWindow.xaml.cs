f189460a0f120403dfb2daf3481b4fa161118498
Scott W Harden <swharden@gmail.com>
2020-03-11 23:52:54 -0400

    start project builder

---
+++ dev/devops/ScottPlotBuilder/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScottPlotBuilder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ProjectFileVersion projVersion;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            string projectFilePath = @"../../../../../../src/ScottPlot/ScottPlot.csproj";
+            projVersion = new ProjectFileVersion(projectFilePath);
+            VersionStartedText.Text = $"initial: {projVersion}";
+            VersionNugetText.Text = $"nuget.org: searching...";
+            VersionCurrentText.Text = $"current: {projVersion}";
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+            worker.RunWorkerAsync();
+        }
+
+        private string DownloadTextFile(string url)
+        {
+            url = url.Trim();
+            WebClient client = new WebClient();
+            MemoryStream stream = new MemoryStream(client.DownloadData(url));
+            StreamReader reader = new StreamReader(stream);
+            string text = reader.ReadToEnd();
+            reader.Close();
+            return text;
+        }
+
+        Version nugetVersion = new Version();
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // do NOT access GUI components from this thread
+            string html = DownloadTextFile("https://www.nuget.org/packages/ScottPlot/");
+            foreach (string line in html.Split("\n"))
+            {
+                if (line.Contains("<small"))
+                {
+                    string[] parts = line.Trim().Replace("<", "|").Replace(">", "|").Split("|");
+                    string versionString = parts[2];
+                    Debug.WriteLine($"version string from NuGet website: {versionString}");
+                    nugetVersion = new Version(versionString);
+                }
+            }
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // you may access GUI components from this thread
+            VersionNugetText.Text = (nugetVersion.Major > 0) ? $"nuget.org: {nugetVersion}" : "nuget.org: HTML parse error";
+        }
+
+        private void VersionReset(object sender, RoutedEventArgs e)
+        {
+            projVersion.Reset();
+            VersionCurrentText.Text = $"current: {projVersion}";
+        }
+
+        private void VersionIncriment(object sender, RoutedEventArgs e)
+        {
+            projVersion.Incriment();
+            VersionCurrentText.Text = $"current: {projVersion}";
+        }
+
+        private void VersionApply(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ dev/devops/ScottPlotBuilder/ProjectFileVersion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ScottPlotBuilder
+{
+    public class ProjectFileVersion
+    {
+        public readonly string path;
+        public Version version { get; private set; }
+        public Version initial { get; private set; }
+
+        public ProjectFileVersion(string projectFilePath)
+        {
+            if (!projectFilePath.EndsWith(".csproj"))
+                throw new ArgumentException("Project files must end with .csproj");
+
+            projectFilePath = System.IO.Path.GetFullPath(projectFilePath);
+            if (!System.IO.File.Exists(projectFilePath))
+                throw new ArgumentException("Project file does not exist: " + projectFilePath);
+
+            path = projectFilePath;
+            initial = Read();
+            Reset();
+        }
+
+        public override string ToString()
+        {
+            return $"{version.Major}.{version.Minor}.{version.Build}";
+        }
+
+        public void Reset()
+        {
+            version = new Version(initial.ToString());
+        }
+
+        private Version Read()
+        {
+            string[] sourceLines = System.IO.File.ReadAllLines(path);
+            foreach (string line in sourceLines)
+            {
+                string[] parts = line.Split("Version>");
+                if (parts.Length == 3)
+                {
+                    string versionString = parts[1].Split("<")[0];
+                    versionString += ".0";
+                    return new Version(versionString);
+                }
+            }
+
+            throw new ArgumentException("file does not contain version information: " + path);
+        }
+
+        public void Incriment()
+        {
+            version = new Version(version.Major, version.Minor, version.Build + 1);
+        }
+    }
+}
