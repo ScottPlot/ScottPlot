29fdd2be4b9d11e41ff02dc9db33d2489555bc1e
Scott W Harden <swharden@gmail.com>
2020-03-11 20:04:40 -0400

    create standalone cookbook generator project #271

---
+++ src/ScottPlot.Demo.CookbookGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScottPlot.Demo.CookbookGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- tests/Cookbook/Chef.cs
+++ tests/Cookbook/Chef.cs
@@ -28,35 +28,5 @@ namespace ScottPlotTests.Cookbook
 
             }
         }
-
-        [Test]
-        public void Test_Cookbook_Chef()
-        {
-            // don't run test on MacOS
-            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.OSX))
-                return; // TODO: figure out how to get this working in MacOS
-
-            var chef = new ScottPlot.Demo.Cookbook.Chef(sourceFolder: "../../../../src/ScottPlot.Demo/");
-        }
-
-        [Test]
-        public void Test_Cookbook_Generate()
-        {
-            // don't run test on MacOS
-            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.OSX))
-                return; // TODO: figure out how to get this working in MacOS
-
-            var chef = new ScottPlot.Demo.Cookbook.Chef(sourceFolder: "../../../../src/ScottPlot.Demo/");
-            chef.ClearFolders();
-            foreach (IPlotDemo recipe in Reflection.GetPlots())
-            {
-                Console.WriteLine($"creating {recipe.categoryMajor}.{recipe.categoryMinor}.{recipe.categoryClass}...");
-                chef.CreateImage(recipe);
-            }
-
-            Console.WriteLine($"creating reports...");
-            chef.MakeReports();
-            Console.WriteLine($"Cookbook generated in: {chef.outputFolder}");
-        }
     }
 }
