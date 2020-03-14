2d8ffe7aacda9a66636c5ceefc481d2f63837b12
Scott W Harden <swharden@gmail.com>
2020-03-14 10:40:53 -0400

    add TI84 regression test
    #293

--- tests/Statistics/Regression.cs
+++ tests/Statistics/Regression.cs
@@ -87,5 +87,15 @@ namespace ScottPlotTests.Statistics
             Assert.AreEqual(actualSlope, model.slope, .1);
             Assert.AreEqual(actualOffset, model.offset, 10);
         }
+
+        [Test]
+        public void GetCoefficients_FirstXNotZero_ResultEqualtoTI_84_Plus()
+        {
+            double[] x = new double[] { 1, 2, 3, 4, 5, 6, 7 };
+            double[] y = new double[] { 2, 2, 3, 3, 3.8, 4.2, 4 };
+            var reg = new ScottPlot.Statistics.LinearRegressionLine(x, y);
+            Assert.AreEqual(0.4, reg.slope, 0.0001);
+            Assert.AreEqual(1.5428, reg.offset, 0.0001);
+        }
     }
 }
