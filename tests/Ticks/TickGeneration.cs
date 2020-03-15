1ffd83702023f87599406f9f11dd8a8e4705d2f3
Scott W Harden <swharden@gmail.com>
2020-03-15 13:41:47 -0400

    create tick test for #299

--- tests/Ticks/TickGeneration.cs
+++ tests/Ticks/TickGeneration.cs
@@ -8,6 +8,34 @@ namespace ScottPlotTests.Ticks
     class TickGeneration
     {
         [Test]
+        public void Test_DefinedSpacing_NumericAxis()
+        {
+            int pointCount = 20;
+
+            // create a series of day numbers
+            double[] days = ScottPlot.DataGen.Consecutive(pointCount);
+
+            // simulate data for each date
+            double[] values = new double[pointCount];
+            Random rand = new Random(0);
+            for (int i = 1; i < pointCount; i++)
+                values[i] = values[i - 1] + rand.NextDouble();
+
+            var mplt = new ScottPlot.MultiPlot(1000, 400, 1, 2);
+
+            var pltDefault = mplt.GetSubplot(0, 0);
+            pltDefault.PlotScatter(days, values);
+            pltDefault.Title("Default xSpacing");
+
+            var pltTest = mplt.GetSubplot(0, 1);
+            pltTest.PlotScatter(days, values);
+            pltTest.Grid(xSpacing: 1); // force inter-tick distance
+            pltTest.Title("xSpacing = 1 unit");
+
+            TestTools.SaveFig(mplt);
+        }
+
+        [Test]
         public void Test_DefinedSpacing_DateTimeAxis()
         {
             int pointCount = 20;
@@ -34,7 +62,7 @@ namespace ScottPlotTests.Ticks
             var pltTest = mplt.GetSubplot(0, 1);
             pltTest.PlotScatter(dates, values);
             pltTest.Ticks(dateTimeX: true);
-            pltTest.Grid(xSpacing: 1);
+            pltTest.Grid(xSpacing: 1); // force 1 tick per day
             pltTest.Title("xSpacing = 1 day");
 
             TestTools.SaveFig(mplt);
