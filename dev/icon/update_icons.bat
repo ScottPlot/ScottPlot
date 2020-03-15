a628352d3fd924a957052f5b2425c140fde9f17b
Scott W Harden <swharden@gmail.com>
2020-03-15 11:45:06 -0400

    update icons

---
+++ dev/icon/update_icons.bat
@@ -0,0 +1,10 @@
+del ..\..\src\ScottPlot\icon.*
+del ..\..\src\ScottPlot.WPF\icon.*
+del ..\..\src\ScottPlot.WinForms\icon.*
+
+copy icon.* ..\..\src\ScottPlot
+copy icon.* ..\..\src\ScottPlot.WPF
+copy icon.* ..\..\src\ScottPlot.WinForms
+
+
+pause
