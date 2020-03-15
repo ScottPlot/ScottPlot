660f7e1bd684477032e90c27279585e42b7a0658
Scott W Harden <swharden@gmail.com>
2020-03-14 23:38:36 -0400

    add custom culture cookbook demo #301

--- src/ScottPlot.Demo/Customize/Ticks.cs
+++ src/ScottPlot.Demo/Customize/Ticks.cs
@@ -137,6 +137,28 @@ namespace ScottPlot.Demo.Customize
             }
         }
 
+        public class CustomCulture : PlotDemo, IPlotDemo
+        {
+            public string name { get; } = "Format Ticks with Custom Culture";
+            public string description { get; } = "SetCulture() as arguments to let the user manually define formatting strings which will be used globally to change how numbers and dates are formatted.";
+
+            public void Render(Plot plt)
+            {
+                // generate 10 days of data
+                int pointCount = 10;
+                double[] values = DataGen.RandomWalk(null, pointCount);
+                double[] days = new double[pointCount];
+                DateTime day1 = new DateTime(1985, 09, 24);
+                for (int i = 0; i < days.Length; i++)
+                    days[i] = day1.AddDays(1).AddDays(i).ToOADate();
+
+                // plot the data with custom tick format (https://tinyurl.com/ycwh45af)
+                plt.PlotScatter(days, values);
+                plt.Ticks(dateTimeX: true);
+                plt.SetCulture(shortDatePattern: "M\\/dd");
+            }
+        }
+
         public class Large : PlotDemo, IPlotDemo
         {
             public string name { get; } = "Accomodating Large Ticks";
