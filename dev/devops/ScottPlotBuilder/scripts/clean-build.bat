9465a401205af3679cf83f1dbbe463d5e8a310e1
Scott W Harden <swharden@gmail.com>
2020-03-14 20:39:18 -0400

    update build script

--- dev/devops/ScottPlotBuilder/scripts/clean-build.bat
+++ dev/devops/ScottPlotBuilder/scripts/clean-build.bat
@@ -3,18 +3,21 @@
 echo.
 echo ########## Building ScottPlot #################################################
 echo.
+del /F /Q ..\..\..\..\src\ScottPlot\bin\Release\*.*
 dotnet clean ../../../../src/ScottPlot/ScottPlot.csproj --configuration Release || goto :error
 dotnet build ../../../../src/ScottPlot/ScottPlot.csproj --configuration Release || goto :error
 
 echo.
 echo ########## Building ScottPlot.WinForms ########################################
 echo.
+del /F /Q ..\..\..\..\src\ScottPlot.WinForms\bin\Release\*.*
 dotnet clean ../../../../src/ScottPlot.WinForms/ScottPlot.WinForms.csproj --configuration Release || goto :error
 dotnet build ../../../../src/ScottPlot.WinForms/ScottPlot.WinForms.csproj --configuration Release || goto :error
 
 echo.
 echo ########## Building ScottPlot.WPF #############################################
 echo.
+del /F /Q ..\..\..\..\src\ScottPlot.WPF\bin\Release\*.*
 dotnet clean ../../../../src/ScottPlot.WPF/ScottPlot.WPF.csproj --configuration Release || goto :error
 dotnet build ../../../../src/ScottPlot.WPF/ScottPlot.WPF.csproj --configuration Release || goto :error
 
