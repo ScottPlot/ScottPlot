683bbcb805af789833e78d72b5712e745616b4bc
Scott W Harden <swharden@gmail.com>
2020-03-15 16:12:14 -0400

    update winforms demo icons

--- src/ScottPlot.Demo.WinForms/FormCookbook.Designer.cs
+++ src/ScottPlot.Demo.WinForms/FormCookbook.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCookbook));
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.DemoNameLabel = new System.Windows.Forms.Label();
@@ -170,6 +171,7 @@
             this.Controls.Add(this.sourceCodeGroupbox);
             this.Controls.Add(this.DemoNameLabel);
             this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormCookbook";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ScottPlot Demos (WinForms)";
--- src/ScottPlot.Demo.WinForms/FormMain.Designer.cs
+++ src/ScottPlot.Demo.WinForms/FormMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.cookbookButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.versionLabel = new System.Windows.Forms.Label();
@@ -313,6 +314,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.versionLabel);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ScottPlot Demos (WinForms)";
