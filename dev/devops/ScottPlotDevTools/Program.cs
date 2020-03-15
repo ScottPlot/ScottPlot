3358329dc7ec2770c008226a4ad0faa2547cd92b
Scott W Harden <swharden@gmail.com>
2020-03-15 11:37:14 -0400

    fully automated demo packaging

--- dev/devops/ScottPlotDevTools/Program.cs
+++ dev/devops/ScottPlotDevTools/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ScottPlotDevTools
 {
@@ -6,48 +7,32 @@ namespace ScottPlotDevTools
     {
         static void Main(string[] args)
         {
-            switch (args.Length)
+            Console.WriteLine($"ScottPlot {ScottPlot.Tools.GetVersionString()}");
+
+            //args = new string[] { "-incrimentVersion" };
+            //args = new string[] { "-makeCookbook" };
+            args = new string[] { "-makeDemo" };
+            ProcessArguments(args);
+        }
+
+        static void ProcessArguments(string[] args)
+        {
+            string command = "help";
+            string argument = "";
+
+            if (args.Length >= 1)
+                command = args[0].Trim('-');
+            if (args.Length >= 2)
+                argument = args[1];
+
+            switch (command)
             {
-                case 0:
-                    Console.WriteLine("ERROR: argument required.");
-                    ShowHelp();
-                    break;
-                case 1:
-                    switch (args[0])
-                    {
-                        case "incrimentVersion": IncrimentVersion(); break;
-                        case "makeCookbook": MakeCookbook(); break;
-                        case "makeDemo": MakeDemo(); break;
-                        default: Console.WriteLine("ERROR: unknown command."); ShowHelp(); break;
-                    }
-                    break;
-                case 2:
-                    switch (args[0])
-                    {
-                        case "setVersion": if (args.Length == 2) SetVersion(args[1]); else ShowHelp(); break;
-                    }
-                    break;
+                case "help": ShowHelp(); break;
+                case "incrimentVersion": IncrimentVersion(); break;
+                case "makeCookbook": MakeCookbook(); break;
+                case "makeDemo": MakeDemo(); break;
+                default: Console.WriteLine("ERROR: unknown command."); ShowHelp(); break;
             }
-        }
-
-        static void IncrimentVersion()
-        {
-            Console.WriteLine("incrimenting version...");
-        }
-
-        static void SetVersion(string newVersion)
-        {
-            Console.WriteLine("setting version...");
-        }
-
-        static void MakeCookbook()
-        {
-            Console.WriteLine("making cookbook...");
-        }
-
-        static void MakeDemo()
-        {
-            Console.WriteLine("making demo...");
         }
 
         static void ShowHelp()
@@ -56,9 +41,174 @@ namespace ScottPlotDevTools
             Console.WriteLine("Command Line Arguments:");
             Console.WriteLine();
             Console.WriteLine(" -incrimentVersion");
-            Console.WriteLine(" -setVersion 1.2.3");
             Console.WriteLine(" -makeCookbook");
             Console.WriteLine(" -makeDemo");
+        }
+
+        static void IncrimentVersion()
+        {
+            Console.WriteLine("incrimenting version...");
+
+            string[] projectNames = new string[] { "ScottPlot", "ScottPlot.WinForms", "ScottPlot.WPF" };
+
+            foreach (string projectName in projectNames)
+            {
+                string projectPath = $"../../../../../../src/{projectName}/{projectName}.csproj";
+                projectPath = System.IO.Path.GetFullPath(projectPath);
+                if (!System.IO.File.Exists(projectPath))
+                    throw new ArgumentException($"file not found: {projectPath}");
+
+                var projVersion = new ProjectFileVersion(projectPath);
+                string oldVersion = projVersion.version.ToString();
+                projVersion.Incriment();
+                string newVersion = projVersion.version.ToString();
+                Console.WriteLine($"{oldVersion} -> {newVersion} ({projVersion.name})");
+                //thisProjVersion.Save();
+            }
+        }
+
+        static void MakeCookbook()
+        {
+            Console.WriteLine("making cookbook...");
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string outputFolder = System.IO.Path.GetFullPath($"./{ScottPlot.Tools.GetVersionString()}");
+            var reportGeneratpr = new ScottPlot.Demo.ReportGenerator(outputFolder: outputFolder);
+
+            Console.WriteLine($"Preparing output folder...");
+            reportGeneratpr.ClearFolders();
+
+            Console.Write("Generating cookbook figures");
+            var recipes = ScottPlot.Demo.Reflection.GetPlots();
+            foreach (var recipe in recipes)
+            {
+                Console.Write(".");
+                reportGeneratpr.CreateImage(recipe);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"Creating reports...");
+            reportGeneratpr.MakeReports();
+
+            double elapsedSec = (double)stopwatch.ElapsedTicks / Stopwatch.Frequency;
+            Console.WriteLine($"Cookbook finished ({recipes.Length} figures generated in {elapsedSec:0.00} seconds)");
+            Console.WriteLine(outputFolder);
+        }
+
+        static void CleanProject(string projectName)
+        {
+            string projectPath = $"../../../../../../src/{projectName}/{projectName}.csproj";
+            projectPath = System.IO.Path.GetFullPath(projectPath);
+            if (!System.IO.File.Exists(projectPath))
+                throw new ArgumentException($"file not found: {projectPath}");
+            Console.WriteLine($"\nCleaning: {projectName}...");
+            RunCommand($"dotnet clean {projectPath} --verbosity quiet --configuration Release");
+        }
+
+        static void BuildProject(string projectName)
+        {
+            string projectPath = $"../../../../../../src/{projectName}/{projectName}.csproj";
+            projectPath = System.IO.Path.GetFullPath(projectPath);
+            if (!System.IO.File.Exists(projectPath))
+                throw new ArgumentException($"file not found: {projectPath}");
+            Console.WriteLine($"\nBuilding: {projectName}...");
+            RunCommand($"dotnet build {projectPath} --verbosity quiet --configuration Release");
+            Console.WriteLine();
+        }
+
+        static void RunCommand(string command)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command) { UseShellExecute = false, };
+            Process process = Process.Start(processInfo);
+            process.WaitForExit();
+            if (process.ExitCode != 0)
+                throw new OperationCanceledException($"ERROR: exit code {process.ExitCode}");
+            process.Close();
+        }
+
+        static void MakeDemo()
+        {
+            string wpfOutputPath = "../../../../../../src/ScottPlot.Demo.WPF/bin/Release/netcoreapp3.0";
+            string demoSourceFolder = "../../../../../../src/ScottPlot.Demo";
+            string wpfOutputSourcePath = System.IO.Path.Combine(wpfOutputPath, "source");
+            string versionOutputPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(wpfOutputPath), ScottPlot.Tools.GetVersionString());
+            versionOutputPath = System.IO.Path.GetFullPath(versionOutputPath);
+            string zipFilePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(wpfOutputPath), $"ScottPlotDemo-{ScottPlot.Tools.GetVersionString()}.zip");
+
+            Console.WriteLine("Deleting old binaries...");
+            if (System.IO.Directory.Exists(wpfOutputPath))
+                System.IO.Directory.Delete(wpfOutputPath, true);
+            if (System.IO.Directory.Exists(versionOutputPath))
+                System.IO.Directory.Delete(versionOutputPath, true);
+            foreach (string zipFilePathToDelete in System.IO.Directory.GetFiles(System.IO.Path.GetDirectoryName(wpfOutputPath), "*.zip"))
+                System.IO.File.Delete(zipFilePathToDelete);
+
+            CleanProject("ScottPlot.Demo.WPF");
+            BuildProject("ScottPlot.Demo.WPF");
+
+            // move things into an output folder named by version
+            CopySource(demoSourceFolder, wpfOutputSourcePath);
+            System.IO.Directory.CreateDirectory(versionOutputPath);
+            System.IO.Directory.Move(wpfOutputPath, versionOutputPath + "/ScottPlot Demo");
+
+            // copy in other useful files
+            Console.WriteLine("Copying bundle files...");
+            string bundleFolder = "../../../../../../dev/devops/build/bundle";
+            foreach (string filePath in System.IO.Directory.GetFiles(bundleFolder))
+            {
+                string fileName = System.IO.Path.GetFileName(filePath);
+                System.IO.File.Copy(filePath, System.IO.Path.Combine(versionOutputPath, fileName));
+            }
+
+            // zip the output folder
+            Console.WriteLine("Creating zip file...");
+            System.IO.Compression.ZipFile.CreateFromDirectory(versionOutputPath, zipFilePath);
+
+            Console.WriteLine("Demo build complete!");
+            Console.WriteLine(versionOutputPath);
+
+            // launch the folder
+            System.Diagnostics.Process.Start("explorer.exe", System.IO.Path.GetDirectoryName(wpfOutputPath));
+        }
+
+        static void CopySource(string sourcePath, string destPath)
+        {
+            // verify the source folder is correct
+            sourcePath = System.IO.Path.GetFullPath(sourcePath);
+            destPath = System.IO.Path.GetFullPath(destPath);
+
+            string fileInDemoRoot = System.IO.Path.Join(sourcePath, "IPlotDemo.cs");
+            if (!System.IO.File.Exists(fileInDemoRoot))
+                throw new ArgumentException("File note found: " + fileInDemoRoot);
+            else
+                Console.WriteLine("Found source code folder");
+
+            Console.WriteLine("Copying source files to output folder...");
+            if (!System.IO.Directory.Exists(destPath))
+                System.IO.Directory.CreateDirectory(destPath);
+
+            foreach (string sourceFile in System.IO.Directory.GetFiles(sourcePath, "*.cs"))
+            {
+                string fileName = System.IO.Path.GetFileName(sourceFile);
+                string outputFileName = System.IO.Path.Combine(destPath, fileName);
+                System.IO.File.Copy(sourceFile, outputFileName, overwrite: true);
+                //Console.WriteLine($"  {fileName}");
+            }
+
+            foreach (string sourceSubFolder in System.IO.Directory.GetDirectories(sourcePath))
+            {
+                string folderName = System.IO.Path.GetFileName(sourceSubFolder);
+                //Console.WriteLine($"  {folderName}/");
+                foreach (string sourceFile in System.IO.Directory.GetFiles(sourceSubFolder, "*.cs"))
+                {
+                    string fileName = System.IO.Path.GetFileName(sourceFile);
+                    string outputFolder = System.IO.Path.Combine(destPath, folderName);
+                    string outputFileName = System.IO.Path.Combine(outputFolder, fileName);
+                    if (!System.IO.Directory.Exists(outputFolder))
+                        System.IO.Directory.CreateDirectory(outputFolder);
+                    System.IO.File.Copy(sourceFile, outputFileName, overwrite: true);
+                    //Console.WriteLine($"    {fileName}");
+                }
+            }
         }
     }
 }
---
+++ dev/devops/ScottPlotDevTools/ProjectFileVersion.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ScottPlotDevTools
+{
+    public class ProjectFileVersion
+    {
+        public readonly string path;
+        public Version version;
+        public readonly string name;
+
+        public ProjectFileVersion(string projectFilePath)
+        {
+            if (!projectFilePath.EndsWith(".csproj"))
+                throw new ArgumentException("Project files must end with .csproj");
+
+            projectFilePath = System.IO.Path.GetFullPath(projectFilePath);
+            if (!System.IO.File.Exists(projectFilePath))
+                throw new ArgumentException("Project file does not exist: " + projectFilePath);
+
+            path = projectFilePath;
+            name = System.IO.Path.GetFileNameWithoutExtension(projectFilePath);
+            version = Read();
+        }
+
+        public override string ToString()
+        {
+            return $"{name} {version.Major}.{version.Minor}.{version.Build}";
+        }
+
+        private Version Read()
+        {
+            string[] sourceLines = System.IO.File.ReadAllLines(path);
+            foreach (string line in sourceLines)
+            {
+                string[] parts = line.Split("Version>");
+                if (parts.Length == 3)
+                {
+                    string versionString = parts[1].Split("<")[0];
+                    //versionString += ".0";
+                    return new Version(versionString);
+                }
+            }
+
+            throw new ArgumentException("file does not contain version information: " + path);
+        }
+
+        public void Incriment()
+        {
+            version = new Version(version.Major, version.Minor, version.Build + 1);
+        }
+
+        public void Save()
+        {
+            Version fileVersion = Read();
+            string fileVersionString = $"{fileVersion.Major}.{fileVersion.Minor}.{fileVersion.Build}";
+            string newVersionString = ToString();
+
+            string[] lines = System.IO.File.ReadAllText(path).Split("\n");
+            for (int i=0; i<lines.Length; i++)
+            {
+                string line = lines[i];
+                if (line.Contains($">{fileVersionString}<"))
+                {
+                    lines[i] = line.Replace(fileVersionString, newVersionString);
+                }
+            }
+            System.IO.File.WriteAllText(path, string.Join("\n", lines));
+        }
+    }
+}
