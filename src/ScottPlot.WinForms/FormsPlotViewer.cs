8149e8c974c486a26f4fe3dd08a04ae30ca98257
Scott W Harden <swharden@gmail.com>
2020-03-16 23:58:56 -0400

    create FormsPlotViewer

---
+++ src/ScottPlot.WinForms/FormsPlotViewer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlot
+{
+    public partial class FormsPlotViewer : Form
+    {
+        public FormsPlotViewer(ScottPlot.Plot plot, int windowWidth = 600, int windowHeight = 400, string windowTitle = "ScottPlot Viewer")
+        {
+            InitializeComponent();
+            Width = windowWidth;
+            Height = windowHeight;
+            Text = windowTitle;
+
+            formsPlot1.Reset(plot);
+        }
+    }
+}
