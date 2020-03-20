1c85a9f691638e87736b1fabfc42f10b0cb559dc
Scott W Harden <swharden@gmail.com>
2020-03-19 21:11:46 -0400

    create empty plot test #313

---
+++ tests/Misc/MiscPlots.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlotTests.Misc
+{
+    public class MiscPlots
+    {
+        [Test]
+        public void Test_EmptyPlot_DrawsGridAndTicks()
+        {
+            var plt = new ScottPlot.Plot();
+            TestTools.SaveFig(plt);
+        }
+    }
+}
