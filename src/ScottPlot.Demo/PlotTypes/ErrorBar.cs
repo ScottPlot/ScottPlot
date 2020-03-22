520b3ef8644f82ca4bd1ca170c171e1f7f13a429
Scott W Harden <swharden@gmail.com>
2020-03-22 00:12:49 -0400

    plot errors beneath points

--- src/ScottPlot.Demo/PlotTypes/ErrorBar.cs
+++ src/ScottPlot.Demo/PlotTypes/ErrorBar.cs
@@ -29,16 +29,16 @@ namespace ScottPlot.Demo.PlotTypes
                 double[] errorXNegative = DataGen.RandomNormal(rand, pointCount);
 
                 // plot errors in all 4 directions
-                plt.PlotScatter(dataX, dataY1, lineWidth: 0, label: $"Asymmetric X and Y errors");
                 plt.PlotErrorBars(dataX, dataY1, errorXPositive, errorXNegative, errorYPositive, errorYNegative);
+                plt.PlotScatter(dataX, dataY1, lineWidth: 0, label: $"Asymmetric X and Y errors");
 
                 // plot upper and right errors only
-                plt.PlotScatter(dataX, dataY2, lineWidth: 0, label: $"Positive errors only");
                 plt.PlotErrorBars(dataX, dataY2, errorXPositive, null, errorYPositive, null);
+                plt.PlotScatter(dataX, dataY2, lineWidth: 0, label: $"Positive errors only");
 
                 // plot lower and left errors only
-                plt.PlotScatter(dataX, dataY3, lineWidth: 0, label: $"Negative errors only");
                 plt.PlotErrorBars(dataX, dataY3, null, errorXNegative, null, errorYNegative);
+                plt.PlotScatter(dataX, dataY3, lineWidth: 0, label: $"Negative errors only");
 
                 plt.Legend();
             }
