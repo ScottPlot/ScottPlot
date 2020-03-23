3906211eb65f87990b65e16423aa2846fdb2873b
Scott W Harden <swharden@gmail.com>
2020-03-22 22:09:09 -0400

    improve function demo

--- src/ScottPlot.Demo/PlotTypes/Function.cs
+++ src/ScottPlot.Demo/PlotTypes/Function.cs
@@ -15,8 +15,17 @@ namespace ScottPlot.Demo.PlotTypes
 
             public void Render(Plot plt)
             {
-                var func = new Func<double, double?>((x) => Math.Sin(x) * Math.Sin(10 * x));
-                plt.PlotFunction(func);
+                var func1 = new Func<double, double?>((x) => Math.Sin(x) * Math.Sin(x / 2));
+                plt.PlotFunction(func1, lineWidth: 2, label: "sin(x) * sin(x/2)");
+
+                var func2 = new Func<double, double?>((x) => Math.Sin(x) * Math.Sin(x / 3));
+                plt.PlotFunction(func2, lineWidth: 2, label: "sin(x) * sin(x/3)", lineStyle: LineStyle.Dot);
+
+                var func3 = new Func<double, double?>((x) => Math.Cos(x) * Math.Sin(x / 5));
+                plt.PlotFunction(func3, lineWidth: 2, label: "cos(x) * cos(x/5)", lineStyle: LineStyle.Dash);
+
+                plt.Title("Plot Mathematical Functions");
+                plt.Legend();
             }
         }
     }
