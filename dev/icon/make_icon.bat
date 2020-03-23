f513756983e4c85e733f9405513676f5a843092b
Scott W Harden <swharden@gmail.com>
2020-03-22 23:50:12 -0400

    remove white border from icon #307

--- dev/icon/make_icon.bat
+++ dev/icon/make_icon.bat
@@ -1,1 +1,12 @@
 convert icon.png icon.ico
+
+copy icon.ico ..\..\src\ScottPlot
+copy icon.png ..\..\src\ScottPlot
+
+copy icon.ico ..\..\src\ScottPlot.WinForms
+copy icon.png ..\..\src\ScottPlot.WinForms
+
+copy icon.ico ..\..\src\ScottPlot.WPF
+copy icon.png ..\..\src\ScottPlot.WPF
+
+pause
