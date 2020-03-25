aa42ed18c033892a8e1d954716a309153b99c6b4
Scott W Harden <swharden@gmail.com>
2020-03-24 23:40:22 -0400

    add demo for single series #315

--- src/ScottPlot.Demo/Experimental/BarExperimental.cs
+++ src/ScottPlot.Demo/Experimental/BarExperimental.cs
@@ -6,6 +6,33 @@ namespace ScottPlot.Demo.Experimental
 {
     class BarExperimental
     {
+        public class Bar3Single : PlotDemo, IPlotDemo
+        {
+            public string name { get; } = "Bar 3 experimental";
+            public string description { get; }
+
+            public void Render(Plot plt)
+            {
+                // define values, labels, and style of each bar series
+                var barSetBBR = new DataSet("brainBodyRatio", new double[] { 7, 12, 40, 40, 100, 125, 172, 550, 560, 600, 2496, 2789 });
+
+                // collect BarSets into groups
+                var datasets = new DataSet[] { barSetBBR };
+                var groupLabels = new string[] { "ant", "bird", "mouse", "human", "cat", "dog", "frog", "lion", "elephant", "horse", "shark", "hippo" };
+
+                // create the experimental plottable
+                var plottableThing = new PlottableBarExperimental(datasets, groupLabels);
+
+                // plot the experimental plottable
+                plt.Add(plottableThing);
+                plt.Title("Body-to-Brain Mass Ratio");
+                plt.XTicks(groupLabels);
+                plt.Axis(null, null, 0, null);
+                plt.Ticks(useMultiplierNotation: false);
+                plt.Grid(enableVertical: false);
+            }
+        }
+
         public class Bar3 : PlotDemo, IPlotDemo
         {
             public string name { get; } = "Bar 3 experimental";
@@ -14,8 +41,8 @@ namespace ScottPlot.Demo.Experimental
             public void Render(Plot plt)
             {
                 // define values, labels, and style of each bar series
-                var barSetMen = new DataSet(new double[] { 15, 22, 45, 17 }, "Men");
-                var barSetWomen = new DataSet(new double[] { 37, 21, 29, 13 }, "Women");
+                var barSetMen = new DataSet("Men", new double[] { 15, 22, 45, 17 }, new double[] { 6, 3, 8, 4 });
+                var barSetWomen = new DataSet("Women", new double[] { 37, 21, 29, 13 }, new double[] { 7, 5, 6, 3 });
 
                 // collect BarSets into groups
                 var datasets = new DataSet[] { barSetMen, barSetWomen };
@@ -30,6 +57,7 @@ namespace ScottPlot.Demo.Experimental
                 plt.Title("How often do you read reviews?");
                 plt.YLabel("Respondents (%)");
                 plt.XTicks(groupLabels);
+                plt.Axis(null, null, 0, null);
                 plt.Grid(enableVertical: false);
             }
         }
@@ -42,8 +70,8 @@ namespace ScottPlot.Demo.Experimental
             public void Render(Plot plt)
             {
                 // define values, labels, and style of each bar series
-                var barSetMen = new DataSet(new double[] { 15, 22, 45, 17 }, "Men");
-                var barSetWomen = new DataSet(new double[] { 37, 21, 29, 13 }, "Women");
+                var barSetMen = new DataSet("Men", new double[] { 15, 22, 45, 17 });
+                var barSetWomen = new DataSet("Women", new double[] { 37, 21, 29, 13 });
 
                 // collect BarSets into groups
                 var datasets = new DataSet[] { barSetMen, barSetWomen };
@@ -58,6 +86,7 @@ namespace ScottPlot.Demo.Experimental
                 plt.Title("How often do you read reviews?");
                 plt.YLabel("Respondents (%)");
                 plt.XTicks(groupLabels);
+                plt.Axis(null, null, 0, null);
                 plt.Grid(enableVertical: false);
             }
         }
