fa27e0bdcec9c7cf665589d4c2e1aa49ee51ad83
Scott W Harden <swharden@gmail.com>
2020-04-04 20:58:15 -0400

    simplify build script

--- dev/devops/deploy-3-cookbook-and-demo.bat
+++ dev/devops/deploy-3-cookbook-and-demo.bat
@@ -3,11 +3,12 @@
 echo.
 echo ### REBUILDING COOKBOOK GENERATOR ###
 :: must first rebuild the cookbook generator so it learns of the new version number
-dotnet build ScottPlotDevTools\ScottPlotDevTools.csproj --configuration Debug
+cd ScottPlotDevTools
+dotnet build ScottPlotDevTools.csproj --configuration Debug
+cd bin\Debug\netcoreapp3.1
 
 echo.
 echo ### GENERATING COOKBOOK ###
-cd ScottPlotDevTools\bin\Debug\netcoreapp3.1
 ScottPlotDevTools.exe -makeCookbook
 
 echo.
