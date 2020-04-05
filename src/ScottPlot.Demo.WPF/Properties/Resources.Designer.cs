b854ddbc30356a5956c3375e0e41df35d742ca7a
Scott W Harden <swharden@gmail.com>
2020-04-04 21:03:15 -0400

    fix icon resource issue affecting WPF demo
    threw errors after #333

--- src/ScottPlot.Demo.WPF/Properties/Resources.Designer.cs
+++ src/ScottPlot.Demo.WPF/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace ScottPlot.Demo.WPF.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon icon {
-            get {
-                object obj = ResourceManager.GetObject("icon", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
