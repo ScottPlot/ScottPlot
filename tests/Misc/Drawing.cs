53ab3bc05804b079fec51b5ffa413e8179b8968b
Scott W Harden <swharden@gmail.com>
2020-04-10 20:05:12 -0400

    create tests for scatter plot alignment #342

--- tests/Misc/Drawing.cs
+++ tests/Misc/Drawing.cs
@@ -33,5 +33,45 @@ namespace ScottPlotTests.Misc
 
             TestTools.SaveFig(bmp);
         }
+
+        [Test]
+        public void Test_PolygonVsScatter_Alignment()
+        {
+            double[] xs = { 75, 250, 280, 100 };
+            double[] ys = { -100, -75, -200, -220 };
+
+            var plt = new ScottPlot.Plot(320, 240);
+            plt.PlotPolygon(xs, ys, fillColor: Color.LightGreen);
+            plt.PlotLine(xs[0], ys[0], xs[1], ys[1], Color.Blue);
+            plt.Grid(false);
+            plt.Frame(false);
+            plt.Ticks(false, false);
+            plt.Title("Line/Scatter");
+
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_PolygonVsSignal_Alignment()
+        {
+            double[] xs = { 75, 250, 280, 100 };
+            double[] ys = { -100, -75, -200, -220 };
+
+            var plt = new ScottPlot.Plot(320, 240);
+            plt.PlotPolygon(xs, ys, fillColor: Color.LightGreen);
+            plt.PlotSignal(
+                ys: new double[] { ys[0], ys[1] },
+                sampleRate: 1.0 / (xs[1] - xs[0]),
+                xOffset: xs[0],
+                color: Color.Blue,
+                markerSize: 0
+                );
+            plt.Grid(false);
+            plt.Frame(false);
+            plt.Ticks(false, false);
+            plt.Title("Signal");
+
+            TestTools.SaveFig(plt);
+        }
     }
 }
