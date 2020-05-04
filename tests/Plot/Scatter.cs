682f0e253d0ee57db09b12ddaf0dda34db4e2be4
Scott W Harden <swharden@gmail.com>
2020-05-04 11:05:58 -0400

    add scatter plot tests #369
    tests plotting all zeros for X and Y
    
    confirms plotting zero-element arrays throws

---
+++ tests/Plot/Scatter.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlotTests.Plot
+{
+    class Scatter
+    {
+        [Test]
+        public void Test_Scatter_AllZeros()
+        {
+            double[] dataX = { 0, 0, 0, 0, 0 };
+            double[] dataY = { 0, 0, 0, 0, 0 };
+
+            var plt = new ScottPlot.Plot();
+            plt.PlotScatter(dataX, dataY);
+
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_Scatter_EmptyArrays()
+        {
+            double[] dataX = { };
+            double[] dataY = { };
+
+            var plt = new ScottPlot.Plot();
+
+            Assert.Throws<ArgumentException>(() => { plt.PlotScatter(dataX, dataY); });
+        }
+    }
+}
