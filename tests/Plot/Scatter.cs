95522312395a30c1884c721e94b3fe4cdb72e13c
Scott W Harden <swharden@gmail.com>
2020-05-04 14:45:46 -0400

    test scatter containing NaN and Inf #369

--- tests/Plot/Scatter.cs
+++ tests/Plot/Scatter.cs
@@ -13,7 +13,7 @@ namespace ScottPlotTests.Plot
             double[] dataX = { 0, 0, 0, 0, 0 };
             double[] dataY = { 0, 0, 0, 0, 0 };
 
-            var plt = new ScottPlot.Plot();
+            var plt = new ScottPlot.Plot(400, 300);
             plt.PlotScatter(dataX, dataY);
 
             TestTools.SaveFig(plt);
@@ -25,9 +25,57 @@ namespace ScottPlotTests.Plot
             double[] dataX = { };
             double[] dataY = { };
 
-            var plt = new ScottPlot.Plot();
+            var plt = new ScottPlot.Plot(400, 300);
 
             Assert.Throws<ArgumentException>(() => { plt.PlotScatter(dataX, dataY); });
+        }
+
+        [Test]
+        public void Test_Scatter_HasNanY()
+        {
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 3, double.NaN, 16, 25 };
+
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.PlotScatter(dataX, dataY);
+
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_Scatter_HasNanX()
+        {
+            double[] dataX = { 1, 2, double.NaN, 4, 5 };
+            double[] dataY = { 1, 3, 9, 16, 25 };
+
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.PlotScatter(dataX, dataY);
+
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_Scatter_HasInfY()
+        {
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 3, double.PositiveInfinity, 16, 25 };
+
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.PlotScatter(dataX, dataY);
+
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_Scatter_HasInfX()
+        {
+            double[] dataX = { 1, 2, double.PositiveInfinity, 4, 5 };
+            double[] dataY = { 1, 3, 9, 16, 25 };
+
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.PlotScatter(dataX, dataY);
+
+            TestTools.SaveFig(plt);
         }
     }
 }
