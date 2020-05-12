83bd5acce2b135def49e9e0cc031b021fbc74182
Henk Burgstra <henk.burgstra@gmail.com>
2020-05-12 20:17:41 +0200

    Added interface IMarkerDrawer

---
+++ src/ScottPlot/IMarkerDrawer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ScottPlot
+{
+    public interface IMarkerDrawer
+    {
+        void DrawMarker(Graphics gfx, PointF pixelLocation, MarkerShape shape, float size, Color color);
+    }
+}
