5711ac7dc88207f82275cfd3f9cbec47a1fabbc8
Scott W Harden <swharden@gmail.com>
2020-05-16 15:16:49 -0400

    add SignalXY tests

---
+++ tests/PlotTypes/SignalXY.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlotTests.PlotTypes
+{
+    class SignalXY
+    {
+        [Test]
+        public void Test_AscendingUnevenlySpacedXs_ShouldRenderWell()
+        {
+            // generate random, ascending, unevenly-spaced data
+            Random rand = new Random(0);
+            int pointCount = 100_000;
+            double[] ys = new double[pointCount];
+            double[] xs = new double[pointCount];
+            for (int i = 1; i < ys.Length; i++)
+            {
+                ys[i] = ys[i - 1] + rand.NextDouble() - .5;
+                xs[i] = xs[i - 1] + rand.NextDouble();
+            }
+
+            var plt = new ScottPlot.Plot(500, 350);
+            plt.PlotSignalXY(xs, ys);
+            plt.Axis(20530, 20560, -61, -57);
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_NotAllXsAscend_ThrowsException()
+        {
+            // generate random, NOT-ALL-ascending, unevenly-spaced data
+            Random rand = new Random(0);
+            int pointCount = 100_000;
+            double[] ys = new double[pointCount];
+            double[] xs = new double[pointCount];
+            for (int i = 1; i < ys.Length; i++)
+            {
+                ys[i] = ys[i - 1] + rand.NextDouble() - .5;
+                xs[i] = xs[i - 1] + rand.NextDouble() - .25; // may be <0
+            }
+
+            var plt = new ScottPlot.Plot(500, 350);
+            Assert.Throws<ArgumentException>(() => { plt.PlotSignalXY(xs, ys); });
+        }
+    }
+}
