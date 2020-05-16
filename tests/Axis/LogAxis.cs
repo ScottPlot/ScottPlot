8f3ae492e5ad091734fcc2763ea4a3cecaa82848
Scott W Harden <swharden@gmail.com>
2020-05-16 15:31:50 -0400

    LogAxis test

---
+++ tests/Axis/LogAxis.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlotTests.Axis
+{
+    class LogAxis
+    {
+        [Test]
+        public void Test_Log_Axis()
+        {
+            var plt = new ScottPlot.Plot(400, 300);
+
+            double[] xs = ScottPlot.DataGen.Range(-10, 10, .1, true);
+            double[] ys = ScottPlot.DataGen.Sin(xs);
+            double[] logYs = ScottPlot.Tools.Log10(ys);
+
+            foreach (double val in logYs)
+                Console.WriteLine(val);
+
+            plt.PlotScatter(xs, logYs);
+            plt.YLabel("Log10(value)");
+            plt.Ticks(logScaleY: true);
+            TestTools.SaveFig(plt);
+        }
+    }
+}
