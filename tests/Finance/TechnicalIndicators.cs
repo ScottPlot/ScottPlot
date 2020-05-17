378f267a8e74511bca790424d0de2af327207f2c
Scott W Harden <swharden@gmail.com>
2020-05-16 20:52:44 -0400

    create test for technical indicators #397

---
+++ tests/Finance/TechnicalIndicators.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ScottPlotTests.Finance
+{
+    class TechnicalIndicators
+    {
+        [Test]
+        public void Test_SMA_DoubleArray()
+        {
+            Random rand = new Random(0);
+            double[] xs = DataGen.Consecutive(150);
+            double[] prices = DataGen.RandomWalk(rand, xs.Length, offset: 50);
+            double[] sma20 = ScottPlot.Statistics.Finance.SMA(prices, 20);
+            double[] sma50 = ScottPlot.Statistics.Finance.SMA(prices, 50);
+
+            var plt = new ScottPlot.Plot(600, 400);
+            plt.PlotScatter(xs, prices, lineWidth: 0, label: "Price");
+            plt.PlotScatter(xs, sma20, label: "20 day SMA", markerSize: 0, lineWidth: 2);
+            plt.PlotScatter(xs, sma50, label: "50 day SMA", markerSize: 0, lineWidth: 2);
+
+            plt.YLabel("Price");
+            plt.XLabel("Days");
+            plt.Legend();
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_SMA_OHLCs()
+        {
+            Random rand = new Random(0);
+            double[] xs = DataGen.Consecutive(150);
+            OHLC[] ohlcs = DataGen.RandomStockPrices(rand, xs.Length);
+            double[] sma20 = ScottPlot.Statistics.Finance.SMA(ohlcs, 20);
+            double[] sma50 = ScottPlot.Statistics.Finance.SMA(ohlcs, 50);
+
+            // replace timestamps with a series of numbers starting at 0
+            for (int i = 0; i < ohlcs.Length; i++)
+                ohlcs[i].time = i;
+
+            var plt = new ScottPlot.Plot(600, 400);
+            plt.PlotCandlestick(ohlcs);
+            plt.PlotScatter(xs, sma20, label: "20 day SMA",
+                color: Color.Blue, markerSize: 0, lineWidth: 2);
+            plt.PlotScatter(xs, sma50, label: "50 day SMA",
+                color: Color.Maroon, markerSize: 0, lineWidth: 2);
+
+            plt.Title("Simple Moving Average (SMA)");
+            plt.YLabel("Price");
+            plt.XLabel("Days");
+            plt.Legend();
+            plt.AxisAutoX(.03);
+            TestTools.SaveFig(plt);
+        }
+    }
+}
