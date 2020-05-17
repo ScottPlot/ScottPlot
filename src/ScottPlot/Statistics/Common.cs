1d4c69d56d27488de0d8ad3f40a2604d3baf7793
Scott W Harden <swharden@gmail.com>
2020-05-16 21:22:51 -0400

    create common statistics module
    eventually move this stuff out of tools

---
+++ src/ScottPlot/Statistics/Common.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlot.Statistics
+{
+    public static class Common
+    {
+        public static double StDev(double[] values)
+        {
+            double mean = Mean(values);
+            double sumVariancesSquared = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                double pointVariance = Math.Abs(mean - values[i]);
+                double pointVarianceSquared = Math.Pow(pointVariance, 2);
+                sumVariancesSquared += pointVarianceSquared;
+            }
+            double meanVarianceSquared = sumVariancesSquared / values.Length;
+            double stDev = Math.Sqrt(meanVarianceSquared);
+            return stDev;
+        }
+
+        public static double Mean(double[] values)
+        {
+            double sum = 0;
+            for (int i = 0; i < values.Length; i++)
+                sum += values[i];
+            double mean = sum / values.Length;
+            return mean;
+        }
+    }
+}
