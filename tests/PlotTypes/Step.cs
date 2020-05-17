9081163cb29cbaf24ae3173b35f0494a9e42897b
Scott W Harden <swharden@gmail.com>
2020-05-16 22:56:25 -0400

    create test for step plot

---
+++ tests/PlotTypes/Step.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using ScottPlot;
+using System;
+
+namespace ScottPlotTests.PlotTypes
+{
+    class Step
+    {
+        [Test]
+        public void Test_Step_RandomData()
+        {
+            Random rand = new Random(0);
+            double[] xs = DataGen.Consecutive(20);
+            double[] ys = DataGen.RandomWalk(rand, xs.Length);
+
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.PlotStep(xs, ys);
+            plt.PlotScatter(xs, ys, lineWidth: 0);
+            TestTools.SaveFig(plt);
+        }
+    }
+}
