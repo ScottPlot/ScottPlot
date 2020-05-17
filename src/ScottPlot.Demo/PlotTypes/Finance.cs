ed53242b31a6260ef8bb49eebc38bfc9b137727b
Scott W Harden <swharden@gmail.com>
2020-05-16 23:17:14 -0400

    SMA and Bollinger cookbook demos

--- src/ScottPlot.Demo/PlotTypes/Finance.cs
+++ src/ScottPlot.Demo/PlotTypes/Finance.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ScottPlot.Demo.PlotTypes
@@ -75,6 +76,58 @@ namespace ScottPlot.Demo.PlotTypes
                 double[] tickPositions = { 0, 6, 13, 20, 27 };
                 string[] tickLabels = { "Sep 23", "Sep 30", "Oct 7", "Oct 14", "Oct 21" };
                 plt.XTicks(tickPositions, tickLabels);
+            }
+        }
+
+        public class SimpleMovingAverage : PlotDemo, IPlotDemo
+        {
+            public string name { get; } = "Simple Moving Average (SMA)";
+            public string description { get; } = "This example demonstrates how to plot OHLC data and display Simple Moving Average (SMA) lines on top.";
+
+            public void Render(Plot plt)
+            {
+                Random rand = new Random(0);
+                ScottPlot.OHLC[] ohlcs = DataGen.RandomStockPrices(rand, 75, sequential: true);
+                double[] xs = DataGen.Consecutive(ohlcs.Length);
+                double[] sma20 = Statistics.Finance.SMA(ohlcs, 8);
+                double[] sma50 = Statistics.Finance.SMA(ohlcs, 20);
+
+                plt.PlotCandlestick(ohlcs);
+                plt.PlotScatter(xs, sma20, label: "8 day SMA",
+                    color: Color.Blue, markerSize: 0, lineWidth: 2);
+                plt.PlotScatter(xs, sma50, label: "20 day SMA",
+                    color: Color.Navy, markerSize: 0, lineWidth: 2);
+
+                // decorate the plot
+                plt.Title("Simple Moving Averages (SMA)");
+                plt.YLabel("Stock Price (USD)");
+                plt.XLabel("Day");
+                plt.Legend();
+            }
+        }
+
+        public class Bollinger : PlotDemo, IPlotDemo
+        {
+            public string name { get; } = "Bollinger Bands";
+            public string description { get; } = "This example demonstrates how to plot OHLC data and display Bollinger Bands on top.";
+
+            public void Render(Plot plt)
+            {
+                Random rand = new Random(0);
+                ScottPlot.OHLC[] ohlcs = DataGen.RandomStockPrices(rand, 100, sequential: true);
+                double[] xs = DataGen.Consecutive(ohlcs.Length);
+                (var sma, var bolL, var bolU) = ScottPlot.Statistics.Finance.Bollinger(ohlcs);
+
+                plt.PlotCandlestick(ohlcs);
+                plt.PlotScatter(xs, bolL, color: Color.Blue, markerSize: 0);
+                plt.PlotScatter(xs, bolU, color: Color.Blue, markerSize: 0);
+                plt.PlotScatter(xs, sma, color: Color.Blue, markerSize: 0, lineStyle: LineStyle.Dash);
+
+                // decorate the plot
+                plt.Title("Bollinger Bands");
+                plt.YLabel("Stock Price (USD)");
+                plt.XLabel("Day");
+                plt.Legend();
             }
         }
     }
