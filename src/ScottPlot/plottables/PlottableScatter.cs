9143594388a6aae08b2e37bcb2adbace7fedd8b4
Scott W Harden <swharden@gmail.com>
2020-05-16 20:24:12 -0400

    NaN support for scatter plot #396

--- src/ScottPlot/plottables/PlottableScatter.cs
+++ src/ScottPlot/plottables/PlottableScatter.cs
@@ -139,32 +139,21 @@ namespace ScottPlot
             return new Config.AxisLimits2D(limits);
         }
 
-        PointF[] points;
-        PointF[] pointsStep;
         public override void Render(Settings settings)
         {
             penLine.Color = color;
             penLine.Width = (float)lineWidth;
 
-            if (points is null)
-                points = new PointF[xs.Length];
-
+            // create a List of only valid points
+            List<PointF> points = new List<PointF>(xs.Length);
             for (int i = 0; i < xs.Length; i++)
-                points[i] = settings.GetPixel(xs[i], ys[i]);
-            
-            if (stepDisplay)
-            {
-                if (pointsStep is null)
-                    pointsStep = new PointF[xs.Length * 2 - 1];
-                for (int i = 0; i < points.Length; i++)
-                    pointsStep[i * 2] = points[i];
-                for (int i = 0; i < points.Length - 1; i++)
-                    pointsStep[i * 2 + 1] = new PointF(points[i+1].X, points[i].Y);
-            }
+                if (!double.IsNaN(xs[i]) && !double.IsNaN(ys[i]))
+                    points.Add(settings.GetPixel(xs[i], ys[i]));
 
+            // draw Y errorbars
             if (errorY != null)
             {
-                for (int i = 0; i < points.Length; i++)
+                for (int i = 0; i < points.Count; i++)
                 {
                     PointF errorBelow = settings.GetPixel(xs[i], ys[i] - errorY[i]);
                     PointF errorAbove = settings.GetPixel(xs[i], ys[i] + errorY[i]);
@@ -177,9 +166,10 @@ namespace ScottPlot
                 }
             }
 
+            // draw X errorbars
             if (errorX != null)
             {
-                for (int i = 0; i < points.Length; i++)
+                for (int i = 0; i < points.Count; i++)
                 {
                     PointF errorLeft = settings.GetPixel(xs[i] - errorX[i], ys[i]);
                     PointF errorRight = settings.GetPixel(xs[i] + errorX[i], ys[i]);
@@ -192,16 +182,27 @@ namespace ScottPlot
                 }
             }
 
-            if (penLine.Width > 0 && points.Length > 1)
+            // draw the lines connecting points
+            if (penLine.Width > 0 && points.Count > 1)
             {
                 if (stepDisplay)
-                    settings.gfxData.DrawLines(penLine, pointsStep);
+                {
+                    List<PointF> pointsStep = new List<PointF>(points.Count * 2);
+                    for (int i = 0; i < points.Count; i++)
+                        pointsStep[i * 2] = points[i];
+                    for (int i = 0; i < points.Count - 1; i++)
+                        pointsStep[i * 2 + 1] = new PointF(points[i + 1].X, points[i].Y);
+                    settings.gfxData.DrawLines(penLine, pointsStep.ToArray());
+                }
                 else
-                    settings.gfxData.DrawLines(penLine, points);
+                {
+                    settings.gfxData.DrawLines(penLine, points.ToArray());
+                }
             }
 
+            // draw a marker at each point
             if ((markerSize > 0) && (markerShape != MarkerShape.none))
-                for (int i = 0; i < points.Length; i++)
+                for (int i = 0; i < points.Count; i++)
                     MarkerTools.DrawMarker(settings.gfxData, points[i], markerShape, markerSize, color);
 
         }
