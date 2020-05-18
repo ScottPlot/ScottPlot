6393c5bf43937e3ba8b616c71a6236ab37a0b10f
Scott W Harden <swharden@gmail.com>
2020-05-18 15:15:45 -0400

    move Test_MeasureString_ShowSize()

--- tests/Misc/Drawing.cs
+++ tests/Misc/Drawing.cs
@@ -121,5 +121,29 @@ namespace ScottPlotTests.Misc
             plt.Axis(0, 3, 0, 3);
             TestTools.SaveFig(plt);
         }
+
+        [Test]
+        public void Test_MeasureString_ShowSize()
+        {
+            string testString = "ScottPlot";
+
+            var fontName = ScottPlot.Config.Fonts.GetDefaultFontName();
+            float fontSize = 14;
+
+            // an active Graphics object is required to measure a string...
+            using (var bmp = new System.Drawing.Bitmap(1, 1))
+            using (var gfx = System.Drawing.Graphics.FromImage(bmp))
+            using (var font = new System.Drawing.Font(fontName, fontSize))
+            {
+                var stringSize = ScottPlot.Drawing.GDI.MeasureString(gfx, testString, font);
+
+                var sb = new StringBuilder();
+                sb.AppendLine(System.Environment.OSVersion.ToString());
+                sb.AppendLine($"The string '{testString}' with font '{fontName}' (size {fontSize}) " +
+                    $"measures: {stringSize.Width}px x {stringSize.Height}px");
+
+                Console.WriteLine(sb);
+            }
+        }
     }
 }
