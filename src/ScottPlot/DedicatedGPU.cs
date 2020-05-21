419d06ab80d1ed891bd34ed0fa419fe81acc4e84
Benjamin Lehmann <bclehmann@yahoo.ca>
2020-05-20 18:22:53 -0600

    Added ability to force high-performance graphics accellerator for nvidia

---
+++ src/ScottPlot/DedicatedGPU.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ScottPlot
+{
+	public class DedicatedGPU
+	{
+		[DllImport("nvapi64.dll", EntryPoint = "fake")] // We do not need to enter, only load the dll
+		public static extern int LoadNvApi64();
+		[System.Runtime.InteropServices.DllImport("nvapi.dll", EntryPoint = "fake")] // Ditto
+		static extern int LoadNvApi32();
+
+		/// <summary>
+		///		Attempts to force the high-performance graphics profile on nvidia systems
+		/// </summary>
+		/// <remarks>
+		///		This code is uniquely sketchy
+		/// </remarks>
+		public static void RunWithDedicatedGraphics() {
+			try
+			{
+				if (Environment.Is64BitProcess)
+				{
+					LoadNvApi64();  //This will always throw as the entry point does not exist
+									//On machines with non-nvidia GPUs it should cause no side effects
+									//On machines with nvidia GPUs it should use the nvidia GPU for accelleration
+				}
+				else {
+					LoadNvApi32();	//Ditto
+				}
+			}
+			catch { 
+
+			}
+		}
+	}
+}
