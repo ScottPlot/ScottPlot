381434b5cf2cac55840c275c34a6121d8b5319ea
Scott W Harden <swharden@gmail.com>
2020-05-23 10:15:18 -0400

    sin+cos heatmap demo

--- src/ScottPlot.Demo/Experimental/Heatmap.cs
+++ src/ScottPlot.Demo/Experimental/Heatmap.cs
@@ -19,10 +19,36 @@ namespace ScottPlot.Demo.Experimental
                 int[] ys = Enumerable.Range(0, 100).ToArray();
                 double[][] intensities = new double[ys.Length][];
 
-                for (int i = 0; i < ys.Length; i++) {
+                for (int i = 0; i < ys.Length; i++)
+                {
                     intensities[i] = new double[xs.Length];
-                    for (int j = 0; j < xs.Length; j++) {
+                    for (int j = 0; j < xs.Length; j++)
+                    {
                         intensities[i][j] = Math.Sqrt(Math.Pow(xs[j] - 50, 2) + Math.Pow(ys[i] - 50, 2));
+                    }
+                }
+
+                plt.PlotHeatmap(intensities);
+            }
+        }
+
+        public class HeatmapSinCos : PlotDemo, IPlotDemo
+        {
+            public string name { get; } = "2D Waveform";
+            public string description { get; } = "Another heatmap example.";
+
+            public void Render(Plot plt)
+            {
+                int[] xs = Enumerable.Range(0, 100).ToArray();
+                int[] ys = Enumerable.Range(0, 100).ToArray();
+
+                double[][] intensities = new double[ys.Length][];
+                for (int i = 0; i < ys.Length; i++)
+                {
+                    intensities[i] = new double[xs.Length];
+                    for (int j = 0; j < xs.Length; j++)
+                    {
+                        intensities[i][j] = (Math.Sin(i * .2) + Math.Cos(j * .2)) * 100;
                     }
                 }
 
