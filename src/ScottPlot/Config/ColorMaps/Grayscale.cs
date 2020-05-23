cba25b519b05587f593352148729fbe78086c96a
Benjamin Lehmann <bclehmann@yahoo.ca>
2020-05-23 17:23:33 -0600

    Viridis

---
+++ src/ScottPlot/Config/ColorMaps/Grayscale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlot.Config.ColorMaps
+{
+    public class Grayscale : Colormap
+    {
+        public override byte[,] IntensityToRGB(double[] intensities)
+        {
+            byte[,] outputGrayscale = new byte[intensities.Length, 3];
+            for (int i = 0; i < intensities.Length; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    outputGrayscale[i, j] = (byte)(intensities[i] * 255);
+                }
+            }
+            return outputGrayscale;
+        }
+    }
+}
