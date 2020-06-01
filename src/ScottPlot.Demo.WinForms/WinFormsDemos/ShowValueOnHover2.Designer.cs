29114514ef5b97fe7cf550828e239c309894ca2c
Scott W Harden <swharden@gmail.com>
2020-06-01 00:28:34 -0400

    add tooltip example to WinForms hover demo #410

--- src/ScottPlot.Demo.WinForms/WinFormsDemos/ShowValueOnHover2.Designer.cs
+++ src/ScottPlot.Demo.WinForms/WinFormsDemos/ShowValueOnHover2.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.formsPlot1 = new ScottPlot.FormsPlot();
             this.label1 = new System.Windows.Forms.Label();
+            this.cbTooltip = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // formsPlot1
@@ -54,11 +55,22 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Message";
             // 
+            // cbTooltip
+            // 
+            this.cbTooltip.AutoSize = true;
+            this.cbTooltip.Location = new System.Drawing.Point(536, 14);
+            this.cbTooltip.Name = "cbTooltip";
+            this.cbTooltip.Size = new System.Drawing.Size(58, 17);
+            this.cbTooltip.TabIndex = 2;
+            this.cbTooltip.Text = "Tooltip";
+            this.cbTooltip.UseVisualStyleBackColor = true;
+            // 
             // ShowValueOnHover2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(606, 350);
+            this.Controls.Add(this.cbTooltip);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.formsPlot1);
             this.Name = "ShowValueOnHover2";
@@ -73,5 +85,6 @@
 
         private FormsPlot formsPlot1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbTooltip;
     }
 }
