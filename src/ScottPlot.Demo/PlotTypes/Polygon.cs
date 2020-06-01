af2bea3d448433431c6594b66f67dff41c944435
Scott W Harden <swharden@gmail.com>
2020-05-31 18:31:23 -0700

    simplify demo

--- src/ScottPlot.Demo/PlotTypes/Polygon.cs
+++ src/ScottPlot.Demo/PlotTypes/Polygon.cs
@@ -9,55 +9,62 @@ namespace ScottPlot.Demo.PlotTypes
     {
         public class BenchmarkPolygon : PlotDemo, IPlotDemo
         {
-            public string name { get; } = "Lots of Polygons";
-            public string description { get; } = "PlottablePolygon for each Polygon";
+            public string name { get; } = "PlotPolygon with many polygons";
+            public string description { get; } = "PlotPolygon can display thousands of polygons, but this is SLOW! Use PlotPolygons for this purpose instead.";
 
             public void Render(Plot plt)
             {
-                int polysCount = 5_000;
-                int pointsInPoly = 100;
-                double polyRadius = 1;
-                double SpiralMaxR = 100;
-                double SpiralRevolutions = 10;
+                int polygonCount = 5_000;
+                int pointsPerPolygon = 100;
+                Random rand = new Random(0);
 
-                for (int i = 0; i < polysCount; i++)
+                // create polygons and plot them one at a time
+                for (int i = 0; i < polygonCount; i++)
                 {
-                    double polyCenterX = SpiralMaxR * i / polysCount * Math.Cos(2.0 * Math.PI * SpiralRevolutions * i / polysCount);
-                    double polyCenterY = SpiralMaxR * i / polysCount * Math.Sin(2.0 * Math.PI * SpiralRevolutions * i / polysCount);
-                    double[] xs = Enumerable.Range(0, pointsInPoly).Select(x => polyRadius * i / polysCount * Math.Cos(2.0 * Math.PI * x / pointsInPoly) + polyCenterX).ToArray();
-                    double[] ys = Enumerable.Range(0, pointsInPoly).Select(x => polyRadius * i / polysCount * Math.Sin(2.0 * Math.PI * x / pointsInPoly) + polyCenterY).ToArray();
+                    double polyX = rand.NextDouble() * 100;
+                    double polyY = rand.NextDouble() * 100;
+                    double polyR = rand.NextDouble();
+                    double[] xs = Enumerable.Range(0, pointsPerPolygon).Select(x => polyR * Math.Cos(2.0 * Math.PI * x / pointsPerPolygon) + polyX).ToArray();
+                    double[] ys = Enumerable.Range(0, pointsPerPolygon).Select(x => polyR * Math.Sin(2.0 * Math.PI * x / pointsPerPolygon) + polyY).ToArray();
                     plt.PlotPolygon(xs, ys, fillColor: Color.Green);
                 }
+
+                // customize the plot
                 plt.EqualAxis = true;
-                plt.Title($"Lots of polygons");
+                plt.Title($"PlotPolygon with {polygonCount:N0} {pointsPerPolygon}-sided polygons");
             }
         }
 
         public class BenchmarkPolygons : PlotDemo, IPlotDemo
         {
-            public string name { get; } = "PlottablePolygons demo";
-            public string description { get; } = "Lots of polygons using single PlottablePolygons";
+            public string name { get; } = "PlotPolygons with many polygons";
+            public string description { get; } = "PlottablePolygons is a speed-optimized method of displaying large numbers of polygons.";
 
             public void Render(Plot plt)
             {
-                int polysCount = 5_000;
-                int pointsInPoly = 100;
-                double polyRadius = 1;
-                double SpiralMaxR = 100;
-                double SpiralRevolutions = 10;
+                int polygonCount = 5_000;
+                int pointsPerPolygon = 100;
+                Random rand = new Random(0);
+
                 List<List<(double x, double y)>> polys = new List<List<(double x, double y)>>();
 
-                for (int i = 0; i < polysCount; i++)
+                // create a List of polygons
+                for (int i = 0; i < polygonCount; i++)
                 {
-                    double polyCenterX = SpiralMaxR * i / polysCount * Math.Cos(2.0 * Math.PI * SpiralRevolutions * i / polysCount);
-                    double polyCenterY = SpiralMaxR * i / polysCount * Math.Sin(2.0 * Math.PI * SpiralRevolutions * i / polysCount);
-                    double[] xs = Enumerable.Range(0, pointsInPoly).Select(x => polyRadius * i / polysCount * Math.Cos(2.0 * Math.PI * x / pointsInPoly) + polyCenterX).ToArray();
-                    double[] ys = Enumerable.Range(0, pointsInPoly).Select(x => polyRadius * i / polysCount * Math.Sin(2.0 * Math.PI * x / pointsInPoly) + polyCenterY).ToArray();
+                    double polyX = rand.NextDouble() * 100;
+                    double polyY = rand.NextDouble() * 100;
+                    double polyR = rand.NextDouble();
+                    double[] xs = Enumerable.Range(0, pointsPerPolygon).Select(x => polyR * Math.Cos(2.0 * Math.PI * x / pointsPerPolygon) + polyX).ToArray();
+                    double[] ys = Enumerable.Range(0, pointsPerPolygon).Select(x => polyR * Math.Sin(2.0 * Math.PI * x / pointsPerPolygon) + polyY).ToArray();
                     polys.Add(xs.Zip(ys, (xp, yp) => (xp, yp)).ToList());
                 }
+
+                // then plot all the polygons with one command
                 plt.PlotPolygons(polys, fillColor: Color.Green);
+
+                // customize the plot
                 plt.EqualAxis = true;
-                plt.Title($"Lots of polygons");
+                plt.Title($"PlotPolygons with {polys.Count:N0} {pointsPerPolygon}-sided polygons");
             }
         }
 
