79e1f7f84a3b6df36e306181fa916f0f021bf894
Scott W Harden <swharden@gmail.com>
2020-05-31 19:54:02 -0700

    refine quickstarts

--- src/ScottPlot.Demo/PlotTypes/Radar.cs
+++ src/ScottPlot.Demo/PlotTypes/Radar.cs
@@ -9,15 +9,18 @@ namespace ScottPlot.Demo.PlotTypes
         public class RadarQuickstart : PlotDemo, IPlotDemo
         {
             public string name { get; } = "Quickstart";
-            public string description { get; } = "A radar chart (or spider or star chart) is good for showing multiple variables concisely.";
+            public string description { get; } = "A radar chart concisely displays multiple values. Radar plots are also called a spider charts or star charts.";
 
             public void Render(Plot plt)
             {
-                double[,] values = { { 78, 83, 84, 76, 43 }, { 100, 50, 70, 60, 90 } };
+                double[,] values = {
+                    { 78, 83, 84, 76, 43 },
+                    { 100, 50, 70, 60, 90 }
+                };
 
                 plt.PlotRadar(values);
 
-                //These aren't necessary, but they look nice
+                // customize the plot
                 plt.Grid(false);
                 plt.Frame(false);
                 plt.Ticks(false, false);
@@ -27,28 +30,28 @@ namespace ScottPlot.Demo.PlotTypes
         public class RadarWithLabels : PlotDemo, IPlotDemo
         {
             public string name { get; } = "Radar With Labels";
-            public string description { get; } = "A radar chart (or spider or star chart) is good for showing multiple variables concisely.";
+            public string description { get; } = "Labels can be displayed on the arms of the radar chart.";
 
             public void Render(Plot plt)
             {
-                //In case you're interested, this is about the 2010 F1 Season
-                //Note, Alonso did not finish the Malaysian GP, but was classified because he completed 90% race distance. This is counted here as a DNF
-
-                double[,] values = {
-                    { 5, 3, 10, 15, 3, 2},
-                    { 5, 2, 10, 10, 1, 4},
-                };
+                double[,] values = { { 5, 3, 10, 15, 3, 2 }, { 5, 2, 10, 10, 1, 4 }, };
                 string[] categories = { "Wins", "Poles", "Podiums", "Points Finishes", "DNFs", "Fastest Laps" };
                 string[] groups = { "Sebastian Vettel", "Fernando Alonso" };
 
                 plt.PlotRadar(values, categories, groups);
                 plt.Legend();
 
-                //These aren't necessary, but they look nice
+                // customize the plot
                 plt.Grid(false);
                 plt.Frame(false);
                 plt.Ticks(false, false);
-                plt.Title("2010 FIA F1 World Drivers Championship");
+                plt.Title("2010 Formula One World Championship");
+
+                /* Data represents the 2010 Formula One World Championship
+                 * https://en.wikipedia.org/wiki/2010_Formula_One_World_Championship
+                 * Note: Alonso did not finish (DNF) in the Malaysian GP, but was included 
+                 * here because he completed >90% of the race distance.
+                 */
             }
         }
     }
