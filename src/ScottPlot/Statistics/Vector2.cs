a5615018ce5888a87a80680f454b0eebc5a9e90e
Scott W Harden <swharden@gmail.com>
2020-06-09 00:13:58 -0400

    create Vector2 struct
    advantages: (1) no new NuGet package dependency, (2) Vector2 supports double which prevents lots of casting

---
+++ src/ScottPlot/Statistics/Vector2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ScottPlot.Statistics
+{
+    // A simpler double-precision floating point alternative to the System.Numerics.Vectors package
+    // https://github.com/microsoft/referencesource/blob/master/System.Numerics/System/Numerics/Vector2.cs
+
+    public struct Vector2
+    {
+        public double X;
+        public double Y;
+
+        public Vector2(double x, double y)
+        {
+            (X, Y) = (x, y);
+        }
+
+        public double LengthSquared()
+        {
+            return X * X + Y * Y;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(LengthSquared());
+        }
+
+        public static Vector2 Multiply(Vector2 left, double right)
+        {
+            return new Vector2(left.X * right, left.Y * right);
+        }
+    }
+}
