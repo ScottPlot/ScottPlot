d10a396d7761549c873ed25f6987f153392db080
Scott W Harden <swharden@gmail.com>
2020-06-09 16:32:17 -0400

    autoformat
    sort order of using statements has been added to .editorconfig

--- src/ScottPlot/plottables/PlottableSignal.cs
+++ src/ScottPlot/plottables/PlottableSignal.cs
@@ -1,11 +1,11 @@
-﻿using ScottPlot.Config;
-using ScottPlot.Drawing;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using ScottPlot.Config;
+using ScottPlot.Drawing;
 
 namespace ScottPlot
 {
