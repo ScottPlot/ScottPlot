550f8ec120cb4215af1ee8564c1914567f597d29
Scott W Harden <swharden@gmail.com>
2020-08-08 20:21:47 -0400

    Create deleteBinaries.bat
    useful for troubleshooting Visual Studio crashes

---
+++ src/deleteBinaries.bat
@@ -0,0 +1,18 @@
+rmdir /s /q .vs
+rmdir /s /q packages
+
+rmdir /s /q ScottPlot\bin
+rmdir /s /q ScottPlot\obj
+rmdir /s /q ScottPlot.WinForms\bin
+rmdir /s /q ScottPlot.WinForms\obj
+rmdir /s /q ScottPlot.WPF\bin
+rmdir /s /q ScottPlot.WPF\obj
+
+rmdir /s /q ScottPlot.Demo\bin
+rmdir /s /q ScottPlot.Demo\obj
+rmdir /s /q ScottPlot.Demo.WinForms\bin
+rmdir /s /q ScottPlot.Demo.WinForms\obj
+rmdir /s /q ScottPlot.Demo.WPF\bin
+rmdir /s /q ScottPlot.Demo.WPF\obj
+
+pause
