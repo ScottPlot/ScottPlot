c16103b58f8c6a265988923deefa34020739473e
Benjamin Lehmann <bclehmann@yahoo.ca>
2020-08-09 23:26:21 -0600

    Avalonia quickstart

---
+++ src/ScottPlot.Demo.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using ScottPlot.Avalonia;
+
+namespace ScottPlot.Demo.Avalonia
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var avaplot1 = this.Find<AvaPlot>("avaPlot1");
+            avaplot1.plt.Resize(400, 300);
+
+            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+            avaplot1.plt.PlotScatter(dataX, dataY);
+            avaplot1.Render();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ src/ScottPlot.Demo.Avalonia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Logging.Serilog;
+
+namespace ScottPlot.Demo.Avalonia
+{
+    class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug();
+    }
+}
