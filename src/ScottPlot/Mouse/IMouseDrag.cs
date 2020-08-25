8509a996746f67eb2a7e5c0d829ff85b557bedb3
Scott W Harden <swharden@gmail.com>
2020-08-24 21:42:26 -0400

    Create Mouse module
    classes describe mouse actions. They store mouse state and can easily be passed around.

---
+++ src/ScottPlot/Mouse/IMouseDrag.cs
@@ -0,0 +1,11 @@
+﻿namespace ScottPlot.Mouse
+{
+    public interface IMouseDrag
+    {
+        float X2 { get; set; }
+        float Y2 { get; set; }
+
+        float DeltaX { get; }
+        float DeltaY { get; }
+    }
+}
