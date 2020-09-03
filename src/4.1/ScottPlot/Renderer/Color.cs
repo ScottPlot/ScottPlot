7ec09d555e1b595558529a83bf3b2a7c5fc73cc7
Scott W Harden <swharden@gmail.com>
2020-09-02 21:27:51 -0400

    Colors.Convert() -> Color.Convert()

--- src/4.1/ScottPlot/Renderer/Color.cs
+++ src/4.1/ScottPlot/Renderer/Color.cs
@@ -7,14 +7,11 @@
         public byte G { get; set; } = 0;
         public byte B { get; set; } = 0;
 
-        public Color(byte r, byte g, byte b)
-        {
-            (A, R, G, B) = (255, r, g, b);
-        }
-
-        public Color(byte a, byte r, byte g, byte b)
-        {
-            (A, R, G, B) = (a, r, g, b);
-        }
+        public Color(byte r, byte g, byte b) => (A, R, G, B) = (255, r, g, b);
+        public Color(byte a, byte r, byte g, byte b) => (A, R, G, B) = (a, r, g, b);
+        
+        public static Color FromARGB(byte a, byte r, byte g, byte b) => new Color(a, r, g, b);
+        public static Color FromARGB(byte a, Color color) => new Color(a, color.R, color.G, color.B);
+        public static Color Convert(System.Drawing.Color color) => new Color(color.A, color.R, color.G, color.B);
     }
 }
