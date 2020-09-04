19c932b866a14e0a9dc304019a0b5e029af4657a
Scott W Harden <swharden@gmail.com>
2020-09-04 15:01:58 -0400

    winforms target net472
    previously core 3.1

--- src/4.1/ScottPlot.WinForms/Program.cs
+++ src/4.1/ScottPlot.WinForms/Program.cs
@@ -14,7 +14,7 @@ namespace ScottPlot.WinForms
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware); // requires .NET Core
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Menu());
