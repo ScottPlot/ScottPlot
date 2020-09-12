b469054bb8e906933d63294de55a9e00290de26a
Stend <StendGit@gmail.com>
2020-09-12 11:32:35 +0300

    Move BetterUnitSelect logic to DateTimeUnitFactorry.

--- src/ScottPlot/Config/DateTimeTickUnits/DateTimeUnitFactory.cs
+++ src/ScottPlot/Config/DateTimeTickUnits/DateTimeUnitFactory.cs
@@ -31,5 +31,36 @@ namespace ScottPlot.Config.DateTimeTickUnits
                     throw new NotImplementedException("unrecognized TickUnit");
             }
         }
+
+        public DateTimeTickUnitBase CreateBetterUnit(DateTime from, DateTime to, CultureInfo culture, int maxTickCount)
+        {
+            double daysApart = to.ToOADate() - from.ToOADate();
+            double hoursApart = daysApart * 24;
+            double minutesApart = hoursApart * 60;
+            double secondsApart = minutesApart * 60;
+            double decisecondsApart = secondsApart * 10;
+            double centisecondsApart = decisecondsApart * 10;
+            double millisecondsApart = centisecondsApart * 10;
+            DateTimeUnit units;
+            if (daysApart > 365 * 2)
+                units = DateTimeUnit.Year;
+            else if (daysApart > 30 * 2)
+                units = DateTimeUnit.Month;
+            else if (hoursApart > 24 * 2)
+                units = DateTimeUnit.Day;
+            else if (minutesApart > 60 * 2)
+                units = DateTimeUnit.Hour;
+            else if (secondsApart > 60 * 2)
+                units = DateTimeUnit.Minute;
+            else if (decisecondsApart > 10 * 2)
+                units = DateTimeUnit.Second;
+            else if (centisecondsApart > 10 * 2)
+                units = DateTimeUnit.Decisecond;
+            else if (millisecondsApart > 10 * 2)
+                units = DateTimeUnit.Centisecond;
+            else
+                units = DateTimeUnit.Millisecond;
+            return Create(units, culture, maxTickCount, null);
+        }
     }
 }
--- src/ScottPlot/Config/DateTimeTicks.cs
+++ src/ScottPlot/Config/DateTimeTicks.cs
@@ -19,42 +19,17 @@ namespace ScottPlot.Config
             if (!(dt1 < dt2))
                 dt2 = dt1.AddSeconds(1);
 
-            DateTimeUnit units;
+            var unitFactory = new DateTimeUnitFactory();
+            DateTimeTickUnitBase tickUnit;
             if (dtManualUnits.HasValue)
             {
-                units = dtManualUnits.Value;
+                tickUnit = unitFactory.Create(dtManualUnits.Value, culture, maxTickCount, dtManualSpacing);
             }
             else
             {
-                // determine the best DateTime unit to use for the given DateTime range
-                double daysApart = dt2.ToOADate() - dt1.ToOADate();
-                double hoursApart = daysApart * 24;
-                double minutesApart = hoursApart * 60;
-                double secondsApart = minutesApart * 60;
-                double decisecondsApart = secondsApart * 10;
-                double centisecondsApart = decisecondsApart * 10;
-                double millisecondsApart = centisecondsApart * 10;
-                if (daysApart > 365 * 2)
-                    units = DateTimeUnit.Year;
-                else if (daysApart > 30 * 2)
-                    units = DateTimeUnit.Month;
-                else if (hoursApart > 24 * 2)
-                    units = DateTimeUnit.Day;
-                else if (minutesApart > 60 * 2)
-                    units = DateTimeUnit.Hour;
-                else if (secondsApart > 60 * 2)
-                    units = DateTimeUnit.Minute;
-                else if (decisecondsApart > 10 * 2)
-                    units = DateTimeUnit.Second;
-                else if (centisecondsApart > 10 * 2)
-                    units = DateTimeUnit.Decisecond;
-                else if (millisecondsApart > 10 * 2)
-                    units = DateTimeUnit.Centisecond;
-                else
-                    units = DateTimeUnit.Millisecond;
+                tickUnit = unitFactory.CreateBetterUnit(dt1, dt2, culture, maxTickCount);
             }
-            var TickUnit = new DateTimeUnitFactory().Create(units, culture, maxTickCount, dtManualUnits == null ? null : (int?)dtManualSpacing);
-            return TickUnit.GetTicksAndLabels(dt1, dt2);
+            return tickUnit.GetTicksAndLabels(dt1, dt2);
         }
     }
 }
