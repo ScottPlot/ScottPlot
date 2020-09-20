44fbb645fe78a62168e4ce88093722921bff56f1
Scott W Harden <swharden@gmail.com>
2020-09-20 14:20:01 -0400

    create extreme axis test
    #549

---
+++ tests/Axis/ExtremeAxes.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScottPlotTests.Axis
+{
+    class ExtremeAxes
+    {
+        [Test]
+        public void Test_Axis_VeryBigNumbers()
+        {
+            var plt = new ScottPlot.Plot();
+
+            plt.PlotSignal(DataGen.Sin(51));
+            plt.PlotSignal(DataGen.Cos(51));
+
+            plt.Axis(y1: -10e50, y2: 10e50);
+            plt.TightenLayout();
+
+            TestTools.SaveFig(plt);
+        }
+    }
+}
