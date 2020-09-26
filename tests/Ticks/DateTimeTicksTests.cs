15967c12e0c147bddc5625eb58f460a5b6ab1ad8
Scott W Harden <swharden@gmail.com>
2020-09-26 15:41:55 -0400

    fix DateTime test on OSX
    OSX can only instantiate DateTimes between 04/30/1900 and 11/16/2077 #561, #564

--- tests/Ticks/DateTimeTicksTests.cs
+++ tests/Ticks/DateTimeTicksTests.cs
@@ -4,6 +4,7 @@ using System;
 using System.Drawing.Printing;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace ScottPlotTests.Ticks
 {
@@ -20,8 +21,11 @@ namespace ScottPlotTests.Ticks
 
             DateTimeUnitFactory factory = new DateTimeUnitFactory();
 
-            DateTime dateLower = new DateTime(1985, 9, 24);
-            DateTime dateUpper = dateLower + TimeSpan.FromDays(1000 * 365);
+            // OSX can only instantiate DateTimes between 04/30/1900 and 11/16/2077
+            DateTime dateLower = new DateTime(1901, 1, 1);
+            DateTime dateUpper = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ?
+                 new DateTime(2070, 1, 1) : new DateTime(3000, 1, 1);
+
             while ((dateUpper - dateLower).TotalMilliseconds > 1)
             {
                 TimeSpan span = dateUpper - dateLower;
