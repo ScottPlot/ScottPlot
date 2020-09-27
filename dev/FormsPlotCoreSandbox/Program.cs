d700ca64c7fac72832738c5aa8d50dc2b224ab11
Scott W Harden <swharden@gmail.com>
2020-09-27 12:00:57 -0400

    use HighDpiMode.SystemAware
    This is the default DPI mode for new .NET Core WinForms apps #563

--- dev/FormsPlotCoreSandbox/Program.cs
+++ dev/FormsPlotCoreSandbox/Program.cs
@@ -14,7 +14,7 @@ namespace FormsPlotCoreSandbox
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.DpiUnawareGdiScaled);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
