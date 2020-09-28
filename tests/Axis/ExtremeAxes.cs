01809457304170b31f40c71a2c275183fb0ef8a6
Scott W Harden <swharden@gmail.com>
2020-09-28 17:10:14 -0400

    add test for small axis tick labels
    #568

--- tests/Axis/ExtremeAxes.cs
+++ tests/Axis/ExtremeAxes.cs
@@ -21,5 +21,27 @@ namespace ScottPlotTests.Axis
 
             TestTools.SaveFig(plt);
         }
+
+        [Test]
+        public void Test_Axis_ModeratelySmallNumbers()
+        {
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.Title("Moderately Small (e-8)");
+            double[] xs = { 1e-8, 2e-8, 3e-8, 4e-8 };
+            double[] ys = { 1e-8, 4e-8, 3e-8, 6e-8 };
+            plt.PlotScatter(xs, ys);
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_Axis_ExtremelySmallNumbers()
+        {
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.Title("Extremely Small (e-20)");
+            double[] xs = { 1e-20, 2e-20, 3e-20, 4e-20 };
+            double[] ys = { 1e-20, 4e-20, 3e-20, 6e-20 };
+            plt.PlotScatter(xs, ys);
+            TestTools.SaveFig(plt);
+        }
     }
 }
