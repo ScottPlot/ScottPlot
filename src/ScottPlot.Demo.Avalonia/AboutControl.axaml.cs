c30112f63fcab091c5ec7644e32a3ca51c279b6d
Benjamin Lehmann <bclehmann@yahoo.ca>
2020-10-02 17:05:54 -0600

    Avalonia cookbook (Avalonia specific demos are dead)

---
+++ src/ScottPlot.Demo.Avalonia/AboutControl.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ScottPlot.Demo.Avalonia
+{
+    public class AboutControl : UserControl
+    {
+        public AboutControl()
+        {
+            this.InitializeComponent();
+            this.Find<TextBlock>("VersionLabel").Text = $"version {Tools.GetVersionString()}";
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
--- src/ScottPlot.Demo.Avalonia/MainWindow.axaml.cs
+++ src/ScottPlot.Demo.Avalonia/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using ScottPlot.Avalonia;
 
@@ -10,18 +12,95 @@ namespace ScottPlot.Demo.Avalonia
         public MainWindow()
         {
             InitializeComponent();
+            this.Find<TextBlock>("VersionLabel").Text = Tools.GetVersionString();
 
-            var avaplot1 = this.Find<AvaPlot>("avaPlot1");
-
-            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
-            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
-            avaplot1.plt.PlotScatter(dataX, dataY);
-            avaplot1.Render();
+            this.Find<Button>("WebsiteLabel").Click += WebsiteLabelClick;
+            this.Find<Button>("LaunchCookbookButton").Click += LaunchCookbook;
         }
 
-        private void InitializeComponent()
+        public void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public void LaunchCookbook(object sender, RoutedEventArgs e)
+        {
+            new CookbookWindow().ShowDialog(this);
+        }
+
+        public void LaunchMouseTracker(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.MouseTracker().ShowDialog();
+        }
+
+        public void LaunchToggleVisibility(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.ToggleVisibility().ShowDialog();
+        }
+
+        public void LaunchWpfConfig(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.WpfConfig().ShowDialog();
+        }
+
+        public void LaunchLinkedAxes(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.LinkedPlots().ShowDialog();
+        }
+
+        public void LaunchLiveDataFixed(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.LiveDataFixed().ShowDialog();
+        }
+
+        public void LaunchLiveDataIncoming(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.LiveDataGrowing().ShowDialog();
+        }
+
+        public void LaunchShowValueUnderMouse(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.ShowValueOnHover().ShowDialog();
+        }
+
+        public void LaunchTransparentBackground(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.TransparentBackground().ShowDialog();
+        }
+
+        public void WebsiteLabelClick(object sender, RoutedEventArgs e)
+        {
+            Tools.LaunchBrowser();
+        }
+
+        public void LaunchPlotViewer(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.PlotViewer().ShowDialog();
+        }
+
+        public void LaunchManyPlot(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.ManyPlots().ShowDialog();
+        }
+
+        public void LaunchCustomRightClick(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.RightClickMenu().ShowDialog();
+        }
+
+        public void LaunchPlotInAScrollViewer(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.PlotInScrollViewer().ShowDialog();
+        }
+
+        public void LaunchAxisLimits(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.AxisLimits().ShowDialog();
+        }
+
+        public void LaunchLayout(object sender, RoutedEventArgs e)
+        {
+            //new WpfDemos.Layout().ShowDialog();
         }
     }
 }
