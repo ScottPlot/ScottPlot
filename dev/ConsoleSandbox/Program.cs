2b3026c4ad6f1577bb7e0c0939697fde5e7c337f
Scott W Harden <swharden@gmail.com>
2020-10-26 20:20:27 -0400

    create ConsoleSandbox
    #607

---
+++ dev/ConsoleSandbox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Plotting...");
+
+        var plt = new ScottPlot.Plot(600, 400);
+        plt.PlotLine(0, 1, 1, 1.0000000000000009);
+        plt.GetBitmap();
+
+        Console.WriteLine("Done!");
+    }
+}
