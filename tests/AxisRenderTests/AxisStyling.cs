bd6be0217b280a5190337b22a25e8a6ca592babc
Scott W Harden <swharden@gmail.com>
2020-11-04 19:38:26 -0500

    improve Axis Tick test module

--- tests/AxisRenderTests/AxisStyling.cs
+++ tests/AxisRenderTests/AxisStyling.cs
@@ -14,23 +14,23 @@ namespace ScottPlotTests.AxisRenderTests
         public void Test_Axis_Left()
         {
             var dims = new PlotDimensions(
-                figureSize: new SizeF(200, 500),
-                dataSize: new SizeF(25, 400),
-                dataOffset: new PointF(150, 50),
+                figureSize: new SizeF(100, 500),
+                dataSize: new SizeF(20, 400),
+                dataOffset: new PointF(75, 50),
                 axisLimits: new AxisLimits2D(-1, 1, -100, 100));
-
-            var ticks = new TickCollection(verticalAxis: true);
-            ticks.Recalculate(dims);
 
             var axis = new ScottPlot.Renderable.Axis();
             axis.Title.Label = "Axis Label";
             axis.Edge = ScottPlot.Renderable.Edge.Left;
-            axis.SetTicks(ticks.tickPositionsMajor, ticks.tickLabels, ticks.tickPositionsMinor);
+            axis.TickCollection.Recalculate(dims);
+            axis.TickCollection.verticalAxis = true;
 
             using (var bmp = new System.Drawing.Bitmap((int)dims.Width, (int)dims.Height))
             using (var gfx = GDI.Graphics(bmp, lowQuality: true))
+            using (var brush = GDI.Brush(Color.FromArgb(25, Color.Black)))
             {
                 gfx.Clear(Color.White);
+                gfx.FillRectangle(brush, dims.DataOffsetX, dims.DataOffsetY, dims.DataWidth, dims.DataHeight);
                 axis.Render(dims, bmp);
                 TestTools.SaveFig(bmp);
             }
