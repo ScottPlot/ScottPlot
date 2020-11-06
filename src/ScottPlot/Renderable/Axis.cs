e8ec82ecc4c5dadbd6fe9287d95658186eee11b2
Scott W Harden <swharden@gmail.com>
2020-11-06 17:21:18 -0500

    improve layout for tick labels

--- src/ScottPlot/Renderable/Axis.cs
+++ src/ScottPlot/Renderable/Axis.cs
@@ -126,8 +126,8 @@ namespace ScottPlot.Renderable
             {
                 float titleHeight = Title.IsVisible ? GDI.MeasureString(Title.Label, Title.Font).Height : 0;
                 PixelSize = IsHorizontal ?
-                    Ticks.TickCollection.maxLabelSize.Height + titleHeight :
-                    Ticks.TickCollection.maxLabelSize.Width + titleHeight + 5;
+                    Ticks.TickCollection.maxLabelSize.Height + titleHeight + Ticks.MajorTickLength:
+                    Ticks.TickCollection.maxLabelSize.Width + titleHeight + Ticks.MajorTickLength;
                 PixelSize = Math.Max(PixelSize, PixelSizeMinimum);
             }
         }
