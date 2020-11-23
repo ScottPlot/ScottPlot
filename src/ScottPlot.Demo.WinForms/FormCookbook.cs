9fef05d6586b9b3f5c18cd455becc33c86b11591
Scott W Harden <swharden@gmail.com>
2020-11-23 18:15:29 -0500

    allow WinForms demo to use local source #625

--- src/ScottPlot.Demo.WinForms/FormCookbook.cs
+++ src/ScottPlot.Demo.WinForms/FormCookbook.cs
@@ -64,7 +64,9 @@ namespace ScottPlot.Demo.WinForms
             DemoNameLabel.Text = demoPlot.name;
             sourceCodeGroupbox.Text = demoPlot.classPath.Replace("+", ".");
             DescriptionTextbox.Text = (demoPlot.description is null) ? "no descriton provided..." : demoPlot.description;
-            sourceCodeTextbox.Text = demoPlot.GetSourceCode("../../../../src/ScottPlot.Demo/");
+
+            string sourceFolderPath = System.IO.Directory.Exists("./source") ? "source/" : "../../../../src/ScottPlot.Demo/";
+            sourceCodeTextbox.Text = demoPlot.GetSourceCode(sourceFolderPath);
 
             formsPlot1.Reset();
 
