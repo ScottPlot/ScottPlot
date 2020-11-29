54ea1f30116a9bb7769ed9e0a989622708b9dd9c
Scott W Harden <swharden@gmail.com>
2020-11-29 13:13:12 -0500

    refine multi-axis cookbook example
    #578 #629

--- src/cookbook/ScottPlot.Demo/Advanced/MultiAxis.cs
+++ src/cookbook/ScottPlot.Demo/Advanced/MultiAxis.cs
@@ -17,21 +17,27 @@ namespace ScottPlot.Demo.Advanced
 
             public void Render(Plot plt)
             {
-                // generate random data to plot
+                // generate sample data to plot
                 Random rand = new Random(0);
-                int pointCount = 10;
+                int pointCount = 100;
                 double[] xs = DataGen.Consecutive(pointCount);
-                double[] rainfall = DataGen.Random(rand, pointCount, 1, 10); // small values
-                double[] organisms = DataGen.Random(rand, pointCount, 1000, 20000); // large values
+                double[] rainfall = DataGen.RandomWalk(rand, pointCount, 1, 10); // small values
+                double[] organisms = DataGen.RandomWalk(rand, pointCount, 1000, 20000); // large values
 
-                // add data to the plot the traditional way and customize the primary Y axis label
-                plt.PlotScatter(xs, rainfall);
-                plt.YLabel("Rainfall (cm)");
+                // customize the primary X axis
+                plt.XAxis.Configure(label: "Days After Volcano Eruption");
 
-                // create a new plottable, customize its vertical axis index, then add a vertical axis with the same index
+                // add a scatter plot and customize the primary Y axis
+                var scatter1 = plt.PlotScatter(xs, rainfall);
+                plt.YAxis.Configure(label: "Daily Rainfall (cm)", color: scatter1.color);
+
+                // add another scatter plot but give it a custom vertical axis index
                 var scatter2 = plt.PlotScatter(xs, organisms);
                 scatter2.VerticalAxisIndex = 3;
-                plt.AddAxis(Renderable.Edge.Left, axisIndex: 3, "Organisms", color: scatter2.color);
+
+                // add a new vertical axis using same axis index as the second scatter plot
+                var secondLeftAxis = plt.AddAxis(Renderable.Edge.Left, axisIndex: 3);
+                secondLeftAxis.Configure(label: "Number of Organisms", color: scatter2.color);
             }
         }
     }
