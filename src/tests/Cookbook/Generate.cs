7eaae83b123a894fb00620c5ba192fc3fa520ffd
Scott W Harden <swharden@gmail.com>
2020-12-02 18:22:44 -0500

    tests for new cookbook #639

---
+++ src/tests/Cookbook/Generate.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlotTests.Cookbook
+{
+    class Generate
+    {
+        const string SourceFolder = "../../../../cookbook/ScottPlot.Cookbook";
+        const string OutputFolder = "./recipes";
+
+        [OneTimeSetUp]
+        public void EmptyOutputFolder()
+        {
+            if (!System.IO.Directory.Exists(OutputFolder))
+                System.IO.Directory.CreateDirectory(OutputFolder);
+
+            foreach (var fname in System.IO.Directory.GetFiles(OutputFolder))
+                System.IO.File.Delete(fname);
+        }
+
+        [Test]
+        public void Test_Cookbook_GenerateImages()
+        {
+            var chef = new ScottPlot.Cookbook.Chef();
+            chef.CreateCookbookImages(OutputFolder);
+        }
+
+        [Test]
+        public void Test_Cookbook_GenerateSource()
+        {
+            var chef = new ScottPlot.Cookbook.Chef();
+            chef.CreateCookbookSource(SourceFolder, OutputFolder);
+        }
+    }
+}
---
+++ src/tests/Cookbook/Validate.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlotTests.Cookbook
+{
+    class Validate
+    {
+        const string SourceFolder = "../../../../cookbook/ScottPlot.Cookbook";
+
+        [Test]
+        public void Test_CookbookRecipes_Exist()
+        {
+            var recipes = ScottPlot.Cookbook.Reflection.GetRecipes();
+            Assert.IsNotEmpty(recipes);
+
+            foreach (var recipe in recipes)
+                Console.WriteLine(recipe);
+        }
+
+        [Test]
+        public void Test_CookbookRecipes_AllHaveSourceCode()
+        {
+            var recipes = ScottPlot.Cookbook.Reflection.GetRecipes();
+            var chef = new ScottPlot.Cookbook.Chef();
+            var sources = chef.GetRecipeSources(SourceFolder);
+
+            foreach (ScottPlot.Cookbook.IRecipe recipe in recipes)
+                Assert.AreEqual(1, sources.Where(x => x.id == recipe.ID).Count());
+        }
+
+        [Test]
+        public void Test_CookbookRecipes_DescriptorsAreValid()
+        {
+            string allowedSpecialCharacters = " _'()[];.,";
+            foreach (var recipe in ScottPlot.Cookbook.Reflection.GetRecipes())
+            {
+                foreach (var c in recipe.Title + recipe.Description)
+                {
+                    if (char.IsLetterOrDigit(c) || allowedSpecialCharacters.Contains(c))
+                        continue;
+                    else
+                        Assert.Fail($"{recipe.ID} Title or Description contains a special character: {c}");
+                }
+            }
+        }
+
+        [Test]
+        public void Test_CookbookRecipes_IDsAreValid()
+        {
+            string allowedSpecialCharacters = "_";
+            foreach (var recipe in ScottPlot.Cookbook.Reflection.GetRecipes())
+            {
+                foreach (var c in recipe.ID.ToCharArray())
+                {
+                    if (char.IsLetterOrDigit(c) || allowedSpecialCharacters.Contains(c))
+                        continue;
+                    else
+                        Assert.Fail("IDs must not contain special characters (except underscore)");
+                }
+            }
+        }
+
+        [Test]
+        public void Test_CookbookRecipes_IDsAreUnique()
+        {
+            var recipes = ScottPlot.Cookbook.Reflection.GetRecipes();
+            string[] ids = recipes.Select(x => x.ID.ToLower()).ToArray();
+
+            if (ids.Distinct().Count() < ids.Length)
+                Assert.Fail("all lowercase recipe IDs must be unique");
+        }
+    }
+}
