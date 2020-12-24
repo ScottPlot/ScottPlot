f752cd2759c44ae342300620f5b73f1319f670a5
Scott W Harden <swharden@gmail.com>
2020-12-24 15:44:17 -0500

    waveform heatmap recipe

--- src/cookbook/ScottPlot.Cookbook/Recipes/Plottable/Heatmap.cs
+++ src/cookbook/ScottPlot.Cookbook/Recipes/Plottable/Heatmap.cs
@@ -10,8 +10,7 @@ namespace ScottPlot.Cookbook.Recipes.Plottable
         public string ID => "heatmap_quickstart";
         public string Title => "Heatmap Quickstart";
         public string Description =>
-            "Annotations are labels placed at a X/Y location on the figure (not coordinates of the data area). " +
-            "Unlike the Text plottable, annotations do not move as the axes are adjusted.";
+            "Heatmaps display a 2D array using a colormap.";
 
         public void ExecuteRecipe(Plot plt)
         {
@@ -19,8 +18,29 @@ namespace ScottPlot.Cookbook.Recipes.Plottable
                                     { 4, 5, 6 } };
 
             plt.AddHeatmap(imageData);
+        }
+    }
 
-            plt.YAxis2.SetSizeLimit(min: 100); // add space the right for the scalebar
+    public class Heatmap2dWaveform : IRecipe
+    {
+        public string Category => "Plottable: Heatmap";
+        public string ID => "heatmap_2dWaveform";
+        public string Title => "2D Waveform";
+        public string Description =>
+            "This example demonstrates a heatmap with 1000 tiles";
+
+        public void ExecuteRecipe(Plot plt)
+        {
+            int width = 100;
+            int height = 100;
+
+            double[,] intensities = new double[width, height];
+
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    intensities[x, y] = (Math.Sin(x * .2) + Math.Cos(y * .2)) * 100;
+
+            plt.AddHeatmap(intensities);
         }
     }
 }
