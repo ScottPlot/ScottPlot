0370963de6c224593e27cc704d5d83857d82ff0c
Scott W Harden <swharden@gmail.com>
2021-01-24 10:12:11 -0500

    invisible axis cookbook example

--- src/cookbook/ScottPlot.Cookbook/Recipes/MultiAxis.cs
+++ src/cookbook/ScottPlot.Cookbook/Recipes/MultiAxis.cs
@@ -84,4 +84,34 @@ namespace ScottPlot.Cookbook.Recipes
             plt.XAxis.Label("Sample Number");
         }
     }
+
+    class MultiAxisInvisible : IRecipe
+    {
+        public string Category => "Multi-Axis";
+        public string ID => "multiAxis_invisible";
+        public string Title => "Axis Visibility";
+        public string Description =>
+            "Visibility of axes can be toggled. " +
+            "In this example an additional Y axis is added but the primary Y axis is hidden. " +
+            "The result is a plot that appears to only have one Y axis.";
+
+        public void ExecuteRecipe(Plot plt)
+        {
+            // plot one set of data using the primary Y axis
+            var sigSmall = plt.AddSignal(DataGen.Sin(51, mult: 1));
+            sigSmall.YAxisIndex = 0;
+            plt.YAxis.Label("Primary Axis");
+            plt.YAxis.Color(sigSmall.Color);
+
+            // plot another set of data using an additional axis
+            var sigBig = plt.AddSignal(DataGen.Cos(51, mult: 100));
+            var yAxis3 = plt.AddAxis(Renderable.Edge.Left, axisIndex: 2);
+            sigBig.YAxisIndex = 2;
+            yAxis3.Label("Additional Axis");
+            yAxis3.Color(sigBig.Color);
+
+            // hide the primary Y axis
+            plt.YAxis.IsVisible = false;
+        }
+    }
 }
