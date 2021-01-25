0080c015774823d624f8d540a2165d883c50c08b
Scott W Harden <swharden@gmail.com>
2021-01-24 20:11:25 -0500

    remove outdated methods from Font module

--- src/ScottPlot/Drawing/Font.cs
+++ src/ScottPlot/Drawing/Font.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace ScottPlot.Drawing
 {
@@ -18,11 +19,5 @@ namespace ScottPlot.Drawing
         }
 
         public Font() => Name = InstalledFont.Sans();
-        //public Font(float size) => (Name, Size) = (InstalledFont.Sans(), size);
-        //public Font(float size, bool bold) => (Name, Size, Bold) = (InstalledFont.Sans(), size, bold);
-        //public Font(float size, Color color) => (Name, Size, Color) = (InstalledFont.Sans(), size, color);
-
-        // TODO: obsolete?
-        public Font(string name, float size, Color color, Alignment align = Alignment.UpperLeft) => (Name, Size, Color, Alignment) = (name, size, color, align);
     }
 }
