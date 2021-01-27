69ea85626773e8cb3a3217881b4a24f1cc2ff95c
Scott W Harden <swharden@gmail.com>
2021-01-26 19:34:07 -0500

    WpfPlot.plt -> WpfPlot.Plot

--- src/controls/ScottPlot.WPF/WpfPlot.xaml.cs
+++ src/controls/ScottPlot.WPF/WpfPlot.xaml.cs
@@ -99,7 +99,7 @@ namespace ScottPlot
                 MainGrid.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#003366"));
                 var sp = new StackPanel() { Orientation = Orientation.Horizontal };
                 sp.Children.Add(new Label() { Content = "ScottPlot", Foreground = Brushes.White });
-                sp.Children.Add(new Label() { Content = Plot.Version, Foreground = Brushes.White });
+                sp.Children.Add(new Label() { Content = ScottPlot.Plot.Version, Foreground = Brushes.White });
                 MainGrid.Children.Add(sp);
             }
             else
