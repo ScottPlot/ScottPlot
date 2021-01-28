c798ad5a982a698bb16a3172119159cbdfd3a792
Scott W Harden <swharden@gmail.com>
2021-01-27 19:39:01 -0500

    revert to original WinForms sandbox app

--- src/sandbox/WinFormsFrameworkApp/Form1.Designer.cs
+++ src/sandbox/WinFormsFrameworkApp/Form1.Designer.cs
@@ -29,198 +29,34 @@ namespace WinFormsFrameworkApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.formsPlot1 = new ScottPlot.FormsPlot();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.tbCursorCoord = new System.Windows.Forms.TextBox();
-            this.tbCursorPixel = new System.Windows.Forms.TextBox();
-            this.tbPointPixel = new System.Windows.Forms.TextBox();
-            this.tbPointCoord = new System.Windows.Forms.TextBox();
-            this.btnSimilar = new System.Windows.Forms.Button();
-            this.btnDifferent = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // formsPlot1
             // 
-            this.formsPlot1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.formsPlot1.BackColor = System.Drawing.Color.Transparent;
-            this.formsPlot1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.formsPlot1.Location = new System.Drawing.Point(12, 123);
+            this.formsPlot1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formsPlot1.Location = new System.Drawing.Point(0, 0);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(577, 358);
+            this.formsPlot1.Size = new System.Drawing.Size(800, 450);
             this.formsPlot1.TabIndex = 0;
             this.formsPlot1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormsPlot1_MouseMove);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(22, 45);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Cursor position:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(33, 81);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Nearest point:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(147, 15);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(87, 20);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Coordinate";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(249, 15);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 20);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Pixel";
-            // 
-            // tbCursorCoord
-            // 
-            this.tbCursorCoord.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCursorCoord.ForeColor = System.Drawing.Color.Blue;
-            this.tbCursorCoord.Location = new System.Drawing.Point(147, 46);
-            this.tbCursorCoord.Name = "tbCursorCoord";
-            this.tbCursorCoord.Size = new System.Drawing.Size(100, 23);
-            this.tbCursorCoord.TabIndex = 9;
-            this.tbCursorCoord.Text = "12, 123";
-            // 
-            // tbCursorPixel
-            // 
-            this.tbCursorPixel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCursorPixel.ForeColor = System.Drawing.Color.Blue;
-            this.tbCursorPixel.Location = new System.Drawing.Point(253, 46);
-            this.tbCursorPixel.Name = "tbCursorPixel";
-            this.tbCursorPixel.Size = new System.Drawing.Size(100, 23);
-            this.tbCursorPixel.TabIndex = 10;
-            this.tbCursorPixel.Text = "12, 123";
-            // 
-            // tbPointPixel
-            // 
-            this.tbPointPixel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbPointPixel.ForeColor = System.Drawing.Color.Red;
-            this.tbPointPixel.Location = new System.Drawing.Point(253, 82);
-            this.tbPointPixel.Name = "tbPointPixel";
-            this.tbPointPixel.Size = new System.Drawing.Size(100, 23);
-            this.tbPointPixel.TabIndex = 12;
-            this.tbPointPixel.Text = "12, 123";
-            // 
-            // tbPointCoord
-            // 
-            this.tbPointCoord.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbPointCoord.ForeColor = System.Drawing.Color.Red;
-            this.tbPointCoord.Location = new System.Drawing.Point(147, 82);
-            this.tbPointCoord.Name = "tbPointCoord";
-            this.tbPointCoord.Size = new System.Drawing.Size(100, 23);
-            this.tbPointCoord.TabIndex = 11;
-            this.tbPointCoord.Text = "12, 123";
-            // 
-            // btnSimilar
-            // 
-            this.btnSimilar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSimilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSimilar.Location = new System.Drawing.Point(432, 40);
-            this.btnSimilar.Name = "btnSimilar";
-            this.btnSimilar.Size = new System.Drawing.Size(157, 34);
-            this.btnSimilar.TabIndex = 15;
-            this.btnSimilar.Text = "similar scales";
-            this.btnSimilar.UseVisualStyleBackColor = true;
-            this.btnSimilar.Click += new System.EventHandler(this.btnSimilar_Click);
-            // 
-            // btnDifferent
-            // 
-            this.btnDifferent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnDifferent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDifferent.Location = new System.Drawing.Point(432, 76);
-            this.btnDifferent.Name = "btnDifferent";
-            this.btnDifferent.Size = new System.Drawing.Size(157, 34);
-            this.btnDifferent.TabIndex = 16;
-            this.btnDifferent.Text = "different scales";
-            this.btnDifferent.UseVisualStyleBackColor = true;
-            this.btnDifferent.Click += new System.EventHandler(this.btnDifferent_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(376, 215);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 20);
-            this.label4.TabIndex = 17;
-            // 
-            // label7
-            // 
-            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(428, 15);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(141, 20);
-            this.label7.TabIndex = 18;
-            this.label7.Text = "Plot new data with:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(601, 493);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.btnDifferent);
-            this.Controls.Add(this.btnSimilar);
-            this.Controls.Add(this.tbPointPixel);
-            this.Controls.Add(this.tbPointCoord);
-            this.Controls.Add(this.tbCursorPixel);
-            this.Controls.Add(this.tbCursorCoord);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.formsPlot1);
             this.Name = "Form1";
             this.Text = "ScottPlot Sandbox - WinForms (.NET Framework)";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private ScottPlot.FormsPlot formsPlot1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox tbCursorCoord;
-        private System.Windows.Forms.TextBox tbCursorPixel;
-        private System.Windows.Forms.TextBox tbPointPixel;
-        private System.Windows.Forms.TextBox tbPointCoord;
-        private System.Windows.Forms.Button btnSimilar;
-        private System.Windows.Forms.Button btnDifferent;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label7;
     }
 }
 
--- src/sandbox/WinFormsFrameworkApp/Form1.cs
+++ src/sandbox/WinFormsFrameworkApp/Form1.cs
@@ -1,89 +1,51 @@
 ﻿using ScottPlot.Plottable;
 using System;
 using System.Drawing;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace WinFormsFrameworkApp
 {
     public partial class Form1 : Form
     {
-        private readonly ScatterPlot MyScatterPlot;
-        private readonly ScatterPlot HighlightedPoint;
+        private readonly HLine HLine;
+        private readonly VLine VLine;
+        private readonly SignalPlot Signal;
         private int LastHighlightedIndex = -1;
 
         public Form1()
         {
             InitializeComponent();
 
-            // create some random data to display
+            // simulate 10 seconds of 48 kHz audio
+            int sampleRate = 48_000;
             Random rand = new Random(0);
-            int pointCount = 20;
-            double[] xs = ScottPlot.DataGen.Random(rand, pointCount);
-            double[] ys = ScottPlot.DataGen.Random(rand, pointCount);
+            double[] data = ScottPlot.DataGen.RandomWalk(rand, sampleRate * 10);
+            Signal = formsPlot1.Plot.AddSignal(data, sampleRate);
 
-            // Add a scatter plot and a point (keeps the objects that are created)
-            MyScatterPlot = formsPlot1.Plot.AddScatterPoints(xs, ys);
-            HighlightedPoint = formsPlot1.Plot.AddPoint(0, 0);
-            HighlightedPoint.Color = Color.Red;
-            HighlightedPoint.MarkerSize = 10;
-            HighlightedPoint.MarkerShape = ScottPlot.MarkerShape.openCircle;
-            HighlightedPoint.IsVisible = false;
+            // markers to indicate where the mouse is
+            HLine = formsPlot1.Plot.AddHorizontalLine(0, Color.Red, 1, ScottPlot.LineStyle.Dash);
+            VLine = formsPlot1.Plot.AddVerticalLine(0, Color.Red, 1, ScottPlot.LineStyle.Dash);
+
+            formsPlot1.Render();
         }
 
         private void FormsPlot1_MouseMove(object sender, MouseEventArgs e)
         {
-            // determine where the cursor is
-            double mousePixelX = e.X;
-            double mousePixelY = e.Y;
-            (double mouseCoordX, double mouseCoordY) = formsPlot1.GetMouseCoordinates();
+            if (e.Button != MouseButtons.None)
+                return; // don't move markers if actively panning or zooming
 
-            // determine point nearest the cursor
-            double xyRatio = formsPlot1.Plot.XAxis.Dims.PxPerUnit / formsPlot1.Plot.YAxis.Dims.PxPerUnit;
-            (double pointCoordX, double pointCoordY, int pointIndex) = MyScatterPlot.GetPointNearest(mouseCoordX, mouseCoordY, xyRatio);
-            (double pointPixelX, double pointPixelY) = formsPlot1.Plot.GetPixel(pointCoordX, pointCoordY);
-
-            // highlight the point of interest
-            HighlightedPoint.Xs[0] = pointCoordX;
-            HighlightedPoint.Ys[0] = pointCoordY;
-            HighlightedPoint.IsVisible = true;
-
-            // display details in GUI
-            tbCursorCoord.Text = $"{mouseCoordX:N1}, {mouseCoordY:N1}";
-            tbCursorPixel.Text = $"{mousePixelX:N1}, {mousePixelY:N1}";
-            tbPointCoord.Text = $"{pointCoordX:N1}, {pointCoordY:N1}";
-            tbPointPixel.Text = $"{pointPixelX:N1}, {pointPixelY:N1}";
+            double mouseX = formsPlot1.Plot.GetCoordinateX(e.X);
+            (double x, double y, int pointIndex) = Signal.GetPointNearestX(mouseX);
+            VLine.X = x;
+            HLine.Y = y;
+            Text = $"Mouse is over point {pointIndex:N0} ({x:.03}, {y:.03})";
 
             // render if the highlighted point chnaged
             if (LastHighlightedIndex != pointIndex)
             {
                 LastHighlightedIndex = pointIndex;
-                formsPlot1.Render();
+                formsPlot1.Render(skipIfCurrentlyRendering: true);
             }
-        }
-
-        private void btnSimilar_Click(object sender, EventArgs e)
-        {
-            Random rand = new Random();
-            int pointCount = 20;
-            double[] newXs = ScottPlot.DataGen.Random(rand, pointCount);
-            double[] newYs = ScottPlot.DataGen.Random(rand, pointCount);
-            MyScatterPlot.Update(newXs, newYs);
-            HighlightedPoint.IsVisible = false;
-            formsPlot1.Plot.AxisAuto();
-            formsPlot1.Render();
-        }
-
-        private void btnDifferent_Click(object sender, EventArgs e)
-        {
-            Random rand = new Random();
-            int pointCount = 20;
-            double[] newXs = ScottPlot.DataGen.Random(rand, pointCount);
-            double[] newYs = ScottPlot.DataGen.Random(rand, pointCount, multiplier: 1000);
-            MyScatterPlot.Update(newXs, newYs);
-            HighlightedPoint.IsVisible = false;
-            formsPlot1.Plot.AxisAuto();
-            formsPlot1.Render();
         }
     }
 }
