0033adfedf534904f52671a557fd1ae9c1bafc86
Scott W Harden <swharden@gmail.com>
2021-01-27 19:57:26 -0500

    obsolete ScatterPlotHighlight

--- src/ScottPlot/Plottable/ScatterPlotHighlight.cs
+++ src/ScottPlot/Plottable/ScatterPlotHighlight.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using ScottPlot.Drawing;
 
 namespace ScottPlot.Plottable
 {
-    [Obsolete("SCATTER PLOTS HAVE MOST OF THESE FUNCTIONS - THIS CLASS WILL BE DELETED SHORTLY")]
+    [Obsolete("This plot type is deprecated: Use a regular scatter plot and call GetPointNearest(). See examples in documentation.", true)]
     public class ScatterPlotHighlight : ScatterPlot, IPlottable, IExportable, IHasPoints, IHasHighlightablePoints
     {
         public MarkerShape highlightedShape = MarkerShape.openCircle;
@@ -17,75 +14,11 @@ namespace ScottPlot.Plottable
         public ScatterPlotHighlight(double[] xs, double[] ys, double[] xErr = null, double[] yErr = null) :
                                     base(xs, ys, xErr, yErr) => HighlightClear();
 
-        public new void Render(PlotDimensions dims, Bitmap bmp, bool lowQuality = false)
-        {
-            base.Render(dims, bmp, lowQuality);
-
-            if (isHighlighted is null || isHighlighted.Length == 0)
-                return;
-
-            using (var gfx = GDI.Graphics(bmp, dims, lowQuality))
-            {
-                var highlightedIndexes = Enumerable.Range(0, isHighlighted.Length).Where(x => isHighlighted[x]);
-                foreach (int i in highlightedIndexes)
-                {
-                    PointF pt = new PointF(dims.GetPixelX(Xs[i]), dims.GetPixelY(Ys[i]));
-                    MarkerTools.DrawMarker(gfx, pt, highlightedShape, highlightedMarkerSize, highlightedColor);
-                }
-            }
-        }
-
-        public void HighlightClear()
-        {
-            isHighlighted = new bool[Xs.Length];
-        }
-
-        public (double x, double y, int index) HighlightPoint(int index)
-        {
-            // if the size of xs changed, reset isHighlighted to match its new size
-            if (isHighlighted.Length != Xs.Length)
-                HighlightClear();
-
-            if (index < 0 || index >= isHighlighted.Length)
-                throw new ArgumentException("Invalid index");
-            isHighlighted[index] = true;
-            return (Xs[index], Ys[index], index);
-        }
-
-        public (double x, double y, int index) HighlightPointNearestX(double x)
-        {
-            var point = GetPointNearestX(x);
-
-            // if the size of xs changed, reset isHighlighted to match its new size
-            if (isHighlighted.Length != Xs.Length)
-                HighlightClear();
-
-            isHighlighted[point.index] = true;
-            return point;
-        }
-
-        public (double x, double y, int index) HighlightPointNearestY(double y)
-        {
-            var point = GetPointNearestY(y);
-
-            // if the size of xs changed, reset isHighlighted to match its new size
-            if (isHighlighted.Length != Xs.Length)
-                HighlightClear();
-
-            isHighlighted[point.index] = true;
-            return point;
-        }
-
-        public (double x, double y, int index) HighlightPointNearest(double x, double y)
-        {
-            var point = GetPointNearest(x, y);
-
-            // if the size of xs changed, reset isHighlighted to match its new size
-            if (isHighlighted.Length != Xs.Length)
-                HighlightClear();
-
-            isHighlighted[point.index] = true;
-            return point;
-        }
+        public new void Render(PlotDimensions dims, Bitmap bmp, bool lowQuality = false) => throw new NotImplementedException();
+        public void HighlightClear() => throw new NotImplementedException();
+        public (double x, double y, int index) HighlightPoint(int index) => throw new NotImplementedException();
+        public (double x, double y, int index) HighlightPointNearestX(double x) => throw new NotImplementedException();
+        public (double x, double y, int index) HighlightPointNearestY(double y) => throw new NotImplementedException();
+        public (double x, double y, int index) HighlightPointNearest(double x, double y) => throw new NotImplementedException();
     }
 }
