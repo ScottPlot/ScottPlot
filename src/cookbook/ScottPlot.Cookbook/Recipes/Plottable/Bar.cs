37c2169e227ecebee8ab669ac5d627e06a5bcacf
Scott W Harden <swharden@gmail.com>
2021-03-20 21:35:32 -0400

    add datetime bar plot demo
    closes #871

--- src/cookbook/ScottPlot.Cookbook/Recipes/Plottable/Bar.cs
+++ src/cookbook/ScottPlot.Cookbook/Recipes/Plottable/Bar.cs
@@ -362,4 +362,40 @@ namespace ScottPlot.Cookbook.Recipes.Plottable
             plt.Legend();
         }
     }
+
+    public class DateTimeBarPlots : IRecipe
+    {
+        public string Category => "Plottable: Bar Graph";
+        public string ID => "bar_dateTime";
+        public string Title => "DateTime Bar Plot";
+        public string Description =>
+            "Bars have a default width of 1.0, but when using DateTime axis this means bars are one day wide. " +
+            "To plot DateTime data you may need to manually set the width of a bar to a desired size (in fractions of a day).";
+
+        public void ExecuteRecipe(Plot plt)
+        {
+            // let's plot 24 hours of data
+            int pointCount = 24;
+
+            // generate some random values
+            var rand = new Random(0);
+            double[] values = DataGen.Random(rand, pointCount, 1, 2);
+
+            // space every time point by 1 hour from a starting point
+            DateTime start = new DateTime(2021, 09, 24, 0, 0, 0);
+            double[] positions = new double[pointCount];
+            for (int i = 0; i < pointCount; i++)
+                positions[i] = start.AddHours(i).ToOADate();
+
+            // display the bar plot using a time axis
+            var bar = plt.AddBar(values, positions);
+            plt.XAxis.DateTimeFormat(true);
+
+            // indicate each bar width should be 1/24 of a day then shrink sligtly to add spacing between bars
+            bar.BarWidth = (1.0 / 24) * .8;
+
+            // adjust axis limits so there is no padding below the bar graph
+            plt.SetAxisLimits(yMin: 0);
+        }
+    }
 }
