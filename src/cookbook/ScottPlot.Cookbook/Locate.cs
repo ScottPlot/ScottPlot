2d7f00ddece8fa257114fbe08685e8baa7401f65
Benjamin Lehmann <bclehmann@yahoo.ca>
2021-03-24 12:30:26 -0600

    Caches Locate.GetRecipes()

--- src/cookbook/ScottPlot.Cookbook/Locate.cs
+++ src/cookbook/ScottPlot.Cookbook/Locate.cs
@@ -10,16 +10,15 @@ namespace ScottPlot.Cookbook
 {
     public static class Locate
     {
-        public static IRecipe[] GetRecipes() =>
-            AppDomain
-            .CurrentDomain
-            .GetAssemblies()
+        private static readonly IRecipe[] _recipes = AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(s => s.GetTypes())
             .Where(x => x.IsAbstract == false)
             .Where(x => x.IsInterface == false)
             .Where(p => typeof(IRecipe).IsAssignableFrom(p))
             .Select(x => (IRecipe)Activator.CreateInstance(x))
             .ToArray();
+
+        public static IRecipe[] GetRecipes() => _recipes;
 
         public static IRecipe GetRecipe(string id) => GetRecipes().Where(x => x.ID == id).First();
 
