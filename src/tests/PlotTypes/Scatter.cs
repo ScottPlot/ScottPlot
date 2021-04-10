97e7fc099a5ebe6cf9468c4bf1693c924a0f3bdc
Scott W Harden <swharden@gmail.com>
2021-04-10 17:37:13 -0400

    single-point scatter plot test
    #948

---
+++ src/tests/PlotTypes/Scatter.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlotTests.PlotTypes
+{
+    class Scatter
+    {
+        [Test]
+        public void Test_Scatter_SinglePoint()
+        {
+            // https://github.com/ScottPlot/ScottPlot/issues/948
+            var plt = new ScottPlot.Plot();
+            plt.AddScatter(
+                xs: new double[1] { 0 },
+                ys: new double[1] { 2 },
+                markerSize: 1,
+                lineWidth: 1);
+            plt.Render();
+        }
+    }
+}
