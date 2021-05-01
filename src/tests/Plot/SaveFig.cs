d16c619430899a3cd870b27580dc6c6034a0d419
Scott W Harden <swharden@gmail.com>
2021-05-01 12:01:12 -0400

    add test to demonstrate SaveFig scaling

---
+++ src/tests/Plot/SaveFig.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlotTests.Plot
+{
+    class SaveFig
+    {
+        [Test]
+        public void Test_SaveFig_OutputScaling()
+        {
+            var plt = new ScottPlot.Plot();
+
+            plt.AddSignal(DataGen.Sin(51));
+            plt.XLabel("Horizontal Axis");
+            plt.YLabel("Vertical Axis");
+
+            plt.SaveFig("TestPlot_LowRes.png", 400, 300);
+            plt.SaveFig("TestPlot_HighRes.png", 4000, 3000, resize: false);
+        }
+    }
+}
