48c0edb7462783a824f3d18dec19bf4526153e9e
Scott W Harden <swharden@gmail.com>
2021-05-01 12:52:12 -0400

    add more scaling dimensions to tests

--- src/tests/Plot/SaveFig.cs
+++ src/tests/Plot/SaveFig.cs
@@ -14,13 +14,16 @@ namespace ScottPlotTests.Plot
         public void Test_SaveFig_OutputScaling()
         {
             var plt = new ScottPlot.Plot();
-
             plt.AddSignal(DataGen.Sin(51));
+            plt.AddSignal(DataGen.Cos(51));
+            plt.Title("Scaled Figure Demo");
             plt.XLabel("Horizontal Axis");
             plt.YLabel("Vertical Axis");
 
-            plt.SaveFig("TestPlot_HighRes.png", 400, 300, scale: 10);
-            plt.SaveFig("TestPlot_LowRes.png", 400, 300);
+            plt.SaveFig("Test_Scaled_Default.png", 400, 300);
+            plt.SaveFig("Test_Scaled_0.5.png", 400, 300, scale: 0.5);
+            plt.SaveFig("Test_Scaled_5.png", 400, 300, scale: 5);
+            plt.SaveFig("Test_Scaled_5_tall.png", 300, 400, scale: 5);
         }
     }
 }
