382b92d9595a75e03fa1520028c0d5190df1a986
Scott W Harden <swharden@gmail.com>
2021-05-23 19:05:45 -0400

    cookbook recipe: SignalConst generic type

--- src/cookbook/ScottPlot.Cookbook/Recipes/Plottable/SignalConst.cs
+++ src/cookbook/ScottPlot.Cookbook/Recipes/Plottable/SignalConst.cs
@@ -17,9 +17,26 @@ namespace ScottPlot.Cookbook.Recipes.Plottable
         public void ExecuteRecipe(Plot plt)
         {
             double[] values = DataGen.RandomWalk(1_000_000);
-            plt.AddSignal(values);
+            plt.AddSignalConst(values);
             plt.Title("One Million Points");
             plt.Benchmark();
+        }
+    }
+
+    public class SignalConstGeneric : IRecipe
+    {
+        public string Category => "Plottable: SignalConst";
+        public string ID => "signalconst_generic";
+        public string Title => "Generic Data Type";
+        public string Description =>
+            "SignalConst supports other data types beyond just double arrays. " +
+            "You can use this plot type to display data in any numerical format that can be cast to a double.";
+
+        public void ExecuteRecipe(Plot plt)
+        {
+            int[] data = { 2, 6, 3, 8, 5, 6, 1, 9, 7 };
+            plt.AddSignalConst(data);
+            plt.Title("SignalConst Displaying int[] Data");
         }
     }
 }
