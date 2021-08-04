071f4f34ff3b35c8a102d5b771112d60ee8bf410
Scott W Harden <swharden@gmail.com>
2021-08-03 22:45:01 -0400

    sandbox: improve WPF disposal demo

--- src/sandbox/WpfApp/MainWindow.xaml.cs
+++ src/sandbox/WpfApp/MainWindow.xaml.cs
@@ -9,33 +9,31 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        Random Rand = new();
-        int WindowNumber = 1;
         List<Window> Windows = new();
+        Random Rand = new();
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Close(object sender, RoutedEventArgs e)
+        private void Button_Open(object sender, RoutedEventArgs e)
         {
-            foreach (Window window in Windows)
-            {
-                window.Close();
-            }
-        }
-
-        private void Button_Launch(object sender, RoutedEventArgs e)
-        {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 50; i++)
             {
                 ScottPlot.Plot plt = new();
                 plt.AddSignal(ScottPlot.DataGen.RandomWalk(Rand, 100));
-                ScottPlot.WpfPlotViewer window = new(plt, windowTitle: $"Window {WindowNumber++}");
-                Windows.Add(window);
+                ScottPlot.WpfPlotViewer window = new(plt, windowTitle: $"Window {i}");
                 window.Show();
+                Windows.Add(window);
             }
+        }
+
+        private void Button_Close(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in Windows)
+                window.Close();
+            Windows.Clear();
         }
     }
 }
