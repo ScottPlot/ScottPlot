e2c04e8ffc6d06272a02d4b5ce4a30a28e289beb
Scott W Harden <swharden@gmail.com>
2021-08-05 22:25:54 -0400

    sandbox: demonstrate tick issue #1118

--- src/sandbox/WinFormsFrameworkApp/Form1.cs
+++ src/sandbox/WinFormsFrameworkApp/Form1.cs
@@ -17,6 +17,13 @@ namespace WinFormsFrameworkApp
             var hm = formsPlot1.Plot.AddHeatmap(data, lockScales: false);
             formsPlot1.Plot.AddColorbar(hm.Colormap);
             formsPlot1.Plot.SetAxisLimits(2.5, 2.6, 2.5, 2.6);
+
+            // hide major tick marks (but not labels)
+            formsPlot1.Plot.YAxis.Ticks(major: false, minor: true, majorLabels: true);
+
+            // make ticks 0 to allow labels to be placed close to the edge of the plot
+            (_, var ticks, _) = formsPlot1.Plot.YAxis.GetSettings(showWarning: false);
+            ticks.MajorTickLength = 0;
         }
     }
 }
