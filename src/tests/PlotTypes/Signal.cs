4a5523954a979beeab84f638ccb53c437f999839
Scott W Harden <swharden@gmail.com>
2021-08-11 21:58:58 -0400

    Tests: demonstrate signal fill bug #1163

---
+++ src/tests/PlotTypes/Signal.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlotTests.PlotTypes
+{
+    class Signal
+    {
+        [Test]
+        public void Test_Signal_Filled()
+        {
+            var plt = new ScottPlot.Plot(400, 300);
+
+            Random rand = new(0);
+            double[] values = ScottPlot.DataGen.Random(rand, pointCount: 10, offset: -.5);
+            var sig = plt.AddSignal(values);
+            sig.Color = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Red);
+            sig.MarkerSize = 0;
+            sig.LineWidth = 5;
+            sig.FillType = ScottPlot.FillType.FillAboveAndBelow;
+            sig.FillColor1 = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Green);
+            sig.FillColor2 = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Blue);
+
+            TestTools.SaveFig(plt);
+        }
+    }
+}
