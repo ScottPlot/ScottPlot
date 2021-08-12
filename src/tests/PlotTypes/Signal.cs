4e091e0dcc99ad6e4d06b8100490ddda4951d6fd
Scott W Harden <swharden@gmail.com>
2021-08-11 23:33:17 -0400

    Tests: additional signal fill tests
    demonstrates fill errors described in #1163

--- src/tests/PlotTypes/Signal.cs
+++ src/tests/PlotTypes/Signal.cs
@@ -23,6 +23,45 @@ namespace ScottPlotTests.PlotTypes
             sig.FillType = ScottPlot.FillType.FillAboveAndBelow;
             sig.FillColor1 = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Green);
             sig.FillColor2 = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Blue);
+            sig.BaselineY = 0.2;
+
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_Signal_FilledAbove()
+        {
+            var plt = new ScottPlot.Plot(400, 300);
+
+            Random rand = new(0);
+            double[] values = ScottPlot.DataGen.Random(rand, pointCount: 10, offset: -.5);
+            var sig = plt.AddSignal(values);
+            sig.Color = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Red);
+            sig.MarkerSize = 0;
+            sig.LineWidth = 5;
+            sig.FillType = ScottPlot.FillType.FillAbove;
+            sig.FillColor1 = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Green);
+            sig.FillColor2 = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Blue);
+            sig.BaselineY = 0.2;
+
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_Signal_FilledBelow()
+        {
+            var plt = new ScottPlot.Plot(400, 300);
+
+            Random rand = new(0);
+            double[] values = ScottPlot.DataGen.Random(rand, pointCount: 10, offset: -.5);
+            var sig = plt.AddSignal(values);
+            sig.Color = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Red);
+            sig.MarkerSize = 0;
+            sig.LineWidth = 5;
+            sig.FillType = ScottPlot.FillType.FillBelow;
+            sig.FillColor1 = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Green);
+            sig.FillColor2 = System.Drawing.Color.FromArgb(100, System.Drawing.Color.Blue);
+            sig.BaselineY = 0.2;
 
             TestTools.SaveFig(plt);
         }
