cce4028fe961be257fe05c529419ded9c1104195
arthurits <simply_alfy@hotmail.com>
2021-09-02 17:13:26 +0200

    Microcharts colorset

---
+++ src/ScottPlot/Drawing/Colorsets/Microcharts.cs
@@ -0,0 +1,15 @@
+﻿/* Sourced from Microcharts:
+ * https://github.com/microcharts-dotnet/Microcharts/blob/main/Sources/Microcharts.Samples/Data.cs
+ */
+namespace ScottPlot.Drawing.Colorsets
+{
+    class Microcharts : HexColorset, IColorset
+    {
+        public override string[] hexColors => new string[]
+        {
+            "#266489", "#68B9C0", "#90D585", "#F3C151", "#F37F64",
+            "#424856", "#8F97A4", "#DAC096", "#76846E", "#DABFAF",
+            "#A65B69", "#97A69D",
+        };
+    }
+}
