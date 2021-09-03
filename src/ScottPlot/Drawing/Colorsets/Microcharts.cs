300a4572e9abba0c029b70cac5581c6dc98336a5
Scott W Harden <swharden@gmail.com>
2021-09-02 21:04:54 -0400

    Colorset: add Microcharts license details

--- src/ScottPlot/Drawing/Colorsets/Microcharts.cs
+++ src/ScottPlot/Drawing/Colorsets/Microcharts.cs
@@ -1,5 +1,7 @@
-﻿/* Sourced from Microcharts:
+﻿/* This color pallette was sourced from the examples provided by Microcharts:
  * https://github.com/microcharts-dotnet/Microcharts/blob/main/Sources/Microcharts.Samples/Data.cs
+ * At the time the license file was accessed (2021-09-02) the original work was
+ * released under a MIT License, Copyright (c) 2017 Aloïs Deniel.
  */
 namespace ScottPlot.Drawing.Colorsets
 {
