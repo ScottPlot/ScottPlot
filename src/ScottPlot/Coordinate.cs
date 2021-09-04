5ce48e7f25ff0c46beefad9c60268209bc31c292
Scott W Harden <swharden@gmail.com>
2021-09-04 15:52:00 -0400

    ScottPlot.Pixel and ScottPlot.Coordinate
    new types make it easier to pass X/Y locations together as a pair

---
+++ src/ScottPlot/Coordinate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ScottPlot
+{
+    /// <summary>
+    /// Describes an X/Y position in coordinate space
+    /// </summary>
+    public class Coordinate
+    {
+        public double X;
+        public double Y;
+
+        public Coordinate(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// True as lone as neither coordinate is NaN or Infinity
+        /// </summary>
+        public bool IsFinite()
+        {
+            return
+                !double.IsNaN(X) &&
+                !double.IsNaN(Y) &&
+                !double.IsInfinity(X) &&
+                !double.IsInfinity(Y);
+        }
+
+        /// <summary>
+        /// Return the distance to another coordinate (in coordinate units)
+        /// </summary>
+        public double Distance(Coordinate other)
+        {
+            double dX = Math.Abs(other.X - X);
+            double dY = Math.Abs(other.Y - Y);
+            return Math.Sqrt(dX * dX + dY * dY);
+        }
+    }
+}
