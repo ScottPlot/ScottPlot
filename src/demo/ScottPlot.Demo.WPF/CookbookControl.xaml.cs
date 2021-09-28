2014f081a7b321627c36d7f674aa919dfa492e3b
Scott W Harden <swharden@gmail.com>
2021-09-27 22:19:57 -0400

    demos: update to read cookbook source from json

--- src/demo/ScottPlot.Demo.WPF/CookbookControl.xaml.cs
+++ src/demo/ScottPlot.Demo.WPF/CookbookControl.xaml.cs
@@ -19,20 +19,24 @@ namespace ScottPlot.Demo.WPF
     /// </summary>
     public partial class CookbookControl : UserControl
     {
+        readonly Dictionary<string, Cookbook.RecipeSource> Recipes;
+
         public CookbookControl()
         {
             InitializeComponent();
+            Recipes = Cookbook.RecipeJson.GetRecipes();
             wpfPlot1.Configuration.WarnIfRenderNotCalledManually = false;
         }
 
         public void LoadDemo(string id)
         {
             var recipe = Cookbook.Locate.GetRecipe(id);
+            string source = Recipes is null ? Cookbook.RecipeJson.NotFoundMessage : Recipes[id].Code;
 
             DemoNameLabel.Content = recipe.Title;
             SourceCodeLabel.Content = recipe.ID;
             DescriptionTextbox.Text = recipe.Description;
-            SourceTextBox.Text = Cookbook.Locate.RecipeSourceCode(id);
+            SourceTextBox.Text = source.Replace("\n", Environment.NewLine);
 
             wpfPlot1.Reset();
             recipe.ExecuteRecipe(wpfPlot1.Plot);
