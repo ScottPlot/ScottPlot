f16bde4bf6197ba6b308abbf9301afc3f81f92db
Scott W Harden <swharden@gmail.com>
2021-09-29 23:10:08 -0400

    icon: ScottPlot 5

---
+++ dev/icon/make-icons.bat
@@ -0,0 +1,15 @@
+convert %~dp0\v5\scottplot-icon-rounded-128.png scottplot-icon-rounded.ico
+
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico ..\..\src\ScottPlot\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.png ..\..\src\ScottPlot\icon.png
+
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico ..\..\src\ScottPlot.WinForms\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.png ..\..\src\ScottPlot.WinForms\icon.png
+
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico ..\..\src\ScottPlot.WPF\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.png ..\..\src\ScottPlot.WPF\icon.png
+
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico ..\..\src\ScottPlot.Demo.WinForms\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.png ..\..\src\ScottPlot.Demo.WPF\icon.png
+
+pause
