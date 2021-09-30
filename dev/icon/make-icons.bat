483ecb51388dfc422c3ee87e3e0383df03f1a844
Scott W Harden <swharden@gmail.com>
2021-09-29 23:17:23 -0400

    icon: generation script

--- dev/icon/make-icons.bat
+++ dev/icon/make-icons.bat
@@ -1,15 +1,19 @@
-convert %~dp0\v5\scottplot-icon-rounded-128.png scottplot-icon-rounded.ico
+convert %~dp0\v5\scottplot-icon-rounded-128.png %~dp0\v5\scottplot-icon-rounded.ico
 
-copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico ..\..\src\ScottPlot\icon.ico
-copy /b/v/y %~dp0\v5\scottplot-icon-rounded.png ..\..\src\ScottPlot\icon.png
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico %~dp0\..\..\src\ScottPlot\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded-128.png %~dp0\..\..\src\ScottPlot\icon.png
 
-copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico ..\..\src\ScottPlot.WinForms\icon.ico
-copy /b/v/y %~dp0\v5\scottplot-icon-rounded.png ..\..\src\ScottPlot.WinForms\icon.png
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico %~dp0\..\..\src\controls\ScottPlot.WinForms\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded-128.png %~dp0\..\..\src\controls\ScottPlot.WinForms\icon.png
 
-copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico ..\..\src\ScottPlot.WPF\icon.ico
-copy /b/v/y %~dp0\v5\scottplot-icon-rounded.png ..\..\src\ScottPlot.WPF\icon.png
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico %~dp0\..\..\src\controls\ScottPlot.WPF\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded-128.png %~dp0\..\..\src\controls\ScottPlot.WPF\icon.png
 
-copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico ..\..\src\ScottPlot.Demo.WinForms\icon.ico
-copy /b/v/y %~dp0\v5\scottplot-icon-rounded.png ..\..\src\ScottPlot.Demo.WPF\icon.png
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico %~dp0\..\..\src\controls\ScottPlot.Avalonia\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded-128.png %~dp0\..\..\src\controls\ScottPlot.Avalonia\icon.png
+
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico %~dp0\..\..\src\demo\ScottPlot.Demo.WPF\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico %~dp0\..\..\src\demo\ScottPlot.Demo.WinForms\icon.ico
+copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico %~dp0\..\..\src\demo\ScottPlot.Demo.Avalonia\icon.ico
 
 pause
