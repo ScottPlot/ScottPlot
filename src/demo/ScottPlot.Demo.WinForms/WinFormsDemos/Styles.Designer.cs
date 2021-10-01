f3f9152e5d80fd05c25be044211bc5710f4b07ac
Scott W Harden <swharden@gmail.com>
2021-09-30 20:56:56 -0400

    WinForms Demo: show palettes in style browser

--- src/demo/ScottPlot.Demo.WinForms/WinFormsDemos/Styles.Designer.cs
+++ src/demo/ScottPlot.Demo.WinForms/WinFormsDemos/Styles.Designer.cs
@@ -28,29 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lbStyles = new System.Windows.Forms.ListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbPalettes = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lbStyles
             // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 21;
-            this.listBox1.Location = new System.Drawing.Point(3, 25);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(209, 395);
-            this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.lbStyles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbStyles.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStyles.FormattingEnabled = true;
+            this.lbStyles.ItemHeight = 21;
+            this.lbStyles.Location = new System.Drawing.Point(3, 25);
+            this.lbStyles.Name = "lbStyles";
+            this.lbStyles.Size = new System.Drawing.Size(209, 395);
+            this.lbStyles.TabIndex = 0;
+            this.lbStyles.SelectedIndexChanged += new System.EventHandler(this.lbStyles_SelectedIndexChanged);
             // 
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.lbStyles);
             this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
@@ -65,29 +68,58 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.formsPlot1.BackColor = System.Drawing.Color.Transparent;
-            this.formsPlot1.Location = new System.Drawing.Point(233, 12);
+            this.formsPlot1.Location = new System.Drawing.Point(454, 12);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(637, 423);
+            this.formsPlot1.Size = new System.Drawing.Size(607, 423);
             this.formsPlot1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.lbPalettes);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(233, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(215, 423);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Palettes";
+            // 
+            // lbPalettes
+            // 
+            this.lbPalettes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbPalettes.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPalettes.FormattingEnabled = true;
+            this.lbPalettes.ItemHeight = 21;
+            this.lbPalettes.Location = new System.Drawing.Point(3, 25);
+            this.lbPalettes.Name = "lbPalettes";
+            this.lbPalettes.Size = new System.Drawing.Size(209, 395);
+            this.lbPalettes.TabIndex = 0;
+            this.lbPalettes.SelectedIndexChanged += new System.EventHandler(this.lbPalettes_SelectedIndexChanged);
             // 
             // Styles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 447);
+            this.ClientSize = new System.Drawing.Size(1073, 447);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.formsPlot1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Styles";
             this.Text = "ScottPlot Styles";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lbStyles;
         private System.Windows.Forms.GroupBox groupBox1;
         private FormsPlot formsPlot1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lbPalettes;
     }
 }
