8bbda76110bb646853f1b12ae6937970107c4193
Scott W Harden <swharden@gmail.com>
2021-09-30 21:48:10 -0400

    icon: automate conversion from .png to .ico

--- dev/icon/make-icons.bat
+++ dev/icon/make-icons.bat
@@ -1,4 +1,8 @@
 convert %~dp0\v5\scottplot-icon-rounded-128.png %~dp0\v5\scottplot-icon-rounded.ico
+convert %~dp0\v5\scottplot-icon-rounded-border-128.png %~dp0\v5\scottplot-icon-rounded-border.ico
+convert %~dp0\v5\scottplot-icon-square-128.png %~dp0\v5\scottplot-icon-square.ico
+
+pause
 
 copy /b/v/y %~dp0\v5\scottplot-icon-rounded.ico %~dp0\..\..\src\ScottPlot\icon.ico
 copy /b/v/y %~dp0\v5\scottplot-icon-rounded-128.png %~dp0\..\..\src\ScottPlot\icon.png
