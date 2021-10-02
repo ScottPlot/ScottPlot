523224c8ce7217c9cc489f661789b0b8e6d93a6d
Scott W Harden <swharden@gmail.com>
2021-10-02 12:12:07 -0400

    Palette.Dark: add copyright notice

--- src/ScottPlot/Palettes/Dark.cs
+++ src/ScottPlot/Palettes/Dark.cs
@@ -1,6 +1,6 @@
-﻿/* This is a qualitative 8-color palette generated using:
- * https://colorbrewer2.org
- * which is both LCD and print friendly but not blind nor photocopy friendly
+﻿/* This a qualitative 8-color palette generated using https://colorbrewer2.org
+ * © Cynthia Brewer, Mark Harrower and The Pennsylvania State University
+ * It is is both LCD and print friendly but not blind nor photocopy friendly
  */
 namespace ScottPlot.Drawing.Colorsets
 {
