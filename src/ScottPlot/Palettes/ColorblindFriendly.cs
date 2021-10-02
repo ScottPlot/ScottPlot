244782dd09aab06be4828fc2d5929658f840fce5
Scott W Harden <swharden@gmail.com>
2021-10-02 10:27:51 -0400

    Palette: ColorBlindness -> ColorblindFriendly

--- src/ScottPlot/Palettes/ColorblindFriendly.cs
+++ src/ScottPlot/Palettes/ColorblindFriendly.cs
@@ -1,11 +1,11 @@
 ﻿/* This color palette was sourced from the examples provided in:
- * https://www.nature.com/articles/nmeth.1618.pdf
+ * Wong 2011, https://www.nature.com/articles/nmeth.1618.pdf
  * This 8-color palette has good overall variability and can be 
  * differentiated by individuals with red-green color blindness.
  */
 namespace ScottPlot.Drawing.Colorsets
 {
-    class ColorBlindness : HexColorset, IPalette
+    class ColorblindFriendly : HexColorset, IPalette
     {
         public override string[] hexColors => new string[]
         {
