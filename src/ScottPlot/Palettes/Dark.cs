8ec097f10a424a9d851108280c81fc1d0a0fe187
arthurits <simply_alfy@hotmail.com>
2021-10-02 12:14:20 +0200

    Dark palette

---
+++ src/ScottPlot/Palettes/Dark.cs
@@ -0,0 +1,15 @@
+﻿/* This is a qualitative 8-color palette generated using:
+ * https://colorbrewer2.org
+ * which is both LCD and print friendly but not blind nor photocopy friendly
+ */
+namespace ScottPlot.Drawing.Colorsets
+{
+    class Dark : HexColorset, IPalette
+    {
+        public override string[] hexColors => new string[]
+        {
+            "#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e",
+            "#e6ab02","#a6761d","#666666",
+        };
+    }
+}
