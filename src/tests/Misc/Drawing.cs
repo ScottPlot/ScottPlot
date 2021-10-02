7597170f31e652f1ce303af804c584ef0106405c
Russ Hammett <kritner@gmail.com>
2021-10-01 20:16:37 -0400

    Ack, my namespace import didn't save

--- src/tests/Misc/Drawing.cs
+++ src/tests/Misc/Drawing.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace ScottPlotTests.Misc
