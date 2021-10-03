ca456da78a8ef682bb9dab55413f4c06529cc448
arthurits <simply_alfy@hotmail.com>
2021-10-03 11:00:54 +0200

    Modify palette name typo

--- src/ScottPlot/Palette.cs
+++ src/ScottPlot/Palette.cs
@@ -24,7 +24,7 @@ namespace ScottPlot
         public static ScottPlot.Drawing.Palette OneHalfDark => new(new ScottPlot.Drawing.Colorsets.OneHalfDark());
         public static ScottPlot.Drawing.Palette PolarNight => new(new ScottPlot.Drawing.Colorsets.PolarNight());
         public static ScottPlot.Drawing.Palette SnowStorm => new(new ScottPlot.Drawing.Colorsets.Snowstorm());
-        public static ScottPlot.Drawing.Palette xgfs24 => new(new ScottPlot.Drawing.Colorsets.xgfs25());
+        public static ScottPlot.Drawing.Palette xgfs25 => new(new ScottPlot.Drawing.Colorsets.xgfs25());
 
         /// <summary>
         /// Create a new color palette from an array of HTML colors
@@ -63,7 +63,7 @@ namespace ScottPlot.Drawing
         public static Palette OneHalfDark => new(new Colorsets.OneHalfDark());
         public static Palette PolarNight => new(new Colorsets.PolarNight());
         public static Palette SnowStorm => new(new Colorsets.Snowstorm());
-        public static Palette xgfs24 => new(new Colorsets.xgfs25());
+        public static Palette xgfs25 => new(new Colorsets.xgfs25());
 
         private readonly IPalette cset;
         public readonly string Name;
