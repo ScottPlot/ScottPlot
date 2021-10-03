522fe776ef14cb8ac9d0aa776b2b86a3dfb101b0
KanishkKhurana <74613246+KanishkKhurana@users.noreply.github.com>
2021-10-03 07:48:26 +0530

    Added SummerSplash.cs
    added a new fresh summer color palette . 
    i hope you like it 
    do tell me if there are some errors .
    happy hacking !

---
+++ src/ScottPlot/Palettes/SummerSplash.cs
@@ -0,0 +1,10 @@
+namespace ScottPlot.Drawing.Colorsets
+{
+    class SummerSplash : HexColorset, IPalette
+    {
+        public override string[] hexColors => new string[]
+        {
+            "#05445E", "#189AB4", "#75E6DA" , "#D4F1F4"
+        };
+    }
+}
