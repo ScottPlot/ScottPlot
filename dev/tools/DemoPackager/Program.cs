d4a7186630229876d17cc372caa89c9cd21861a9
Scott W Harden <swharden@gmail.com>
2021-10-09 20:42:51 -0400

    DemoPackager: generate recipes.json

--- dev/tools/DemoPackager/Program.cs
+++ dev/tools/DemoPackager/Program.cs
@@ -23,11 +23,20 @@ namespace DemoPackager
 
             ResetOutputFolder();
 
+            GenerateRecipes();
+
             BuildAndZip(Path.Combine(RepoFolder, @"src\demo\ScottPlot.Demo.WinForms"), true, "ScottPlot-Demo-WinForms.zip");
             BuildAndZip(Path.Combine(RepoFolder, @"src\demo\ScottPlot.Demo.WPF"), true, "ScottPlot-Demo-WPF.zip");
             BuildAndZip(Path.Combine(RepoFolder, @"src\demo\ScottPlot.Demo.Avalonia"), true, "ScottPlot-Demo-Avalonia.zip");
 
             FtpUpload("/scottplot.net/public_html/demos");
+        }
+
+        static void GenerateRecipes()
+        {
+            Console.WriteLine("Generating cookbook recipes from source code...");
+            string cookbookFolder = Path.Combine(RepoFolder, "src/cookbook/ScottPlot.Cookbook");
+            ScottPlot.Cookbook.RecipeJson.Generate(cookbookFolder, "recipes.json");
         }
 
         static void ResetOutputFolder()
@@ -40,7 +49,13 @@ namespace DemoPackager
 
         static void BuildAndZip(string projectFolder, bool msbuild, string zipFileName)
         {
+            Console.WriteLine();
             Console.WriteLine($"Building {Path.GetFileName(projectFolder)}...");
+
+            Console.WriteLine("Deleting previous builds...");
+            string releaseFolder = Path.Combine(projectFolder, @"bin\Release");
+            if (Directory.Exists(releaseFolder))
+                Directory.Delete(releaseFolder, true);
 
             string cmd = msbuild
                 ? $"msbuild \"{projectFolder}\" /p:Configuration=Release"
@@ -62,8 +77,11 @@ namespace DemoPackager
                 throw new InvalidOperationException($"exited with code {pProcess.ExitCode}");
             }
 
+            releaseFolder = Directory.GetDirectories(releaseFolder)[0];
+            Console.WriteLine("Coping recipes.json...");
+            File.Copy("recipes.json", Path.Combine(releaseFolder, "recipes.json"), overwrite: true);
+
             Console.WriteLine($"Creating {zipFileName}...");
-            string releaseFolder = Path.Combine(projectFolder, @"bin\Release");
             string zipFilePath = Path.Combine(DemoOutputFolder, zipFileName);
             ZipFile.CreateFromDirectory(releaseFolder, zipFilePath);
         }
@@ -77,6 +95,7 @@ namespace DemoPackager
             foreach (var child in config.GetChildren())
                 Environment.SetEnvironmentVariable(child.Key, child.Value);
 
+            Console.WriteLine();
             Console.WriteLine("FTP Connecting...");
             string username = Environment.GetEnvironmentVariable("FTP_USERNAME");
             string password = Environment.GetEnvironmentVariable("FTP_PASSWORD");
