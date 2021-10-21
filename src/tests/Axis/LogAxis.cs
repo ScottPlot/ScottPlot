6e9e17a8e377619bd3ee405915d25b61eb21c21a
Scott W Harden <swharden@gmail.com>
2021-10-20 20:25:01 -0400

    Tests: demonstrate log scale issues
    #1386

---
+++ src/tests/Axis/LogAxis.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlotTests.Axis
+{
+    internal class LogAxis
+    {
+        [Test]
+        public void Test_LogAxis_Y()
+        {
+            double[] xs = { 1, 2, 3, 4, 5 };
+            double[] ys = { 10, 2_000, 50_000, 1_000_000, 1_500_000 };
+
+            var plt = new ScottPlot.Plot();
+
+            // Plot the log of the Ys
+            double[] logYs = ys.Select(y => Math.Log10(y)).ToArray();
+            var scatter = plt.AddScatter(xs, logYs);
+
+            // label each point with a red line
+            plt.Palette = ScottPlot.Palette.ColorblindFriendly;
+            for (int i = 0; i < scatter.PointCount; i++)
+            {
+                double x = scatter.Xs[i];
+                double y = scatter.Ys[i];
+                double actualY = Math.Pow(10, y);
+                var color = plt.Palette.GetColor(i);
+                plt.AddHorizontalLine(y, color, 1, ScottPlot.LineStyle.Dot);
+                plt.AddVerticalLine(x, color, 1, ScottPlot.LineStyle.Dot);
+                plt.AddPoint(x, y, color, 10, ScottPlot.MarkerShape.openCircle);
+                var txt = plt.AddText(actualY.ToString("N0"), x, y, 12, System.Drawing.Color.Black);
+            }
+
+            // Use a custom tick formatter to label tick marks as the antilog of their position
+            Func<double, string> tickLabeler = (y) => Math.Pow(10, y).ToString("N0");
+            plt.YAxis.TickLabelFormat(tickLabeler);
+
+            // Use log-spaced tick marks and grid lines to make it more convincing
+            plt.YAxis.MinorGrid(enable: true);
+            plt.YAxis.MinorLogScale(true);
+
+            // Set the axis limits manually to ensure edges terminate at a nice locations in log space
+            plt.SetAxisLimits(.5, 5.5, 0, Math.Log10(10_000_000));
+
+            TestTools.SaveFig(plt);
+        }
+    }
+}
