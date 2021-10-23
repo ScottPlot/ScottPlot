dbb0ca8a6c6350096fc3c2abbdc9470788ed6999
Scott W Harden <swharden@gmail.com>
2021-10-23 11:04:22 -0400

    IHasColormap
    This interface will be implemented by plottables containing a colormap to expose their colors/values easier to the colorbar class

---
+++ src/ScottPlot/Plottable/IHasColormap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlot.Plottable
+{
+    /// <summary>
+    /// Describes plottables with a Colormap that may use a Colorbar
+    /// </summary>
+    public interface IHasColormap
+    {
+        /// <summary>
+        /// Value representing the lowest color on the colormap
+        /// </summary>
+        double ColormapMin { get; set; }
+
+        /// <summary>
+        /// Value representing the highest color on the colormap
+        /// </summary>
+        double ColormapMax { get; set; }
+
+        /// <summary>
+        /// Colormap to use for translating values to colors
+        /// </summary>
+        Drawing.Colormap Colormap { get; set; }
+    }
+}
