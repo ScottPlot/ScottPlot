54de16bd9d697596967a7ae03ff7a78e5be9420b
Scott W Harden <swharden@gmail.com>
2021-10-23 11:31:37 -0400

    IHasColormap: remove min/max values
    Now that the value range can be stored in the Colormap itself this interface does not need properties to hold these values

--- src/ScottPlot/Plottable/IHasColormap.cs
+++ src/ScottPlot/Plottable/IHasColormap.cs
@@ -6,24 +6,8 @@ using System.Threading.Tasks;
 
 namespace ScottPlot.Plottable
 {
-    /// <summary>
-    /// Describes plottables with a Colormap that may use a Colorbar
-    /// </summary>
     public interface IHasColormap
     {
-        /// <summary>
-        /// Value representing the lowest color on the colormap
-        /// </summary>
-        double ColormapMin { get; set; }
-
-        /// <summary>
-        /// Value representing the highest color on the colormap
-        /// </summary>
-        double ColormapMax { get; set; }
-
-        /// <summary>
-        /// Colormap to use for translating values to colors
-        /// </summary>
         Drawing.Colormap Colormap { get; set; }
     }
 }
