ad5f0b95558498e51d9396239b07b75fa7938eca
Scott W Harden <swharden@gmail.com>
2021-12-24 17:38:04 -0500

    Sandbox: demonstrate multi-axis AxisAuto

--- src/sandbox/WinFormsApp/Form1.Designer.cs
+++ src/sandbox/WinFormsApp/Form1.Designer.cs
@@ -1,15 +1,14 @@
-﻿
-namespace WinFormsApp
+﻿namespace WinFormsApp
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,25 +23,110 @@ namespace WinFormsApp
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.btnX = new System.Windows.Forms.Button();
+            this.btnXY = new System.Windows.Forms.Button();
+            this.btnY = new System.Windows.Forms.Button();
+            this.cb1 = new System.Windows.Forms.CheckBox();
+            this.cb2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.formsPlot1.BackColor = System.Drawing.Color.Transparent;
+            this.formsPlot1.Location = new System.Drawing.Point(0, 41);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(721, 316);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // btnX
+            // 
+            this.btnX.Location = new System.Drawing.Point(12, 12);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(75, 23);
+            this.btnX.TabIndex = 1;
+            this.btnX.Text = "Auto X";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            // 
+            // btnXY
+            // 
+            this.btnXY.Location = new System.Drawing.Point(174, 12);
+            this.btnXY.Name = "btnXY";
+            this.btnXY.Size = new System.Drawing.Size(75, 23);
+            this.btnXY.TabIndex = 2;
+            this.btnXY.Text = "Auto XY";
+            this.btnXY.UseVisualStyleBackColor = true;
+            this.btnXY.Click += new System.EventHandler(this.btnXY_Click);
+            // 
+            // btnY
+            // 
+            this.btnY.Location = new System.Drawing.Point(93, 12);
+            this.btnY.Name = "btnY";
+            this.btnY.Size = new System.Drawing.Size(75, 23);
+            this.btnY.TabIndex = 3;
+            this.btnY.Text = "Auto Y";
+            this.btnY.UseVisualStyleBackColor = true;
+            this.btnY.Click += new System.EventHandler(this.btnY_Click);
+            // 
+            // cb1
+            // 
+            this.cb1.AutoSize = true;
+            this.cb1.Checked = true;
+            this.cb1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cb1.Location = new System.Drawing.Point(255, 15);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(67, 19);
+            this.cb1.TabIndex = 4;
+            this.cb1.Text = "primary";
+            this.cb1.UseVisualStyleBackColor = true;
+            // 
+            // cb2
+            // 
+            this.cb2.AutoSize = true;
+            this.cb2.Checked = true;
+            this.cb2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cb2.Location = new System.Drawing.Point(328, 16);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(80, 19);
+            this.cb2.TabIndex = 5;
+            this.cb2.Text = "secondary";
+            this.cb2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(721, 357);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.btnY);
+            this.Controls.Add(this.btnXY);
+            this.Controls.Add(this.formsPlot1);
             this.Name = "Form1";
-            this.Text = "ScottPlot Sandbox - WinForms";
+            this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.Button btnX;
+        private System.Windows.Forms.Button btnXY;
+        private System.Windows.Forms.Button btnY;
+        private System.Windows.Forms.CheckBox cb1;
+        private System.Windows.Forms.CheckBox cb2;
     }
 }
-
--- src/sandbox/WinFormsApp/Form1.cs
+++ src/sandbox/WinFormsApp/Form1.cs
@@ -1,26 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinFormsApp
 {
     public partial class Form1 : Form
     {
-        private readonly ScottPlot.FormsPlot formsPlot1;
-
         public Form1()
         {
             InitializeComponent();
 
-            // add this manually because the .NET winforms designer is still in preview mode
-            // and the FormsPlot user control typically does not appear in the toolbox
-            formsPlot1 = new ScottPlot.FormsPlot() { Dock = DockStyle.Fill };
-            Controls.Add(formsPlot1);
+            var sig1 = formsPlot1.Plot.AddSignal(ScottPlot.DataGen.Sin(51));
+            sig1.XAxisIndex = 0;
+            sig1.YAxisIndex = 0;
+            sig1.Label = "primary";
 
-            int pointCount = 10;
-            var rand = new Random(0);
-            double[] xs = ScottPlot.DataGen.Random(rand, pointCount);
-            double[] ys = ScottPlot.DataGen.Random(rand, pointCount);
-            formsPlot1.Plot.AddScatter(xs, ys);
+            var sig2 = formsPlot1.Plot.AddSignal(ScottPlot.DataGen.Cos(51));
+            sig2.XAxisIndex = 1;
+            sig2.YAxisIndex = 1;
+            sig2.Label = "secondary";
+
+            formsPlot1.Plot.Legend();
+
+            formsPlot1.Refresh();
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            if (cb1.Checked)
+                formsPlot1.Plot.AxisAutoX(xAxisIndex: 0);
+
+            if (cb2.Checked)
+                formsPlot1.Plot.AxisAutoX(xAxisIndex: 1);
+
+            formsPlot1.Refresh();
+        }
+
+        private void btnY_Click(object sender, EventArgs e)
+        {
+            if (cb1.Checked)
+                formsPlot1.Plot.AxisAutoY(yAxisIndex: 0);
+
+            if (cb2.Checked)
+                formsPlot1.Plot.AxisAutoY(yAxisIndex: 1);
+
+            formsPlot1.Refresh();
+        }
+
+        private void btnXY_Click(object sender, EventArgs e)
+        {
+            if (cb1.Checked)
+                formsPlot1.Plot.AxisAuto(null, null, xAxisIndex: 0, yAxisIndex: 0);
+
+            if (cb2.Checked)
+                formsPlot1.Plot.AxisAuto(null, null, xAxisIndex: 1, yAxisIndex: 1);
+
             formsPlot1.Refresh();
         }
     }
