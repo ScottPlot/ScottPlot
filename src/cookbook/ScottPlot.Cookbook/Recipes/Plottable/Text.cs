7d897fb9940951153b745f8fb625f7ccfb73bff6
Scott W Harden <swharden@gmail.com>
2021-12-30 22:32:08 -0500

    Text: update cookbook
    #1417

--- src/cookbook/ScottPlot.Cookbook/Recipes/Plottable/Text.cs
+++ src/cookbook/ScottPlot.Cookbook/Recipes/Plottable/Text.cs
@@ -34,42 +34,31 @@ namespace ScottPlot.Cookbook.Recipes.Plottable
         public string ID => "text_alignment";
         public string Title => "Text Alignment and Rotation";
         public string Description =>
-            "Advanced options are available to customize rotation and alignment. " +
-            "Note that if rotation is used, alignment is ignored.";
+            "Alignment indicates which corner is placed at the X/Y coordinate.";
 
         public void ExecuteRecipe(Plot plt)
         {
-            int pointCount = 51;
-            double[] x = DataGen.Consecutive(pointCount);
-            double[] sin = DataGen.Sin(pointCount);
-            double[] cos = DataGen.Cos(pointCount);
+            ScottPlot.Alignment[] alignments = (ScottPlot.Alignment[])Enum.GetValues(typeof(ScottPlot.Alignment));
 
-            plt.AddScatter(x, sin);
-            plt.AddScatter(x, cos);
+            for (int i = 0; i < alignments.Length; i++)
+            {
+                double frac = (double)i / alignments.Length;
+                double x = Math.Sin(frac * Math.PI * 2);
+                double y = Math.Cos(frac * Math.PI * 2);
 
-            plt.AddPoint(25, 0.8, color: Color.Green);
-            var t1 = plt.AddText(" Important Point (1)", 25, 0.8, 16, Color.Green);
+                var txt = plt.AddText(alignments[i].ToString(), x, y);
+                txt.Alignment = alignments[i];
+                txt.Font.Color = Color.Black; ;
+                txt.BackgroundColor = Color.LightSteelBlue;
+                txt.BackgroundFill = true;
+                txt.Rotation = 5;
+                txt.BorderSize = 2;
+                txt.BorderColor = Color.Navy;
 
-            plt.AddPoint(30, 0.3, color: Color.Black, size: 15);
-            var t2 = plt.AddText(" Default alignment (2)", 30, 0.3, 16);
+                plt.AddPoint(x, y, Color.Red, 10);
+            }
 
-            plt.AddPoint(30, 0, color: Color.Black, size: 15);
-            var t3 = plt.AddText("Middle center (3)", 30, 0, 16);
-            t3.Alignment = Alignment.MiddleCenter;
-
-            plt.AddPoint(30, -0.3, color: Color.Black, size: 15);
-            var t4 = plt.AddText("Upper left (4)", 30, -0.3, 16);
-            t4.Alignment = Alignment.UpperLeft;
-
-            plt.AddPoint(5, -.5, color: Color.Blue, size: 15);
-            var t5 = plt.AddText(" Rotated Text (5)", 5, -.5, 16);
-            t5.Rotation = -30;
-
-            var t6 = plt.AddText(" Filled Background (6)", 15, -.6, 16);
-            t6.Color = Color.Red;
-            t6.FontBold = true;
-            t6.BackgroundFill = true;
-            t6.BackgroundColor = Color.LightBlue;
+            plt.Margins(.5, .2);
         }
     }
 
