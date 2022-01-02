53e93baba46f3b9686ddbd0443a6d27044f274b0
Scott W Harden <swharden@gmail.com>
2022-01-02 13:00:31 -0500

    WinForms Sandbox: multi-target

--- src/sandbox/WinFormsApp/Form1.cs
+++ src/sandbox/WinFormsApp/Form1.cs
@@ -16,50 +16,11 @@ namespace WinFormsApp
         {
             InitializeComponent();
 
-            var sig1 = formsPlot1.Plot.AddSignal(ScottPlot.DataGen.Sin(51));
-            sig1.XAxisIndex = 0;
-            sig1.YAxisIndex = 0;
-            sig1.Label = "primary";
-
-            var sig2 = formsPlot1.Plot.AddSignal(ScottPlot.DataGen.Cos(51));
-            sig2.XAxisIndex = 1;
-            sig2.YAxisIndex = 1;
-            sig2.Label = "secondary";
-
-            formsPlot1.Plot.Legend();
-
-            formsPlot1.Refresh();
-        }
-
-        private void btnX_Click(object sender, EventArgs e)
-        {
-            if (cb1.Checked)
-                formsPlot1.Plot.AxisAutoX(xAxisIndex: 0);
-
-            if (cb2.Checked)
-                formsPlot1.Plot.AxisAutoX(xAxisIndex: 1);
-
-            formsPlot1.Refresh();
-        }
-
-        private void btnY_Click(object sender, EventArgs e)
-        {
-            if (cb1.Checked)
-                formsPlot1.Plot.AxisAutoY(yAxisIndex: 0);
-
-            if (cb2.Checked)
-                formsPlot1.Plot.AxisAutoY(yAxisIndex: 1);
-
-            formsPlot1.Refresh();
-        }
-
-        private void btnXY_Click(object sender, EventArgs e)
-        {
-            if (cb1.Checked)
-                formsPlot1.Plot.AxisAuto(null, null, xAxisIndex: 0, yAxisIndex: 0);
-
-            if (cb2.Checked)
-                formsPlot1.Plot.AxisAuto(null, null, xAxisIndex: 1, yAxisIndex: 1);
+            Random rand = new(0);
+            double[] xs = ScottPlot.DataGen.Consecutive(10_000);
+            double[] ys = ScottPlot.DataGen.RandomWalk(rand, 10_000);
+            var sig1 = formsPlot1.Plot.AddSignalXY(xs, ys);
+            sig1.MarkerSize = 20;
 
             formsPlot1.Refresh();
         }
