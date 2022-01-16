dc76af4cd39391448bca68b71d0b0bd871051346
Scott W Harden <swharden@gmail.com>
2022-01-16 15:53:27 -0500

    ScottPlot5: create WinForms application using Maui.Graphics

---
+++ dev/ScottPlot5/ScottPlot.WinForms/Form1.cs
@@ -0,0 +1,21 @@
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Skia;
+
+namespace ScottPlot.WinForms
+{
+    public partial class Form1 : Form
+    {
+        readonly Plot Plot = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void skglControl1_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
+        {
+            ICanvas canvas = new SkiaCanvas() { Canvas = e.Surface.Canvas };
+            Plot.Draw(canvas, skglControl1.Width, skglControl1.Height);
+        }
+    }
+}
