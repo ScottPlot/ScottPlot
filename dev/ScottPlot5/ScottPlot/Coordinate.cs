2387f88a56754fa88b56d45edbf628b0624d6bba
Scott W Harden <swharden@gmail.com>
2022-01-16 22:19:19 -0500

    create Pixel and Coordinate classes

---
+++ dev/ScottPlot5/ScottPlot/Coordinate.cs
@@ -0,0 +1,20 @@
+﻿namespace ScottPlot;
+
+/// <summary>
+/// Represents the X/Y location of a point on the plot (in plot units, not pixels)
+/// </summary>
+public struct Coordinate
+{
+    public readonly double X;
+    public readonly double Y;
+
+    public Coordinate(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Pixel ToPixel(PlotView view) => view.GetPixel(X, Y);
+
+    public override string ToString() => $"(X={X}, Y={Y})";
+}
---
+++ dev/ScottPlot5/ScottPlot/Pixel.cs
@@ -0,0 +1,20 @@
+﻿namespace ScottPlot;
+
+/// <summary>
+/// Represents the X/Y location of a point on the screen (in pixel units, not plot units)
+/// </summary>
+public struct Pixel
+{
+    public readonly float X;
+    public readonly float Y;
+
+    public Pixel(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Coordinate ToCoordinate(PlotView view) => view.GetCoordinate(X, Y);
+
+    public override string ToString() => $"[X={X}, Y={Y}]";
+}
