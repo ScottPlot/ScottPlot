907cc4630a405180b579089abdb9ca3529c60c84
Scott W Harden <swharden@gmail.com>
2022-01-19 21:37:16 -0500

    Test: Legend GetItems(), Count, and HasItems

--- src/tests/Legend/LegendTests.cs
+++ src/tests/Legend/LegendTests.cs
@@ -15,7 +15,10 @@ namespace ScottPlotTests.Legend
             var plt = new ScottPlot.Plot();
             plt.AddSignal(ScottPlot.DataGen.Sin(51), label: "test");
             var leg = plt.Legend();
-            Assert.Throws<InvalidOperationException>(() => { leg.GetBitmap(false); });
+            System.Drawing.Bitmap bmp = leg.GetBitmap();
+
+            Assert.AreEqual(1, bmp.Width);
+            Assert.AreEqual(1, bmp.Height);
         }
 
         [Test]
@@ -24,7 +27,9 @@ namespace ScottPlotTests.Legend
             var plt = new ScottPlot.Plot();
             plt.AddSignal(ScottPlot.DataGen.Sin(51), label: "test");
             System.Drawing.Bitmap bmp = plt.RenderLegend();
-            TestTools.SaveBitmap(bmp);
+
+            Assert.AreNotEqual(1, bmp.Width);
+            Assert.AreNotEqual(1, bmp.Height);
         }
 
         [Test]
@@ -33,8 +38,40 @@ namespace ScottPlotTests.Legend
             var plt = new ScottPlot.Plot();
             plt.AddSignal(ScottPlot.DataGen.Sin(51));
             plt.AddSignal(ScottPlot.DataGen.Cos(51));
+            var leg = plt.Legend();
             System.Drawing.Bitmap bmp = plt.RenderLegend();
-            TestTools.SaveBitmap(bmp);
+
+            Assert.AreEqual(1, bmp.Width);
+            Assert.AreEqual(1, bmp.Height);
+            Assert.False(leg.HasItems);
+            Assert.AreEqual(0, leg.Count);
+            Assert.AreEqual(0, leg.GetItems().Length);
+        }
+
+        [Test]
+        public void Test_Legend_ItemsAndCounts()
+        {
+            var plt = new ScottPlot.Plot();
+            var sig1 = plt.AddSignal(ScottPlot.DataGen.Sin(51), label: "sin");
+            var sig2 = plt.AddSignal(ScottPlot.DataGen.Cos(51), label: "cos");
+            var leg = plt.Legend();
+
+            System.Drawing.Bitmap bmp1 = plt.RenderLegend();
+            Assert.AreNotEqual(1, bmp1.Width);
+            Assert.AreNotEqual(1, bmp1.Height);
+            Assert.True(leg.HasItems);
+            Assert.AreEqual(2, leg.Count);
+            Assert.AreEqual(2, leg.GetItems().Length);
+
+            sig1.Label = string.Empty;
+            sig2.Label = string.Empty;
+
+            System.Drawing.Bitmap bmp2 = plt.RenderLegend();
+            Assert.AreEqual(1, bmp2.Width);
+            Assert.AreEqual(1, bmp2.Height);
+            Assert.False(leg.HasItems);
+            Assert.AreEqual(0, leg.Count);
+            Assert.AreEqual(0, leg.GetItems().Length);
         }
     }
 }
