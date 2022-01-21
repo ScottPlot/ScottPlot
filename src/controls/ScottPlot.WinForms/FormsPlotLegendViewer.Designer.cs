02e9488e259c8f7a8c56b4e03a6880161c88f986
Nicolas Guérin <nicolas.guerin@onera.fr>
2022-01-21 18:17:30 +0100

    First attempt to create detacheable Legend in WinForms

---
+++ src/controls/ScottPlot.WinForms/FormsPlotLegendViewer.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace ScottPlot
+{
+    partial class FormsPlotLegendViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxLegend = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLegend)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.pictureBoxLegend);
+            this.panel1.Location = new System.Drawing.Point(1, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(190, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBoxLegend
+            // 
+            this.pictureBoxLegend.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxLegend.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxLegend.Name = "pictureBoxLegend";
+            this.pictureBoxLegend.Size = new System.Drawing.Size(190, 450);
+            this.pictureBoxLegend.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxLegend.TabIndex = 2;
+            this.pictureBoxLegend.TabStop = false;
+            // 
+            // FormsPlotLegendViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(321, 576);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormsPlotLegendViewer";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormsPlotLegendViewer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLegend)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxLegend;
+    }
+}
---
+++ src/controls/ScottPlot.WinForms/FormsPlotLegendViewer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlot
+{
+    public partial class FormsPlotLegendViewer : Form
+    {
+        public FormsPlotLegendViewer(ScottPlot.Plot plot, string windowTitle = "Detached Legend")
+        {
+            InitializeComponent();
+            var legend = plot.Legend(false);
+            var li = legend.GetItems();
+            if (legend.HasItems)
+            {
+                this.Show();
+                legend.OutlineColor = Color.White;
+                legend.ShadowColor = Color.White;
+                pictureBoxLegend.Image = legend.GetBitmap();
+                var width = pictureBoxLegend.Image.Width + 2*SystemInformation.HorizontalScrollBarHeight;
+                var height = pictureBoxLegend.Image.Height + 2*SystemInformation.CaptionHeight;
+                MinimumSize = new(width, height);
+                MaximumSize = new(width, height);
+            }
+            else
+            {
+                //this.Close();
+                MessageBox.Show("Current legend has no items", "Detached Legend", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
