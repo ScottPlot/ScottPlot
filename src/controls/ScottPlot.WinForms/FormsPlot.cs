180b58dc642a9f46ffec15fc0b198734cd239cc9
BambOoxX <42067365+BambOoxX@users.noreply.github.com>
2022-01-24 21:36:30 +0100

    Corrected type to avoid `nullable` warnings

--- src/controls/ScottPlot.WinForms/FormsPlot.cs
+++ src/controls/ScottPlot.WinForms/FormsPlot.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows.Forms;
@@ -213,7 +213,7 @@ namespace ScottPlot
         private void RightClickMenu_Help_Click(object sender, EventArgs e) => new FormHelp().Show();
         private void RightClickMenu_AutoAxis_Click(object sender, EventArgs e) { Plot.AxisAuto(); Refresh(); }
         private void RightClickMenu_OpenInNewWindow_Click(object sender, EventArgs e) => new FormsPlotViewer(Plot).Show();
-        private void RightClickMenu_DetachLegend_Click(object? sender, EventArgs e) => new FormsPlotLegendViewer(this);
+        private void RightClickMenu_DetachLegend_Click(object sender, EventArgs e) => new FormsPlotLegendViewer(this);
         private void RightClickMenu_SaveImage_Click(object sender, EventArgs e)
         {
             var sfd = new SaveFileDialog
