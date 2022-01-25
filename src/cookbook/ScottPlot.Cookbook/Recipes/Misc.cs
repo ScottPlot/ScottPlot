cad21d865fde80b5e42611561db3e2b327c9f2d6
Scott W Harden <swharden@gmail.com>
2022-01-25 18:44:20 -0500

    Cookbook: spline interpolation

--- src/cookbook/ScottPlot.Cookbook/Recipes/Misc.cs
+++ src/cookbook/ScottPlot.Cookbook/Recipes/Misc.cs
@@ -31,6 +31,45 @@ namespace ScottPlot.Cookbook.Recipes
         }
     }
 
+    class MiscInterpolationTypes : IRecipe
+    {
+        public string Category => "Misc";
+        public string ID => "misc_interpolation_types";
+        public string Title => "Spline Interpolation Types";
+        public string Description =>
+            "Different methods are available for smoothing data using interpolation, curve fitting, or corner cutting. " +
+            "Notice that some methods produce curves which pass through the original data points and others do not.";
+
+        public void ExecuteRecipe(Plot plt)
+        {
+            plt.Palette = ScottPlot.Palette.ColorblindFriendly;
+
+            // generate random data
+            System.Random rand = new(12345);
+            var xs = ScottPlot.DataGen.RandomWalk(rand, 20);
+            var ys = ScottPlot.DataGen.RandomWalk(rand, 20);
+            plt.AddScatter(xs, ys, lineStyle: ScottPlot.LineStyle.Dash, markerSize: 10, label: "original");
+
+            // interpolate the same data in different ways
+            (double[] bzX, double[] bzY) = ScottPlot.Statistics.Interpolation.Bezier.InterpolateXY(xs, ys, .005);
+            (double[] crX, double[] crY) = ScottPlot.Statistics.Interpolation.CatmullRom.InterpolateXY(xs, ys, 15);
+            (double[] chX, double[] chY) = ScottPlot.Statistics.Interpolation.Chaikin.InterpolateXY(xs, ys, 4);
+            (double[] cbX, double[] cbY) = ScottPlot.Statistics.Interpolation.Cubic.InterpolateXY(xs, ys, 200);
+
+            // display the different curves as line plots
+            plt.AddScatterLines(bzX, bzY, lineWidth: 2, label: $"Bezier");
+            plt.AddScatterLines(crX, crY, lineWidth: 2, label: $"Catmull-Rom");
+            plt.AddScatterLines(chX, chY, lineWidth: 2, label: $"Chaikin");
+            plt.AddScatterLines(cbX, cbY, lineWidth: 2, label: $"Cubic");
+
+            // style the plot
+            plt.Legend();
+            plt.Frameless();
+            plt.Grid(false);
+            plt.XAxis2.Label("Spline Interpolation", size: 28, bold: true);
+        }
+    }
+
     class ActionPotential : IRecipe
     {
         public string Category => "Misc";
--- src/sandbox/ConsoleApp/Program.cs
+++ src/sandbox/ConsoleApp/Program.cs
@@ -1,18 +1,27 @@
-﻿System.Random rand = new();
+﻿var plt = new ScottPlot.Plot(600, 400);
+plt.Palette = ScottPlot.Palette.ColorblindFriendly;
+
+// generate random data
+System.Random rand = new(12345);
 var xs = ScottPlot.DataGen.RandomWalk(rand, 20);
-//xs = ScottPlot.DataGen.Consecutive(20);
 var ys = ScottPlot.DataGen.RandomWalk(rand, 20);
+plt.AddScatter(xs, ys, lineStyle: ScottPlot.LineStyle.Dash, markerSize: 10, label: "original");
 
-var plt = new ScottPlot.Plot();
-plt.AddScatter(xs, ys, markerSize: 10);
+// interpolate the same data in different ways
+(double[] bzX, double[] bzY) = ScottPlot.Statistics.Interpolation.Bezier.InterpolateXY(xs, ys, .005);
+(double[] crX, double[] crY) = ScottPlot.Statistics.Interpolation.CatmullRom.InterpolateXY(xs, ys, 15);
+(double[] chX, double[] chY) = ScottPlot.Statistics.Interpolation.Chaikin.InterpolateXY(xs, ys, 4);
+(double[] cbX, double[] cbY) = ScottPlot.Statistics.Interpolation.Cubic.InterpolateXY(xs, ys, 200);
 
-//var cat = ScottPlot.Statistics.Interpolation.CatmullRom.InterpolateXY(xs, ys, 5);
-//plt.AddScatter(cat.xs, cat.ys);
+// display the different curves as line plots
+plt.AddScatterLines(bzX, bzY, lineWidth: 2, label: $"Bezier");
+plt.AddScatterLines(crX, crY, lineWidth: 2, label: $"Catmull-Rom");
+plt.AddScatterLines(chX, chY, lineWidth: 2, label: $"Chaikin");
+plt.AddScatterLines(cbX, cbY, lineWidth: 2, label: $"Cubic");
 
-//var cha = ScottPlot.Statistics.Interpolation.Chaikin.InterpolateXY(xs, ys, 2);
-//plt.AddScatter(cha.xs, cha.ys);
-
-var bez = ScottPlot.Statistics.Interpolation.Bezier.InterpolateXY(xs, ys, .005);
-plt.AddScatter(bez.xs, bez.ys);
-
-plt.SaveFig("demo.png");
+// style the plot
+plt.Legend();
+plt.Frameless();
+plt.Grid(false);
+plt.XAxis2.Label("Spline Interpolation", size: 28, bold: true);
+System.Console.WriteLine(plt.SaveFig("demo.png"));
