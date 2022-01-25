a67a3809d69f56356a39ee5deb71409d6b1217e2
Scott W Harden <swharden@gmail.com>
2022-01-24 22:20:52 -0500

    Interpolation:  Catmull-Rom curve

--- src/sandbox/ConsoleApp/Program.cs
+++ src/sandbox/ConsoleApp/Program.cs
@@ -3,7 +3,11 @@ var xs = ScottPlot.DataGen.RandomWalk(rand, 20);
 var ys = ScottPlot.DataGen.RandomWalk(rand, 20);
 
 var plt = new ScottPlot.Plot();
-plt.AddScatter(xs, ys);
+plt.AddScatter(xs, ys, markerSize: 10);
+
+var cat = ScottPlot.Statistics.Interpolation.CatmullRom.InterpolateXY(xs, ys, 5);
+plt.AddScatter(cat.xs, cat.ys);
+
 var cha = ScottPlot.Statistics.Interpolation.Chaikin.InterpolateXY(xs, ys, 2);
 plt.AddScatter(cha.xs, cha.ys);
 
