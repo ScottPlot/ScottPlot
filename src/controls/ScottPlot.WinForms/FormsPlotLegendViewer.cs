d7575098eb71ef1016bd0a9442612d84c4544c46
Scott W Harden <swharden@gmail.com>
2022-01-25 22:22:13 -0500

    FormsPlot LegendViewer: refactor

--- src/controls/ScottPlot.WinForms/FormsPlotLegendViewer.cs
+++ src/controls/ScottPlot.WinForms/FormsPlotLegendViewer.cs
@@ -12,50 +12,66 @@ namespace ScottPlot
 {
     public partial class FormsPlotLegendViewer : Form
     {
-        public FormsPlotLegendViewer(FormsPlot Fplot, string windowTitle = "Detached Legend")
+        private readonly FormsPlot FormsPlot;
+        private readonly Renderable.Legend Legend;
+
+        public FormsPlotLegendViewer(FormsPlot fPlot, string windowTitle = "Detached Legend")
         {
+            FormsPlot = fPlot;
+            Legend = fPlot.Plot.Legend(null);
+
             InitializeComponent();
-            Legend = Fplot.Plot.Legend(false);
-            var plottable_array = Fplot.Plot.GetPlottables();
-            Legend.UpdateLegendItems(plottable_array, true);
-            Fplot.Refresh();
+            PictureBoxLegend.Click += PictureBoxLegend_Click;
+
+            UpdateLegendImage();
             if (Legend.HasItems)
             {
-                Legend.OutlineColor = Color.White;
-                Legend.ShadowColor = Color.White;
-                PictureBoxLegend.Image = Legend.GetBitmap(false);
-                PictureBoxLegend.Click += delegate (object sender, EventArgs e) { PictureBoxLegend_Click(sender, (MouseEventArgs)e, Fplot); };
-                var frmmaxwidth = PictureBoxLegend.Image.Width + 2 * SystemInformation.VerticalScrollBarWidth;
-                var frmminwidth = frmmaxwidth;
-                var frmmaxheight = PictureBoxLegend.Image.Height + 3 * SystemInformation.HorizontalScrollBarHeight;
-                var frmminheight = frmmaxheight > 500 ? 500 : frmmaxheight;
-                MinimumSize = new(frmminwidth, frmminheight);
-                MaximumSize = new(frmmaxwidth, frmmaxheight);
-                Size = new(frmminwidth, frmminheight);
-                this.Show();
+                SetSizeBasedOnPictureboxImage();
+                Show();
             }
             else
             {
-                //this.Close();
                 MessageBox.Show("Current legend has no items", "Detached Legend", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Close();
             }
         }
 
-        private void PictureBoxLegend_Click(object sender, MouseEventArgs e, FormsPlot Fplot)
+        private void UpdateLegendImage()
         {
-            var plottable_array = Fplot.Plot.GetPlottables();
-            Legend.UpdateLegendItems(plottable_array, true);
-            var li = Legend.GetItems();
-            double totheight = PictureBoxLegend.Image.Height;
-            double singleheight = (totheight / ((double)li.Length));
-            double proxindex = Math.Floor(e.Y / singleheight);
-
-            plottable_array[(int)proxindex].IsVisible = !plottable_array[(int)proxindex].IsVisible;
-            Fplot.Refresh();
-            Legend.UpdateLegendItems(plottable_array, true);
+            var allPlottables = FormsPlot.Plot.GetPlottables();
+            Legend.UpdateLegendItems(allPlottables, true);
+            FormsPlot.Refresh();
             PictureBoxLegend.Image = Legend.GetBitmap(false);
         }
 
-        private Renderable.Legend Legend;
+        private void SetSizeBasedOnPictureboxImage()
+        {
+            var widthMax = PictureBoxLegend.Image.Width + 2 * SystemInformation.VerticalScrollBarWidth;
+            var heightMax = PictureBoxLegend.Image.Height + 3 * SystemInformation.HorizontalScrollBarHeight;
+            MaximumSize = new(widthMax, heightMax);
+            MinimumSize = new(widthMax, Math.Min(500, heightMax));
+            Size = MinimumSize;
+        }
+
+        private void PictureBoxLegend_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs e2 = (MouseEventArgs)e;
+
+            // TODO: move this logic inside the Legend class if possible
+            // public IPlottable Legend.GetPlottableUnderMouse(float xPixel, float yPixel) { }
+
+            // TODO: I don't think this logic is sound...
+            // some plottables have multiple items in their legend (e.g., coxcomb)
+
+            // TODO: maybe plottables with multiple legend items should not have this click-to-toggle-visibility feature
+
+            double legendItemHeight = (double)PictureBoxLegend.Image.Height / Legend.Count;
+            int clickedItemIndex = (int)Math.Floor(e2.Y / legendItemHeight);
+            var clickedPlottable = FormsPlot.Plot.GetPlottables()[clickedItemIndex];
+
+            clickedPlottable.IsVisible = !clickedPlottable.IsVisible;
+
+            UpdateLegendImage();
+        }
     }
 }
