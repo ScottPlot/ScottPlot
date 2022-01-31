61acb9b7d5e5d0f8efcfa549fc8e5d2d611c0eb5
Scott W Harden <swharden@gmail.com>
2022-01-30 22:40:11 -0500

    WinForms Demo: live finance data

--- src/demo/ScottPlot.Demo.WinForms/WinFormsDemos/FinanceDemo.Designer.cs
+++ src/demo/ScottPlot.Demo.WinForms/WinFormsDemos/FinanceDemo.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.formsPlot1 = new ScottPlot.FormsPlot();
             this.formsPlot2 = new ScottPlot.FormsPlot();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // formsPlot1
@@ -40,30 +43,51 @@
             this.formsPlot1.Location = new System.Drawing.Point(13, 12);
             this.formsPlot1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(749, 264);
+            this.formsPlot1.Size = new System.Drawing.Size(749, 325);
             this.formsPlot1.TabIndex = 0;
             // 
             // formsPlot2
             // 
             this.formsPlot2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.formsPlot2.Location = new System.Drawing.Point(13, 282);
+            this.formsPlot2.Location = new System.Drawing.Point(13, 343);
             this.formsPlot2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.formsPlot2.Name = "formsPlot2";
-            this.formsPlot2.Size = new System.Drawing.Size(749, 128);
+            this.formsPlot2.Size = new System.Drawing.Size(749, 193);
             this.formsPlot2.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(679, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(47, 19);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Live";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // FinanceDemo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 422);
+            this.ClientSize = new System.Drawing.Size(775, 548);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.formsPlot2);
             this.Controls.Add(this.formsPlot1);
             this.Name = "FinanceDemo";
             this.Text = "FinanceDemo";
             this.Load += new System.EventHandler(this.FinanceDemo_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +95,7 @@
 
         private FormsPlot formsPlot1;
         private FormsPlot formsPlot2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- src/demo/ScottPlot.Demo.WinForms/WinFormsDemos/FinanceDemo.cs
+++ src/demo/ScottPlot.Demo.WinForms/WinFormsDemos/FinanceDemo.cs
@@ -12,33 +12,69 @@ namespace ScottPlot.Demo.WinForms.WinFormsDemos
 {
     public partial class FinanceDemo : Form
     {
+        readonly ScottPlot.Plottable.FinancePlot CandlePlot;
+        readonly ScottPlot.Plottable.BarPlot BarPlot;
+        readonly Random Rand = new Random(0);
+
         public FinanceDemo()
         {
             InitializeComponent();
-        }
 
-        private void FinanceDemo_Load(object sender, EventArgs e)
-        {
-            Random rand = new Random(0);
-            OHLC[] prices = ScottPlot.DataGen.RandomStockPrices(rand, 100);
-            double[] dates = prices.Select(x => x.DateTime.ToOADate()).ToArray();
-            double[] volumes = ScottPlot.DataGen.Random(rand, prices.Length, multiplier: 10, offset: 20);
+            CandlePlot = new Plottable.FinancePlot() { Candle = true };
 
-            formsPlot1.Plot.AddCandlesticks(prices);
+            formsPlot1.Plot.Add(CandlePlot);
             formsPlot1.Plot.YLabel("Price");
             formsPlot1.Plot.XAxis.DateTimeFormat(true);
             formsPlot1.Plot.XAxis.Ticks(false);
             formsPlot1.Plot.XAxis.SetSizeLimit(max: 0);
             formsPlot1.AxesChanged += FormsPlot1_AxesChanged;
-            formsPlot1.Refresh();
 
-            formsPlot2.Plot.AddBar(volumes, dates);
+            double[] xs = { 1, 2, 3 };
+            double[] ys = { 1, 2, 3 };
+            BarPlot = new Plottable.BarPlot(xs, ys, null, null);
+
+            formsPlot2.Plot.Add(BarPlot);
             formsPlot2.Plot.YLabel("Volume");
             formsPlot2.Plot.XAxis.DateTimeFormat(true);
             formsPlot2.Plot.XAxis2.SetSizeLimit(max: 0);
-            formsPlot2.Plot.AxisAuto();
-            formsPlot2.Plot.SetAxisLimits(yMin: 0);
-            formsPlot2.Refresh();
+        }
+
+        private void FinanceDemo_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+                AddNewDataPoint(false);
+
+            AddNewDataPoint(true);
+        }
+
+        private void AddNewDataPoint(bool refresh = true)
+        {
+            double open = Rand.NextDouble() * 50 + 100;
+            double close = open + (Rand.NextDouble() - .5) * 10;
+            double low = Math.Min(open, close) - Rand.NextDouble() * 5;
+            double high = Math.Max(open, close) + Rand.NextDouble() * 5;
+            double volume = Rand.NextDouble() * 500 + 100;
+
+            TimeSpan span = TimeSpan.FromDays(1);
+            DateTime date = CandlePlot.OHLCs.Any() ? CandlePlot.OHLCs.Last().DateTime + span : new DateTime(1985, 09, 24);
+
+            OHLC ohlc = new OHLC(open, high, low, close, date, span, volume);
+            CandlePlot.Add(ohlc);
+
+            if (refresh)
+            {
+                formsPlot1.Plot.AxisAuto();
+
+                BarPlot.Replace(
+                    positions: CandlePlot.OHLCs.Select(x => x.DateTime.ToOADate()).ToArray(),
+                    values: CandlePlot.OHLCs.Select(x => x.Volume).ToArray());
+
+                formsPlot2.Plot.AxisAuto();
+                formsPlot2.Plot.SetAxisLimits(yMin: 0);
+
+                formsPlot1.Refresh();
+                formsPlot2.Refresh();
+            }
         }
 
         private void FormsPlot1_AxesChanged(object sender, EventArgs e)
@@ -46,6 +82,16 @@ namespace ScottPlot.Demo.WinForms.WinFormsDemos
             formsPlot2.Plot.MatchAxis(formsPlot1.Plot, horizontal: true, vertical: false);
             formsPlot2.Plot.MatchLayout(formsPlot1.Plot, horizontal: true, vertical: false);
             formsPlot2.Refresh();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = checkBox1.Checked;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            AddNewDataPoint();
         }
     }
 }
