f28ca0b79123b06241266d9df42dd52098e1355a
Scott W Harden <swharden@gmail.com>
2022-01-31 19:13:29 -0500

    AxisDimensions: fix GetCoordinateY()
    fixes #1625

--- src/ScottPlot/PlotDimensions.cs
+++ src/ScottPlot/PlotDimensions.cs
@@ -41,7 +41,7 @@ namespace ScottPlot
 
         public Coordinate GetCoordinate(Pixel pixel) => new Coordinate(GetCoordinateX(pixel.X), GetCoordinateY(pixel.Y));
         public double GetCoordinateX(float pixel) => (pixel - DataOffsetX) / PxPerUnitX + XMin;
-        public double GetCoordinateY(float pixel) => DataHeight - ((pixel - YMin) * PxPerUnitY);
+        public double GetCoordinateY(float pixel) => YMax - (pixel - DataOffsetY) / PxPerUnitY;
 
         public PlotDimensions(SizeF figureSize, SizeF dataSize, PointF dataOffset, AxisLimits axisLimits, double scaleFactor)
         {
