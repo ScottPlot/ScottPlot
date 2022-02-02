1594d583e3c6896419a8aa9e30ec295074f41643
Scott W Harden <swharden@gmail.com>
2022-02-01 19:43:47 -0500

    Text: discrete math for rectangle hit detection

--- src/ScottPlot/Plottable/Text.cs
+++ src/ScottPlot/Plottable/Text.cs
@@ -33,7 +33,7 @@ namespace ScottPlot.Plottable
         public Color BorderColor { get; set; } = Color.Black;
         public float PixelOffsetX { get; set; } = 0;
         public float PixelOffsetY { get; set; } = 0;
-        RectangleF RectangleUnits;
+        RectangleF LastRenderRectangleCoordinates;
         private double DeltaCX = 0;
         private double DeltaCY = 0;
 
@@ -96,7 +96,7 @@ namespace ScottPlot.Plottable
                 PointF pointA = new(xA, yA);
                 PointF pointC = new(xC, yC);
 
-                RectangleUnits = RectangleF.FromLTRB(
+                LastRenderRectangleCoordinates = RectangleF.FromLTRB(
                     left: (float)dims.GetCoordinateX(pointA.X),
                     top: (float)dims.GetCoordinateY(pointC.Y),
                     right: (float)dims.GetCoordinateX(pointC.X),
@@ -169,8 +169,9 @@ namespace ScottPlot.Plottable
         /// <returns></returns>
         public bool IsUnderMouse(double coordinateX, double coordinateY, double snapX, double snapY)
         {
-            bool test = RectangleUnits.Contains((float)coordinateX, (float)coordinateY);
-            if (test)
+            PointF pt = new((float)coordinateX, (float)coordinateY);
+
+            if (IsPointInsideRectangle(pt, LastRenderRectangleCoordinates))
             {
                 DeltaCX = X - coordinateX;
                 DeltaCY = Y - coordinateY;
@@ -180,6 +181,44 @@ namespace ScottPlot.Plottable
             {
                 return false;
             }
+        }
+
+        private static bool IsPointInsideRectangle(PointF pt, RectangleF rect)
+        {
+            // https://swharden.com/blog/2022-02-01-point-in-rectangle/
+
+            double x1 = rect.Left;
+            double x2 = rect.Right;
+            double x3 = rect.Right;
+            double x4 = rect.Left;
+
+            double y1 = rect.Top;
+            double y2 = rect.Top;
+            double y3 = rect.Bottom;
+            double y4 = rect.Bottom;
+
+            double a1 = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            double a2 = Math.Sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+            double a3 = Math.Sqrt((x3 - x4) * (x3 - x4) + (y3 - y4) * (y3 - y4));
+            double a4 = Math.Sqrt((x4 - x1) * (x4 - x1) + (y4 - y1) * (y4 - y1));
+
+            double b1 = Math.Sqrt((x1 - pt.X) * (x1 - pt.X) + (y1 - pt.Y) * (y1 - pt.Y));
+            double b2 = Math.Sqrt((x2 - pt.X) * (x2 - pt.X) + (y2 - pt.Y) * (y2 - pt.Y));
+            double b3 = Math.Sqrt((x3 - pt.X) * (x3 - pt.X) + (y3 - pt.Y) * (y3 - pt.Y));
+            double b4 = Math.Sqrt((x4 - pt.X) * (x4 - pt.X) + (y4 - pt.Y) * (y4 - pt.Y));
+
+            double u1 = (a1 + b1 + b2) / 2;
+            double u2 = (a2 + b2 + b3) / 2;
+            double u3 = (a3 + b3 + b4) / 2;
+            double u4 = (a4 + b4 + b1) / 2;
+
+            double A1 = Math.Sqrt(u1 * (u1 - a1) * (u1 - b1) * (u1 - b2));
+            double A2 = Math.Sqrt(u2 * (u2 - a2) * (u2 - b2) * (u2 - b3));
+            double A3 = Math.Sqrt(u3 * (u3 - a3) * (u3 - b3) * (u3 - b4));
+            double A4 = Math.Sqrt(u4 * (u4 - a4) * (u4 - b4) * (u4 - b1));
+
+            double difference = A1 + A2 + A3 + A4 - a1 * a2;
+            return difference < .001 * (rect.Height + rect.Width);
         }
     }
 }
