2c4f7962e5bb93bfcf3c7ed97caa4860c7cfc37b
BambOoxX <42067365+BambOoxX@users.noreply.github.com>
2022-02-13 13:01:39 +0100

    Implement `IIsHighlightable` interface

---
+++ src/ScottPlot/Plottable/IIsHighlightable.cs
@@ -0,0 +1,8 @@
+﻿namespace ScottPlot.Plottable
+{
+    public interface IIsHighlightable
+    {
+        bool IsHighlighted { get; set; }
+        double HighlightCoefficient { get; set; }
+    }
+}
