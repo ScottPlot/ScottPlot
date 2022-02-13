63fabd088a4369e8b5c8b26a2e29bdc19d2ce03a
BambOoxX <42067365+BambOoxX@users.noreply.github.com>
2022-02-13 13:01:18 +0100

    Implement `IHasMarker` interface

---
+++ src/ScottPlot/Plottable/IHasMarker.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace ScottPlot.Plottable
+{
+    public interface IHasMarker
+    {
+        float MarkerSize { get; set; }
+        MarkerShape MarkerShape { get; set; }
+    }
+}
