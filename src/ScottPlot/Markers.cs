520e4a673eb861d2fd3d18cf2a27c71d50633d8b
Scott W Harden <swharden@gmail.com>
2022-02-15 23:21:18 -0500

    Marker: remove unused usings

--- src/ScottPlot/Markers.cs
+++ src/ScottPlot/Markers.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
 
 namespace ScottPlot
 {
