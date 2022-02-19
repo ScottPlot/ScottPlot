6cd461bc490ce6922e3dc7b36f44a86d17ccac56
Scott W Harden <swharden@gmail.com>
2022-02-19 11:27:18 -0500

    MarkerShapes: Eks

---
+++ src/ScottPlot/MarkerShapes/Eks.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace ScottPlot.MarkerShapes;
+
+public class Eks : IMarker
+{
+    public void Draw(Graphics gfx, PointF center, float size, Brush brush, Pen pen)
+    {
+        gfx.DrawLine(pen, center.X - size, center.Y - size, center.X + size, center.Y + size);
+        gfx.DrawLine(pen, center.X - size, center.Y + size, center.X + size, center.Y - size);
+    }
+}
