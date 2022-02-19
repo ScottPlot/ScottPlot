36f6f7e17131d04831c3c15957d67301f5f7f0a3
Scott W Harden <swharden@gmail.com>
2022-02-18 23:50:13 -0500

    Markers: refactor DrawMarker()

--- src/ScottPlot/Markers.cs
+++ src/ScottPlot/Markers.cs
@@ -31,10 +31,6 @@ namespace ScottPlot
             if (size == 0 || shape == MarkerShape.none)
                 return;
 
-            Pen pen = new Pen(color);
-
-            Brush brush = new SolidBrush(color);
-
             // Improve marker vs. line alignment on Linux and MacOS
             // https://github.com/ScottPlot/ScottPlot/issues/340
             // https://github.com/ScottPlot/ScottPlot/pull/1660
@@ -43,116 +39,49 @@ namespace ScottPlot
                 pixelLocation = new PointF(pixelLocation.X + .5f, pixelLocation.Y);
             }
 
-            PointF corner1 = new PointF(pixelLocation.X - size / 2, pixelLocation.Y - size / 2);
-            PointF corner2 = new PointF(pixelLocation.X + size / 2, pixelLocation.Y + size / 2);
-            SizeF bounds = new SizeF(size, size);
-            RectangleF rect = new RectangleF(corner1, bounds);
+            RectangleF rect = new(x: pixelLocation.X - size / 2, y: pixelLocation.Y - size / 2, width: size, height: size);
 
+            // NOTE: refactored in Feb 2022 to make it easier to convert this to individual marker classes soon
             switch (shape)
             {
                 case MarkerShape.filledCircle:
-                    gfx.FillEllipse(brush, rect);
+                    DrawFilledCircle(gfx, rect, color);
                     break;
                 case MarkerShape.openCircle:
-                    gfx.DrawEllipse(pen, rect);
+                    DrawOpenCircle(gfx, rect, color);
                     break;
                 case MarkerShape.filledSquare:
-                    gfx.FillRectangle(brush, rect);
+                    DrawFilledSquare(gfx, rect, color);
                     break;
                 case MarkerShape.openSquare:
-                    gfx.DrawRectangle(pen, corner1.X, corner1.Y, size, size);
+                    DrawOpenSquare(gfx, rect, color);
                     break;
                 case MarkerShape.filledDiamond:
-
-                    // Create points that define polygon.
-                    PointF point1 = new PointF(pixelLocation.X, pixelLocation.Y + size / 2);
-                    PointF point2 = new PointF(pixelLocation.X - size / 2, pixelLocation.Y);
-                    PointF point3 = new PointF(pixelLocation.X, pixelLocation.Y - size / 2);
-                    PointF point4 = new PointF(pixelLocation.X + size / 2, pixelLocation.Y);
-
-                    PointF[] curvePoints = { point1, point2, point3, point4 };
-
-                    //Draw polygon to screen
-                    gfx.FillPolygon(brush, curvePoints);
+                    DrawFilledDiamond(gfx, rect, color);
                     break;
                 case MarkerShape.openDiamond:
-
-                    // Create points that define polygon.
-                    PointF point5 = new PointF(pixelLocation.X, pixelLocation.Y + size / 2);
-                    PointF point6 = new PointF(pixelLocation.X - size / 2, pixelLocation.Y);
-                    PointF point7 = new PointF(pixelLocation.X, pixelLocation.Y - size / 2);
-                    PointF point8 = new PointF(pixelLocation.X + size / 2, pixelLocation.Y);
-
-                    PointF[] curvePoints2 = { point5, point6, point7, point8 };
-
-                    //Draw polygon to screen
-                    gfx.DrawPolygon(pen, curvePoints2);
-
+                    DrawOpenDiamond(gfx, rect, color);
                     break;
                 case MarkerShape.asterisk:
-                    Font drawFont = new Font("CourierNew", size * 3);
-                    SizeF textSize = Drawing.GDI.MeasureString(gfx, "*", drawFont);
-                    PointF asteriskPoint = new PointF(pixelLocation.X - textSize.Width / 2, pixelLocation.Y - textSize.Height / 4);
-                    gfx.DrawString("*", drawFont, brush, asteriskPoint);
-
+                    DrawString(gfx, rect, color, "*");
                     break;
                 case MarkerShape.hashTag:
-                    Font drawFont2 = new Font("CourierNew", size * 2);
-                    SizeF textSize2 = Drawing.GDI.MeasureString(gfx, "#", drawFont2);
-                    PointF asteriskPoint2 = new PointF(pixelLocation.X - textSize2.Width / 2, pixelLocation.Y - textSize2.Height / 3);
-                    gfx.DrawString("#", drawFont2, brush, asteriskPoint2);
-
+                    DrawString(gfx, rect, color, "#");
                     break;
                 case MarkerShape.cross:
-                    Font drawFont3 = new Font("CourierNew", size * 2);
-                    SizeF textSize3 = Drawing.GDI.MeasureString(gfx, "+", drawFont3);
-                    PointF asteriskPoint3 = new PointF(pixelLocation.X - textSize3.Width / (5 / 2), pixelLocation.Y - textSize3.Height / 2);
-                    gfx.DrawString("+", drawFont3, brush, asteriskPoint3);
-
+                    DrawString(gfx, rect, color, "+");
                     break;
                 case MarkerShape.eks:
-                    Font drawFont4 = new Font("CourierNew", size * 2);
-                    SizeF textSize4 = Drawing.GDI.MeasureString(gfx, "x", drawFont4);
-                    PointF asteriskPoint4 = new PointF(pixelLocation.X - textSize4.Width / (5 / 2), pixelLocation.Y - textSize4.Height / 2);
-                    gfx.DrawString("x", drawFont4, brush, asteriskPoint4);
-
+                    DrawString(gfx, rect, color, "x");
                     break;
                 case MarkerShape.verticalBar:
-                    Font drawFont5 = new Font("CourierNew", size * 2);
-                    SizeF textSize5 = Drawing.GDI.MeasureString(gfx, "|", drawFont5);
-                    PointF asteriskPoint5 = new PointF(pixelLocation.X - textSize5.Width / (5 / 2), pixelLocation.Y - textSize5.Height / 2);
-                    gfx.DrawString("|", drawFont5, brush, asteriskPoint5);
-
+                    DrawString(gfx, rect, color, "|");
                     break;
                 case MarkerShape.triUp:
-                    // Create points that define polygon.
-                    PointF point9 = new PointF(pixelLocation.X, pixelLocation.Y - size);
-                    PointF point10 = new PointF(pixelLocation.X, pixelLocation.Y);
-                    PointF point11 = new PointF(pixelLocation.X - size * (float)0.866, pixelLocation.Y + size * (float)0.5);
-                    PointF point12 = new PointF(pixelLocation.X, pixelLocation.Y);
-                    PointF point13 = new PointF(pixelLocation.X + size * (float)0.866, (pixelLocation.Y + size * (float)0.5));
-
-
-                    PointF[] curvePoints3 = { point12, point9, point10, point11, point12, point13 };
-
-                    //Draw polygon to screen
-                    gfx.DrawPolygon(pen, curvePoints3);
-
+                    DrawTriStarUp(gfx, rect, color);
                     break;
                 case MarkerShape.triDown:
-                    // Create points that define polygon.
-                    PointF point14 = new PointF(pixelLocation.X, pixelLocation.Y + size);
-                    PointF point15 = new PointF(pixelLocation.X, pixelLocation.Y);
-                    PointF point16 = new PointF(pixelLocation.X - size * (float)0.866, pixelLocation.Y - size * (float)0.5);
-                    PointF point17 = new PointF(pixelLocation.X, pixelLocation.Y);
-                    PointF point18 = new PointF(pixelLocation.X + size * (float)0.866, (pixelLocation.Y - size * (float)0.5));
-
-
-                    PointF[] curvePoints4 = { point17, point14, point15, point16, point17, point18 };
-
-                    //Draw polygon to screen
-                    gfx.DrawPolygon(pen, curvePoints4);
-
+                    DrawTriStarDown(gfx, rect, color);
                     break;
                 case MarkerShape.none:
                     break;
@@ -167,6 +96,97 @@ namespace ScottPlot
             {
                 DrawMarker(gfx, pt, shape, size, color);
             }
+        }
+
+        private static void DrawFilledCircle(Graphics gfx, RectangleF rect, Color color)
+        {
+            using Brush brush = new SolidBrush(color);
+            gfx.FillEllipse(brush, rect);
+        }
+
+        private static void DrawOpenCircle(Graphics gfx, RectangleF rect, Color color)
+        {
+            using Pen pen = new(color);
+            gfx.DrawEllipse(pen, rect);
+        }
+
+        private static void DrawFilledSquare(Graphics gfx, RectangleF rect, Color color)
+        {
+            using Brush brush = new SolidBrush(color);
+            gfx.FillRectangle(brush, rect);
+        }
+
+        private static void DrawOpenSquare(Graphics gfx, RectangleF rect, Color color)
+        {
+            using Pen pen = new(color);
+            gfx.DrawRectangle(pen, rect.Left, rect.Top, rect.Width, rect.Height);
+        }
+
+        private static PointF[] DiamondPoints(RectangleF rect)
+        {
+            float centerX = rect.Left + rect.Width / 2;
+            float centerY = rect.Top + rect.Height / 2;
+
+            PointF[] points =
+            {
+                new PointF(rect.Left, centerY),
+                new PointF(centerX, rect.Top),
+                new PointF(rect.Right, centerY),
+                new PointF(centerX, rect.Bottom),
+            };
+
+            return points;
+        }
+
+        private static void DrawFilledDiamond(Graphics gfx, RectangleF rect, Color color)
+        {
+            PointF[] points = DiamondPoints(rect);
+            using Brush brush = new SolidBrush(color);
+            gfx.FillPolygon(brush, points);
+        }
+
+        private static void DrawOpenDiamond(Graphics gfx, RectangleF rect, Color color)
+        {
+            PointF[] points = DiamondPoints(rect);
+            using Pen pen = new(color);
+            gfx.DrawPolygon(pen, points);
+        }
+
+        private static void DrawString(Graphics gfx, RectangleF rect, Color color, string str)
+        {
+            float centerX = rect.Left + rect.Width / 2;
+            float centerY = rect.Top + rect.Height / 2;
+
+            string fontName = Drawing.InstalledFont.Monospace();
+            Font font = new(fontName, rect.Width * 2);
+
+            using Brush brush = new SolidBrush(color);
+            var sf = Drawing.GDI.StringFormat(HorizontalAlignment.Center, VerticalAlignment.Middle);
+            gfx.DrawString(str, font, brush, centerX, centerY, sf);
+        }
+
+        private static void DrawTriStarUp(Graphics gfx, RectangleF rect, Color color)
+        {
+            float centerX = rect.Left + rect.Width / 2;
+            float centerY = rect.Top + rect.Height / 2;
+            float size = rect.Width;
+
+            using Pen pen = new(color);
+            gfx.DrawLine(pen, centerX, centerY, centerX, centerY - size);
+            gfx.DrawLine(pen, centerX, centerY, centerX - size * (float)0.866, centerY + size * (float).5);
+            gfx.DrawLine(pen, centerX, centerY, centerX + size * (float)0.866, centerY + size * (float).5);
+        }
+
+        private static void DrawTriStarDown(Graphics gfx, RectangleF rect, Color color)
+        {
+            float centerX = rect.Left + rect.Width / 2;
+            float centerY = rect.Top + rect.Height / 2;
+            float size = rect.Width;
+
+            using Pen pen = new(color);
+            gfx.DrawLine(pen, centerX, centerY, centerX, centerY + size);
+            gfx.DrawLine(pen, centerX, centerY, centerX - size * (float)0.866, centerY - size * (float).5);
+            gfx.DrawLine(pen, centerX, centerY, centerX + size * (float)0.866, centerY - size * (float).5);
         }
     }
 }
