a4c61f61591b3ceff1ddd4d21bca54427dad60c3
BambOoxX <42067365+BambOoxX@users.noreply.github.com>
2022-02-19 21:51:10 +0100

    Add `MarkerLineWidth` property

--- src/ScottPlot/Plottable/IHasMarker.cs
+++ src/ScottPlot/Plottable/IHasMarker.cs
@@ -5,6 +5,7 @@ namespace ScottPlot.Plottable
     public interface IHasMarker
     {
         float MarkerSize { get; set; }
+        float MarkerLineWidth { get; set; }
         MarkerShape MarkerShape { get; set; }
         Color MarkerColor { get; set; }
     }
