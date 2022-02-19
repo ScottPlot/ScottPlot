ca5ba3c1b0f4f0c9f626f2474de696f159d36a0a
Scott W Harden <swharden@gmail.com>
2022-02-19 11:27:23 -0500

    MarkerShapes: None

---
+++ src/ScottPlot/MarkerShapes/None.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace ScottPlot.MarkerShapes;
+
+public class None : IMarker
+{
+    public void Draw(Graphics gfx, PointF center, float size, Brush brush, Pen pen)
+    {
+    }
+}
