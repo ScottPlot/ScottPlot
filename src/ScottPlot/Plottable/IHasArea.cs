02a6ef68cbc31b21714f3a0303d8d438eff1f92a
BambOoxX <42067365+BambOoxX@users.noreply.github.com>
2022-02-20 11:58:25 +0100

    Create `IHasArea` interface

---
+++ src/ScottPlot/Plottable/IHasArea.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace ScottPlot.Plottable
+{
+    public interface IHasArea
+    {
+        Color BorderColor { get; set; }
+        float BorderLineWidth { get; set; }
+        LineStyle BorderLineStyle { get; set; }
+        Color HatchColor { get; set; }
+        Drawing.HatchStyle HatchStyle { get; set; }
+    }
+}
