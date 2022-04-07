1b40b9d326cc72d86430057b5aabc93f567c0975
Scott W Harden <swharden@gmail.com>
2022-04-07 08:38:32 -0400

    Sandbox: demonstrate multi-axis line label
    recreates issue described by #1766

--- src/ScottPlot4/ScottPlot.Sandbox/WinFormsApp/Form1.Designer.cs
+++ src/ScottPlot4/ScottPlot.Sandbox/WinFormsApp/Form1.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             this.formsPlot1 = new ScottPlot.FormsPlot();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // formsPlot1
@@ -40,65 +37,27 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.formsPlot1.BackColor = System.Drawing.Color.Transparent;
-            this.formsPlot1.Location = new System.Drawing.Point(0, 37);
+            this.formsPlot1.Location = new System.Drawing.Point(0, 12);
             this.formsPlot1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(560, 312);
+            this.formsPlot1.Size = new System.Drawing.Size(808, 405);
             this.formsPlot1.TabIndex = 0;
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.DecimalPlaces = 2;
-            this.numericUpDown1.Increment = new decimal(new int[] {
-            5,
-            0,
-            0,
-            131072});
-            this.numericUpDown1.Location = new System.Drawing.Point(124, 7);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            2,
-            0,
-            0,
-            0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(64, 23);
-            this.numericUpDown1.TabIndex = 2;
-            this.numericUpDown1.Value = new decimal(new int[] {
-            5,
-            0,
-            0,
-            65536});
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 15);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Horizontal margin:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 349);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDown1);
+            this.ClientSize = new System.Drawing.Size(808, 417);
             this.Controls.Add(this.formsPlot1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormsPlot Sandbox";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private ScottPlot.FormsPlot formsPlot1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Label label1;
     }
 }
--- src/ScottPlot4/ScottPlot.Sandbox/WinFormsApp/Form1.cs
+++ src/ScottPlot4/ScottPlot.Sandbox/WinFormsApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScottPlot;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,19 +18,63 @@ namespace WinFormsApp
             InitializeComponent();
 
             Random rand = new(0);
-            double[] ys = ScottPlot.DataGen.Random(rand, 10);
-            double[] xs = ScottPlot.DataGen.Consecutive(ys.Length);
+            double[] data1 = DataGen.RandomWalk(rand, 100, mult: 1);
+            double[] data2 = DataGen.RandomWalk(rand, 100, mult: 10);
+            double[] data3 = DataGen.RandomWalk(rand, 100, mult: 100);
+            double avg1 = data1.Sum() / data1.Length;
+            double avg2 = data2.Sum() / data2.Length;
+            double avg3 = data3.Sum() / data3.Length;
 
-            var sig = formsPlot1.Plot.AddSignalXY(xs, ys);
-            sig.StepDisplay = true;
-            sig.FillAbove(Color.Blue);
+            // Add signals specifying the vertical axis index for each
+            var plt1 = formsPlot1.Plot.AddSignal(data1);
+            plt1.YAxisIndex = 0;
+            plt1.LineWidth = 3;
+            plt1.Color = Color.Magenta;
 
-            formsPlot1.Refresh();
-        }
+            var plt2 = formsPlot1.Plot.AddSignal(data2);
+            plt2.YAxisIndex = 3;
+            plt2.LineWidth = 3;
+            plt2.Color = Color.Green;
 
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
-            formsPlot1.Plot.AxisAuto(horizontalMargin: (double)numericUpDown1.Value);
+            var plt3 = formsPlot1.Plot.AddSignal(data3);
+            plt3.YAxisIndex = 2;
+            plt3.LineWidth = 3;
+            plt3.Color = Color.Navy;
+
+            // Add draggable horizontal lines specifying the vertical axis index for each
+            var hline1 = formsPlot1.Plot.AddHorizontalLine(avg1);
+            hline1.DragEnabled = true;
+            hline1.Color = plt1.Color;
+            hline1.LineStyle = LineStyle.Dash;
+            hline1.YAxisIndex = 0;
+            hline1.PositionLabelBackground = plt1.Color;
+            hline1.PositionFormatter = (double position) => position.ToString("N0");
+            hline1.PositionLabel = true;
+
+            var hline3 = formsPlot1.Plot.AddHorizontalLine(avg3);
+            hline3.DragEnabled = true;
+            hline3.Color = plt3.Color;
+            hline3.LineStyle = LineStyle.Dash;
+            hline3.YAxisIndex = 2;
+            hline3.PositionLabelBackground = plt3.Color;
+            hline3.PositionFormatter = (double position) => position.ToString("N0");
+            hline3.PositionLabel = true;
+
+            var hline2 = formsPlot1.Plot.AddHorizontalLine(avg2);
+            hline2.DragEnabled = true;
+            hline2.Color = plt2.Color;
+            hline2.LineStyle = LineStyle.Dash;
+            hline2.YAxisIndex = 3;
+            hline2.PositionLabelBackground = plt2.Color;
+            hline2.PositionFormatter = (double position) => position.ToString("N0");
+            hline2.PositionLabel = true;
+
+            // Create additional vertical axis and customize it
+            formsPlot1.Plot.YAxis.Color(Color.Magenta);
+            formsPlot1.Plot.AddAxis(ScottPlot.Renderable.Edge.Left, 2, "", Color.Navy);
+            formsPlot1.Plot.AddAxis(ScottPlot.Renderable.Edge.Left, 3, "", Color.Green);
+
+            formsPlot1.Plot.AxisAuto();
             formsPlot1.Refresh();
         }
     }
