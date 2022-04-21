fec3904f092bf0e6c6377338a6bf1c1c6e5cb7b7
MareMare <mare.fujii@gmail.com>
2022-04-22 04:54:34 +0900

    FormsPlot: Crosshair.GetLegendItems() returns null.

--- src/ScottPlot4/ScottPlot.WinForms/FormsPlot.cs
+++ src/ScottPlot4/ScottPlot.WinForms/FormsPlot.cs
@@ -209,6 +209,7 @@ namespace ScottPlot
         public void DefaultRightClickEvent(object sender, EventArgs e)
         {
             bool legendHasItems = Plot.GetPlottables()
+                .Where(x => x.GetLegendItems() != null)
                 .SelectMany(x => x.GetLegendItems())
                 .Where(x => !string.IsNullOrEmpty(x.label))
                 .Any();
