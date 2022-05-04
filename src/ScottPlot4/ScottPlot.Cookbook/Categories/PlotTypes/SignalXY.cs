296b0133585838c0059d704616287e5560ca7fbe
Scott W Harden <swharden@gmail.com>
2022-05-03 21:19:31 -0400

    autoformat

--- src/ScottPlot4/ScottPlot.Cookbook/Categories/PlotTypes/SignalXY.cs
+++ src/ScottPlot4/ScottPlot.Cookbook/Categories/PlotTypes/SignalXY.cs
@@ -6,6 +6,6 @@ public class SignalXY : ICategory
 
     public string Folder => "plottable-signalxy";
 
-    public string Description => "SignalXY is a speed-optimized plot for displaying Y vaues " + 
+    public string Description => "SignalXY is a speed-optimized plot for displaying Y vaues " +
         "with unevenly-spaced (but always increasing) X positions.";
 }
