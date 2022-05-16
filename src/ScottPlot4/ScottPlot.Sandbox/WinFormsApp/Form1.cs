bf57df4cdbfb1901b0d44238bbc9f7072c97ecac
Scott W Harden <swharden@gmail.com>
2022-05-16 10:35:39 -0400

    Sandbox: demonstrate LeftClickedPlottable
    #1844

--- src/ScottPlot4/ScottPlot.Sandbox/WinFormsApp/Form1.cs
+++ src/ScottPlot4/ScottPlot.Sandbox/WinFormsApp/Form1.cs
@@ -17,14 +17,28 @@ namespace WinFormsApp
         {
             InitializeComponent();
 
-            formsPlot1.Configuration.EnablePlotObjectEditor = true;
+            formsPlot1.LeftClickedPlottable += FormsPlot1_LeftClickedPlottable;
 
-            formsPlot1.Plot.AddScatter(DataGen.Consecutive(51), DataGen.Sin(51));
+            formsPlot1.Plot.AddSignal(DataGen.Sin(51));
             formsPlot1.Plot.AddSignal(DataGen.Cos(51));
-            formsPlot1.Plot.AddVerticalLine(25);
-            formsPlot1.Plot.AddText("test 123", 20, .5);
-            formsPlot1.Plot.AddMarker(15, -.5, MarkerShape.openCircle, 20);
+
+            var vl = formsPlot1.Plot.AddVerticalLine(25);
+            vl.DragEnabled = true;
+
+            formsPlot1.Plot.AddTooltip("clickable", 30, .6);
+
             formsPlot1.Refresh();
+        }
+
+        private void FormsPlot1_LeftClickedPlottable(object sender, EventArgs e)
+        {
+            if (sender is ScottPlot.Plottable.Tooltip tt)
+            {
+                Random rand = new();
+                Color randomColor = Color.FromArgb(255, rand.Next(256), rand.Next(256), rand.Next(256));
+                tt.Color = randomColor;
+                formsPlot1.Refresh();
+            }
         }
     }
 }
