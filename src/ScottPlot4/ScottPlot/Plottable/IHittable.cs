fa595644a230981e716040736629d6899a3a5961
Scott W Harden <swharden@gmail.com>
2022-05-16 10:29:13 -0400

    create IHittable
    #1844

---
+++ src/ScottPlot4/ScottPlot/Plottable/IHittable.cs
@@ -0,0 +1,17 @@
+﻿namespace ScottPlot.Plottable;
+
+/// <summary>
+/// Describes a Plottable that can report whether or not it is beneath the mouse cursor
+/// </summary>
+public interface IHittable
+{
+    /// <summary>
+    /// Cursor to display when the Plottable is under the mouse
+    /// </summary>
+    Cursor Cursor { get; set; }
+
+    /// <summary>
+    /// Returns true if the Plottable is at the given coordinate
+    /// </summary>
+    bool HitTest(Coordinate coord);
+}
