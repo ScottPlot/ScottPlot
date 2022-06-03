802012201e22703dc786532843bf1c94445aa04f
Scott W Harden <swharden@gmail.com>
2022-06-02 23:12:58 -0400

    Cookbook: extend Bracket recipes

--- src/ScottPlot4/ScottPlot.Cookbook/Recipes/Plottable/Bracket.cs
+++ src/ScottPlot4/ScottPlot.Cookbook/Recipes/Plottable/Bracket.cs
@@ -14,19 +14,14 @@ namespace ScottPlot.Cookbook.Recipes.Plottable
 
         public void ExecuteRecipe(Plot plt)
         {
-            Func<double, double?> xSquared = (double x) => x * x;
-            var function = plt.AddFunction(xSquared);
+            plt.AddSignal(DataGen.Sin(51));
+            plt.AddSignal(DataGen.Cos(51));
 
-            var x = 2;
-            var dx = 0.1;
+            plt.AddBracket(0, 1, 0, 0, "Bracket A");
+            plt.AddBracket(25, -1, 38, -1, "Bracket B");
+            plt.AddBracket(20, .55, 27, -.3, "Bracket C");
 
-            var bracket1 = plt.AddBracket(x, xSquared(x).Value, x + dx, xSquared(x).Value);
-            bracket1.Label = "dx";
-
-            var bracket2 = plt.AddBracket(x + dx, xSquared(x).Value, x + dx, xSquared(x + dx).Value);
-            bracket2.Label = "dy";
-
-            plt.SetAxisLimits(1, 3, 1, 6);
+            plt.Margins(.2, .2); // zoom out slightly to make room for labels
         }
     }
 
@@ -35,23 +30,56 @@ namespace ScottPlot.Cookbook.Recipes.Plottable
         public ICategory Category => new Categories.PlotTypes.Bracket();
         public string ID => "bracket_invert";
         public string Title => "Inverted Brackets";
-        public string Description => "Sometimes the orientation of a bracket should be flipped.";
+        public string Description => "By default bracket labels appear clockwise " +
+            "relative to the line formed by their two points. " +
+            "Users can invert the direction of brackets as needed.";
 
         public void ExecuteRecipe(Plot plt)
         {
-            double x1 = 0;
-            double y1 = 0;
-            double x2 = 1;
-            double y2 = 1;
+            plt.AddSignal(DataGen.Sin(51));
+            plt.AddSignal(DataGen.Cos(51));
 
-            var defaultBracket = plt.AddBracket(x1, y1, x2, y2);
-            defaultBracket.Label = "Default";
+            var bracketA = plt.AddBracket(50, 1, 50, 0, "Bracket A");
+            bracketA.LabelCounterClockwise = true;
 
-            var invertedBracket = plt.AddBracket(x1, y1, x2, y2);
-            invertedBracket.Label = "Inverted";
-            invertedBracket.LabelCounterClockwise = true;
+            var backetB = plt.AddBracket(13, 1, 50, 1, "Bracket B");
+            backetB.LabelCounterClockwise = true;
 
-            plt.SetAxisLimits(-2, 2, -2, 2);
+            var backetC = plt.AddBracket(20, .65, 27, -.20, "Bracket C");
+            backetC.LabelCounterClockwise = true;
+
+            plt.Margins(.2, .2); // zoom out slightly to make room for labels
+        }
+    }
+
+
+    public class BracketStyle : IRecipe
+    {
+        public ICategory Category => new Categories.PlotTypes.Bracket();
+        public string ID => "bracket_style";
+        public string Title => "Styling Brackets";
+        public string Description => "Brackets have additional options for customizations.";
+
+        public void ExecuteRecipe(Plot plt)
+        {
+            plt.AddBracket(-1, 0, 0, 1, "Default Style");
+
+            var bracketA = plt.AddBracket(0, 0, 1, 1, "Large Font");
+            bracketA.Font.Size = 24;
+
+            var bracketB = plt.AddBracket(1, 0, 2, 1, "Custom Color");
+            bracketB.Color = Color.Magenta;
+
+            var bracketC = plt.AddBracket(2, 0, 3, 1, "Longer Stem & Edges");
+            bracketC.EdgeLength = 20;
+
+            var bracketD = plt.AddBracket(3, 0, 4, 1, "Thicker Bracket");
+            bracketD.LineWidth = 3;
+            bracketD.Font.Bold = true;
+
+            plt.AddBracket(4, 0, 5, 1, label: null);
+
+            plt.Margins(.2, .2); // zoom out slightly to make room for labels
         }
     }
 }
