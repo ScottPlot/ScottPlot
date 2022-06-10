57a434a5fce087ccf810e25f67038b72ea48ef54
Scott W Harden <swharden@gmail.com>
2022-06-10 15:48:38 -0400

    IPlottable: update XML docs

--- src/ScottPlot5/ScottPlot/IPlottable.cs
+++ src/ScottPlot5/ScottPlot/IPlottable.cs
@@ -1,8 +1,20 @@
 ﻿namespace ScottPlot;
 
+/// <summary>
+/// Any object that renders shapes on the canvas using scale information from the axes must implement this interface.
+/// </summary>
 public interface IPlottable
 {
+    /// <summary>
+    /// Indicates whether or not this plottable will be drawn on the plot.
+    /// The calling method will check this variable (it does not need to be checked inside the Render method).
+    /// </summary>
     public bool IsVisible { get; set; }
 
+    /// <summary>
+    /// Draw the plotable on the given surface
+    /// </summary>
+    /// <param name="surface">Surface containing the Canvas to be drawn on</param>
+    /// <param name="dataRect">Size (in pixels) of the data area</param>
     public void Render(SkiaSharp.SKSurface surface, PixelRect dataRect, HorizontalAxis xAxis, VerticalAxis yAxis);
 }
