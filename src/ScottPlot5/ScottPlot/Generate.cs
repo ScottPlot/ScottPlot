1e997ba7ca0fe62a97c598613ef18d1f911f6987
Scott W Harden <swharden@gmail.com>
2022-06-10 18:57:36 -0400

    Create Generate module (analogous to DataGen)

---
+++ src/ScottPlot5/ScottPlot/Generate.cs
@@ -0,0 +1,43 @@
+﻿namespace ScottPlot;
+
+public static class Generate
+{
+    public static double[] Consecutive(int count)
+    {
+        double[] result = new double[count];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = i;
+        }
+
+        return result;
+    }
+    public static double[] Sin(int count)
+    {
+        double[] result = new double[count];
+
+        double step = 2 * Math.PI / count;
+
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = Math.Sin(i * step);
+        }
+
+        return result;
+    }
+
+    public static double[] Cos(int count)
+    {
+        double[] result = new double[count];
+
+        double step = 2 * Math.PI / count;
+
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = Math.Cos(i * step);
+        }
+
+        return result;
+    }
+}
