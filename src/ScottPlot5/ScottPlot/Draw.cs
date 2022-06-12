a6e540d6538de92f0be28080254460be480dc59a
Scott W Harden <swharden@gmail.com>
2022-06-12 11:44:42 -0400

    Draw: new module for common drawing tasks

---
+++ src/ScottPlot5/ScottPlot/Draw.cs
@@ -0,0 +1,33 @@
+﻿using SkiaSharp;
+
+namespace ScottPlot;
+
+/// <summary>
+/// Common render tasks
+/// </summary>
+public static class Draw
+{
+    /// <summary>
+    /// A semi-transparent filled rectangle with an outline and X over it
+    /// </summary>
+    public static void DebugRect(SKSurface surface, PixelRect rect)
+    {
+        SKColor fillColor = SKColors.Blue.WithAlpha(100);
+        SKColor lineColor = SKColors.Magenta;
+
+        SKPaint paint = new()
+        {
+            IsAntialias = true,
+        };
+
+        paint.Color = fillColor;
+        paint.IsStroke = false;
+        surface.Canvas.DrawRect(rect.ToSKRect(), paint);
+
+        paint.Color = lineColor;
+        paint.IsStroke = true;
+        surface.Canvas.DrawRect(rect.ToSKRect(), paint);
+        surface.Canvas.DrawLine(rect.TopLeft.ToSKPoint(), rect.BottomRight.ToSKPoint(), paint);
+        surface.Canvas.DrawLine(rect.BottomLeft.ToSKPoint(), rect.TopRight.ToSKPoint(), paint);
+    }
+}
