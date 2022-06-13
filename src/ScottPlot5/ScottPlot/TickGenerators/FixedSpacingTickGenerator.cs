be0f8d37f8acfcbeba1956312509eba68bfa96be
Scott W Harden <swharden@gmail.com>
2022-06-12 20:47:22 -0400

    ScottPlot5: FixedSpacingTickGenerator

---
+++ src/ScottPlot5/ScottPlot/TickGenerators/FixedSpacingTickGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace ScottPlot.TickGenerators;
+
+public class FixedSpacingTickGenerator : ITickGenerator
+{
+    public double InterTickSpacing = 1;
+
+    public Tick[] GenerateTicks(double min, double max, float edgeSize)
+    {
+        List<Tick> ticks = new();
+
+        double lowest = min - min % InterTickSpacing;
+        double highest = max - max % InterTickSpacing + InterTickSpacing;
+        int tickCount = (int)((highest - lowest) / InterTickSpacing);
+
+        for (int i = 0; i < tickCount; i++)
+        {
+            double position = lowest + i * InterTickSpacing;
+            string label = position.ToString();
+            ticks.Add(new Tick(position, label, true));
+        }
+
+        return ticks.ToArray();
+    }
+}
