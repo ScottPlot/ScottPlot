224eb00f66ffcc7ecf8cd49dae9a35c7d53dc692
Scott W Harden <swharden@gmail.com>
2022-06-21 18:35:38 -0400

    Cookbook: BarSeries

--- src/ScottPlot4/ScottPlot.Tests/PlotTypes/BarSeriesTests.cs
+++ src/ScottPlot4/ScottPlot.Tests/PlotTypes/BarSeriesTests.cs
@@ -17,12 +17,14 @@ namespace ScottPlotTests.PlotTypes
             List<ScottPlot.Plottable.Bar> bars = new();
             for (int i = 0; i < 10; i++)
             {
+                int value = rand.Next(25, 100);
                 ScottPlot.Plottable.Bar bar = new()
                 {
                     // Each bar can be extensively customized
-                    Value = rand.Next(25, 100),
+                    Value = value,
                     Position = i,
                     FillColor = ScottPlot.Palette.Category10.GetColor(i),
+                    Label = value.ToString(),
                 };
                 bars.Add(bar);
             };
@@ -31,6 +33,35 @@ namespace ScottPlotTests.PlotTypes
             ScottPlot.Plot plt = new(600, 400);
             plt.AddBarSeries(bars);
             plt.SetAxisLimitsY(0, 120);
+
+            TestTools.SaveFig(plt);
+        }
+
+        [Test]
+        public void Test_BarSeries_Horizontal()
+        {
+            // Create a bunch of Bar objects
+            Random rand = new(0);
+            List<ScottPlot.Plottable.Bar> bars = new();
+            for (int i = 0; i < 10; i++)
+            {
+                int value = rand.Next(25, 100);
+                ScottPlot.Plottable.Bar bar = new()
+                {
+                    // Each bar can be extensively customized
+                    Value = value,
+                    Position = i,
+                    FillColor = ScottPlot.Palette.Category10.GetColor(i),
+                    Label = value.ToString(),
+                    IsVertical = false,
+                };
+                bars.Add(bar);
+            };
+
+            // Add the BarSeries to the plot
+            ScottPlot.Plot plt = new(600, 400);
+            plt.AddBarSeries(bars);
+            plt.SetAxisLimitsX(0, 120);
 
             TestTools.SaveFig(plt);
         }
