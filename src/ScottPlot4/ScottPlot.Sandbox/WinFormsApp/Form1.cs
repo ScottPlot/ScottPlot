d4f3768f732087a68114587b09930430e11b988f
Scott W Harden <swharden@gmail.com>
2022-06-26 18:58:22 -0400

    Sandbox: demonstrate #1906 using a crosshair

--- src/ScottPlot4/ScottPlot.Sandbox/WinFormsApp/Form1.cs
+++ src/ScottPlot4/ScottPlot.Sandbox/WinFormsApp/Form1.cs
@@ -13,20 +13,23 @@ namespace WinFormsApp
 {
     public partial class Form1 : Form
     {
-        readonly Random Rand = new(0);
+        readonly ScottPlot.Plottable.Crosshair MyCrosshair;
 
         public Form1()
         {
             InitializeComponent();
             Width = 1200;
             Height = 1000;
+            MyCrosshair = formsPlot1.Plot.AddCrosshair(0, 0);
             formsPlot1.Refresh();
             formsPlot1.MouseMove += FormsPlot1_MouseMove;
         }
 
         private void FormsPlot1_MouseMove(object sender, MouseEventArgs e)
         {
+            (MyCrosshair.X, MyCrosshair.Y) = formsPlot1.GetMouseCoordinates();
             formsPlot1.Refresh(skipIfCurrentlyRendering: true);
+
             int depth = new System.Diagnostics.StackTrace().FrameCount;
             label1.Text = $"Stack depth: {depth}";
             progressBar1.Value = depth;
