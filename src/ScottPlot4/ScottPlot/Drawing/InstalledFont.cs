c72d78c0c741ea05c9be85b1e3647b217e2cbbbe
Scott W Harden <swharden@gmail.com>
2022-06-30 08:22:18 -0400

    InstalledFonts: StringComparer.InvariantCultureIgnoreCase
    #1924

--- src/ScottPlot4/ScottPlot/Drawing/InstalledFont.cs
+++ src/ScottPlot4/ScottPlot/Drawing/InstalledFont.cs
@@ -10,14 +10,14 @@ namespace ScottPlot.Drawing
     /// </summary>
     public static class InstalledFont
     {
-        private readonly static Dictionary<string, FontFamily> InstalledFonts = new();
+        private readonly static Dictionary<string, FontFamily> InstalledFonts = new(StringComparer.InvariantCultureIgnoreCase);
 
         static InstalledFont()
         {
             // non-Windows systems may have multiple fonts with the same names
             InstalledFonts = FontFamily.Families
-                .GroupBy(x => x.Name.ToLower())
-                .ToDictionary(x => x.Key, x => x.First());
+                .GroupBy(x => x.Name, StringComparer.InvariantCultureIgnoreCase)
+                .ToDictionary(x => x.Key, x => x.First(), StringComparer.InvariantCultureIgnoreCase);
 
             SerifFamily = ValidFontFamily(new string[] { "Times New Roman", "DejaVu Serif", "Times" });
             SansFamily = ValidFontFamily(new string[] { "Segoe UI", "DejaVu Sans", "Helvetica" });
@@ -55,8 +55,8 @@ namespace ScottPlot.Drawing
         /// </summary>
         public static FontFamily ValidFontFamily(string fontName)
         {
-            if (fontName is not null && InstalledFonts.ContainsKey(fontName.ToLower()))
-                return InstalledFonts[fontName.ToLower()];
+            if (fontName is not null && InstalledFonts.ContainsKey(fontName))
+                return InstalledFonts[fontName];
 
             return SystemFonts.DefaultFont.FontFamily;
         }
@@ -68,8 +68,8 @@ namespace ScottPlot.Drawing
         {
             foreach (string fontName in fontNames)
             {
-                if (fontName is not null && InstalledFonts.ContainsKey(fontName.ToLower()))
-                    return InstalledFonts[fontName.ToLower()];
+                if (fontName is not null && InstalledFonts.ContainsKey(fontName))
+                    return InstalledFonts[fontName];
             }
 
             return SystemFonts.DefaultFont.FontFamily;
