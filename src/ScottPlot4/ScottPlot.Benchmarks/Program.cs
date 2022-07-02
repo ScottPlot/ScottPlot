4c5d07391b34f556d6da9cfa24cbcb430909a0fe
Benjamin Lehmann <bclehmann@yahoo.ca>
2022-07-01 20:16:33 -0600

    Creates Benchmark project using BenchmarkDotNet

---
+++ src/ScottPlot4/ScottPlot.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ScottPlot.Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
