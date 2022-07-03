a64a46ff58615bf2c8759fdde9b0818ef7f4f110
Scott W Harden <swharden@gmail.com>
2022-07-02 21:27:31 -0400

    Benchmark: show menu if no arguments

--- src/ScottPlot4/ScottPlot.Benchmarks/Program.cs
+++ src/ScottPlot4/ScottPlot.Benchmarks/Program.cs
@@ -7,17 +7,13 @@ namespace ScottPlot.Benchmarks
     {
         static void Main(string[] args)
         {
-            if (args.Length == 1 && args[0] == "menu")
-            {
-                BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-            }
-            else if (args.Length == 1 && args[0] == "run")
+            if (args.Length == 1 && args[0] == "benchmark")
             {
                 BenchmarkRunner.Run(typeof(Program).Assembly);
             }
             else
             {
-                Console.WriteLine("Single argument required: 'menu' or 'run'");
+                BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
             }
         }
     }
