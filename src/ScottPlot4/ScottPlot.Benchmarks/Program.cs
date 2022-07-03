73548b83b0e6553b8c4eca873103f9eb6a33dddc
Scott W Harden <swharden@gmail.com>
2022-07-02 21:20:27 -0400

    Benchmark: startup based on command line arguments

--- src/ScottPlot4/ScottPlot.Benchmarks/Program.cs
+++ src/ScottPlot4/ScottPlot.Benchmarks/Program.cs
@@ -1,9 +1,24 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using BenchmarkDotNet.Running;
 
 namespace ScottPlot.Benchmarks
 {
     internal class Program
     {
-        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        static void Main(string[] args)
+        {
+            if (args.Length == 1 && args[0] == "menu")
+            {
+                BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            }
+            else if (args.Length == 1 && args[0] == "run")
+            {
+                BenchmarkRunner.Run(typeof(Program).Assembly);
+            }
+            else
+            {
+                Console.WriteLine("Single argument required: 'menu' or 'run'");
+            }
+        }
     }
 }
