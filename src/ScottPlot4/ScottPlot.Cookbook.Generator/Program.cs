ddbf8d4db67eef4144db444c18dec71057cc6129
Scott W Harden <swharden@gmail.com>
2022-07-06 20:09:30 -0400

    Cookbook: move generator to solution top level

---
+++ src/ScottPlot4/ScottPlot.Cookbook.Generator/Program.cs
@@ -0,0 +1,21 @@
+﻿/* This application generates the images and Markdown pages used to build the cookbook website.
+ * This application is intended to be executed from inside Visual Studio.
+ */
+
+namespace Cookbook_Generator;
+
+public static class Program
+{
+    public static void Main()
+    {
+        string cookbookProjectFolder = Path.GetFullPath("../../../../ScottPlot.Cookbook");
+        string outputFolder = Path.GetFullPath("../../../../ScottPlot.Cookbook/CookbookOutput");
+
+        ScottPlot.Cookbook.RecipeSource[] recipes = ScottPlot.Cookbook.Generator.Generate(
+            cookbookProjectFolder, outputFolder, regenerate: true);
+
+        Website.Generate(outputFolder, recipes);
+
+        Console.WriteLine($"COMPLETE: {outputFolder}");
+    }
+}
