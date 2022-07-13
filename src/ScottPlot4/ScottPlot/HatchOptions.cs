75040ee90ff38f5ffaa263fb85bcdac37991a3ec
Benjamin Lehmann <bclehmann@yahoo.ca>
2022-07-12 18:45:42 -0600

    Adds HatchOptions struct

---
+++ src/ScottPlot4/ScottPlot/HatchOptions.cs
@@ -0,0 +1,16 @@
+﻿using ScottPlot.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlot
+{
+    public struct HatchOptions
+    {
+        public HatchStyle HatchStyle { get; set; }
+        public Color HatchColor { get; set; }
+    }
+}
