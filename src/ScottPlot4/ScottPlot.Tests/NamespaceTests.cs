493f344cc35759bead46c6f6857f8155d9e98e58
Scott W Harden <swharden@gmail.com>
2022-07-16 15:18:03 -0400

    Test: using ScottPlot and System.Drawing

---
+++ src/ScottPlot4/ScottPlot.Tests/NamespaceTests.cs
@@ -0,0 +1,29 @@
+﻿// This file tests what happens when you have "using" statements for two libraries with the same Color
+
+using ScottPlot;
+using System.Drawing;
+
+using NUnit.Framework;
+using System;
+
+namespace ScottPlotTests;
+
+internal class NamespaceTests
+{
+    private void DoSomething(System.Drawing.Color color) => Console.WriteLine(color);
+
+    private void DoSomething(ScottPlot.Color color) => Console.WriteLine(color);
+
+    [Test]
+    public void Test_Namespace_Check()
+    {
+        ScottPlot.Color color1 = new();
+        DoSomething(color1);
+
+        System.Drawing.Color color2 = new();
+        DoSomething(color2);
+
+        // NOT OK (AMBIGUOUS)
+        // var c = new Color();
+    }
+}
