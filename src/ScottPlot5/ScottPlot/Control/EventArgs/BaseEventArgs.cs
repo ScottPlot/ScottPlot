2c75b4025e917950333ffc32c84c92342516854b
Scott W Harden <swharden@gmail.com>
2022-07-18 19:25:27 -0400

    ScottPlot5: make BaseEventArgs abstract

--- src/ScottPlot5/ScottPlot/Control/EventArgs/BaseEventArgs.cs
+++ src/ScottPlot5/ScottPlot/Control/EventArgs/BaseEventArgs.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ScottPlot.Control.EventArgs
 {
-    public class BaseEventArgs : System.EventArgs
+    public abstract class BaseEventArgs : System.EventArgs
     {
         public bool Handled { get; set; }
     }
