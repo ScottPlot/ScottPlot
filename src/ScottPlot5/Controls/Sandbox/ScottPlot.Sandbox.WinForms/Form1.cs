d3623d21b46f40bb97c07a5143777c510484bee7
Scott W Harden <swharden@gmail.com>
2022-07-19 20:17:10 -0400

    autoformat

--- src/ScottPlot5/Controls/Sandbox/ScottPlot.Sandbox.WinForms/Form1.cs
+++ src/ScottPlot5/Controls/Sandbox/ScottPlot.Sandbox.WinForms/Form1.cs
@@ -1,4 +1,4 @@
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace ScottPlot.Sandbox.WinForms;
 
--- src/ScottPlot5/Controls/Sandbox/ScottPlot.Sandbox.WinForms/Program.cs
+++ src/ScottPlot5/Controls/Sandbox/ScottPlot.Sandbox.WinForms/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Windows.Forms;
 
 namespace ScottPlot.Sandbox.WinForms;
