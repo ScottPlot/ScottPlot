a5b419d1f86039384f44d4b68ce1ff105aa95f50
Benjamin Lehmann <bclehmann@yahoo.ca>
2022-08-05 14:37:18 -0600

    Adds Colors.Transparent

--- src/ScottPlot5/ScottPlot/Style/Fill.cs
+++ src/ScottPlot5/ScottPlot/Style/Fill.cs
@@ -9,7 +9,7 @@ namespace ScottPlot.Style
     public struct Fill
     {
         public Color Color { get; set; } = Colors.CornflowerBlue;
-        public Color HatchColor { get; set; } = Colors.Black.WithAlpha(0); // TODO: I think transparent is a web color?
+        public Color HatchColor { get; set; } = Colors.Transparent;
         public HatchStyle HatchStyle { get; set; } = HatchStyle.None;
         
         public Fill() { }
