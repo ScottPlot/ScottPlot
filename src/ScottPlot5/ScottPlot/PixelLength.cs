55d0bb27c8f14f019dab8f36cb692d587b027057
Scott W Harden <swharden@gmail.com>
2022-08-06 16:53:25 -0400

    ScottPlot5: PixelLength

---
+++ src/ScottPlot5/ScottPlot/PixelLength.cs
@@ -0,0 +1,24 @@
+﻿namespace ScottPlot;
+
+/// <summary>
+/// Represents a distance in pixel units
+/// </summary>
+public readonly struct PixelLength
+{
+    public readonly float Length;
+
+    public PixelLength(float length)
+    {
+        Length = length;
+    }
+
+    public static implicit operator PixelLength(float length)
+    {
+        return new PixelLength(length);
+    }
+
+    public override string ToString()
+    {
+        return $"{Length} pixels";
+    }
+}
