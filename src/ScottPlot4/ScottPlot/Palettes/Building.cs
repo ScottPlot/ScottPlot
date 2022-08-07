6da23b0f57d1de5548089108437c87aa35a459bd
Scott W Harden <swharden@gmail.com>
2022-08-07 12:28:04 -0400

    Palette: Building
    #2005

---
+++ src/ScottPlot4/ScottPlot/Palettes/Building.cs
@@ -0,0 +1,6 @@
+﻿namespace ScottPlot.Drawing.Colorsets;
+
+public class Building : HexColorset, IPalette
+{
+    public override string[] hexColors => Common.HexPalettes.Building.Colors;
+}
---
+++ src/ScottPlot5/ScottPlot/Palettes/Building.cs
@@ -0,0 +1,6 @@
+﻿namespace ScottPlot.Palettes;
+
+public class Building : PaletteBase
+{
+    protected override Color[] colors => Common.HexPalettes.Building.Colors.Select(c => Color.FromHex(c)).ToArray();
+}
