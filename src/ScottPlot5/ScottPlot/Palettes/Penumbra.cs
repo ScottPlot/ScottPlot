9726982ddc94957e970e061d2df5909d040cfcfe
Scott W Harden <swharden@gmail.com>
2022-08-07 12:28:16 -0400

    Palette: Penumba
    #2005

---
+++ src/ScottPlot5/ScottPlot/Palettes/Penumbra.cs
@@ -0,0 +1,6 @@
+﻿namespace ScottPlot.Palettes;
+
+public class Penumbra : PaletteBase
+{
+    protected override Color[] colors => Common.HexPalettes.Penumbra.Colors.Select(c => Color.FromHex(c)).ToArray();
+}
