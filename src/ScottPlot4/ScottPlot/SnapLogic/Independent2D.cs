4bac56ac045645621a6808fa695a44ec699919cc
Scott W Harden <swharden@gmail.com>
2022-08-07 17:33:12 -0400

    SnapLogic.Independent2D: add constructors

--- src/ScottPlot4/ScottPlot/SnapLogic/Independent2D.cs
+++ src/ScottPlot4/ScottPlot/SnapLogic/Independent2D.cs
@@ -8,6 +8,17 @@ public class Independent2D : ISnap2D
     public ISnap SnapX { get; set; } = new Smooth();
     public ISnap SnapY { get; set; } = new Smooth();
 
+    public Independent2D()
+    {
+
+    }
+
+    public Independent2D(ISnap x, ISnap y)
+    {
+        SnapX = x;
+        SnapY = y;
+    }
+
     public Coordinate Snap(Coordinate value)
     {
         double x = SnapX.Snap(value.X);
