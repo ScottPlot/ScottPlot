3f75259f6e35f4609f1859e76dbfd5a046122d2d
Scott W Harden <swharden@gmail.com>
2022-08-07 17:25:14 -0400

    SnapLogic.Independent2D
    this logic object allows separate 1D snap logic objects to be combined into a 2D snap system

---
+++ src/ScottPlot4/ScottPlot/SnapLogic/Independent2D.cs
@@ -0,0 +1,17 @@
+﻿namespace ScottPlot.SnapLogic;
+
+/// <summary>
+/// Customizable 2D snap system from independent 1D snap objects.
+/// </summary>
+public class Independent2D : ISnap2D
+{
+    public ISnap SnapX { get; set; } = new Smooth();
+    public ISnap SnapY { get; set; } = new Smooth();
+
+    public Coordinate Snap(Coordinate value)
+    {
+        double x = SnapX.Snap(value.X);
+        double y = SnapY.Snap(value.Y);
+        return new Coordinate(x, y);
+    }
+}
