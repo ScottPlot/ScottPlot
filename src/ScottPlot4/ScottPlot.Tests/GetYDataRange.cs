a7d3bae4c672b5408bce9f50d934eb3ea9b3be02
Scott W Harden <swharden@gmail.com>
2022-08-15 18:43:49 -0400

    ScottPlot4 Tests: Fix old broken Signal test
    Original test miscalculated effect of sample rate on X positions

--- src/ScottPlot4/ScottPlot.Tests/GetYDataRange.cs
+++ src/ScottPlot4/ScottPlot.Tests/GetYDataRange.cs
@@ -36,18 +36,25 @@ namespace ScottPlotTests
             Assert.AreEqual(expectedMax, yMax);
         }
 
-        [TestCase(new double[] { 10, 12, 14, 8, 10 }, 0, 4, 8, 14)]
-        [TestCase(new double[] { 10, 12, 14, 8, 10 }, 0, 2, 10, 14)]
-        [TestCase(new double[] { 10, 12, 14, 8, 10 }, 2, 2, 14, 14)]
-        public void SignalWithXOffsetAndSampleRate(double[] ys, double xStart, double xEnd, double expectedMin, double expectedMax)
+        [TestCase("full", new double[] { 10, 12, 14, 8, 10 }, 0, 4, 8, 14)]
+        [TestCase("first half", new double[] { 10, 12, 14, 8, 10 }, 0, 2, 10, 14)]
+        [TestCase("single point", new double[] { 10, 12, 14, 8, 10 }, 2, 2, 14, 14)]
+        public void SignalWithXOffsetAndSampleRate(string title, double[] ys, double xStart, double xEnd, double expectedMin, double expectedMax)
         {
             const double sampleRate = 25;
             const double xOffset = -100;
 
             var plt = new ScottPlot.Plot();
+            plt.Title(title);
             var sig = plt.AddSignal(ys, sampleRate: sampleRate);
             sig.OffsetX = xOffset;
-            (double yMin, double yMax) = sig.GetYDataRange(xStart * sampleRate + xOffset, xEnd * sampleRate + xOffset);
+
+            double startCoordinate = xStart / sampleRate + xOffset;
+            double endCoordinate = xEnd / sampleRate + xOffset;
+            plt.AddHorizontalSpan(startCoordinate, endCoordinate, System.Drawing.Color.FromArgb(100, System.Drawing.Color.Red));
+
+            (double yMin, double yMax) = sig.GetYDataRange(startCoordinate, endCoordinate);
+            //TestTools.SaveFig(plt, title);
 
             Assert.AreEqual(expectedMin, yMin);
             Assert.AreEqual(expectedMax, yMax);
