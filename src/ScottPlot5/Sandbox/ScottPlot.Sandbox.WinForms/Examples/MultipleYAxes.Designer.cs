be71e9df2b73e3ef29b0bbca282b0ef579badfa2
Scott W Harden <swharden@gmail.com>
2022-08-21 18:41:24 -0400

    ScottPlot5 sandbox: multi-axis demo
    Y axis view is not yet properly rendered, but adding plottables to multiple axes works

---
+++ src/ScottPlot5/Sandbox/ScottPlot.Sandbox.WinForms/Examples/MultipleYAxes.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WinForms.Examples
+{
+    partial class MultipleYAxes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.formsPlot1.Location = new System.Drawing.Point(12, 41);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(776, 397);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // MultipleYAxes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.formsPlot1);
+            this.Name = "MultipleYAxes";
+            this.Text = "MultiAxis";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ src/ScottPlot5/Sandbox/ScottPlot.Sandbox.WinForms/Examples/MultipleYAxes.cs
@@ -0,0 +1,49 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms.Examples
+{
+    public partial class MultipleYAxes : Form, IExampleForm
+    {
+        Random Rand = new();
+        public string SandboxTitle => "Multiple Y Axes";
+        public string SandboxDescription => "Plot data with greatly different scales using separate Y axes";
+
+        public MultipleYAxes()
+        {
+            InitializeComponent();
+            button1.Click += (s, e) => AddYAxis();
+            button2.Click += (s, e) => AddYAxis();
+            formsPlot1.Plot.Plottables.AddSignal(GetRandomData());
+            formsPlot1.Refresh();
+        }
+
+        private void AddYAxis()
+        {
+            // create a new axis and add it to the plot
+            var yAxis = new ScottPlot.Axis.StandardAxes.LeftAxis();
+            formsPlot1.Plot.YAxes.Add(yAxis);
+
+            // add a new plottable and tell it to use our custom Y axis
+            var sig = formsPlot1.Plot.Plottables.AddSignal(GetRandomData());
+            sig.Axes.YAxis = yAxis;
+
+            formsPlot1.Refresh();
+        }
+
+        private double[] GetRandomData()
+        {
+            return Generate.Sin(51,
+                mult: Math.Pow(10, Rand.NextDouble() * 6),
+                phase: Rand.NextDouble() * Math.PI * 2);
+        }
+    }
+}
