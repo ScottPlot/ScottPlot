47a4d58dc806acc9c4ea583b936965fb0d132414
Scott W Harden <swharden@gmail.com>
2022-09-03 19:53:21 -0400

    ScottPlot5: Alignment

---
+++ src/ScottPlot5/ScottPlot/Alignment.cs
@@ -0,0 +1,29 @@
+﻿namespace ScottPlot;
+
+public struct Alignment
+{
+    public HorizontalAlignment X { get; set; }
+    public VerticalAlignment Y { get; set; }
+
+    public Alignment(
+        HorizontalAlignment x = HorizontalAlignment.Center,
+        VerticalAlignment y = VerticalAlignment.Center)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+public enum HorizontalAlignment
+{
+    Left,
+    Center,
+    Right,
+}
+
+public enum VerticalAlignment
+{
+    Bottom,
+    Center,
+    Top,
+}
