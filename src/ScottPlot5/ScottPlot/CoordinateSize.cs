6ecb8f8ff78f6b7d3fc85bfe5d5af3bffefcd280
Scott W Harden <swharden@gmail.com>
2022-09-03 20:29:30 -0400

    ScottPlot5: CoordinateSize

---
+++ src/ScottPlot5/ScottPlot/CoordinateSize.cs
@@ -0,0 +1,14 @@
+﻿namespace ScottPlot;
+
+public struct CoordinateSize
+{
+    public double Width;
+    public double Height;
+    public double Area => Width * Height;
+
+    public CoordinateSize(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
