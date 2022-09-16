a6820be36877065ccb2511461fbefc44c30f124b
Benjamin Lehmann <bclehmann@yahoo.ca>
2022-09-15 20:10:54 -0600

    ScopedTransform

---
+++ src/ScottPlot5/ScottPlot5/ScopedTransform.cs
@@ -0,0 +1,26 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlot
+{
+    internal class ScopedTransform : IDisposable // TODO: I'm a little uneasy on this name?
+    {
+        private readonly SKCanvas canvas;
+        private readonly int restoreIndex;
+
+        public ScopedTransform(SKCanvas canvas)
+        {
+            this.canvas = canvas;
+            restoreIndex = canvas.Save();
+        }
+
+        public void Dispose()
+        {
+            canvas.RestoreToCount(restoreIndex);
+        }
+    }
+}
