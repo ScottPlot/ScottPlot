5980daa96ad796252573568aba2aa7457532ec3a
Benjamin Lehmann <bclehmann@yahoo.ca>
2022-09-23 18:35:48 -0600

    Adds test for detecting 24 hour clock in ticks

--- src/ScottPlot4/ScottPlot.Tests/Ticks/DateTimeTicksTests.cs
+++ src/ScottPlot4/ScottPlot.Tests/Ticks/DateTimeTicksTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using ScottPlot.Ticks;
 using ScottPlot.Ticks.DateTimeTickUnits;
 using System;
 using System.Collections.Generic;
@@ -47,6 +48,37 @@ namespace ScottPlotTests.Ticks
                 });
 
                 dateUpper -= span / 2;
+            }
+        }
+
+        [Test]
+        public void TicksRespectLocale24HourClock()
+        {
+            DateTime almostMidnight = new DateTime(2000, 1, 1, 23, 59, 59);
+            CultureInfo culture12HourClock = new CultureInfo("en-US");
+            culture12HourClock.DateTimeFormat.ShortTimePattern = "hh:mm:ss";
+            culture12HourClock.DateTimeFormat.LongTimePattern = "hh:mm:ss";
+
+            CultureInfo culture24HourClock = new CultureInfo("en-US");
+            culture24HourClock.DateTimeFormat.ShortTimePattern = "HH:mm:ss";
+            culture24HourClock.DateTimeFormat.LongTimePattern = "HH:mm:ss";
+
+            DateTimeUnitFactory factory = new DateTimeUnitFactory();
+            // Yes I know this is janky, I was feeling lazy
+            IEnumerable<DateTimeUnit> dateTimeKinds = Enumerable.Range((int)DateTimeUnit.Hour, DateTimeUnit.Millisecond - DateTimeUnit.Hour + 1).Select(i => (DateTimeUnit)i);
+
+            var ticks12Hour = dateTimeKinds.Select(kind => factory.Create(kind, culture12HourClock, 1, null).GetTicksAndLabels(almostMidnight, almostMidnight, null));
+            var ticks24Hour = dateTimeKinds.Select(kind => factory.Create(kind, culture24HourClock, 1, null).GetTicksAndLabels(almostMidnight, almostMidnight, null));
+            
+            foreach(var label in ticks12Hour.SelectMany(c => c.Labels))
+            {
+                // The first line contains the date, the second line contains the time
+                StringAssert.StartsWith("11:59:59", label.Split('\n')[1]);
+            }
+
+            foreach (var label in ticks24Hour.SelectMany(c => c.Labels))
+            {
+                StringAssert.StartsWith("23:59:59", label.Split('\n')[1]);
             }
         }
     }
