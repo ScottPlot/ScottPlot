199c279479e205481f90ca01aaf44882892b3646
Benjamin Lehmann <bclehmann@yahoo.ca>
2022-10-29 15:32:50 -0600

    Label: Use Font

--- src/ScottPlot5/ScottPlot5/Label.cs
+++ src/ScottPlot5/ScottPlot5/Label.cs
@@ -5,20 +5,18 @@ namespace ScottPlot;
 public class Label
 {
     public string Text { get; set; } = string.Empty;
-    public float FontSize { get; set; } = 12;
-    public string Font { get; set; } = "Consolas";
+    public Style.Font Font { get; set; } = new() { 
+        Family = "Consolas",
+        Size = 12,
+    };
     public Color Color { get; set; } = Colors.Black;
-    public bool Bold { get; set; } = false;
-    public bool Italic { get; set; } = false;
     public bool AntiAlias { get; set; } = true;
     public float Rotation { get; set; } = 0;
 
     public SKPaint MakePaint()
     {
-        return new SKPaint()
+        return new SKPaint(MakeFont())
         {
-            TextSize = FontSize,
-            FakeBoldText = Bold,
             Color = Color.ToSKColor(),
             IsAntialias = AntiAlias,
             TextAlign = SKTextAlign.Center,
@@ -27,7 +25,7 @@ public class Label
 
     public SKFont MakeFont()
     {
-        return new SKFont(SKTypeface.FromFamilyName(Font), FontSize);
+        return Font.GetFont();
     }
 
     public Label()
@@ -37,11 +35,10 @@ public class Label
     public void Draw(SKSurface surface, Pixel point, SKPaint paint)
     {
         using SKFont font = MakeFont();
-        font.Embolden = Bold;
         surface.Canvas.Save();
         surface.Canvas.Translate(point.X, point.Y);
         surface.Canvas.RotateDegrees(Rotation);
-        surface.Canvas.DrawText(Text, 0, FontSize, font, paint);
+        surface.Canvas.DrawText(Text, 0, font.Size, font, paint);
         surface.Canvas.Restore();
     }
 }
