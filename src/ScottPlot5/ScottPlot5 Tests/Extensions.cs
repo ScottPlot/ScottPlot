c951eb470ff2c612fd53616daaf11fbc3e8b3a67
Scott W Harden <swharden@gmail.com>
2022-12-26 13:06:59 -0500

    SP5 Tests: additional image saving extension methods

--- src/ScottPlot5/ScottPlot5 Tests/Extensions.cs
+++ src/ScottPlot5/ScottPlot5 Tests/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using SkiaSharp;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace ScottPlotTests;
@@ -7,6 +8,7 @@ internal static class Extensions
 {
     internal static void SaveTestImage(this Plot plt, string subName = "", int width = 600, int height = 400)
     {
+        // determine filename based on name of calling function
         StackTrace stackTrace = new();
         StackFrame frame = stackTrace.GetFrame(1) ?? throw new InvalidOperationException("bad caller");
         MethodBase method = frame.GetMethod() ?? throw new InvalidDataException("bad method");
@@ -21,8 +23,33 @@ internal static class Extensions
 
         string fileName = callingMethod + subName + ".png";
         string filePath = Path.Combine(saveFolder, fileName);
-
-        plt.SaveJpeg(filePath, width, height);
         Console.WriteLine(filePath);
+
+        // actually save the thing
+        plt.SaveJpeg(filePath, width, height);
+    }
+
+    internal static void SaveTestImage(this SKBitmap bmp, string subName = "")
+    {
+        // determine filename based on name of calling function
+        StackTrace stackTrace = new();
+        StackFrame frame = stackTrace.GetFrame(1) ?? throw new InvalidOperationException("bad caller");
+        MethodBase method = frame.GetMethod() ?? throw new InvalidDataException("bad method");
+        string callingMethod = method.Name;
+
+        if (!string.IsNullOrWhiteSpace(subName))
+            subName = "_" + subName;
+
+        string saveFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "test-images");
+        if (!Directory.Exists(saveFolder))
+            Directory.CreateDirectory(saveFolder);
+
+        string fileName = callingMethod + subName + ".png";
+        string filePath = Path.Combine(saveFolder, fileName);
+        Console.WriteLine(filePath);
+
+        // actually save the thing
+        using SKFileWStream fs = new(filePath);
+        bmp.Encode(fs, SKEncodedImageFormat.Png, quality: 100);
     }
 }
