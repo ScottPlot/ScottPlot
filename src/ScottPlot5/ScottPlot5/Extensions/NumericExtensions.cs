64123181868969a573ba96201eb0c2f6410bfe17
Scott W Harden <swharden@gmail.com>
2022-12-26 15:14:53 -0500

    SP5: move extensions into subfolder

---
+++ src/ScottPlot5/ScottPlot5/Extensions/NumericExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace ScottPlot;
+
+internal static class NumericExtensions
+{
+    public static bool IsInfiniteOrNaN(this double x)
+    {
+        return !IsFinite(x);
+    }
+
+    public static bool IsFinite(this double x)
+    {
+        if (double.IsInfinity(x))
+            return false;
+
+        if (double.IsNaN(x))
+            return false;
+
+        return true;
+    }
+}
---
+++ src/ScottPlot5/ScottPlot5/Extensions/SkiaSharpExtensions.cs
@@ -0,0 +1,40 @@
+﻿using SkiaSharp;
+
+namespace ScottPlot;
+
+internal static class SkiaSharpExtensions
+{
+    public static PixelSize ToPixelSize(this SKRect rect)
+    {
+        return new PixelSize(rect.Width, rect.Height);
+    }
+
+    public static SKTextAlign ToSKTextAlign(this Alignment2 alignment)
+    {
+        return alignment switch
+        {
+            Alignment2.UpperLeft => SKTextAlign.Left,
+            Alignment2.UpperCenter => SKTextAlign.Center,
+            Alignment2.UpperRight => SKTextAlign.Right,
+            Alignment2.MiddleLeft => SKTextAlign.Left,
+            Alignment2.MiddleCenter => SKTextAlign.Center,
+            Alignment2.MiddleRight => SKTextAlign.Right,
+            Alignment2.LowerLeft => SKTextAlign.Left,
+            Alignment2.LowerCenter => SKTextAlign.Center,
+            Alignment2.LowerRight => SKTextAlign.Right,
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    public static SKEncodedImageFormat ToSKFormat(this ImageFormat fmt)
+    {
+        return fmt switch
+        {
+            ImageFormat.Jpeg => SKEncodedImageFormat.Jpeg,
+            ImageFormat.Png => SKEncodedImageFormat.Png,
+            ImageFormat.Bmp => SKEncodedImageFormat.Bmp,
+            ImageFormat.Webp => SKEncodedImageFormat.Webp,
+            _ => throw new NotImplementedException($"unsupported format: {fmt}")
+        };
+    }
+}
