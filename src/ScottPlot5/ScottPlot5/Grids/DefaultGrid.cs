2c7ecb2d3120e6acfd41792dad0c45140d1fcdc1
Scott W Harden <swharden@gmail.com>
2022-12-28 09:55:48 -0500

    SP5: do not draw minor grid lines by default

--- src/ScottPlot5/ScottPlot5/Grids/DefaultGrid.cs
+++ src/ScottPlot5/ScottPlot5/Grids/DefaultGrid.cs
@@ -1,13 +1,18 @@
 ﻿using ScottPlot.Axis;
 using SkiaSharp;
+using System.Data;
 using System.Linq;
 
 namespace ScottPlot.Grids;
 
 public class DefaultGrid : IGrid
 {
-    public float LineWidth = 1;
-    public Color LineColor = Colors.Black.WithAlpha(20);
+    public float MajorGridLineWidth = 1;
+    public Color MajorGridLineColor = Colors.Black.WithOpacity(.1);
+    public float MinorGridLineWidth = 0;
+    public Color MinorGridLineColor = Colors.Black.WithOpacity(.05);
+    public int MaximumNumberOfGridLines = 1000;
+
     public bool IsBeneathPlottables { get; set; } = true;
 
     public readonly IXAxis XAxis;
@@ -21,24 +26,36 @@ public class DefaultGrid : IGrid
 
     public void Render(SKSurface surface, PixelRect dataRect)
     {
-        RenderGridLines(surface, dataRect, XAxis);
-        RenderGridLines(surface, dataRect, YAxis);
-    }
 
-    private void RenderGridLines(SKSurface surface, PixelRect dataRect, IAxis axis)
-    {
-        // TODO: restrict to visible ticks and max 1000?
-        Tick[] ticks = axis.TickGenerator.Ticks;
-        Pixel[] starts = new Pixel[ticks.Length];
-        Pixel[] ends = new Pixel[ticks.Length];
-
-        for (int i = 0; i < ticks.Length; i++)
+        if (MinorGridLineWidth > 0)
         {
-            float px = axis.GetPixel(ticks[i].Position, dataRect);
-            starts[i] = axis.Edge.IsHorizontal() ? new Pixel(px, dataRect.Bottom) : new Pixel(dataRect.Left, px);
-            ends[i] = axis.Edge.IsHorizontal() ? new Pixel(px, dataRect.Top) : new Pixel(dataRect.Right, px);
+            float[] xTicksMinor = XAxis.TickGenerator.Ticks.Where(x => !x.IsMajor).Select(x => XAxis.GetPixel(x.Position, dataRect)).ToArray();
+            float[] yTicksMinor = YAxis.TickGenerator.Ticks.Where(x => !x.IsMajor).Select(x => YAxis.GetPixel(x.Position, dataRect)).ToArray();
+            RenderGridLines(surface, dataRect, xTicksMinor, XAxis.Edge, MinorGridLineColor, MinorGridLineWidth);
+            RenderGridLines(surface, dataRect, yTicksMinor, YAxis.Edge, MinorGridLineColor, MinorGridLineWidth);
         }
 
-        Drawing.DrawLines(surface, starts, ends, LineColor, LineWidth);
+        if (MajorGridLineWidth > 0)
+        {
+            float[] xTicksMajor = XAxis.TickGenerator.Ticks.Where(x => x.IsMajor).Select(x => XAxis.GetPixel(x.Position, dataRect)).ToArray();
+            float[] yTicksMajor = YAxis.TickGenerator.Ticks.Where(x => x.IsMajor).Select(x => YAxis.GetPixel(x.Position, dataRect)).ToArray();
+            RenderGridLines(surface, dataRect, xTicksMajor, XAxis.Edge, MajorGridLineColor, MajorGridLineWidth);
+            RenderGridLines(surface, dataRect, yTicksMajor, YAxis.Edge, MajorGridLineColor, MajorGridLineWidth);
+        }
+    }
+
+    private void RenderGridLines(SKSurface surface, PixelRect dataRect, float[] positions, Edge edge, Color color, float lineWidth)
+    {
+        Pixel[] starts = new Pixel[positions.Length];
+        Pixel[] ends = new Pixel[positions.Length];
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            float px = positions[i];
+            starts[i] = edge.IsHorizontal() ? new Pixel(px, dataRect.Bottom) : new Pixel(dataRect.Left, px);
+            ends[i] = edge.IsHorizontal() ? new Pixel(px, dataRect.Top) : new Pixel(dataRect.Right, px);
+        }
+
+        Drawing.DrawLines(surface, starts, ends, color, lineWidth);
     }
 }
