10af1cde5dfc44b1f250dfe2ba6173024f13c576
Scott W Harden <swharden@gmail.com>
2022-12-28 09:55:25 -0500

    Create TODO.cs

---
+++ src/ScottPlot5/ScottPlot5/TODO.cs
@@ -0,0 +1,11 @@
+﻿namespace ScottPlot;
+
+internal static class TODO
+{
+    // default axis limits are too tight
+
+    // control designer view sucks - at least add some axes and a grid
+
+    // signal plots containing only horizontal data look bad at the edges.
+    // Plot a 10k point sine wave to demonstrate thi sissue.
+}
