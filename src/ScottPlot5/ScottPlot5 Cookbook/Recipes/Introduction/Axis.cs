8a2a9378a69645190345636c57d8d3af1db3d3e2
Scott W Harden <swharden@gmail.com>
2022-12-30 15:17:54 -0500

    SP5 Cookbook: set and get axis limits

--- src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Introduction/Axis.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Introduction/Axis.cs
@@ -11,7 +11,7 @@ internal class Axis : RecipePageBase
 
     internal class AxisLabels : RecipeTestBase
     {
-        public override string Name => "Customize Axis Labels";
+        public override string Name => "Axis Labels";
         public override string Description => "Axis labels are the text labels centered on each axis. " +
             "The text inside these labels can be changed, and the style of the text can be extensively customized.";
 
@@ -23,6 +23,70 @@ internal class Axis : RecipePageBase
 
             myPlot.XAxis.Label.Text = "Horizontal Axis";
             myPlot.YAxis.Label.Text = "Vertical Axis";
+        }
+    }
+
+    internal class SetAxisLimits : RecipeTestBase
+    {
+        public override string Name => "Manually Set Axis Limits";
+        public override string Description => "Axis Limits can be set manually in different ways.";
+
+        [Test]
+        public override void Recipe()
+        {
+            myPlot.Add.Signal(Generate.Sin(51));
+            myPlot.Add.Signal(Generate.Cos(51));
+
+            // Interact with a specific axis
+            myPlot.XAxis.Min = -100;
+            myPlot.XAxis.Max = 150;
+            myPlot.YAxis.Min = -5;
+            myPlot.YAxis.Max = 5;
+
+            // Call a helper function
+            myPlot.SetAxisLimits(-100, 150, -5, 5);
+        }
+    }
+
+    internal class GetAxisLimits : RecipeTestBase
+    {
+        public override string Name => "Read Axis Limits";
+        public override string Description => "The current axis limits can be read in multiple ways.";
+
+        [Test]
+        public override void Recipe()
+        {
+            myPlot.Add.Signal(Generate.Sin(51));
+            myPlot.Add.Signal(Generate.Cos(51));
+
+            // Interact with a specific axis
+            double top = myPlot.YAxis.Max;
+            double bottom = myPlot.YAxis.Min;
+
+            // Call a helper function
+            AxisLimits limits = myPlot.GetAxisLimits();
+            double left = limits.Rect.XMin;
+            double center = limits.Rect.XCenter;
+        }
+    }
+
+    internal class AutoScale : RecipeTestBase
+    {
+        public override string Name => "Zoom to Fit Data";
+        public override string Description => "The axis limits can be automatically adjusted to fit the data. " +
+            "Optional arguments allow users to define the amount of whitespace around the edges of the data.";
+
+        [Test]
+        public override void Recipe()
+        {
+            myPlot.Add.Signal(Generate.Sin(51));
+            myPlot.Add.Signal(Generate.Cos(51));
+
+            // set limits that do not fit the data
+            myPlot.SetAxisLimits(-100, 150, -5, 5);
+
+            // reset limits to fit the data
+            myPlot.AutoScale();
         }
     }
 
--- src/ScottPlot5/ScottPlot5/TODO.cs
+++ src/ScottPlot5/ScottPlot5/TODO.cs
@@ -2,16 +2,11 @@
 
 internal static class TODO
 {
-    // what do empty plots look like?
-
-    // control designer view sucks - at least add some axes and a grid
-
-    // signal plots containing only horizontal data look bad at the edges.
-    // Plot a 10k point sine wave to demonstrate thi sissue.
-
-    // custom object for line style
-
-    // need more marker shapes
-
-    // tick styles need to be exposed. Right now color is just whatever the label color is.
+    /* TASKS TO COMPLETE AFTER THE FIRST RELEASE:
+     * DateTime tick generator
+     * Manual tick generator
+     * Marker class
+     * Style classes to group common configuration options: LineStyle and FontStyle
+     * Improve signal plot rendering when several repeated values produce a vertical height of zero
+     */
 }
