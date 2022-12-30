e3016378e5befcda38515c277f2253abbf48a04c
Scott W Harden <swharden@gmail.com>
2022-12-30 10:23:23 -0500

    SP5: create TitlePanel

---
+++ src/ScottPlot5/ScottPlot5/Panels/TitlePanel.cs
@@ -0,0 +1,55 @@
+﻿using ScottPlot.LayoutSystem;
+
+namespace ScottPlot.Panels;
+
+public class TitlePanel : IPanel
+{
+    public Edge Edge => Edge.Top;
+
+    public bool ShowDebugInformation { get; set; } = false;
+
+    public Label Label { get; } = new()
+    {
+        Text = string.Empty,
+        FontName = FontService.SansFontName,
+        FontSize = 16,
+        Bold = true,
+        Alignment = Alignment.LowerCenter,
+    };
+
+    /// <summary>
+    /// Extra space to add above the title text so the title does not touch the edge of the image
+    /// </summary>
+    public float VerticalPadding = 10;
+
+    public PixelRect GetPanelRect(PixelRect dataRect, float size, float offset)
+    {
+        return new PixelRect(
+            left: dataRect.Left,
+            right: dataRect.Right,
+            bottom: dataRect.Top - offset,
+            top: dataRect.Top - offset - size);
+    }
+
+    public float Measure()
+    {
+        if (string.IsNullOrWhiteSpace(Label.Text))
+            return 0;
+        else
+            return Label.Measure().Height + VerticalPadding;
+    }
+
+    public void Render(SKSurface surface, PixelRect dataRect, float size, float offset)
+    {
+        PixelRect panelRect = GetPanelRect(dataRect, size, offset);
+
+        Pixel labelPoint = new(panelRect.HorizontalCenter, panelRect.Bottom);
+
+        if (ShowDebugInformation)
+        {
+            Drawing.DrawDebugRectangle(surface.Canvas, panelRect, labelPoint, Label.Color);
+        }
+
+        Label.Draw(surface.Canvas, labelPoint);
+    }
+}
