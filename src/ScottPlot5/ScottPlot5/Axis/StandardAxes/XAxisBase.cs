f5ff254fb53f3c8d3140c76e2f501e69db7f495f
Scott W Harden <swharden@gmail.com>
2022-12-31 13:36:56 -0500

    SP5: refactor AxisBase

--- src/ScottPlot5/ScottPlot5/Axis/StandardAxes/XAxisBase.cs
+++ src/ScottPlot5/ScottPlot5/Axis/StandardAxes/XAxisBase.cs
@@ -1,62 +1,8 @@
-﻿using ScottPlot.Style;
+﻿namespace ScottPlot.Axis.StandardAxes;
 
-namespace ScottPlot.Axis.StandardAxes;
-
-public abstract class XAxisBase : IAxis
+public abstract class XAxisBase : AxisBase, IAxis
 {
-    public bool IsVisible { get; set; } = true;
-
-    public double Min
-    {
-        get => Range.Min;
-        set => Range.Min = value;
-    }
-
-    public double Max
-    {
-        get => Range.Max;
-        set => Range.Max = value;
-    }
-
     public double Width => Range.Span;
-
-    public virtual CoordinateRange Range { get; private set; } = CoordinateRange.NotSet;
-
-    public ITickGenerator TickGenerator { get; set; } = null!;
-
-    public Label Label { get; private set; } = new()
-    {
-        Text = string.Empty,
-        Font = new() { Size = 16, Bold = true },
-    };
-
-    public FontStyle TickFont { get; set; } = new();
-
-    public abstract Edge Edge { get; }
-    public bool ShowDebugInformation { get; set; } = false;
-
-    public float MajorTickLength { get; set; } = 4;
-    public float MajorTickWidth { get; set; } = 1;
-
-    public Color MajorTickColor { get; set; } = Colors.Black;
-    public TickStyle MajorTickStyle => new()
-    {
-        Length = MajorTickLength,
-        Width = MajorTickWidth,
-        Color = MajorTickColor
-    };
-
-    public float MinorTickLength { get; set; } = 2;
-    public float MinorTickWidth { get; set; } = 1;
-    public Color MinorTickColor { get; set; } = Colors.Black;
-    public TickStyle MinorTickStyle => new()
-    {
-        Length = MinorTickLength,
-        Width = MinorTickWidth,
-        Color = MinorTickColor
-    };
-
-    public LineStyle FrameLineStyle { get; } = new();
 
     public float Measure()
     {
--- src/ScottPlot5/ScottPlot5/Axis/StandardAxes/YAxisBase.cs
+++ src/ScottPlot5/ScottPlot5/Axis/StandardAxes/YAxisBase.cs
@@ -1,65 +1,8 @@
-﻿using ScottPlot.Style;
+﻿namespace ScottPlot.Axis.StandardAxes;
 
-namespace ScottPlot.Axis.StandardAxes;
-
-// TODO: move shared code into a common AxisBase class
-
-public abstract class YAxisBase : IAxis
+public abstract class YAxisBase : AxisBase, IAxis
 {
-    public bool IsVisible { get; set; } = true;
-
-    public double Min
-    {
-        get => Range.Min;
-        set => Range.Min = value;
-    }
-
-    public double Max
-    {
-        get => Range.Max;
-        set => Range.Max = value;
-    }
-
     public double Height => Range.Span;
-
-    public virtual CoordinateRange Range { get; private set; } = CoordinateRange.NotSet;
-
-    public ITickGenerator TickGenerator { get; set; } = null!;
-
-    public Label Label { get; private set; } = new()
-    {
-        Text = string.Empty,
-        Font = new() { Size = 16, Bold = true },
-        Rotation = -90,
-    };
-
-    public FontStyle TickFont { get; set; } = new();
-
-    public abstract Edge Edge { get; }
-
-    public bool ShowDebugInformation { get; set; } = false;
-
-    public float MajorTickLength { get; set; } = 4;
-    public float MajorTickWidth { get; set; } = 1;
-    public Color MajorTickColor { get; set; } = Colors.Black;
-    public TickStyle MajorTickStyle => new()
-    {
-        Length = MajorTickLength,
-        Width = MajorTickWidth,
-        Color = MajorTickColor
-    };
-
-    public float MinorTickLength { get; set; } = 2;
-    public float MinorTickWidth { get; set; } = 1;
-    public Color MinorTickColor { get; set; } = Colors.Black;
-    public TickStyle MinorTickStyle => new()
-    {
-        Length = MinorTickLength,
-        Width = MinorTickWidth,
-        Color = MinorTickColor
-    };
-
-    public LineStyle FrameLineStyle { get; } = new();
 
     public float GetPixel(double position, PixelRect dataArea)
     {
