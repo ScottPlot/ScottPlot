1f1a5614a769db12c582970834aea1f6fc29e17b
Scott W Harden <swharden@gmail.com>
2022-12-31 07:42:02 -0500

    SP5 Cookbook: customizing ticks

---
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Axis/CustomizingTicks.cs
@@ -0,0 +1,29 @@
+﻿namespace ScottPlotCookbook.Recipes.Axis;
+
+internal class CustomizingTicks : RecipePageBase
+{
+    public override RecipePageDetails PageDetails => new()
+    {
+        Chapter = Chapter.Customization,
+        PageName = "Customizing Ticks",
+        PageDescription = "Advanced customization of tick marks and tick labels",
+    };
+
+    internal class AltTickGen : RecipeTestBase
+    {
+        public override string Name => "Custom Tick Generators";
+        public override string Description => "Alternative tick generators can be created and assigned to axes. " +
+            "Some common tick generators are provided with ScottPlot, and users also have the option create their own.";
+
+        [Test]
+        public override void Recipe()
+        {
+            myPlot.Add.Signal(Generate.Sin(51));
+            myPlot.Add.Signal(Generate.Cos(51));
+
+            ITickGenerator customTickGenerator = new ScottPlot.TickGenerators.NumericFixedInterval(11);
+
+            myPlot.XAxis.TickGenerator = customTickGenerator;
+        }
+    }
+}
