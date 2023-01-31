21496c76d5a3565e071c9dee7fedfb0800b3cbb9
Scott W Harden <swharden@gmail.com>
2023-01-30 19:35:42 -0500

    SP5: Axis.DateXAxis → Axis.DateTimeAxes.DateTimeXAxis

---
+++ src/ScottPlot5/ScottPlot5/Axis/DateTimeAxes/DateTimeXAxis.cs
@@ -0,0 +1,27 @@
+﻿using ScottPlot.Axis.StandardAxes;
+using ScottPlot.TickGenerators;
+
+namespace ScottPlot.Axis.DateTimeAxes;
+
+public class DateTimeXAxis : XAxisBase, IXAxis, IDateAxis
+{
+    public override Edge Edge { get; } = Edge.Bottom;
+
+    private IDateTickGenerator _tickGenerator = new DateAutomatic();
+    public override ITickGenerator TickGenerator
+    {
+        get => _tickGenerator;
+        set
+        {
+            if (value is not IDateTickGenerator)
+                throw new ArgumentException($"Date axis must have a {nameof(ITickGenerator)} generator");
+
+            _tickGenerator = (IDateTickGenerator)value;
+        }
+    }
+
+    public IEnumerable<double> ConvertToCoordinateSpace(IEnumerable<DateTime> dates) =>
+        TickGenerator is IDateTickGenerator dateTickGenerator
+            ? dateTickGenerator.ConvertToCoordinateSpace(dates)
+            : throw new InvalidOperationException("Date axis configured with non-date tick generator");
+}
