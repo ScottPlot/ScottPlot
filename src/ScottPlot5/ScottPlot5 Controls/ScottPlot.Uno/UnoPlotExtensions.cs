2c8634e82a7e8325ec80687c6b4706efa945cf63
Tore Fuglestad <tore@fuglestad.no>
2023-02-03 17:30:11 +0100

    add projects, wpf-clones

---
+++ src/ScottPlot5/ScottPlot5 Controls/ScottPlot.Uno/UnoPlotExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ScottPlot.Uno;
+
+internal static class UnoPlotExtensions
+{
+    internal static Pixel Pixel(this MouseEventArgs e, UnoPlot plot)
+    {
+        DpiScale dpiScale = VisualTreeHelper.GetDpi(plot);
+        double x = e.GetPosition(plot).X * dpiScale.DpiScaleX;
+        double y = e.GetPosition(plot).Y * dpiScale.DpiScaleY;
+        return new Pixel((float)x, (float)y);
+    }
+
+    internal static Control.MouseButton ToButton(this MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Middle)
+            return Control.MouseButton.Middle;
+        else if (e.ChangedButton == MouseButton.Left)
+            return Control.MouseButton.Left;
+        else if (e.ChangedButton == MouseButton.Right)
+            return Control.MouseButton.Right;
+        else
+            return Control.MouseButton.Unknown;
+    }
+
+    internal static Control.Key Key(this KeyEventArgs e)
+    {
+        // Uno likes to snatch Alt, in which case we have to grab the system key value
+        var key = e.Key == System.Windows.Input.Key.System ? e.SystemKey : e.Key;
+
+        return key switch
+        {
+            System.Windows.Input.Key.LeftCtrl => Control.Key.Ctrl,
+            System.Windows.Input.Key.RightCtrl => Control.Key.Ctrl,
+            System.Windows.Input.Key.LeftAlt => Control.Key.Alt,
+            System.Windows.Input.Key.RightAlt => Control.Key.Alt,
+            System.Windows.Input.Key.LeftShift => Control.Key.Shift,
+            System.Windows.Input.Key.RightShift => Control.Key.Shift,
+            _ => Control.Key.Unknown,
+        };
+    }
+}
+
