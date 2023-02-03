71e72c33e7b19e19c57b244f22eb3f4894697b7c
Tore Fuglestad <tore@fuglestad.no>
2023-02-03 20:31:14 +0100

    switch demo-app to uno-template app

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Uno/App.xaml.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Uno/App.xaml.cs
@@ -1,17 +1,112 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
 
 namespace Sandbox.Uno
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	public sealed partial class App : Application
+	{
+		private Window _window;
+
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			this.InitializeComponent();
+
+#if HAS_UNO || NETFX_CORE
+            this.Suspending += OnSuspending;
+#endif
+		}
+
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="args">Details about the launch request and process.</param>
+		protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+		{
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+
+#if NET6_0_OR_GREATER && WINDOWS && !HAS_UNO
+            _window = new Window();
+            _window.Activate();
+#else
+			_window = Microsoft.UI.Xaml.Window.Current;
+#endif
+			_window.Title = "ScottPlot 5 - Uno Sandbox";
+
+			var rootFrame = _window.Content as Frame;
+
+			// Do not repeat app initialization when the Window already has content,
+			// just ensure that the window is active
+			if (rootFrame == null)
+			{
+				// Create a Frame to act as the navigation context and navigate to the first page
+				rootFrame = new Frame();
+
+				rootFrame.NavigationFailed += OnNavigationFailed;
+
+				if (args.UWPLaunchActivatedEventArgs.PreviousExecutionState == ApplicationExecutionState.Terminated)
+				{
+					// TODO: Load state from previously suspended application
+				}
+
+				// Place the frame in the current Window
+				_window.Content = rootFrame;
+			}
+
+#if !(NET6_0_OR_GREATER && WINDOWS)
+			if (args.UWPLaunchActivatedEventArgs.PrelaunchActivated == false)
+#endif
+			{
+				if (rootFrame.Content == null)
+				{
+					// When the navigation stack isn't restored navigate to the first page,
+					// configuring the new page by passing required information as a navigation
+					// parameter
+					rootFrame.Navigate(typeof(MainPage), args.Arguments);
+				}
+				// Ensure the current window is active
+				_window.Activate();
+			}
+		}
+
+		/// <summary>
+		/// Invoked when Navigation to a certain page fails
+		/// </summary>
+		/// <param name="sender">The Frame which failed navigation</param>
+		/// <param name="e">Details about the navigation failure</param>
+		void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+		{
+			throw new InvalidOperationException($"Failed to load {e.SourcePageType.FullName}: {e.Exception}");
+		}
+
+		/// <summary>
+		/// Invoked when application execution is being suspended.  Application state is saved
+		/// without knowing whether the application will be terminated or resumed with the contents
+		/// of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			var deferral = e.SuspendingOperation.GetDeferral();
+			//TODO: Save application state and stop any background activity
+			deferral.Complete();
+		}
+	}
 }
