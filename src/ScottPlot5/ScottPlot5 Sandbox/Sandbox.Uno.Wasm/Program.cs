34eb088a6129352f23a13e53ac22a9aae90d06bf
Tore Fuglestad <tore@fuglestad.no>
2023-02-09 18:34:21 +0100

    add Sandbox.Uno.Wasm project

---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Uno.Wasm/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+
+namespace Sandbox.Uno.Wasm
+{
+	public class Program
+	{
+		private static Sandbox.WinUI.App _app;
+
+		static int Main(string[] args)
+		{
+			Microsoft.UI.Xaml.Application.Start(_ => _app = new Sandbox.WinUI.App());
+
+			return 0;
+		}
+	}
+}
