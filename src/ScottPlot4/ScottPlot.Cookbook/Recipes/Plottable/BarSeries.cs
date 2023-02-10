7ed968b9c51beda5bc5f143926286b4bdd17a650
Scott W Harden <swharden@gmail.com>
2023-02-09 19:38:05 -0500

    SP4 Cookbook: bar series with custom labels
    resolves #2357

--- src/ScottPlot4/ScottPlot.Cookbook/Recipes/Plottable/BarSeries.cs
+++ src/ScottPlot4/ScottPlot.Cookbook/Recipes/Plottable/BarSeries.cs
@@ -165,4 +165,48 @@ namespace ScottPlot.Cookbook.Recipes.Plottable
             plt.SetAxisLimits(-5, 5, -5, 70);
         }
     }
+
+    public class BarSeriesCustomLabel : IRecipe
+    {
+        public ICategory Category => new Categories.PlotTypes.BarSeries();
+        public string ID => "barseries_custom_label";
+        public string Title => "Custom Label";
+        public string Description => "Value labels above bars can be customized.";
+
+        public void ExecuteRecipe(Plot plt)
+        {
+            // This is the data we will plot
+            double[] values = { 10, 23, 13, 7, 9 };
+
+            // Create a palette to use for bar and label colors
+            IPalette myPalette = new ScottPlot.Palettes.Aurora();
+
+            // Add a BarSeries to the plot
+            BarSeries barSeries = plt.AddBarSeries();
+
+            // iterate values
+            for (int i = 0; i < values.Length; i++)
+            {
+                // add a bar to the bar series
+                Bar bar = new()
+                {
+                    Value = values[i],
+                    Position = i,
+                    FillColor = myPalette.GetColor(i),
+                    LineColor = Color.Black,
+                    LineWidth = 1,
+                };
+                barSeries.Bars.Add(bar);
+
+                // add a label and customize it as desired
+                var txt = plt.AddText(values[i].ToString(), i, values[i]);
+                txt.Color = myPalette.GetColor(i);
+                txt.Font.Alignment = Alignment.LowerCenter;
+                txt.Font.Size = 24;
+                txt.Font.Bold = true;
+            }
+
+            plt.SetAxisLimits(-1, 5, 0, 30);
+        }
+    }
 }
