56598783a74d4800745dedf49e441be53b2e38c7
Benjamin Lehmann <bclehmann@yahoo.ca>
2023-02-12 17:03:39 -0700

    OHLC Struct

---
+++ src/ScottPlot5/ScottPlot5/OHLC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlot
+{
+    public readonly struct OHLC
+    {
+        public double Open { get; }
+        public double High { get; }
+        public double Low { get; }
+        public double Close { get; }
+
+        public OHLC(double open, double high, double low, double close)
+        {
+            Open = open;
+            High = high;
+            Low = low;
+            Close = close;
+        }
+    }
+}
