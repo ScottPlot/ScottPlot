26c3e4fd2e2d4761456521eea7cd2940115e3c16
Scott W Harden <swharden@gmail.com>
2023-02-19 13:04:59 -0500

    SP4 EtoPlot: simplify linked axis demo

--- src/ScottPlot4/ScottPlot.Demo/ScottPlot.Demo.Eto/EtoFormsDemos/LinkedPlots.cs
+++ src/ScottPlot4/ScottPlot.Demo/ScottPlot.Demo.Eto/EtoFormsDemos/LinkedPlots.cs
@@ -1,47 +1,20 @@
-﻿using System;
-using Eto.Forms;
-using ScottPlot.Eto;
+﻿using Eto.Forms;
 
 namespace ScottPlot.Demo.Eto.EtoFormsDemos
 {
     public partial class LinkedPlots : Form
     {
-        readonly PlotView[] FormsPlots;
-
         public LinkedPlots()
         {
             InitializeComponent();
-            formsPlot1.Plot.AddSignal(DataGen.Sin(51));
-            formsPlot2.Plot.AddSignal(DataGen.Cos(51));
+            formsPlot1.Plot.AddSignal(DataGen.Sin(51), color: System.Drawing.Color.Blue);
+            formsPlot2.Plot.AddSignal(DataGen.Cos(51), color: System.Drawing.Color.Red);
 
-            // create a list of plot controls we can easily iterate through later
-            FormsPlots = new PlotView[] { formsPlot1, formsPlot2 };
-            foreach (var fp in FormsPlots)
-                fp.AxesChanged += OnAxesChanged;
-        }
-        private void OnAxesChanged(object sender, EventArgs e)
-        {
-            if (cbLinked.Checked == false)
-                return;
+            formsPlot1.Refresh();
+            formsPlot2.Refresh();
 
-            PlotView changedPlot = (PlotView)sender;
-            var newAxisLimits = changedPlot.Plot.GetAxisLimits();
-
-            SuspendLayout();
-
-            foreach (var fp in FormsPlots)
-            {
-                if (fp == changedPlot)
-                    continue;
-
-                // disable events briefly to avoid an infinite loop
-                fp.Configuration.AxesChangedEventEnabled = false;
-                fp.Plot.SetAxisLimits(newAxisLimits);
-                fp.Refresh();
-                fp.Configuration.AxesChangedEventEnabled = true;
-            }
-
-            ResumeLayout();
+            formsPlot1.Configuration.AddLinkedControl(formsPlot2); // update plot 2 when plot 1 changes
+            formsPlot2.Configuration.AddLinkedControl(formsPlot1); // update plot 1 when plot 2 changes
         }
     }
 }
--- src/ScottPlot4/ScottPlot.Demo/ScottPlot.Demo.Eto/EtoFormsDemos/LinkedPlots.eto.cs
+++ src/ScottPlot4/ScottPlot.Demo/ScottPlot.Demo.Eto/EtoFormsDemos/LinkedPlots.eto.cs
@@ -9,10 +9,9 @@ namespace ScottPlot.Demo.Eto.EtoFormsDemos
         {
             this.formsPlot1 = new ScottPlot.Eto.PlotView();
             this.formsPlot2 = new ScottPlot.Eto.PlotView();
-            this.cbLinked = new CheckBox();
             this.SuspendLayout();
 
-            this.Content = new DynamicLayout(cbLinked, formsPlot1, formsPlot2) { Padding = 5 };
+            this.Content = new DynamicLayout(formsPlot1, formsPlot2) { Padding = 5 };
 
             // 
             // formsPlot1
@@ -25,12 +24,6 @@ namespace ScottPlot.Demo.Eto.EtoFormsDemos
             this.formsPlot2.BackgroundColor = Colors.Transparent;
             this.formsPlot2.Size = new Size(-1, 201);
             // 
-            // cbLinked
-            // 
-            this.cbLinked.Checked = true;
-            this.cbLinked.Size = new Size(119, 19);
-            this.cbLinked.Text = "link axes together";
-            // 
             // LinkedPlots
             // 
             this.ClientSize = new Size(769, -1);
@@ -40,6 +33,5 @@ namespace ScottPlot.Demo.Eto.EtoFormsDemos
 
         private ScottPlot.Eto.PlotView formsPlot1;
         private ScottPlot.Eto.PlotView formsPlot2;
-        private CheckBox cbLinked;
     }
 }
