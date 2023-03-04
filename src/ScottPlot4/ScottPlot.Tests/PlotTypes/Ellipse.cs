af66ebbf89144082174847dedbd8a18aa8e3cacb
Scott W Harden <swharden@gmail.com>
2023-03-04 11:59:09 -0500

    SP4 Test: Ellipse

---
+++ src/ScottPlot4/ScottPlot.Tests/PlotTypes/Ellipse.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using ScottPlotTests;
+
+namespace ScottPlot.Tests.PlotTypes;
+
+internal class Ellipse
+{
+    [Test]
+    public void Test_Ellipse_AddCircle()
+    {
+        Plot plt = new(400, 300);
+
+        plt.AddCircle(0, 0, 5);
+        plt.AddCircle(2, 3, 5);
+        plt.AddCircle(2, 3, 0.5);
+        plt.AddCircle(0, 0, -1);
+
+        TestTools.SaveFig(plt);
+    }
+
+    [Test]
+    public void Test_Ellipse_AddElipse()
+    {
+        Plot plt = new(400, 300);
+
+        plt.AddEllipse(3, 0, 2, 2);
+        plt.AddEllipse(2, 3, 5, 1);
+        plt.AddEllipse(0, 0, 1, 5);
+
+        TestTools.SaveFig(plt);
+    }
+}
