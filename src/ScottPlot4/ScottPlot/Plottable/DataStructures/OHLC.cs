7f05223edfc23827062bbe5d6543f900e89c041a
Scott W Harden <swharden@gmail.com>
2023-03-28 23:26:56 -0400

    SP4 Finance demo: use IOHLC

--- src/ScottPlot4/ScottPlot/Plottable/DataStructures/OHLC.cs
+++ src/ScottPlot4/ScottPlot/Plottable/DataStructures/OHLC.cs
@@ -14,23 +14,10 @@ namespace ScottPlot
         public DateTime DateTime { get; set; }
         public TimeSpan TimeSpan { get; set; }
 
-        // TODO: obsolete this
+        //[Obsolete("The `Volume` property of OHLCs has been deprecated.")]
         public double Volume { get; set; }
 
         private bool IsNanOrInfinity(double val) => double.IsInfinity(val) || double.IsNaN(val);
-
-        public bool IsValid
-        {
-            get
-            {
-                if (IsNanOrInfinity(Open)) return false;
-                if (IsNanOrInfinity(High)) return false;
-                if (IsNanOrInfinity(Low)) return false;
-                if (IsNanOrInfinity(Close)) return false;
-                if (IsNanOrInfinity(Volume)) return false;
-                return true;
-            }
-        }
 
         public override string ToString() =>
             $"OHLC: open={Open}, high={High}, low={Low}, close={Close}, start={DateTime}, span={TimeSpan}, volume={Volume}";
@@ -44,8 +31,7 @@ namespace ScottPlot
         /// <param name="close">closing price</param>
         /// <param name="timeStart">open time</param>
         /// <param name="timeSpan">width of the OHLC</param>
-        /// <param name="volume">transaction volume for this time span</param>
-        public OHLC(double open, double high, double low, double close, DateTime timeStart, TimeSpan timeSpan, double volume = 0)
+        public OHLC(double open, double high, double low, double close, DateTime timeStart, TimeSpan timeSpan)
         {
             Open = open;
             High = high;
@@ -53,19 +39,9 @@ namespace ScottPlot
             Close = close;
             DateTime = timeStart;
             TimeSpan = timeSpan;
-            Volume = volume;
         }
 
-        /// <summary>
-        /// OHLC price over a specific period of time
-        /// </summary>
-        /// <param name="open">opening price</param>
-        /// <param name="high">maximum price</param>
-        /// <param name="low">minimum price</param>
-        /// <param name="close">closing price</param>
-        /// <param name="timeStart">open time (DateTime.ToOADate() units)</param>
-        /// <param name="timeSpan">width of the OHLC in days</param>
-        /// <param name="volume">transaction volume for this time span</param>
+        //[Obsolete("This constructor has been deprecated.")]
         public OHLC(double open, double high, double low, double close, double timeStart, double timeSpan = 1, double volume = 0)
         {
             Open = open;
