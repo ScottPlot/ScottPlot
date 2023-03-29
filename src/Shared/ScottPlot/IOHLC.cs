388fb7f58a0900d19123752e4e1c5b9c6b763787
Scott W Harden <swharden@gmail.com>
2023-03-28 22:34:28 -0400

    Shared: IOHLC
    #2404

---
+++ src/Shared/ScottPlot/IOHLC.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ScottPlot;
+
+public interface IOHLC
+{
+    double Open { get; }
+    double High { get; }
+    double Low { get; }
+    double Close { get; }
+    DateTime DateTime { get; }
+    TimeSpan TimeSpan { get; }
+}
