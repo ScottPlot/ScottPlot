4826496790063addbc8946fcacaf90716cb4d23d
Scott W Harden <swharden@gmail.com>
2023-04-01 19:36:42 -0400

    SP4 Test: demonstrate AxisPanCenter issue #2483

--- src/ScottPlot4/ScottPlot.Tests/Axis/PanAndZoom.cs
+++ src/ScottPlot4/ScottPlot.Tests/Axis/PanAndZoom.cs
@@ -1,11 +1,64 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
-using ScottPlotTests;
 
 namespace ScottPlot.Tests.Axis;
 
 internal class PanAndZoom
 {
+    [Test]
+    public void Test_PanCenter_PrimaryAxes()
+    {
+        ScottPlot.Plot plt = new();
+
+        plt.SetAxisLimits(-5, 5, -25, 25);
+        var limits1 = plt.GetAxisLimits();
+        limits1.XMin.Should().Be(-5);
+        limits1.XMax.Should().Be(5);
+        limits1.XCenter.Should().Be(0);
+        limits1.YMin.Should().Be(-25);
+        limits1.YMax.Should().Be(25);
+        limits1.YCenter.Should().Be(0);
+
+        plt.AxisPanCenter(1, 1);
+        var limits2 = plt.GetAxisLimits();
+        limits2.XMin.Should().Be(-5 + 1);
+        limits2.XMax.Should().Be(5 + 1);
+        limits2.XCenter.Should().Be(0 + 1);
+        limits2.YMin.Should().Be(-25 + 1);
+        limits2.YMax.Should().Be(25 + 1);
+        limits2.YCenter.Should().Be(0 + 1);
+    }
+
+    [Test]
+    public void Test_PanCenter_SecondaryAxes()
+    {
+        ScottPlot.Plot plt = new();
+
+        plt.SetAxisLimits(-1, 1, -1, 1);
+        var originalPrimaryLimits = plt.GetAxisLimits();
+
+        plt.SetAxisLimits(-5, 5, -25, 25, xAxisIndex: 1, yAxisIndex: 1);
+
+        var limits1 = plt.GetAxisLimits(xAxisIndex: 1, yAxisIndex: 1);
+        limits1.XMin.Should().Be(-5);
+        limits1.XMax.Should().Be(5);
+        limits1.XCenter.Should().Be(0);
+        limits1.YMin.Should().Be(-25);
+        limits1.YMax.Should().Be(25);
+        limits1.YCenter.Should().Be(0);
+
+        plt.AxisPanCenter(1, 1, xAxisIndex: 1, yAxisIndex: 1);
+        var limits2 = plt.GetAxisLimits(xAxisIndex: 1, yAxisIndex: 1);
+        limits2.XMin.Should().Be(-5 + 1);
+        limits2.XMax.Should().Be(5 + 1);
+        limits2.XCenter.Should().Be(0 + 1);
+        limits2.YMin.Should().Be(-25 + 1);
+        limits2.YMax.Should().Be(25 + 1);
+        limits2.YCenter.Should().Be(0 + 1);
+
+        plt.GetAxisLimits().Should().Be(originalPrimaryLimits);
+    }
+
     [Test]
     public void Test_SecondaryAxisPan_ShouldNotChangePrimaryAxisLimits()
     {
