0f6f2bce006d07963b8b6fa2b989f868a6ad67a8
Scott W Harden <swharden@gmail.com>
2023-04-01 22:35:13 -0400

    SP4 OHLC: add OADate constructor
    eliminates build warnings

--- src/ScottPlot4/ScottPlot/Plottable/DataStructures/OHLC.cs
+++ src/ScottPlot4/ScottPlot/Plottable/DataStructures/OHLC.cs
@@ -39,6 +39,25 @@ namespace ScottPlot
             TimeSpan = timeSpan;
         }
 
+        /// <summary>
+        /// OHLC price over a specific period of time using the OADate format
+        /// </summary>
+        /// <param name="open">opening price</param>
+        /// <param name="high">maximum price</param>
+        /// <param name="low">minimum price</param>
+        /// <param name="close">closing price</param>
+        /// <param name="oaDate">start time (days since 1900)</param>
+        /// <param name="oaDateSpan">width of the OHLC (days)</param>
+        public OHLC(double open, double high, double low, double close, double oaDate, double oaDateSpan = 1)
+        {
+            Open = open;
+            High = high;
+            Low = low;
+            Close = close;
+            DateTime = DateTime.FromOADate(oaDate);
+            TimeSpan = TimeSpan.FromDays(oaDateSpan);
+        }
+
         [Obsolete("This constructor has been deprecated.")]
         public OHLC(double open, double high, double low, double close, double timeStart, double timeSpan = 1, double volume = 0)
         {
