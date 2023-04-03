ad54b356bd81b919da1f8bed7e11a7e6439d49cf
Scott W Harden <swharden@gmail.com>
2023-04-03 19:02:36 -0400

    SP4 Lollipop: LineWidth property
    #2556

---
+++ src/ScottPlot4/ScottPlot.Tests/PlotTypes/Lollipop.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using ScottPlotTests;
+
+namespace ScottPlot.Tests.PlotTypes;
+
+internal class Lollipop
+{
+    [Test]
+    public void Test_Lollipop_LineWidth()
+    {
+        var xs = new double[] { 2, 3, 4, 5 };
+        var ys = new double[] { 4, 5, 6, 7 };
+
+        ScottPlot.Plot plt = new(400, 300);
+        var lp = plt.AddLollipop(xs, ys);
+        lp.LineWidth = 5;
+        lp.LollipopRadius = 10;
+
+        TestTools.SaveFig(plt);
+    }
+}
