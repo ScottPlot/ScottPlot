6ccc8a438fe2cead31b8e4760f0634f68baaeb5a
Scott W Harden <swharden@gmail.com>
2023-04-06 01:30:56 -0400

    SP4: Plot.Launch
    resolves #2543

---
+++ src/ScottPlot4/ScottPlot/Launcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ScottPlot;
+
+public class Launcher
+{
+    public Plot Plot { get; }
+
+    public Launcher(Plot plot) { Plot = plot; }
+}
+
+public static class LauncherExtensions
+{
+    /// <summary>
+    /// Save the plot as an image file and open it with the default file launcher
+    /// </summary>
+    public static void ImageFile(this Launcher launcher, string saveAs = "plot.png", int width = 600, int height = 400)
+    {
+        saveAs = Path.GetFullPath(saveAs);
+        launcher.Plot.SaveFig(saveAs, width, height);
+        Console.WriteLine($"Launching: {saveAs}");
+
+        ProcessStartInfo psi = new(saveAs) { UseShellExecute = true };
+        Process.Start(psi);
+    }
+}
