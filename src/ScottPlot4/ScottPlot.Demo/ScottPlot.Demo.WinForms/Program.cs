9dcc82a5173724ceb8adf28038e82c3de2a526b6
Scott W Harden <swharden@gmail.com>
2023-05-17 18:30:32 -0400

    SP4 WinForms Demo: launch menu on startup

--- src/ScottPlot4/ScottPlot.Demo/ScottPlot.Demo.WinForms/Program.cs
+++ src/ScottPlot4/ScottPlot.Demo/ScottPlot.Demo.WinForms/Program.cs
@@ -17,8 +17,8 @@ namespace ScottPlot.Demo.WinForms
             //Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FormMain());
-            Application.Run(new WinFormsDemos.DataLogger());
+            Application.Run(new FormMain());
+            //Application.Run(new WinFormsDemos.DataLogger());
         }
     }
 }
