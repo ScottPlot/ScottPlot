120c523c401ea59c615a41cf4ec09a3a3b59ba62
Scott W Harden <swharden@gmail.com>
2023-05-22 08:35:18 -0400

    SP4 WinForms Demo: DataStreamer

---
+++ src/ScottPlot4/ScottPlot.Demo/ScottPlot.Demo.WinForms/WinFormsDemos/DataStreamer.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ScottPlot.Demo.WinForms.WinFormsDemos;
+
+partial class DataStreamer
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        formsPlot1 = new FormsPlot();
+        comboBox1 = new System.Windows.Forms.ComboBox();
+        SuspendLayout();
+        // 
+        // formsPlot1
+        // 
+        formsPlot1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+        formsPlot1.Location = new System.Drawing.Point(13, 41);
+        formsPlot1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        formsPlot1.Name = "formsPlot1";
+        formsPlot1.Size = new System.Drawing.Size(774, 397);
+        formsPlot1.TabIndex = 0;
+        // 
+        // comboBox1
+        // 
+        comboBox1.FormattingEnabled = true;
+        comboBox1.Location = new System.Drawing.Point(12, 12);
+        comboBox1.Name = "comboBox1";
+        comboBox1.Size = new System.Drawing.Size(121, 23);
+        comboBox1.TabIndex = 1;
+        // 
+        // DataStreamer
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(comboBox1);
+        Controls.Add(formsPlot1);
+        Name = "DataStreamer";
+        Text = "DataStreamer";
+        ResumeLayout(false);
+    }
+
+    #endregion
+
+    private FormsPlot formsPlot1;
+    private System.Windows.Forms.ComboBox comboBox1;
+}
---
+++ src/ScottPlot4/ScottPlot.Demo/ScottPlot.Demo.WinForms/WinFormsDemos/DataStreamer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlot.Demo.WinForms.WinFormsDemos;
+public partial class DataStreamer : Form
+{
+    readonly Timer AddNewDataTimer = new() { Interval = 10, Enabled = true };
+    readonly Timer UpdatePlotTimer = new() { Interval = 50, Enabled = true };
+
+    readonly ScottPlot.Plottable.ScatterDataLogger Logger;
+
+    readonly Random Rand = new();
+
+    double LastPointValue = 0;
+
+    public DataStreamer()
+    {
+        InitializeComponent();
+
+        Logger = formsPlot1.Plot.AddScatterLogger();
+
+        AddRandomWalkData(1000);
+        formsPlot1.Refresh();
+
+        AddNewDataTimer.Tick += (s, e) => AddRandomWalkData(10);
+        UpdatePlotTimer.Tick += UpdatePlotTimer_Tick;
+    }
+
+    private void AddRandomWalkData(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            LastPointValue = LastPointValue + Rand.NextDouble() - .5;
+            Logger.Add(Logger.Count, LastPointValue);
+        }
+    }
+
+    private void UpdatePlotTimer_Tick(object sender, EventArgs e)
+    {
+        if (Logger.Count == Logger.LastRenderCount)
+            return;
+
+        formsPlot1.Refresh();
+
+        Text = $"DataStreamer Demo ({Logger.Count:N0} points)";
+    }
+}
