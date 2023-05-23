c50f39fac194390c3ae77d030569f9d9c1d8089a
Guillaume Deville <GDeville@set-sas.fr>
2023-05-23 12:04:06 +0200

    Remove launcher extension method #2627

--- src/ScottPlot4/ScottPlot/Launcher.cs
+++ src/ScottPlot4/ScottPlot/Launcher.cs
@@ -20,32 +20,29 @@ public class Launcher
         ProcessStartInfo psi = new(filePath) { UseShellExecute = true };
         Process.Start(psi);
     }
-}
 
-public static class LauncherExtensions
-{
     /// <summary>
     /// Save the plot as an image file and open it with the default file launcher
     /// </summary>
-    public static void ImageFile(this Launcher launcher, string saveAs = "plot.png", int width = 600, int height = 400)
+    public void ImageFile(string saveAs = "plot.png", int width = 600, int height = 400)
     {
-        launcher.Plot.SaveFig(saveAs, width, height);
-        launcher.Launch(saveAs);
+        Plot.SaveFig(saveAs, width, height);
+        Launch(saveAs);
     }
 
     /// <summary>
     /// Save the plot as an image embedded in a HTML file and launch it with the default web browser
     /// </summary>
-    public static void Web(this Launcher launcher, string saveAs = "plot.html", int width = 600, int height = 400)
+    public void Web(string saveAs = "plot.html", int width = 600, int height = 400)
     {
-        launcher.Plot.Resize(width, height);
+        Plot.Resize(width, height);
         string html = "<html>" +
             "<body style='text-align: center; margin-top: 2em;'>" +
             $"<code style='font-size: 1.5em;'>{DateTime.Now}</code>" +
-            $"<div style='margin: 1em;'>{launcher.Plot.GetImageHTML()}</div>" +
+            $"<div style='margin: 1em;'>{Plot.GetImageHTML()}</div>" +
             "</body>" +
             "</html>";
         File.WriteAllText(saveAs, html);
-        launcher.Launch(saveAs);
+        Launch(saveAs);
     }
 }
