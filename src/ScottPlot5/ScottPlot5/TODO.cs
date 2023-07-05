ff6ad7243b3e55be03209b18c55a9b65faa4155f
Scott W Harden <swharden@gmail.com>
2023-07-05 19:31:41 -0400

    Update TODO.cs
    closes #2419
    closes #2467
    closes #2596

--- src/ScottPlot5/ScottPlot5/TODO.cs
+++ src/ScottPlot5/ScottPlot5/TODO.cs
@@ -2,20 +2,39 @@
 
 internal static class TODO
 {
-    /* ### SCOTT'S RUNNING LIST OF UPCOMING TASKS ##    
-     * DateTime tick generator
-     *   IAxis.cs has lots of tick styling that should be a Axis.TickStyle
-     * Manual tick generator
-     * Make more marker class
-     * Improve signal plot rendering when several repeated values produce a vertical height of zero
-     * AutoScale() is called in the first render if SetAxisLimits() was used to set only a single axis value
-     * Add offset X and offset Y support to signal plots
-     * Multiplot (#1337)
-     * Add more data source classes
-     * Add XLabel(), YLabel(), and Title() shortcuts methods
-     * Need Plot.Remove to match Plot.Add and maybe even Plot.GetPlottables()
-     * Use something like an ILegendItemGenerator for each plottable so users can inject their own logic for formatting legend items
-     * A label class that can draw rotated text at a point respecting alignment would be great (https://github.com/ScottPlot/ScottPlot/issues/2202#issuecomment-1499708534)
-     * Add ScaleY to signal plot https://github.com/ScottPlot/ScottPlot/pull/2656
-     */
+    /*  
+
+    #########################################################################
+    ## This file tracks features that Scott will soon add to ScottPlot 5.  ##
+    ## I'm tracking these here instead of in GitHub issues.                ##
+    ## Items are grouped by topic (not ordered by priority).               ##
+    #########################################################################
+
+    ## SYSTEM ##
+    Fonts: Cache typefaces to improve performance https://github.com/ScottPlot/ScottPlot/issues/2419
+    Markers: add new shapes
+
+    ## AXIS AND TICKS ##
+    Ticks: DateTime tick generator
+    Ticks: Manual tick generator
+    Axis: Support axis scale locking https://github.com/ScottPlot/ScottPlot/issues/2467
+    Axis: IAxis.cs has lots of tick styling that should be a Axis.TickStyle
+    Axis: AutoScale() is called in the first render if SetAxisLimits() was used to set only a single axis value
+
+    ## PLOTTABLES ##
+    Signal: Fix issue where several repeated values produce a vertical height of zero
+    Signal: Add offset X and offset Y support to signal plots
+    Signal: Add ScaleY to signal plot https://github.com/ScottPlot/ScottPlot/pull/2656
+    Bar: Improve API https://github.com/ScottPlot/ScottPlot/issues/2596
+
+    ## PLOT ##
+    Plot: Add XLabel(), YLabel(), and Title() shortcuts methods
+    Plot: Need Plot.Remove to match Plot.Add and maybe even Plot.GetPlottables()
+    Legend: Use something like an ILegendItemGenerator for each plottable so users can inject their own logic for formatting legend items
+
+    ## UTILITY CLASSES ##
+    MultiPlot: Helper class for creating multi-plot figures https://github.com/ScottPlot/ScottPlot/issues/1137
+    Label: A class that can draw text at a point supporting alignment and rotation https://github.com/ScottPlot/ScottPlot/issues/2202#issuecomment-1499708534
+
+    */
 }
