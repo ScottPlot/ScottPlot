408874ca98f5266c58edadcca26dc3c29ad8847e
Scott W Harden <swharden@gmail.com>
2023-07-05 19:46:32 -0400

    FormsPlotViewer: use original code
    pulls code related to #2710 out of this PR (#2711)

--- src/ScottPlot4/ScottPlot.WinForms/FormsPlotViewer.cs
+++ src/ScottPlot4/ScottPlot.WinForms/FormsPlotViewer.cs
@@ -12,9 +12,6 @@ namespace ScottPlot
 {
     public partial class FormsPlotViewer : Form
     {
-        private readonly FormsPlot ParentControl;
-        public enum SyncType { NoSync, SyncToParent, SyncFromParent };
-
         public FormsPlotViewer(ScottPlot.Plot plot, int windowWidth = 600, int windowHeight = 400, string windowTitle = "ScottPlot Viewer")
         {
             InitializeComponent();
@@ -24,33 +21,6 @@ namespace ScottPlot
 
             formsPlot1.Reset(plot);
             formsPlot1.Refresh();
-        }
-
-        // CALL THIS OVERLOAD WHEN LAUNCHING A NEW WINDOW FROM AN EXISTING FORM
-        public FormsPlotViewer(FormsPlot parent, int windowWidth = 600, int windowHeight = 400, string windowTitle = "ScottPlot Viewer", SyncType syncType = SyncType.NoSync)
-        {
-            InitializeComponent();
-            Width = windowWidth;
-            Height = windowHeight;
-            Text = windowTitle;
-
-            ParentControl = parent;
-            formsPlot1.Reset(ParentControl.Plot);
-            formsPlot1.Refresh();
-
-            switch (syncType)
-            {
-                case SyncType.NoSync:
-                    break;
-
-                case SyncType.SyncToParent:
-                    formsPlot1.AxesChanged += (s, e) => ParentControl.Refresh();
-                    break;
-
-                case SyncType.SyncFromParent:
-                    parent.BitmapChangedOrUpdated += (s, e) => formsPlot1.Refresh();
-                    break;
-            }
         }
     }
 }
