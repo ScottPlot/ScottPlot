2bc0278bee406ca802a11c5d07b12998ef195f77
Scott W Harden <swharden@gmail.com>
2023-07-10 06:22:38 -0400

    SP4 Test: data plots with low point count
    demonstrates issue described in #2733

---
+++ src/ScottPlot4/ScottPlot.Tests/PlotTypes/DataPlots.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace ScottPlot.Tests.PlotTypes;
+
+internal class DataPlots
+{
+    [Test]
+    public void Test_DataLogger_NoPoints()
+    {
+        ScottPlot.Plot plt = new();
+        plt.AddDataLogger();
+        plt.Render();
+    }
+
+    [Test]
+    public void Test_DataLogger_SinglePoint()
+    {
+        ScottPlot.Plot plt = new();
+        var dl = plt.AddDataLogger();
+        dl.Add(42, 69);
+        plt.Render();
+    }
+
+    [Test]
+    public void Test_DataStreamer_NoPoints()
+    {
+        ScottPlot.Plot plt = new();
+        double[] values = new double[10];
+        plt.AddDataStreamer(values);
+        plt.Render();
+    }
+
+    [Test]
+    public void Test_DataStreamer_SinglePoint()
+    {
+        ScottPlot.Plot plt = new();
+        double[] values = new double[10];
+        var ds = plt.AddDataStreamer(values);
+        ds.Add(42);
+        plt.Render();
+    }
+}
