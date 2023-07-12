61cc8caec95a74a45c9bf8d10f19b43ec9883a9e
Scott W Harden <swharden@gmail.com>
2023-07-11 21:53:10 -0400

    SP5: ScatterTests
    demonstrate issue #2714

---
+++ src/ScottPlot5/ScottPlot5 Tests/RenderTests/Plottable/ScatterTests.cs
@@ -0,0 +1,43 @@
+﻿namespace ScottPlotTests.RenderTests.Plottable;
+
+internal class ScatterTests
+{
+    [Test]
+    public void Test_Scatter_Empty_Render()
+    {
+        // TODO: https://github.com/ScottPlot/ScottPlot/issues/2714
+
+        ScottPlot.Plot plt = new();
+        double[] xs = { };
+        double[] ys = { };
+        plt.Add.Scatter(xs, ys);
+        Assert.Throws<InvalidOperationException>(() => plt.Render());
+    }
+
+    [Test]
+    public void Test_Scatter_SinglePoint_Render()
+    {
+        ScottPlot.Plot plt = new();
+        double[] xs = { 1 };
+        double[] ys = { 1 };
+        plt.Add.Scatter(xs, ys);
+        plt.Render();
+        plt.SaveTestImage();
+
+        Assert.That(plt.GetAxisLimits().Rect.Area, Is.Not.Zero);
+    }
+
+
+    [Test]
+    public void Test_Scatter_RepeatedYs_Render()
+    {
+        ScottPlot.Plot plt = new();
+        double[] xs = { 1, 2, 3, 4, 5 };
+        double[] ys = { 7, 7, 7, 7, 7 };
+        plt.Add.Scatter(xs, ys);
+        plt.Render();
+        plt.SaveTestImage();
+
+        Assert.That(plt.GetAxisLimits().Rect.Area, Is.Not.Zero);
+    }
+}
