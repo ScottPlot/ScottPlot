8b26a908a5d978a8ce4d0e5df83b690f41eb8d6d
Scott W Harden <swharden@gmail.com>
2023-07-12 18:40:14 -0400

    SP5: create console sandbox
    also demonstrates #2746

---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using SkiaSharp;
+
+ScottPlot.Plot plot = new();
+
+plot.Add.Signal(ScottPlot.Generate.Sin());
+plot.Add.Signal(ScottPlot.Generate.Cos());
+
+plot.TopAxis.Label.Text = "频率信号削减1/32";
+
+Console.WriteLine(SKFontManager.Default.MatchCharacter('频').FamilyName);
+plot.TopAxis.Label.Font.Name = SKFontManager.Default.MatchCharacter('频').FamilyName;
+
+plot.SavePng("test.png", 400, 300);
