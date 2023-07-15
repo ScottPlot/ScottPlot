2e95f25402ad7099f6babc16b02face5bb569fc0
Scott W Harden <swharden@gmail.com>
2023-07-15 13:00:46 -0400

    SP4: import shared tests

---
+++ src/ScottPlot4/ScottPlot.Tests/GenerateTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using ScottPlot;
+using System;
+using System.Linq;
+
+namespace SharedTests;
+
+internal class GenerateTests
+{
+    [Test]
+    public void Test_RandomNormal()
+    {
+        double[] values = Generate.RandomNormal(10);
+        Console.WriteLine(string.Join(Environment.NewLine, values.Select(x => x.ToString())));
+    }
+}
