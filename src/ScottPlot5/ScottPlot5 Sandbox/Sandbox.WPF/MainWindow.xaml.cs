c46137941390fa4038cbd2f7f26af0b93953e841
Scott W Harden <swharden@gmail.com>
2023-07-25 20:27:24 -0400

    SP5 WPF sandbox: international font demonstration

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
@@ -16,4 +16,21 @@ public partial class MainWindow : Window
 
         WpfPlot.Refresh();
     }
+
+    private void SetLabelText(string text)
+    {
+        WpfPlot.Plot.Style.SetFontFromText(text);
+        WpfPlot.Plot.Title(text, 24);
+        WpfPlot.Plot.YLabel(text, 24);
+        WpfPlot.Plot.XLabel(text, 24);
+        WpfPlot.Refresh();
+    }
+
+    private void English_Click(object sender, RoutedEventArgs e) => SetLabelText("Test");
+
+    private void Chinese_Click(object sender, RoutedEventArgs e) => SetLabelText("测试");
+
+    private void Japanese_Click(object sender, RoutedEventArgs e) => SetLabelText("試験");
+
+    private void Korean_Click(object sender, RoutedEventArgs e) => SetLabelText("테스트");
 }
