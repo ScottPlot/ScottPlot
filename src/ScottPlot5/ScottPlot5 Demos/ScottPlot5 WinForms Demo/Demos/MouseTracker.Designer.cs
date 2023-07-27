98f46f6d969b3ee4fb5e94b8fcc0dea4e2dda45c
Scott W Harden <swharden@gmail.com>
2023-07-27 19:07:48 -0400

    SP5 WinForms demo: mouse tracker

---
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/MouseTracker.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace WinForms_Demo.Demos;
+
+partial class MouseTracker
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+        SuspendLayout();
+        // 
+        // formsPlot1
+        // 
+        formsPlot1.Dock = DockStyle.Fill;
+        formsPlot1.Location = new Point(0, 0);
+        formsPlot1.Name = "formsPlot1";
+        formsPlot1.Size = new Size(800, 450);
+        formsPlot1.TabIndex = 0;
+        // 
+        // MouseTracker
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(formsPlot1);
+        Name = "MouseTracker";
+        Text = "MouseTracker";
+        ResumeLayout(false);
+    }
+
+    #endregion
+
+    private ScottPlot.WinForms.FormsPlot formsPlot1;
+}
---
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/MouseTracker.cs
@@ -0,0 +1,36 @@
+﻿using ScottPlot;
+
+namespace WinForms_Demo.Demos;
+
+public partial class MouseTracker : Form, IDemoWindow
+{
+    public string Title => "Mouse Tracker";
+
+    public string Description => "Demonstrates how to interact with the mouse " +
+        "and convert between screen units (pixels) and axis units (coordinates)";
+
+    readonly ScottPlot.Plottables.Crosshair CH;
+
+    public MouseTracker()
+    {
+        InitializeComponent();
+
+        CH = formsPlot1.Plot.Add.Crosshair(0, 0);
+
+        formsPlot1.MouseMove += (s, e) =>
+        {
+            // demonstrate pixel-to-coordinates
+            Coordinates coordinates = formsPlot1.Plot.GetCoordinates(e.X, e.Y);
+
+            // demonstrate coordinates-to-pixel
+            Pixel px = formsPlot1.Plot.GetPixel(coordinates);
+
+            // place the crosshair where the mouse is
+            CH.Position = coordinates;
+            formsPlot1.Refresh();
+
+            // display where the mouse is in the titlebar
+            Text = $"{px} {coordinates}";
+        };
+    }
+}
