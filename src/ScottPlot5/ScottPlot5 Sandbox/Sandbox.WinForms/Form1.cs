5cdc8d85f0de05612b08ccf03f41eb50e0faca28
Scott W Harden <swharden@gmail.com>
2023-07-28 07:50:27 -0400

    SP5 Sandbox: WinForms mouse tracker

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
@@ -8,9 +8,14 @@ public partial class Form1 : Form
     {
         InitializeComponent();
 
-        formsPlot1.Plot.Add.Signal(Generate.Sin());
-        formsPlot1.Plot.Add.Signal(Generate.Cos());
+        var crosshair = formsPlot1.Plot.Add.Crosshair(0, 0);
 
-        formsPlot1.Refresh();
+        formsPlot1.MouseMove += (s, e) =>
+        {
+            Pixel mousePixel = new(e.X, e.Y);
+            Coordinates mouseCoordinates = formsPlot1.GetCoordinates(mousePixel);
+            crosshair.Position = mouseCoordinates;
+            formsPlot1.Refresh();
+        };
     }
 }
