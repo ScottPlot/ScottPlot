8d4c8b4dbc11f757a4c69e39bceece5b14f4e633
Scott W Harden <swharden@gmail.com>
2023-07-28 07:45:42 -0400

    SP5 Sandbox: WinForms .NET Framework app

---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinFormsFramework/Form1.cs
@@ -0,0 +1,25 @@
+﻿using ScottPlot;
+using ScottPlot.WinForms;
+using System.Windows.Forms;
+
+namespace Sandbox.WinFormsFramework;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+        FormsPlot formsPlot1 = new() { Dock = DockStyle.Fill };
+        Controls.Add(formsPlot1);
+
+        var crosshair = formsPlot1.Plot.Add.Crosshair(0, 0);
+
+        formsPlot1.MouseMove += (s, e) =>
+        {
+            Pixel mousePixel = new(e.X, e.Y);
+            Coordinates mouseCoordinates = formsPlot1.GetCoordinates(mousePixel);
+            crosshair.Position = mouseCoordinates;
+            formsPlot1.Refresh();
+        };
+    }
+}
