7eb8de71c8df98fc66104fb9cfb220c008eb7806
oleg_trushkov <o.trushkov95@gmail.com>
2023-08-02 21:52:44 +0300

    Add Sandbox.Avalonia.Android project.

---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Avalonia.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content.PM;
+
+using Avalonia;
+using Avalonia.Android;
+
+namespace Sandbox.Avalonia.Android;
+
+[Activity(
+    Label = "Sandbox.Avalonia.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont();
+    }
+}
