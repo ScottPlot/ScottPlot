131f735b8d59d7b081bf0d149fa70bfb36880ba7
Scott W Harden <swharden@gmail.com>
2023-08-12 22:38:20 -0400

    SP5 Sandbox: multiplot

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
@@ -7,15 +7,5 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
-
-        var crosshair = formsPlot1.Plot.Add.Crosshair(0, 0);
-
-        formsPlot1.MouseMove += (s, e) =>
-        {
-            Pixel mousePixel = new(e.X, e.Y);
-            Coordinates mouseCoordinates = formsPlot1.GetCoordinates(mousePixel);
-            crosshair.Position = mouseCoordinates;
-            formsPlot1.Refresh();
-        };
     }
 }
