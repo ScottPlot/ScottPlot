17360f01d2dec3e7940b35c783e636ebdfe0e0fd
Scott W Harden <swharden@gmail.com>
2023-10-16 08:02:58 -0400

    SP5 Test: signal plot with repeating values
    demonstrates issue #2933

---
+++ src/ScottPlot5/ScottPlot5 Tests/RenderTests/Plottable/SignalTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ScottPlotTests.RenderTests.Plottable;
+
+internal class SignalTests
+{
+    [Test]
+    public void Test_Signal_HorizontalLine()
+    {
+        // https://github.com/ScottPlot/ScottPlot/issues/2933
+        // https://github.com/ScottPlot/ScottPlot/pull/2935
+
+        double[] data = new double[10_000];
+        double[] sin = Generate.Sin(data.Length / 10);
+        Array.Copy(sin, 0, data, 0, sin.Length);
+        Array.Copy(sin, 0, data, data.Length - sin.Length, sin.Length);
+
+        ScottPlot.Plot plt = new();
+        plt.Add.Signal(data);
+        plt.Grids.Clear();
+        plt.SaveTestImage();
+    }
+}
