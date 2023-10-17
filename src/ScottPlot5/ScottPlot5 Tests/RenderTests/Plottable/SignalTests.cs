0a7b71127f0b5e3bf2a10b2bed23689ac06b9fd3
Scott W Harden <swharden@gmail.com>
2023-10-17 07:59:08 -0400

    SP5 Test: signal with vertical gap

--- src/ScottPlot5/ScottPlot5 Tests/RenderTests/Plottable/SignalTests.cs
+++ src/ScottPlot5/ScottPlot5 Tests/RenderTests/Plottable/SignalTests.cs
@@ -19,4 +19,23 @@ internal class SignalTests
         plt.Grids.Clear();
         plt.SaveTestImage();
     }
+
+    [Test]
+    public void Test_Signal_VerticalGap()
+    {
+        // https://github.com/ScottPlot/ScottPlot/issues/2933
+        // https://github.com/ScottPlot/ScottPlot/pull/2935
+
+        double[] data = Generate.Sin(10_000, oscillations: 100);
+        for (int i = (int)(data.Length * .3); i < (int)(data.Length * .7); i++)
+        {
+            data[i] = data[i] + 10;
+        }
+
+        ScottPlot.Plot plt = new();
+        var sig = plt.Add.Signal(data);
+        sig.LineStyle.AntiAlias = false;
+        plt.Grids.Clear();
+        plt.SaveTestImage();
+    }
 }
