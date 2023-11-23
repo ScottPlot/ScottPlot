3ba49f60e02b92f33c5b4db728f9f9f231da55f1
Scott W Harden <swharden@gmail.com>
2023-11-22 19:18:45 -0500

    SP5: VerticalLine

---
+++ src/ScottPlot5/ScottPlot5 Tests/RenderTests/Plottable/AxisLineTests.cs
@@ -0,0 +1,17 @@
+﻿namespace ScottPlotTests.RenderTests.Plottable;
+
+internal class AxisLineTests
+{
+    [Test]
+    public void Test_AxisLine_Render()
+    {
+        ScottPlot.Plot plot = new();
+        plot.DisableGrid();
+
+        plot.Add.VerticalLine(123.45, width: 1, pattern: LinePattern.Dash);
+        plot.Add.VerticalLine(123.55, width: 2, pattern: LinePattern.Dot);
+        plot.Add.VerticalLine(123.75, width: 10);
+
+        plot.SaveTestImage();
+    }
+}
---
+++ src/ScottPlot5/ScottPlot5/Plottables/VerticalLine.cs
@@ -0,0 +1,44 @@
+﻿namespace ScottPlot.Plottables;
+
+// TODO: make vertical and horizontal lines inherit an abstract AxisLine class
+public class VerticalLine : IPlottable
+{
+    public bool IsVisible { get; set; } = true;
+    public IAxes Axes { get; set; } = new Axes();
+
+    public Label Label { get; set; } = new();
+    public LineStyle LineStyle { get; set; } = new();
+
+    internal double X { get; set; } = 0;
+
+    public IEnumerable<LegendItem> LegendItems
+    {
+        get
+        {
+            return LegendItem.Single(new LegendItem()
+            {
+                Label = Label.Text,
+                Line = LineStyle,
+            });
+        }
+    }
+
+    public AxisLimits GetAxisLimits()
+    {
+        return AxisLimits.HorizontalOnly(X, X);
+    }
+
+    public void Render(RenderPack rp)
+    {
+        if (!IsVisible)
+            return;
+
+        using SKPaint paint = new();
+        LineStyle.ApplyToPaint(paint);
+
+        float y1 = rp.DataRect.Bottom;
+        float y2 = rp.DataRect.Top;
+        float x = Axes.GetPixelX(X);
+        rp.Canvas.DrawLine(x, y1, x, y2, paint);
+    }
+}
