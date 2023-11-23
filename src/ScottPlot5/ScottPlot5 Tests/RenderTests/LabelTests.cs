b3182e8cdc1eda5d28fc954009a1dd268f0226a8
Scott W Harden <swharden@gmail.com>
2023-11-23 11:14:47 -0500

    SP5: LabelExperimental
    eventually aims to replace Label

---
+++ src/ScottPlot5/ScottPlot5 Tests/RenderTests/LabelTests.cs
@@ -0,0 +1,43 @@
+﻿using SkiaSharp;
+
+namespace ScottPlotTests.RenderTests;
+
+internal class LabelTests
+{
+    [Test]
+    public void Test_Label_Basic()
+    {
+        ScottPlot.LabelExperimental lbl = new()
+        {
+            Text = "Test",
+            PointSize = 5,
+            BackColor = Colors.White.WithAlpha(.1),
+            ForeColor = Colors.White.WithAlpha(.3),
+            BorderColor = Colors.White.WithAlpha(.5),
+            PointColor = Colors.Yellow,
+            FontSize = 26,
+        };
+
+        SKSurface surface = Drawing.CreateSurface(500, 1000);
+        SKCanvas canvas = surface.Canvas;
+        canvas.Clear(SKColors.Navy);
+
+        float[] rotations = { 0, 90, 180, 270 };
+        Alignment[] alignments = Enum.GetValues<Alignment>();
+
+        for (int i=0; i<alignments.Length; i++)
+        {
+            lbl.Alignment = alignments[i];
+            for (int j = 0; j < rotations.Length; j++)
+            {
+                lbl.Rotation = rotations[j];
+
+                float x = 100 + 100 * j;
+                float y = 100 + 100 * i;
+                lbl.Render(canvas, x, y);
+            }
+        }
+
+        surface.SaveTestImage();
+    }
+}
---
+++ src/ScottPlot5/ScottPlot5/Primitives/LabelExperimental.cs
@@ -0,0 +1,108 @@
+﻿using System.Drawing;
+
+namespace ScottPlot;
+
+public class LabelExperimental
+{
+    public bool IsVisible { get; set; } = true;
+    public string Text { get; set; } = string.Empty;
+
+    public Alignment Alignment { get; set; } = 0;
+
+    public float Rotation { get; set; } = 0;
+
+    public Color ForeColor { get; set; } = Colors.Black;
+
+    public Color BackColor { get; set; } = Colors.Gray;
+
+    public Color BorderColor { get; set; } = Colors.Black;
+    public float BorderWidth { get; set; } = 1;
+
+    private SKTypeface? CachedTypeface = null;
+    private SKTypeface Typeface => CachedTypeface ??= FontStyle.CreateTypeface(FontName, Bold, Italic);
+    public string FontName { get; set; } = Fonts.Default;
+    public float FontSize { get; set; } = 12;
+    public bool Bold = false;
+    public bool Italic = false;
+    public bool AntiAlias = true;
+    public float Padding = 0;
+
+    public float PointSize = 0;
+    public bool PointFilled = false;
+    public Color PointColor = Colors.Magenta;
+
+    public float OffsetX = 0; // TODO: automatic padding support for arbitrary rotations
+    public float OffsetY = 0; // TODO: automatic padding support for arbitrary rotations
+
+    private void ApplyPointPaint(SKPaint paint)
+    {
+        paint.IsStroke = !PointFilled;
+        paint.StrokeWidth = 1;
+        paint.Color = PointColor.ToSKColor();
+        paint.IsAntialias = AntiAlias;
+    }
+
+    private void ApplyBorderPaint(SKPaint paint)
+    {
+        paint.IsStroke = true;
+        paint.StrokeWidth = BorderWidth;
+        paint.Color = BorderColor.ToSKColor();
+        paint.IsAntialias = AntiAlias;
+    }
+
+    private void ApplyBackgroundPaint(SKPaint paint)
+    {
+        paint.IsStroke = false;
+        paint.Color = BackColor.ToSKColor();
+        paint.IsAntialias = AntiAlias;
+    }
+
+    private void ApplyTextPaint(SKPaint paint)
+    {
+        paint.TextAlign = SKTextAlign.Left;
+        paint.IsStroke = false;
+        paint.Typeface = Typeface;
+        paint.TextSize = FontSize;
+        paint.Color = ForeColor.ToSKColor();
+        paint.IsAntialias = AntiAlias;
+    }
+
+    public void Render(SKCanvas canvas, float x, float y)
+    {
+        using SKPaint paint = new();
+        Render(canvas, x, y, paint);
+    }
+
+    public void Render(SKCanvas canvas, float x, float y, SKPaint paint)
+    {
+        ApplyTextPaint(paint);
+        SKRect textBounds = new();
+        paint.MeasureText(Text, ref textBounds);
+
+        float xOffset = textBounds.Width * Alignment.HorizontalFraction();
+        float yOffset = textBounds.Height * Alignment.VerticalFraction();
+        PixelRect textRect = new(0, textBounds.Width, textBounds.Height, 0);
+        textRect = textRect.WithDelta(-xOffset, yOffset - textBounds.Height);
+        PixelRect backgroundRect = textRect.Expand(Padding);
+
+        canvas.Save();
+        canvas.Translate(x + OffsetX, y + OffsetY); // compensate for padding
+        canvas.RotateDegrees(Rotation);
+        ApplyBackgroundPaint(paint);
+        canvas.DrawRect(backgroundRect.ToSKRect(), paint);
+        ApplyTextPaint(paint);
+        canvas.DrawText(Text, textRect.Left, textRect.Bottom, paint);
+        ApplyBorderPaint(paint);
+        canvas.DrawRect(backgroundRect.ToSKRect(), paint);
+        canvas.Restore();
+
+        canvas.Save();
+        canvas.Translate(x, y); // do not compensate for padding
+        canvas.RotateDegrees(Rotation);
+        ApplyPointPaint(paint);
+        canvas.DrawCircle(0, 0, PointSize, paint);
+        canvas.DrawLine(-PointSize, 0, PointSize, 0, paint);
+        canvas.DrawLine(0, -PointSize, 0, PointSize, paint);
+        canvas.Restore();
+    }
+}
