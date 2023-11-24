d65ec777076f1a71df54014ac2ff6888ddc1a818
Scott W Harden <swharden@gmail.com>
2023-11-24 13:25:36 -0500

    SP4 WPF Sandbox: demonstrate issue #3002

--- src/ScottPlot4/ScottPlot.Sandbox/WpfApp/MainWindow.xaml.cs
+++ src/ScottPlot4/ScottPlot.Sandbox/WpfApp/MainWindow.xaml.cs
@@ -8,14 +8,16 @@ namespace WpfApp
         {
             InitializeComponent();
 
-            WpfPlot1.Plot.AddSignal(ScottPlot.DataGen.Sin(51, 2));
-            WpfPlot2.Plot.AddSignal(ScottPlot.DataGen.Cos(51, 2));
+            WpfPlot1.Plot.AddSignal(ScottPlot.DataGen.Sin(51));
+            WpfPlot1.Plot.AddSignal(ScottPlot.DataGen.Cos(51));
 
             WpfPlot1.Refresh();
-            WpfPlot2.Refresh();
+            WpfPlot1.Refresh();
 
-            WpfPlot1.Configuration.AddLinkedControl(WpfPlot2); // update plot 2 when plot 1 changes
-            WpfPlot2.Configuration.AddLinkedControl(WpfPlot1); // update plot 1 when plot 2 changes
+            WpfPlot1.SizeChanged += (s, e) =>
+            {
+                Title = $"Actual={WpfPlot1.ActualWidth}, Reported={WpfPlot1.Plot.XAxis.Dims.FigureSizePx}";
+            };
         }
     }
 }
