e5139bdcb1e9261015ac8bb849e7fd225f3506fd
Scott W Harden <swharden@gmail.com>
2023-11-24 14:13:06 -0500

    SP5 Test: SVG export

--- src/ScottPlot5/ScottPlot5 Tests/RenderTests/FileFormatTests.cs
+++ src/ScottPlot5/ScottPlot5 Tests/RenderTests/FileFormatTests.cs
@@ -8,7 +8,7 @@ internal class FileFormatTests
         Plot plt = new();
         plt.Add.Signal(Generate.Sin(51));
         plt.Add.Signal(Generate.Cos(51));
-        plt.SaveBmp("test_save.bmp", 200, 100);
+        plt.SaveBmp("test-images/test_save.bmp", 200, 100);
     }
 
     [Test]
@@ -17,7 +17,7 @@ internal class FileFormatTests
         Plot plt = new();
         plt.Add.Signal(Generate.Sin(51));
         plt.Add.Signal(Generate.Cos(51));
-        plt.SaveJpeg("test_save.jpg", 200, 100);
+        plt.SaveJpeg("test-images/test_save.jpg", 200, 100);
     }
 
     [Test]
@@ -26,7 +26,7 @@ internal class FileFormatTests
         Plot plt = new();
         plt.Add.Signal(Generate.Sin(51));
         plt.Add.Signal(Generate.Cos(51));
-        plt.SavePng("test_save.png", 200, 100);
+        plt.SavePng("test-images/test_save.png", 200, 100);
     }
 
     [Test]
@@ -35,7 +35,7 @@ internal class FileFormatTests
         Plot plt = new();
         plt.Add.Signal(Generate.Sin(51));
         plt.Add.Signal(Generate.Cos(51));
-        plt.SaveWebp("test_save.webp", 200, 100);
+        plt.SaveWebp("test-images/test_save.webp", 200, 100);
     }
 
     [Test]
@@ -47,5 +47,14 @@ internal class FileFormatTests
         string img = plt.GetImageHtml(300, 200);
         string html = $"<html><body>{img}</body></html>";
         html.SaveTestString();
+    }
+
+    [Test]
+    public void Test_Save_Svg()
+    {
+        Plot plt = new();
+        plt.Add.Signal(Generate.Sin(51));
+        plt.Add.Signal(Generate.Cos(51));
+        plt.SaveSvg("test-images/test_save.svg", 400, 300);
     }
 }
