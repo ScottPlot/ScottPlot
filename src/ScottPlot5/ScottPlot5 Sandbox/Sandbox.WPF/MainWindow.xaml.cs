4851337892f5898add5cade5edc1d276b2f732fb
Scott W Harden <swharden@gmail.com>
2023-11-25 13:23:43 -0500

    SP5 WPF: delete MultiPlot

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
@@ -14,23 +14,7 @@ public partial class MainWindow : Window
 
         Loaded += (s, e) =>
         {
-            // customize the layout
-            MultiWpfPlot1.Multiplot.Layout = new ScottPlot.MultiplotLayouts.Grid(2, 3);
-
-            // create plots and add them to the multiplot
-            RandomDataGenerator gen = new();
-            for (int i = 0; i < 6; i++)
-            {
-                Plot plot = new();
-                plot.Add.Signal(gen.RandomWalk(100));
-                plot.Title($"Plot {i + 1}");
-                MultiWpfPlot1.Multiplot.Add(plot);
-            }
-
-            // apply the layout from the first plot to all subplots
-            MultiWpfPlot1.Multiplot.SharedLayoutSourcePlot = MultiWpfPlot1.Multiplot.Plots.First();
-
-            MultiWpfPlot1.Refresh();
+            WpfPlot1.Refresh();
         };
     }
 }
