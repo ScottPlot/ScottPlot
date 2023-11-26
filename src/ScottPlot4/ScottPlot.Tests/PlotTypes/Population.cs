84f4ddfea33bc18903c61b8b856639af44c41975
Gray-lab <martin.grym@gmail.com>
2023-11-25 23:35:07 -0600

    Add tests for BoxAlphaOverride and MarkerAlpha fields

--- src/ScottPlot4/ScottPlot.Tests/PlotTypes/Population.cs
+++ src/ScottPlot4/ScottPlot.Tests/PlotTypes/Population.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using ScottPlotTests;
 using System;
+using System.Runtime.Intrinsics.X86;
 
 namespace ScottPlot.Tests.PlotTypes;
 
@@ -74,5 +75,83 @@ internal class Population
         ScottPlot.Plot plt = new();
         plt.AddPopulations(poulations);
         Assert.DoesNotThrow(() => plt.Render());
+    }
+
+    [Test]
+    public void Test_Population_MarkerAlpha()
+    {
+        // issue #2967 - custom alpha values for population plots
+        // https://github.com/ScottPlot/ScottPlot/issues/2967
+
+        Random rand = new(0);
+        double[] valuesA = DataGen.RandomNormal(rand, 35, 85, 5);
+        double[] valuesB = DataGen.RandomNormal(rand, 42, 87, 3);
+        double[] valuesC = DataGen.RandomNormal(rand, 23, 92, 3);
+
+        var popA = new Statistics.Population(valuesA);
+        var popB = new Statistics.Population(valuesB);
+        var popC = new Statistics.Population(valuesC);
+
+        var populations = new Statistics.Population[] { popA, popB, popC };
+
+        ScottPlot.Plot plt = new(600, 400);
+        var pplt = plt.AddPopulations(populations);
+    
+        // plot with default semitransparency
+        var bmp1 = TestTools.GetLowQualityBitmap(plt);
+
+        // plot with no transparency
+        pplt.MarkerAlpha = 255;
+        var bmp2 = TestTools.GetLowQualityBitmap(plt);
+
+        // measure what changed
+        // TestTools.SaveFig(bmp1, "1");
+        // TestTools.SaveFig(bmp2, "2");
+        var before = new MeanPixel(bmp1);
+        var after = new MeanPixel(bmp2);
+        Console.WriteLine($"Before: {before}");
+        Console.WriteLine($"After: {after}");
+
+        // less transparent markers -> darker image
+        Assert.That(after.IsDarkerThan(before));
+    }
+
+    [Test]
+    public void Test_Population_BoxAlphaOverride()
+    {
+        // issue #2967 - custom alpha values for population plots
+        // https://github.com/ScottPlot/ScottPlot/issues/2967
+
+        Random rand = new(0);
+        double[] valuesA = DataGen.RandomNormal(rand, 35, 85, 5);
+        double[] valuesB = DataGen.RandomNormal(rand, 42, 87, 3);
+        double[] valuesC = DataGen.RandomNormal(rand, 23, 92, 3);
+
+        var popA = new Statistics.Population(valuesA);
+        var popB = new Statistics.Population(valuesB);
+        var popC = new Statistics.Population(valuesC);
+
+        var populations = new Statistics.Population[] { popA, popB, popC };
+
+        ScottPlot.Plot plt = new(600, 400);
+        var pplt = plt.AddPopulations(populations);
+        
+        // plot with default semitransparency
+        var bmp1 = TestTools.GetLowQualityBitmap(plt);
+
+        // plot nearly transparent boxes
+        pplt.BoxAlphaOverride = 20;
+        var bmp2 = TestTools.GetLowQualityBitmap(plt);
+
+        // measure what changed
+        // TestTools.SaveFig(bmp1, "1");
+        // TestTools.SaveFig(bmp2, "2");
+        var before = new MeanPixel(bmp1);
+        var after = new MeanPixel(bmp2);
+        Console.WriteLine($"Before: {before}");
+        Console.WriteLine($"After: {after}");
+
+        // more transparent boxes -> lighter image
+        Assert.That(before.IsDarkerThan(after));
     }
 }
