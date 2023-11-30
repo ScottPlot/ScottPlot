0c4ba4747d8c82562b0514356a22da8aa4bc10d9
Gray-lab <martin.grym@gmail.com>
2023-11-30 16:12:38 -0600

    Add renderers for TriUp and TriDown markers

---
+++ src/ScottPlot5/ScottPlot5/Markers/TriDown.cs
@@ -0,0 +1,27 @@
+﻿namespace ScottPlot.Markers;
+
+internal class TriDown : IMarker
+{
+    public void Render(SKCanvas canvas, SKPaint paint, Pixel center, float size, FillStyle fill, LineStyle outline)
+    {
+        // Length of each side of inscribing triangle = size
+        float radius = (float)(size / 1.732); // size / sqrt(3)
+        float xOffset = (float)(radius * 0.866); // r * sqrt(3)/2
+        float yOffset = radius / 2;
+
+        var path = new SKPath();
+
+        // Bottom line
+        path.MoveTo(center.X, center.Y);
+        path.LineTo(center.X, center.Y - radius);
+        // Left top line
+        path.MoveTo(center.X, center.Y);
+        path.LineTo(center.X - xOffset, center.Y + yOffset);
+        // Right top line
+        path.MoveTo(center.X, center.Y);
+        path.LineTo(center.X + xOffset, center.Y + yOffset);
+
+        outline.ApplyToPaint(paint);
+        canvas.DrawPath(path, paint);
+    }
+}
---
+++ src/ScottPlot5/ScottPlot5/Markers/TriUp.cs
@@ -0,0 +1,38 @@
+﻿using ScottPlot.AxisPanels;
+
+namespace ScottPlot.Markers;
+
+internal class TriUp : IMarker
+{
+    public void Render(SKCanvas canvas, SKPaint paint, Pixel center, float size, FillStyle fill, LineStyle outline)
+    {
+        // Length of each side of inscribing triangle = size
+        float radius = (float)(size / 1.732); // size / sqrt(3)
+        float xOffset = (float)(radius * 0.866); // r * sqrt(3)/2
+        float yOffset = radius / 2;
+
+        fill.ApplyToPaint(paint);
+
+        // Top, right, and left vertices
+        SKPoint[] pointsList = new SKPoint[]
+        {
+            new SKPoint(center.X, center.Y - radius),
+            new SKPoint(center.X + xOffset, center.Y + yOffset),
+            new SKPoint(center.X - xOffset, center.Y + yOffset),
+        };
+
+        var path = new SKPath();
+        // Top line
+        path.MoveTo(center.X, center.Y);
+        path.LineTo(center.X, center.Y + radius);
+        // Left bottom line
+        path.MoveTo(center.X, center.Y);
+        path.LineTo(center.X - xOffset, center.Y - yOffset);
+        // Right bottom line
+        path.MoveTo(center.X, center.Y);
+        path.LineTo(center.X + xOffset, center.Y - yOffset);
+
+        outline.ApplyToPaint(paint);
+        canvas.DrawPath(path, paint);
+    }
+}
