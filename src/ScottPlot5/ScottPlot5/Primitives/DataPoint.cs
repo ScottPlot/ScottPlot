c6c7478d8018fadaeb2e679965998a70a709882c
Scott W Harden <swharden@gmail.com>
2023-12-03 17:28:44 -0500

    SP5: DataPoint

---
+++ src/ScottPlot5/ScottPlot5/Primitives/DataPoint.cs
@@ -0,0 +1,29 @@
+﻿namespace ScottPlot;
+
+/// <summary>
+/// Represents a specific point in a DataSource
+/// </summary>
+public readonly struct DataPoint
+{
+    public readonly double X;
+    public readonly double Y;
+    public readonly int Index;
+    public Coordinates Coordinates => new(X, Y);
+    public bool IsReal => NumericConversion.IsReal(X) && NumericConversion.IsReal(Y);
+
+    public DataPoint(double x, double y, int index)
+    {
+        X = x;
+        Y = y;
+        Index = index;
+    }
+
+    public DataPoint(Coordinates coordinates, int index)
+    {
+        X = coordinates.X;
+        Y = coordinates.Y;
+        Index = index;
+    }
+
+    public static DataPoint None => new(double.NaN, double.NaN, -1);
+}
