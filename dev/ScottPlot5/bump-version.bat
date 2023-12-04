d1cab27a9c62adf6cbc1e2c7ca9b077bdb1567ed
Scott W Harden <swharden@gmail.com>
2023-12-03 19:27:31 -0500

    ScottPlot 5.0.10-beta

--- dev/ScottPlot5/bump-version.bat
+++ dev/ScottPlot5/bump-version.bat
@@ -2,9 +2,10 @@
 cd ../BumpVersion
 dotnet run "../../src/ScottPlot5/ScottPlot5"
 dotnet run "../../src/ScottPlot5/ScottPlot5 Controls/ScottPlot.Avalonia"
+dotnet run "../../src/ScottPlot5/ScottPlot5 Controls/ScottPlot.Blazor"
 dotnet run "../../src/ScottPlot5/ScottPlot5 Controls/ScottPlot.Eto"
+dotnet run "../../src/ScottPlot5/ScottPlot5 Controls/ScottPlot.OpenGL"
 dotnet run "../../src/ScottPlot5/ScottPlot5 Controls/ScottPlot.WinForms"
 dotnet run "../../src/ScottPlot5/ScottPlot5 Controls/ScottPlot.WinUI"
 dotnet run "../../src/ScottPlot5/ScottPlot5 Controls/ScottPlot.WPF"
-dotnet run "../../src/ScottPlot5/ScottPlot5 Controls/ScottPlot.OpenGL"
 pause
