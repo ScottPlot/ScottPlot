e49049346de3dde4a843a9c297561def8c5feb27
arthurits <simply_alfy@hotmail.com>
2023-12-07 12:18:42 +0100

    Improve descriptive statistics class

--- src/ScottPlot5/ScottPlot5/Statistics/Descriptive.cs
+++ src/ScottPlot5/ScottPlot5/Statistics/Descriptive.cs
@@ -7,27 +7,43 @@ namespace ScottPlot.Statistics;
 
 public static class Descriptive
 {
-    public static double Mean(double[] values)
+    public static double Mean<T>(IEnumerable<T> values)
     {
-        return values.Sum() / values.Length;
+        IEnumerable<double> data = values.Select(value => Convert.ToDouble(value));
+        return data.Average();
     }
 
-    public static double StDev(double[] values)
+    public static double StDev<T>(IEnumerable<T> values, bool asSample = false)
     {
-        return StDev(values, Mean(values));
+        return StDev(values, Mean(values), asSample);
     }
 
-    public static double StDev(double[] values, double mean)
+    public static double StDev<T>(IEnumerable<T> values, double mean, bool asSample = false)
     {
-        double sumVariancesSquared = 0;
-        for (int i = 0; i < values.Length; i++)
-        {
-            double pointVariance = Math.Abs(mean - values[i]);
-            double pointVarianceSquared = Math.Pow(pointVariance, 2);
-            sumVariancesSquared += pointVarianceSquared;
-        }
-        double meanVarianceSquared = sumVariancesSquared / values.Length;
-        double stDev = Math.Sqrt(meanVarianceSquared);
-        return stDev;
+        double[] data = values.Select(value => Convert.ToDouble(value)).ToArray();
+        double sumVariancesSquared = data.Sum(x => (x - mean) * (x - mean));
+        double denominator = data.Length - (asSample ? 1 : 0);
+        return denominator > 0.0 ? Math.Sqrt(sumVariancesSquared / denominator) : -1.0;
+
+        //for (int i = 0; i < data.Length; i++)
+        //{
+        //    double pointVariance = Math.Abs(mean - data[i]);
+        //    double pointVarianceSquared = Math.Pow(pointVariance, 2);
+        //    sumVariancesSquared += pointVarianceSquared;
+        //}
+        //double meanVarianceSquared = sumVariancesSquared / (data.Length - (asSample ? 1 : 0));
+        //double stDev = Math.Sqrt(meanVarianceSquared);
+        //return stDev;
+    }
+    
+    public static double StdErr<T>(IEnumerable<T> values, bool asSample = false)
+    {
+        return StdErr(values, Mean(values), asSample);
+    }
+
+    public static double StdErr<T>(IEnumerable<T> values, double mean, bool asSample = false)
+    {
+        double denominator = values.Count() - (asSample ? 1 : 0);
+        return denominator > 0.0 ? StDev(values, mean, asSample) / Math.Sqrt(denominator) : -1.0;
     }
 }
