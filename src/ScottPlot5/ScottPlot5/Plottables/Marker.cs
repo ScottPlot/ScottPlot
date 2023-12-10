f23cae38793f4a016c85c779b4326e42c4d8b7c7
Gray-lab <martin.grym@gmail.com>
2023-12-09 19:40:03 -0600

    Add Marker plottable

---
+++ src/ScottPlot5/ScottPlot5/Plottables/Marker.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ScottPlot.Plottables;
+
+public class Marker : IPlottable
+{
+    public string Label { get; set; } = string.Empty;
+    public Coordinates Location { get; set; }
+    public bool IsVisible { get; set; } = true;
+    public MarkerStyle MarkerStyle { get; set; } = MarkerStyle.Default;
+    public IAxes Axes { get; set; } = new Axes();
+    public IEnumerable<LegendItem> LegendItems => EnumerableExtensions.One<LegendItem>(
+    new LegendItem
+    {
+        Label = Label,
+        Marker = MarkerStyle,
+    });
+
+    public override string ToString()
+    {
+        string label = string.IsNullOrWhiteSpace(this.Label) ? "" : $" ({this.Label})";
+        return $"PlottableMarker{label} of shape {MarkerStyle}";
+    }
+
+    public AxisLimits GetAxisLimits()
+    {
+        return new AxisLimits(Location);
+    }
+
+    public void Render(RenderPack rp)
+    {
+        Pixel pixelLocation = Axes.GetPixel(Location);
+        MarkerStyle.Render(rp.Canvas, pixelLocation);
+    }
+}
