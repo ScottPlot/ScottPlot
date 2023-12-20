6a193dad3b788d00ba7bc56d0e970447c3082893
Scott W Harden <swharden@gmail.com>
2023-12-20 08:29:28 -0500

    SP5 SvgImage: new primitive type
    Allows a SVG image to be created with an exposed SKCanvas that can be drawn on and saved later

---
+++ src/ScottPlot5/ScottPlot5/Primitives/SvgImage.cs
@@ -0,0 +1,26 @@
+﻿namespace ScottPlot;
+
+public class SvgImage
+{
+    public readonly int Width;
+    public readonly int Height;
+    private readonly MemoryStream Stream;
+    public readonly SKCanvas Canvas;
+
+    public SvgImage(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        SKRect rect = new(0, 0, width, height);
+        Stream = new MemoryStream();
+        Canvas = SKSvgCanvas.Create(rect, Stream);
+    }
+
+    public void Save(string filename)
+    {
+        FileStream fs = new(filename, FileMode.Create);
+        Stream.WriteTo(fs);
+        using StreamWriter sw = new(fs);
+        sw.Write("</svg>");
+    }
+}
