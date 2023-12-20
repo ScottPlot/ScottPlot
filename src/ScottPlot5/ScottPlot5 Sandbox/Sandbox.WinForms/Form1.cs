23a4742e1a7fc7b49f047706aeabb5bb28f20983
Scott W Harden <swharden@gmail.com>
2023-12-20 07:08:07 -0500

    SP5 WinForms sandbox: simplify

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
@@ -8,13 +8,8 @@ public partial class Form1 : Form
     {
         InitializeComponent();
 
-        double[] clean = Generate.SquareWave(low: 0, high: 5);
-        formsPlot1.Plot.Add.Signal(clean);
-
-        double[] noisy = Generate.SquareWave(low: 10, high: 15);
-        Generate.AddNoiseInPlace(noisy, magnitude: .001);
-        formsPlot1.Plot.Add.Signal(noisy);
-
-        formsPlot1.Plot.Grids.Clear();
+        formsPlot1.Plot.Add.Signal(Generate.Sin());
+        formsPlot1.Plot.Add.Signal(Generate.Cos());
+        formsPlot1.Refresh();
     }
 }
