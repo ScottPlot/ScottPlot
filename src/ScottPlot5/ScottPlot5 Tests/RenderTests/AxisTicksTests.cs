1a5cb26649aa2c8bb73088350b61c02bcadecd47
Scott W Harden <swharden@gmail.com>
2023-12-23 14:51:55 -0500

    SP5 Test: Vertical DateTime ticks
    #2970

---
+++ src/ScottPlot5/ScottPlot5 Tests/RenderTests/AxisTicksTests.cs
@@ -0,0 +1,31 @@
+﻿namespace ScottPlotTests.RenderTests;
+
+internal class AxisTicksTests
+{
+    [Test]
+    public void Test_DateTimeTicks_Bottom()
+    {
+        Plot plt = new();
+        plt.BottomAxis.TickGenerator = new ScottPlot.TickGenerators.DateTimeAutomatic();
+
+        DateTime dt1 = new(2023, 01, 01);
+        DateTime dt2 = new(2024, 01, 01);
+        plt.SetAxisLimitsX(dt1.ToOADate(), dt2.ToOADate());
+
+        plt.SaveTestImage();
+    }
+
+    [Test]
+    public void Test_DateTimeTicks_Left()
+    {
+        Plot plt = new();
+
+        plt.LeftAxis.TickGenerator = new ScottPlot.TickGenerators.DateTimeAutomatic();
+
+        DateTime dt1 = new(2023, 01, 01);
+        DateTime dt2 = new(2024, 01, 01);
+        plt.SetAxisLimitsY(dt1.ToOADate(), dt2.ToOADate());
+
+        plt.SaveTestImage();
+    }
+}
