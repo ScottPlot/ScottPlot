eda2e79408ce32247380931b3f529da964146405
arthurits <simply_alfy@hotmail.com>
2023-12-24 12:11:05 +0100

    Statistics for 2D arrays

--- src/ScottPlot5/ScottPlot5/Statistics/Descriptive.cs
+++ src/ScottPlot5/ScottPlot5/Statistics/Descriptive.cs
@@ -164,4 +164,165 @@ public static class Descriptive
     {
         return StandardDeviation(values) / Math.Sqrt(values.Count);
     }
+
+
+    public static IReadOnlyList<double> RemoveNaN<T>(IReadOnlyList<T> values)
+    {
+        double[] values2 = NumericConversion.GenericToDoubleArray(values);
+        List<double> values3 = new();
+
+        foreach (double value in values2)
+        {
+            if (!double.IsNaN(value))
+                values3.Add(value);
+        }
+
+        return values3;
+    }
+
+    public static double[] RemoveNaN(double[] values)
+    {
+        List<double> values2 = new();
+
+        foreach (double value in values)
+        {
+            if (!double.IsNaN(value))
+                values2.Add(value);
+        }
+
+        return values2.ToArray();
+    }
+
+    /// <summary>
+    /// Return the sample mean. NaN values are ignored.
+    /// </summary>
+    public static double NaNMean<T> (IReadOnlyList<T> values)
+    {
+        return Mean(RemoveNaN(values));
+    }
+
+    /// <summary>
+    /// Return the sample variance (second moment about the mean) of data.
+    /// Input must contain at least two values. NaN values are ignored.
+    /// Use this function when your data is a sample from a population.
+    /// To calculate the variance from the entire population use <see cref="VarianceP(double[])"/>.
+    /// </summary>
+    public static double NaNVariance<T>(IReadOnlyList<T> values)
+    {
+        return Variance(RemoveNaN(values));
+    }
+
+    /// <summary>
+    /// Return the sample variance (second moment about the mean) of data.
+    /// Input must contain at least two values. NaN values are ignored.
+    /// Use this function when your data is a sample from a population.
+    /// To calculate the variance from the entire population use <see cref="VarianceP(double[])"/>.
+    /// </summary>
+    public static double NaNVarianceP<T>(IReadOnlyList<T> values)
+    {
+        return VarianceP(RemoveNaN(values));
+    }
+
+    /// <summary>
+    /// Return the sample standard deviation (the square root of the sample variance).
+    /// NaN values are ignored.
+    /// </summary>
+    public static double NaNStandardDeviation<T>(IReadOnlyList<T> values)
+    {
+        return StandardDeviation(RemoveNaN(values));
+    }
+
+    /// <summary>
+    /// Return the population standard deviation (the square root of the sample variance).
+    /// NaN values are ignored.
+    /// </summary>
+    public static double NaNStandardDeviationP<T>(IReadOnlyList<T> values)
+    {
+        return StandardDeviationP(RemoveNaN(values));
+    }
+
+    /// <summary>
+    /// Standard error of the mean.
+    /// NaN values are ignored.
+    /// </summary>
+    public static double NaNStandardError<T>(IReadOnlyList<T> values)
+    {
+        return StandardError(RemoveNaN(values));
+    }
+
+    /// <summary>
+    /// Transpose a multidimensional (not jagged) array
+    /// </summary>
+    public static double[,] Rotate90(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        double[,] result = new double[cols, rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+                result[j, i] = matrix[i, j];
+
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Extracts a row or column from a 2D array
+    /// </summary>
+    public static double[] ArrayToVector(double[,] values, uint? row = 0, uint? column = null)
+    {
+        if (values.GetLength(0) == 0)
+            throw new ArgumentException($"{nameof(values)} cannot be empty");
+
+        double[] vector = Array.Empty<double>();
+        if (row is not null)
+        {
+            vector = new double[values.GetLength(1)];
+            Array.Copy(values, (int)row * values.GetLength(1), vector, 0, values.GetLength(1));
+        }
+        else if (column is not null)
+        {
+            vector = ArrayToVector(Rotate90(values), column);
+        }
+
+        return vector;
+    }
+
+    public static double NaNMean(double[,] values, uint row = 0, uint? column = null)
+    {
+        double[] vector = ArrayToVector(values, row, column);
+        return NaNMean(vector);
+    }
+
+    public static double NaNVariance<T>(double[,] values, uint row = 0, uint? column = null)
+    {
+        double[] vector = ArrayToVector(values, row, column);
+        return Variance(RemoveNaN(vector));
+    }
+
+    public static double NaNVarianceP<T>(double[,] values, uint row = 0, uint? column = null)
+    {
+        double[] vector = ArrayToVector(values, row, column);
+        return VarianceP(RemoveNaN(vector));
+    }
+
+    public static double NaNStandardDeviation<T>(double[,] values, uint row = 0, uint? column = null)
+    {
+        double[] vector = ArrayToVector(values, row, column);
+        return StandardDeviation(RemoveNaN(vector));
+    }
+
+    public static double NaNStandardDeviationP<T>(double[,] values, uint row = 0, uint? column = null)
+    {
+        double[] vector = ArrayToVector(values, row, column);
+        return StandardDeviationP(RemoveNaN(vector));
+    }
+
+    public static double NaNStandardError<T>(double[,] values, uint row = 0, uint? column = null)
+    {
+        double[] vector = ArrayToVector(values, row, column);
+        return NaNStandardError(RemoveNaN(vector));
+    }
 }
