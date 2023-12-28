f9cf0db5dd498a079344491b18179a5dd06bbeb1
Scott W Harden <swharden@gmail.com>
2023-12-28 11:32:51 -0500

    SP5 Sandbox: simplify WPF and WinForms
    resolves #3107

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ public partial class MainWindow : Window
         {
             WpfPlot1.Plot.Add.Signal(Generate.Sin());
             WpfPlot1.Plot.Add.Signal(Generate.Cos());
-            WpfPlot1.Plot.AutoScale(); // https://github.com/ScottPlot/ScottPlot/issues/3107
             WpfPlot1.Refresh();
         };
     }
