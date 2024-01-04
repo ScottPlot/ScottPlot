04342851e97f31e80da02b72f6e6853118bc17d9
Scott W Harden <swharden@gmail.com>
2024-01-03 19:20:01 -0500

    SP5 console sandbox: target .NET 8

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Console/Program.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Console/Program.cs
@@ -1,7 +1,11 @@
-﻿ScottPlot.Plot plot = new();
-plot.Add.Signal(ScottPlot.Generate.Sin());
-plot.Add.Signal(ScottPlot.Generate.Cos());
-plot.SavePng("test.png", 400, 300);
+﻿ScottPlot.Plot plt = new();
 
-plot.ScaleFactor = 2;
-plot.SavePng("test2.png", 400, 300);
+double[] sales = { 123, 456, 789 };
+double[] salesError = { 50, 150, 350 };
+DateTime[] dates = { new(2024, 01, 01), new(2024, 01, 02), new(2024, 01, 03) };
+double[] dateXs = dates.Select(x => x.ToOADate()).ToArray();
+
+plt.Add.Scatter(dateXs, sales);
+plt.Add.ErrorBar(dateXs, sales, salesError);
+
+plt.SavePng("test.png", 400, 300);
