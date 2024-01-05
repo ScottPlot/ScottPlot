74a609c3e8b2d5d2acb9c513019029fc985c4dc1
Scott W Harden <swharden@gmail.com>
2024-01-04 20:01:11 -0500

    SP5 Tests: Ensure all line patterns have a matching path effect

---
+++ src/ScottPlot5/ScottPlot5 Tests/UnitTests/PrimitiveTests/LinePatternTests.cs
@@ -0,0 +1,14 @@
+﻿namespace ScottPlotTests.UnitTests.PrimitiveTests;
+
+public class LinePatternTests
+{
+    [Test]
+    public void Test_EveryLinePattern_HasPathEffect()
+    {
+        foreach (LinePattern pattern in Enum.GetValues<LinePattern>())
+        {
+            Action act = () => pattern.GetPathEffect();
+            act.Should().NotThrow();
+        }
+    }
+}
