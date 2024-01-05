5a5df867b64bd63f489c0ccbbdaefc76b56c1cb1
Scott W Harden <swharden@gmail.com>
2024-01-04 20:00:50 -0500

    SP5 LinePattern: move GetPathEffect() into the same file

--- src/ScottPlot5/ScottPlot5/Primitives/LinePattern.cs
+++ src/ScottPlot5/ScottPlot5/Primitives/LinePattern.cs
@@ -7,3 +7,18 @@ public enum LinePattern
     ShortDash,
     Dot,
 }
+
+public static class LinePatternExtensions
+{
+    public static SKPathEffect? GetPathEffect(this LinePattern pattern)
+    {
+        return pattern switch
+        {
+            LinePattern.Solid => null,
+            LinePattern.Dash => SKPathEffect.CreateDash(new float[] { 10, 10 }, 0),
+            LinePattern.ShortDash => SKPathEffect.CreateDash(new float[] { 6, 6 }, 0),
+            LinePattern.Dot => SKPathEffect.CreateDash(new float[] { 3, 5 }, 0),
+            _ => throw new NotImplementedException($"Line pattern '{pattern}' has no matching path effect"),
+        };
+    }
+}
