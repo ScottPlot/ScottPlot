0855f8759cf0320c6dc226e050de044878a13603
Scott W Harden <swharden@gmail.com>
2024-01-09 19:36:28 -0500

    SP5: move CompilerServices

---
+++ src/ScottPlot5/ScottPlot5/CompilerServices.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Allows init-only setters in older .NET versions
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class IsExternalInit { }
+
+/// <summary>
+/// Allows required members in older .NET versions
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class RequiredMemberAttribute : Attribute { }
+
+/// <summary>
+/// Allows required members in older .NET versions
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class CompilerFeatureRequiredAttribute : Attribute
+{
+    public CompilerFeatureRequiredAttribute(string _) { }
+}
--- src/ScottPlot5/ScottPlot5/CompilerServices/IsExternalInit.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel;
-
-// This is required to allow init-only setters in .NET <5.0. See more here: https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
-namespace System.Runtime.CompilerServices
-{
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    internal static class IsExternalInit { }
-}
