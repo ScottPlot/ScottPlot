bcb43ceda3d2a160f21bdcf5bdb151cfc9cdc48a
Scott W Harden <swharden@gmail.com>
2024-01-11 21:12:47 -0500

    SP5 WPF Sandbox: multiple heavy signal plots
    demonstrates a performance issue described in #3188

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using ScottPlot;
+using ScottPlot.WPF;
 
 namespace Sandbox.WPF;
 
@@ -8,9 +11,56 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        ShowCharts();
+    }
 
-        WpfPlot1.Plot.Add.Signal(Generate.Sin());
-        WpfPlot1.Plot.Add.Signal(Generate.Cos());
-        WpfPlot1.Refresh();
+    private void ShowCharts()
+    {
+        List<WpfPlot> wpfPlotList = new();
+
+        for (int i = 0; i < 11; i++)
+        {
+            double[] doubles = Generate.RandomWalk(7_016_960);
+            float[] floats = doubles.Select(x => (float)x).ToArray();
+
+            var plot = new WpfPlot();
+            plot.Plot.Add.Signal(doubles, 256);
+            wpfPlotList.Add(plot);
+        }
+
+        for (int sourceIndex = 0; sourceIndex < wpfPlotList.Count; sourceIndex++)
+        {
+            var sourceChart = wpfPlotList[sourceIndex];
+            sourceChart.Height = 200;
+
+            for (int targetIndex = 0; targetIndex < wpfPlotList.Count; targetIndex++)
+            {
+                if (sourceIndex == targetIndex)
+                    continue;
+                var targetChart = wpfPlotList[targetIndex];
+                sourceChart.MouseUp += (s, e) =>
+                {
+                    ApplyLayoutToOtherPlot(sourceChart, targetChart);
+                };
+            }
+
+            var rule = new ScottPlot.AxisRules.LockedVertical(sourceChart.Plot.Axes.Left);
+            sourceChart.Plot.Axes.Rules.Clear();
+            sourceChart.Plot.Axes.Rules.Add(rule);
+            sourceChart.Refresh();
+
+            charts.Children.Add(sourceChart);
+        }
+    }
+
+    private void ApplyLayoutToOtherPlot(IPlotControl source, IPlotControl target)
+    {
+        AxisLimits axesBefore = target.Plot.Axes.GetLimits();
+        target.Plot.Axes.SetLimitsX(source.Plot.Axes.GetLimits());
+        AxisLimits axesAfter = target.Plot.Axes.GetLimits();
+        if (axesBefore != axesAfter)
+        {
+            target.Refresh();
+        }
     }
 }
