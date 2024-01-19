bb0ee71a1ffa3d7d62b888cd231b16d5ddec3123
Noah Sigl <sigl@xnet-online.de>
2024-01-19 10:23:07 +0100

    SP5: Added Step Display for ScatterPlot

--- src/ScottPlot5/ScottPlot5/Plottables/Scatter.cs
+++ src/ScottPlot5/ScottPlot5/Plottables/Scatter.cs
@@ -2,6 +2,8 @@
  * Avoid temptation to use generics or generic math at this early stage of development!
  */
 
+using System.Drawing;
+
 namespace ScottPlot.Plottables;
 
 public class Scatter : IPlottable
@@ -16,6 +18,27 @@ public class Scatter : IPlottable
 
     public float LineWidth { get => LineStyle.Width; set => LineStyle.Width = value; }
     public float MarkerSize { get => MarkerStyle.Size; set => MarkerStyle.Size = value; }
+
+
+    /// <summary>
+    /// If enabled, scatter plot points will be connected by square corners rather than straight diagnal lines
+    /// </summary>
+    public bool StepDisplay { get; set; } = false;
+    /// <summary>
+    /// Describes orientation of steps if <see cref="StepDisplay"/> is enabled.
+    /// If true, lines will extend to the right before ascending or descending to the level of the following point.
+    /// </summary>
+    public bool StepDisplayRight { get; set; } = false;
+    /// <summary>
+    /// If enabled, points will be connected by smooth lines instead of straight diagnal lines.
+    /// <see cref="SmoothTension"/> adjusts the smoothnes of the lines.
+    /// </summary>
+    public bool Smooth = false;
+    /// <summary>
+    /// Tension to use for smoothing when <see cref="Smooth"/> is enabled
+    /// </summary>
+    public double SmoothTension = 0.5;
+
     public Color Color
     {
         get => LineStyle.Color;
@@ -45,13 +68,51 @@ public class Scatter : IPlottable
     public void Render(RenderPack rp)
     {
         // TODO: can this be more effecient by moving this logic into the DataSource to avoid copying?
-        IEnumerable<Pixel> pixels = Data.GetScatterPoints().Select(Axes.GetPixel);
+        Pixel[] pixels = Data.GetScatterPoints().Select(Axes.GetPixel).ToArray();
 
         if (!pixels.Any())
             return;
 
         using SKPaint paint = new();
-        Drawing.DrawLines(rp.Canvas, paint, pixels, LineStyle);
+
+        if (StepDisplay)
+        {
+            IEnumerable<Pixel> stepPixels = GetStepDisplayPixels(pixels, StepDisplayRight);
+            Drawing.DrawLines(rp.Canvas, paint, stepPixels, LineStyle);
+        }
+        else if (Smooth)
+        {
+            // TODO: Implement SmoothTension
+            Drawing.DrawLines(rp.Canvas, paint, pixels, LineStyle);
+        }
+        else
+        {
+            Drawing.DrawLines(rp.Canvas, paint, pixels, LineStyle);
+        }
         Drawing.DrawMarkers(rp.Canvas, paint, pixels, MarkerStyle);
+    }
+
+    /// <summary>
+    /// Convert scatter plot points (connected by diagnal lines) to step plot points (connected by right angles)
+    /// by inserting an extra point between each of the original data points to result in L-shaped steps.
+    /// </summary>
+    /// <param name="points">Array of corner positions</param>
+    /// <param name="right">Indicates that a line will extend to the right before rising or falling.</param>
+    public static IEnumerable<Pixel> GetStepDisplayPixels(Pixel[] pixels, bool right)
+    {
+        Pixel[] pixelsStep = new Pixel[pixels.Count() * 2 - 1];
+
+        int offsetX = right ? 1 : 0;
+        int offsetY = right ? 0 : 1;
+
+        for (int i = 0; i < pixels.Count() - 1; i++)
+        {
+            pixelsStep[i * 2] = pixels[i];
+            pixelsStep[i * 2 + 1] = new Pixel(pixels[i + offsetX].X, pixels[i + offsetY].Y);
+        }
+
+        pixelsStep[pixelsStep.Length - 1] = pixels[pixels.Length - 1];
+
+        return pixelsStep;
     }
 }
