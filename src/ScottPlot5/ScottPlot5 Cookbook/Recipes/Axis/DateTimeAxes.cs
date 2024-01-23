bb0be7eb9943089a51f1d3b8f3d0b896438bc09e
Scott W Harden <swharden@gmail.com>
2024-01-23 18:55:51 -0500

    SP5 Cookbook: datetime axis values
    resolves #3267

--- src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Axis/DateTimeAxes.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Axis/DateTimeAxes.cs
@@ -23,4 +23,41 @@ public class DateTimeAxes : ICategory
             myPlot.Axes.DateTimeTicks(Edge.Bottom);
         }
     }
+
+    public class DateTimeAxisMixed : RecipeBase
+    {
+        public override string Name => "DateTime Axis Values";
+        public override string Description => "DateTime axes are achieved using Microsoft's " +
+            "DateTime.ToOADate() and DateTime.FromOADate() methods to convert between " +
+            "dates and numeric values. Advanced users who wish to display data on DateTime axes " +
+            "may prefer to work with collections of doubles rather than collections of DateTimes.";
+
+        [Test]
+        public override void Execute()
+        {
+            // create an array of DateTimes one hour apart
+            int numberOfHours = 72;
+            DateTime[] dateTimes = new DateTime[numberOfHours];
+            DateTime startDateTime = new(2024, 1, 1);
+            TimeSpan deltaTimeSpan = TimeSpan.FromHours(1);
+            for (int i = 0; i < numberOfHours; i++)
+            {
+                dateTimes[i] = startDateTime + i * deltaTimeSpan;
+            }
+
+            // create an array of doubles representing the same DateTimes one hour apart
+            double[] dateDoubles = new double[numberOfHours];
+            double startDouble = startDateTime.ToOADate(); // days since 1900
+            double deltaDouble = 1.0 / 24.0; // an hour is 1/24 of a day
+            for (int i = 0; i < numberOfHours; i++)
+            {
+                dateDoubles[i] = startDouble + i * deltaDouble;
+            }
+
+            // now both arrays represent the same dates
+            myPlot.Add.Scatter(dateTimes, Generate.Sin(numberOfHours));
+            myPlot.Add.Scatter(dateDoubles, Generate.Cos(numberOfHours));
+            myPlot.Axes.DateTimeTicks(Edge.Bottom);
+        }
+    }
 }
