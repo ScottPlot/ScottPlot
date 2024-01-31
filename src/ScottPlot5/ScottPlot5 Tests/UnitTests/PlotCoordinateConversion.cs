1bb0505ad6715aa64bd86af85117da323b62e714
Mike Fellows <mike.fellows@shaw.ca>
2024-01-31 11:42:38 -0800

    SP5 Tests: Test Plot coordinate conversions
    Convert from Pixel to Coordinates and back again - with the default
    ScaleFactor of 1.0 and with a non-default of 2.5.

---
+++ src/ScottPlot5/ScottPlot5 Tests/UnitTests/PlotCoordinateConversion.cs
@@ -0,0 +1,35 @@
+﻿namespace ScottPlotTests.UnitTests;
+
+internal class PlotCoordinateConversion
+{
+    [Test]
+    public void Test_Plot_Pixel_To_Coords_And_Back()
+    {
+        Plot plt = new();
+        plt.Axes.SetLimits(-5, 25, 10, 30);
+        plt.SaveTestImage();
+
+        Pixel initialPx = new(55.0F, 333.0F);
+        Coordinates coordinates = plt.GetCoordinates(initialPx);
+        Pixel convertedPx = plt.GetPixel(coordinates);
+
+        convertedPx.X.Should().Be(initialPx.X);
+        convertedPx.Y.Should().Be(initialPx.Y);
+    }
+
+    [Test]
+    public void Test_Plot_Pixel_To_Coords_And_Back_Scaled()
+    {
+        Plot plt = new();
+        plt.Axes.SetLimits(1, 2, -15, -5);
+        plt.ScaleFactor = 2.5F;     // Change from default of 1.0F
+        plt.SaveTestImage();
+
+        Pixel initialPx = new(329.0F, 200.0F);
+        Coordinates coordinates = plt.GetCoordinates(initialPx);
+        Pixel convertedPx = plt.GetPixel(coordinates);
+
+        convertedPx.X.Should().Be(initialPx.X);
+        convertedPx.Y.Should().Be(initialPx.Y);
+    }
+}
