2ee2a48f34f7c61880661986214e4b76c0a06634
BrianAtZetica <brian@zetica.com>
2024-02-24 18:52:55 +1030

    Bug fix - setting InvertedX or InvertedY flag shoul donly be done if values are not Infinity. (caught by unit tests)

--- src/ScottPlot5/ScottPlot5/AxisManager.cs
+++ src/ScottPlot5/ScottPlot5/AxisManager.cs
@@ -208,14 +208,15 @@ public class AxisManager
     {
         xAxis.Min = left;
         xAxis.Max = right;
-        AutoScaler.InvertedX = left > right ? true : false;
+        if (xAxis.Range.HasBeenSet) AutoScaler.InvertedX = left > right ? true : false;
     }
 
     public void SetLimitsY(double bottom, double top, IYAxis yAxis)
     {
         yAxis.Min = bottom;
         yAxis.Max = top;
-        AutoScaler.InvertedY = bottom > top ? true : false;
+
+        if(yAxis.Range.HasBeenSet) AutoScaler.InvertedY = bottom > top ? true : false;
     }
 
     public void SetLimitsX(double left, double right)
