162db6e1222d20c77742cd1372cae04107a419ac
Andrij Abyzov <aabyzov@slb.com>
2024-02-26 11:07:16 +0100

    Add finer steps to DateTime axis

--- src/ScottPlot5/ScottPlot5/TickGenerators/TimeUnits/StandardDivisors.cs
+++ src/ScottPlot5/ScottPlot5/TickGenerators/TimeUnits/StandardDivisors.cs
@@ -2,8 +2,8 @@
 
 internal static class StandardDivisors
 {
-    public static readonly IReadOnlyList<int> Decimal = new int[] { 1, 5, 10 };
-    public static readonly IReadOnlyList<int> Sexagesimal = new int[] { 1, 5, 10, 15, 20, 30, 60 };
+    public static readonly IReadOnlyList<int> Decimal = new int[] { 1, 2, 5, 10 };
+    public static readonly IReadOnlyList<int> Sexagesimal = new int[] { 1, 2, 5, 10, 15, 20, 30, 60 };
     public static readonly IReadOnlyList<int> Dozenal = new int[] { 1, 2, 3, 4, 6, 12 };
     public static readonly IReadOnlyList<int> Hexadecimal = new int[] { 1, 2, 3, 4, 6, 8, 16 };
     public static readonly IReadOnlyList<int> Days = new int[] { 1, 3, 7, 14, 28 };
