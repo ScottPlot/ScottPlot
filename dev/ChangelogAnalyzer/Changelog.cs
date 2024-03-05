c91e727d4dad123e9ff7466c24298049b8f4940d
Scott W Harden <swharden@gmail.com>
2024-03-05 17:50:38 -0500

    dev: changelog editor

---
+++ dev/ChangelogAnalyzer/Changelog.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace ChangelogAnalyzer;
+
+public class Changelog
+{
+    readonly List<VersionNotes> Versions = [];
+
+    public Changelog(string filePath)
+    {
+        string[] lines = File.ReadAllLines(filePath);
+
+        VersionNotes? currentVersion = null;
+
+        foreach (string line in lines)
+        {
+            if (VersionNotes.IsFirstLine(line))
+            {
+                currentVersion = new();
+                Versions.Add(currentVersion);
+            }
+            currentVersion?.AddLine(line);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Changelog with {Versions.Count} versions.";
+    }
+
+    public string GetMarkdown()
+    {
+        StringBuilder sb = new();
+        foreach(var version in Versions)
+        {
+            sb.AppendLine(version.GetMarkdown());
+        }
+        return sb.ToString();
+    }
+}
---
+++ dev/ChangelogAnalyzer/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ChangelogAnalyzer;
+
+public static class Program
+{
+    public static void Main()
+    {
+        string changelogFilePath = Path.GetFullPath("../../../../../CHANGELOG.md");
+        Changelog log = new(changelogFilePath);
+        File.WriteAllText(changelogFilePath, log.GetMarkdown());
+    }
+}
