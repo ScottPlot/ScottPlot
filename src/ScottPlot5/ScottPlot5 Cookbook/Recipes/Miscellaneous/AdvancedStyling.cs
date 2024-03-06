327ae443e77e6f92d8581ff58618330fe3a867bf
Scott W Harden <swharden@gmail.com>
2024-03-06 18:58:08 -0500

    Cookbook: background image

--- src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Miscellaneous/AdvancedStyling.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Miscellaneous/AdvancedStyling.cs
@@ -5,4 +5,20 @@ public class AdvancedStyling : ICategory
     public string Chapter => "Miscellaneous";
     public string CategoryName => "Advanced Styling";
     public string CategoryDescription => "Features for users seeking extensive customization options.";
+
+    public class AdvancedStylingFigureBackground : RecipeBase
+    {
+        public override string Name => "Figure Background Image";
+        public override string Description => "The background of the figure can be set to an image.";
+
+        [Test]
+        public override void Execute()
+        {
+            myPlot.Add.Signal(Generate.Sin());
+            myPlot.Add.Signal(Generate.Cos());
+
+            Image img = SampleImages.ScottPlotLogo(600, 400);
+            myPlot.Style.DataBackgroundImage(img);
+        }
+    }
 }
