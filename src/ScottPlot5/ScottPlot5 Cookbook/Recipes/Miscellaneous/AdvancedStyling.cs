d7f2ac2c60d93a3562c8600818704c493be289e2
Scott W Harden <swharden@gmail.com>
2024-03-06 18:57:58 -0500

    Cookbook: advanced styling

---
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Miscellaneous/AdvancedStyling.cs
@@ -0,0 +1,8 @@
+﻿namespace ScottPlotCookbook.Recipes.Miscellaneous;
+
+public class AdvancedStyling : ICategory
+{
+    public string Chapter => "Miscellaneous";
+    public string CategoryName => "Advanced Styling";
+    public string CategoryDescription => "Features for users seeking extensive customization options.";
+}
