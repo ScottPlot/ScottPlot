d8c855dfea611c7c1c256840b17e75dfc0049b7d
Scott W Harden <swharden@gmail.com>
2024-03-06 19:22:03 -0500

    BackgroundStyle: class for wrapping styling settings

---
+++ src/ScottPlot5/ScottPlot5/Primitives/BackgroundStyle.cs
@@ -0,0 +1,13 @@
+﻿namespace ScottPlot.Primitives;
+
+public class BackgroundStyle : IDisposable
+{
+    public SKBitmap? Image { get; set; } = null;
+    public ImageScalingStyle ImageScaling { get; set; } = ImageScalingStyle.FillRetainAspect;
+    public Color Color { get; set; } = Colors.White;
+
+    public void Dispose()
+    {
+        Image?.Dispose();
+    }
+}
