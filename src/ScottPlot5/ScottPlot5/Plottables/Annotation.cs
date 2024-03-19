c247a302d09e4897be67d46916cf12b378fba1b7
Scott W Harden <swharden@gmail.com>
2024-03-19 18:56:15 -0400

    Annotation: support OffsetX and OffsetY
    Padding

--- src/ScottPlot5/ScottPlot5/Plottables/Annotation.cs
+++ src/ScottPlot5/ScottPlot5/Plottables/Annotation.cs
@@ -3,14 +3,13 @@
 public class Annotation : IPlottable
 {
     public bool IsVisible { get; set; } = true;
-
     public IAxes Axes { get; set; } = new Axes();
-
     public IEnumerable<LegendItem> LegendItems => LegendItem.None;
 
     public Label Label { get; set; } = new();
-
     public Alignment Alignment { get; set; } = Alignment.UpperLeft;
+    public float OffsetX { get; set; } = 5;
+    public float OffsetY { get; set; } = 5;
 
     public AxisLimits GetAxisLimits() => AxisLimits.NoLimits;
 
@@ -19,35 +18,33 @@ public class Annotation : IPlottable
         if (!IsVisible)
             return;
 
-        //!TODO replace margin with PaddingX/Y
-        float margin = 0;
         PixelSize textSize = Label.Measure();
 
         float _verticalOrigin = Alignment switch
         {
-            Alignment.UpperLeft => rp.DataRect.Top + 0.5f * textSize.Height + margin,
-            Alignment.UpperCenter => rp.DataRect.Top + 0.5f * textSize.Height + margin,
-            Alignment.UpperRight => rp.DataRect.Top + 0.5f * textSize.Height + margin,
-            Alignment.MiddleLeft => rp.DataRect.LeftCenter.Y - 0.5f * textSize.Height + margin,
-            Alignment.MiddleCenter => rp.DataRect.LeftCenter.Y - 0.5f * textSize.Height + margin,
-            Alignment.MiddleRight => rp.DataRect.LeftCenter.Y - 0.5f * textSize.Height + margin,
-            Alignment.LowerLeft => rp.DataRect.Bottom - textSize.Height - 4 + margin,
-            Alignment.LowerCenter => rp.DataRect.Bottom - textSize.Height - 4 + margin,
-            Alignment.LowerRight => rp.DataRect.Bottom - textSize.Height - 4 + margin,
+            Alignment.UpperLeft => rp.DataRect.Top + 0.5f * textSize.Height + OffsetY,
+            Alignment.UpperCenter => rp.DataRect.Top + 0.5f * textSize.Height + OffsetY,
+            Alignment.UpperRight => rp.DataRect.Top + 0.5f * textSize.Height + OffsetY,
+            Alignment.MiddleLeft => rp.DataRect.LeftCenter.Y - 0.5f * textSize.Height,
+            Alignment.MiddleCenter => rp.DataRect.LeftCenter.Y - 0.5f * textSize.Height,
+            Alignment.MiddleRight => rp.DataRect.LeftCenter.Y - 0.5f * textSize.Height,
+            Alignment.LowerLeft => rp.DataRect.Bottom - textSize.Height - 4 - OffsetY,
+            Alignment.LowerCenter => rp.DataRect.Bottom - textSize.Height - 4 - OffsetY,
+            Alignment.LowerRight => rp.DataRect.Bottom - textSize.Height - 4 - OffsetY,
             _ => throw new NotImplementedException()
         };
 
         float _horizontalOrigin = Alignment switch
         {
-            Alignment.UpperLeft => rp.DataRect.Left + 4 + margin,
-            Alignment.UpperCenter => rp.DataRect.TopCenter.X - 0.5f * textSize.Width + margin,
-            Alignment.UpperRight => rp.DataRect.Right - textSize.Width - 4 + margin,
-            Alignment.MiddleLeft => rp.DataRect.Left + 4 + margin,
-            Alignment.MiddleCenter => rp.DataRect.BottomCenter.X - 0.5f * textSize.Width + margin,
-            Alignment.MiddleRight => rp.DataRect.Right - textSize.Width - 4 + margin,
-            Alignment.LowerLeft => rp.DataRect.Left + 4 + margin,
-            Alignment.LowerCenter => rp.DataRect.BottomCenter.X - 0.5f * textSize.Width + margin,
-            Alignment.LowerRight => rp.DataRect.Right - textSize.Width - 4 + margin,
+            Alignment.UpperLeft => rp.DataRect.Left + 4 + OffsetX,
+            Alignment.UpperCenter => rp.DataRect.TopCenter.X - 0.5f * textSize.Width,
+            Alignment.UpperRight => rp.DataRect.Right - textSize.Width - 4 - OffsetX,
+            Alignment.MiddleLeft => rp.DataRect.Left + 4 + OffsetX,
+            Alignment.MiddleCenter => rp.DataRect.BottomCenter.X - 0.5f * textSize.Width,
+            Alignment.MiddleRight => rp.DataRect.Right - textSize.Width - 4 - OffsetX,
+            Alignment.LowerLeft => rp.DataRect.Left + 4 + OffsetX,
+            Alignment.LowerCenter => rp.DataRect.BottomCenter.X - 0.5f * textSize.Width,
+            Alignment.LowerRight => rp.DataRect.Right - textSize.Width - 4 - OffsetX,
             _ => throw new NotImplementedException()
         };
 
