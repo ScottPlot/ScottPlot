a086bb98c17cf85e59bac23a963695de0ccdda6b
Scott W Harden <swharden@gmail.com>
2024-03-19 18:02:43 -0400

    Text: add new font properties

--- src/ScottPlot5/ScottPlot5/Plottables/Text.cs
+++ src/ScottPlot5/ScottPlot5/Plottables/Text.cs
@@ -9,53 +9,19 @@ public class Text : IPlottable
     public IEnumerable<LegendItem> LegendItems => LegendItem.None;
     public PixelSize PixelOffset { get; set; }
 
-    public Color Color
-    {
-        get => Label.ForeColor;
-        set => Label.ForeColor = value;
-    }
-
-    public float Size
-    {
-        get => Label.FontSize;
-        set => Label.FontSize = value;
-    }
-
-    public bool Bold
-    {
-        get => Label.Bold;
-        set => Label.Bold = value;
-    }
-
-    public float Rotation
-    {
-        get => Label.Rotation;
-        set => Label.Rotation = value;
-    }
-
-    public string LabelText
-    {
-        get => Label.Text;
-        set => Label.Text = value;
-    }
-
-    public Color FontColor
-    {
-        get => Color;
-        set => Color = value;
-    }
-
-    public string FontName
-    {
-        get => Label.FontName;
-        set => Label.FontName = value;
-    }
-
-    public float? LineSpacing
-    {
-        get => Label.LineSpacing;
-        set => Label.LineSpacing = value;
-    }
+    public Color Color { get => Label.ForeColor; set => Label.ForeColor = value; }
+    public Color FontColor { get => Label.ForeColor; set => Label.ForeColor = value; }
+    public Color BackColor { get => Label.BackColor; set => Label.BackColor = value; }
+    public Color BorderColor { get => Label.BorderColor; set => Label.BorderColor = value; }
+    public float BorderWidth { get => Label.BorderWidth; set => Label.BorderWidth = value; }
+    public float Padding { get => Label.Padding; set => Label.Padding = value; }
+    public float Size { get => Label.FontSize; set => Label.FontSize = value; }
+    public float FontSize { get => Label.FontSize; set => Label.FontSize = value; }
+    public bool Bold { get => Label.Bold; set => Label.Bold = value; }
+    public float Rotation { get => Label.Rotation; set => Label.Rotation = value; }
+    public string LabelText { get => Label.Text; set => Label.Text = value; }
+    public string FontName { get => Label.FontName; set => Label.FontName = value; }
+    public float? LineSpacing { get => Label.LineSpacing; set => Label.LineSpacing = value; }
 
     public Text()
     {
