33860ce00cf327670526a27f9fb32261cda93901
Scott W Harden <swharden@gmail.com>
2024-03-20 19:21:12 -0400

    Plottables: create IsoLines plot type

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.Designer.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.Designer.cs
@@ -31,6 +31,8 @@ partial class Form1
         formsPlot1 = new ScottPlot.WinForms.FormsPlot();
         button1 = new Button();
         button2 = new Button();
+        trackBar1 = new TrackBar();
+        ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
         SuspendLayout();
         // 
         // formsPlot1
@@ -60,18 +62,31 @@ partial class Form1
         button2.Text = "button2";
         button2.UseVisualStyleBackColor = true;
         // 
+        // trackBar1
+        // 
+        trackBar1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        trackBar1.Location = new Point(174, 12);
+        trackBar1.Maximum = 100;
+        trackBar1.Name = "trackBar1";
+        trackBar1.Size = new Size(614, 45);
+        trackBar1.TabIndex = 3;
+        trackBar1.Value = 10;
+        // 
         // Form1
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(800, 450);
+        Controls.Add(trackBar1);
         Controls.Add(button2);
         Controls.Add(button1);
         Controls.Add(formsPlot1);
         Name = "Form1";
         StartPosition = FormStartPosition.CenterScreen;
         Text = "ScottPlot 5 - Windows Forms Sandbox";
+        ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
         ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
@@ -79,4 +94,5 @@ partial class Form1
     private ScottPlot.WinForms.FormsPlot formsPlot1;
     private Button button1;
     private Button button2;
+    private TrackBar trackBar1;
 }
---
+++ src/ScottPlot5/ScottPlot5/Plottables/IsoLines.cs
@@ -0,0 +1,111 @@
+﻿namespace ScottPlot.Plottables;
+
+public class IsoLines : IPlottable
+{
+    public bool IsVisible { get; set; } = true;
+    public IAxes Axes { get; set; } = new Axes();
+    public IEnumerable<LegendItem> LegendItems => LegendItem.None;
+    public AxisLimits GetAxisLimits() => AxisLimits.NoLimits;
+    LineStyle LineStyle { get; set; } = new()
+    {
+        Width = 3,
+        Color = Colors.Blue.WithAlpha(.2),
+    };
+
+    public void Render(RenderPack rp)
+    {
+        AxisLimits limits = rp.Plot.Axes.GetLimits();
+        bool isWider = limits.HorizontalSpan > limits.VerticalSpan;
+        List<PixelLine> lines = isWider ? GetLinesByX(rp) : GetLinesByY(rp);
+
+        using SKPaint paint = new();
+        LineStyle.ApplyToPaint(paint);
+        lines.ForEach(line => Drawing.DrawLine(rp.Canvas, paint, line));
+    }
+
+    private List<PixelLine> GetLinesByX(RenderPack rp)
+    {
+        List<PixelLine> lines = new();
+
+        // determine where X ticks are placed and how far apart they are
+        var xTickPositions = Axes.XAxis.TickGenerator.Ticks
+            .Where(x => x.IsMajor)
+            .Select(x => x.Position)
+            .Reverse()
+            .ToArray();
+
+        if (xTickPositions.Length < 2)
+            return lines;
+
+        double xLast = xTickPositions[0];
+        double xDelta = xTickPositions[0] - xTickPositions[1];
+
+        // determine X positions (many start off the screen to the left)
+        AxisLimits limits = rp.Plot.Axes.GetLimits();
+        double[] isoLineXs = Enumerable.Range(0, 50)
+            .Select(x => xLast - x * xDelta)
+            .ToArray();
+
+        // create isolines
+        foreach (double x in isoLineXs)
+        {
+            // create a small piece of an isoline
+            double slope = 1.0;
+            Coordinates pt1 = new(x, 0);
+            Coordinates pt2 = new(x + 1, slope);
+            CoordinateLine line = new(pt1, pt2);
+            line = line.ExtendTo(limits.Rect);
+            if (line.End.X < limits.Left)
+                return lines;
+
+            // extend it to fit the data area
+            PixelLine pxLine = Axes.GetPixelLine(line);
+            lines.Add(pxLine);
+        }
+
+        return lines;
+    }
+
+    private List<PixelLine> GetLinesByY(RenderPack rp)
+    {
+        List<PixelLine> lines = new();
+
+        // determine where X ticks are placed and how far apart they are
+        var yTickPositions = Axes.YAxis.TickGenerator.Ticks
+            .Where(x => x.IsMajor)
+            .Select(x => x.Position)
+            .Reverse()
+            .ToArray();
+
+        if (yTickPositions.Length < 2)
+            return lines;
+
+        double yLast = yTickPositions[0];
+        double yDelta = yTickPositions[0] - yTickPositions[1];
+
+        // determine Y positions (many start off the screen to the bottom)
+        AxisLimits limits = rp.Plot.Axes.GetLimits();
+        double[] isoLineYs = Enumerable.Range(0, 50)
+            .Select(y => yLast - y * yDelta)
+            .ToArray();
+
+        // create isolines
+        foreach (double y in isoLineYs)
+        {
+            // create a small piece of an isoline
+            double slope = 1.0;
+            Coordinates pt1 = new(0, y);
+            Coordinates pt2 = new(1, y + slope);
+            CoordinateLine line = new(pt1, pt2);
+            line = line.ExtendTo(limits.Rect);
+            if (line.Start.X > limits.Right)
+                return lines;
+
+            // extend it to fit the data area
+            PixelLine pxLine = Axes.GetPixelLine(line);
+            lines.Add(pxLine);
+        }
+
+        return lines;
+    }
+}
