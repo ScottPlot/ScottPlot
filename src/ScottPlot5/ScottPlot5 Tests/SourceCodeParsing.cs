72b2befb17ba82e5382c1ee96ac91063f79604e9
Scott W Harden <swharden@gmail.com>
2024-03-23 20:55:49 -0400

    Revert "Tests: ensure that .Save() is never called directly"
    This reverts commit a8a3a974119eda6bfa491cc8787acbe4e310298f.

--- src/ScottPlot5/ScottPlot5 Tests/SourceCodeParsing.cs
+++ src/ScottPlot5/ScottPlot5 Tests/SourceCodeParsing.cs
@@ -76,10 +76,4 @@ internal static class SourceCodeParsing
 
         return methodNames;
     }
-
-    // TODO: cache source file paths and their contents for quicker searching by multiple tests
-    public static string[] GetSourceFilePaths()
-    {
-        return Directory.GetFiles(SourceFolder, "*.cs", SearchOption.AllDirectories);
-    }
 }
