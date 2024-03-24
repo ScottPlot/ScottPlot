a8a3a974119eda6bfa491cc8787acbe4e310298f
Scott W Harden <swharden@gmail.com>
2024-03-23 20:13:47 -0400

    Tests: ensure that .Save() is never called directly
    currently fails to demonstrate issue being worked on in #3527

---
+++ src/ScottPlot5/ScottPlot5 Tests/CodeTests/ForbiddenCodeTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace ScottPlotTests.CodeTests;
+
+internal class ForbiddenCodeTests
+{
+    private readonly string[] SourceFilePaths = SourceCodeParsing.GetSourceFilePaths();
+
+    [Test]
+    public void Test_CanvasSave_IsNotCalledDirectly()
+    {
+        StringBuilder errorMessages = new();
+        foreach (string filePath in SourceFilePaths)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string file2 = filePath.Replace(SourceCodeParsing.SourceFolder, string.Empty);
+                string line = lines[i];
+                if (line.Contains(".Save();"))
+                {
+                    errorMessages.AppendLine($"{file2} line {i + 1}");
+                    errorMessages.AppendLine(line.Trim());
+                    errorMessages.AppendLine();
+                }
+            }
+        }
+
+        errorMessages.ToString().Should().BeEmpty(
+            $"SKCanvas.Save() should never be called directly.\n" +
+            $"{errorMessages}");
+    }
+}
