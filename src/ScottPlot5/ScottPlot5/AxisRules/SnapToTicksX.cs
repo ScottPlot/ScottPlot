072410db321b66d2a2ab3375a604cf52f88cd18c
BrianAtZetica <brian@zetica.com>
2024-03-26 12:40:36 +1030

    New Axis rules to force axes to snap to a major tick interval If the max or min limit of hte axis is locked, it wil lnot snap to a tick interval.

---
+++ src/ScottPlot5/ScottPlot5/AxisRules/SnapToTicksX.cs
@@ -0,0 +1,88 @@
+﻿namespace ScottPlot.AxisRules;
+
+public class SnapToTicksX : IAxisRule
+{
+    public readonly IXAxis XAxis;
+
+    public SnapToTicksX(IXAxis xAxis)
+    {
+        XAxis = xAxis;
+    }
+
+    public void Apply(RenderPack rp, bool beforeLayout)
+    {
+        // rules that refer to the last render must wait for a render to occur
+        if (rp.Plot.LastRender.Count == 0)
+            return;
+
+        bool leftIsLocked = false;
+        bool rightIsLocked = false;
+        // a locked axis wont be snapped (locking rules take priority)
+        foreach (var rule in rp.Plot.Axes.Rules)
+        {
+            if (rule is LockedHorizontal lockedHorizontalRule)
+            {
+                if (lockedHorizontalRule.XAxis == XAxis)
+                {
+                    // the requested axis already has a horizontal lock
+                    return;
+                }
+            }
+
+            if (rule is LockedLeft lockedLeftRule)
+            {
+                if (lockedLeftRule.XAxis == XAxis)
+                {
+                    // the requested axis already has a left lock
+                    leftIsLocked = true;
+                }
+            }
+
+            if (rule is LockedRight lockedRightRule)
+            {
+                if (lockedRightRule.XAxis == XAxis)
+                {
+                    // the requested axis already has a right lock
+                    rightIsLocked = true;
+                }
+            }
+        }
+
+
+        var newLimits = XAxis.Range;
+        var ticks =XAxis.TickGenerator.Ticks.Where(tick => tick.IsMajor).ToList();
+        //var ticks = rp.Plot.Axes.Bottom.TickGenerator.Ticks.Where(tick => tick.IsMajor).ToList();
+        
+        if (ticks.Count < 2) return;
+        var tickInterval = ticks[1].Position - ticks[0].Position;
+
+        double newRight = newLimits.Max;
+        double newLeft = newLimits.Min;
+
+        if (!rightIsLocked)
+        {
+            if (newLimits.Max >= rp.Plot.LastRender.AxisLimitsByAxis[XAxis].Max)
+            {
+                newRight = Math.Ceiling(newLimits.Max / tickInterval) * tickInterval;
+            }
+            else
+            {
+                newRight = Math.Floor(newLimits.Max / tickInterval) * tickInterval;
+            }
+        }
+
+        if (!leftIsLocked)
+        {
+            if (newLimits.Min <= rp.Plot.LastRender.AxisLimitsByAxis[XAxis].Min)
+            {
+                newLeft = Math.Floor(newLimits.Min / tickInterval) * tickInterval;
+            }
+            else
+            {
+                newLeft = Math.Ceiling(newLimits.Min / tickInterval) * tickInterval;
+            }
+        }
+
+        XAxis.Range.Set(newLeft, newRight);
+    }
+}
---
+++ src/ScottPlot5/ScottPlot5/AxisRules/SnapToTicksY.cs
@@ -0,0 +1,87 @@
+﻿namespace ScottPlot.AxisRules;
+
+public class SnapToTicksY : IAxisRule
+{
+    readonly IYAxis YAxis;
+
+    public SnapToTicksY(IYAxis yAxis)
+    {
+        YAxis = yAxis;
+    }
+
+    public void Apply(RenderPack rp, bool beforeLayout)
+    {
+        // rules that refer to the last render must wait for a render to occur
+        if (rp.Plot.LastRender.Count == 0)
+            return;
+
+        bool topIsLocked = false;
+        bool bottomIsLocked = false;
+        // a locked axis wont be snapped (locking rules take priority)
+        foreach (var rule in rp.Plot.Axes.Rules)
+        {
+            if (rule is LockedVertical lockedVerticalRule)
+            {
+                if(lockedVerticalRule.YAxis == YAxis)
+                {
+                    // the requested axis already has a vertical lock
+                    return;
+                }
+            }
+
+            if (rule is LockedTop lockedTopRule)
+            {
+                if (lockedTopRule.YAxis == YAxis)
+                {
+                    // the requested axis already has a top lock
+                    topIsLocked = true;
+                }
+            }
+
+            if (rule is LockedBottom lockedBottomRule)
+            {
+                if (lockedBottomRule.YAxis == YAxis)
+                {
+                    // the requested axis already has a bottom lock
+                    bottomIsLocked = true;
+                }
+            }
+        }
+
+        var newLimits = YAxis.Range;
+        var ticks = YAxis.TickGenerator.Ticks.Where(tick => tick.IsMajor).ToList();
+        //var ticks = rp.Plot.Axes.Bottom.TickGenerator.Ticks.Where(tick => tick.IsMajor).ToList();
+        
+        if (ticks.Count < 2) return;
+        var tickInterval = ticks[1].Position - ticks[0].Position;
+
+        double newTop = newLimits.Max;
+        double newBottom = newLimits.Min;
+
+        if (!topIsLocked)
+        {
+            if (newLimits.Max >= rp.Plot.LastRender.AxisLimitsByAxis[YAxis].Max)
+            {
+                newTop = Math.Ceiling(newLimits.Max / tickInterval) * tickInterval;
+            }
+            else
+            {
+                newTop = Math.Floor(newLimits.Max / tickInterval) * tickInterval;
+            }
+        }
+
+        if (!bottomIsLocked)
+        {
+            if (newLimits.Min <= rp.Plot.LastRender.AxisLimitsByAxis[YAxis].Min)
+            {
+                newBottom = Math.Floor(newLimits.Min / tickInterval) * tickInterval;
+            }
+            else
+            {
+                newBottom = Math.Ceiling(newLimits.Min / tickInterval) * tickInterval;
+            }
+        }
+
+        YAxis.Range.Set(newBottom, newTop);
+    }
+}
