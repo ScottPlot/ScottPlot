4f67ded7fb1164b5956f86f7a57d7942f84efe93
Scott W Harden <swharden@gmail.com>
2024-03-31 14:19:41 -0400

    WPF Sandbox: simplify SignalXY GetNearest() demo

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WPF/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Windows.Input;
 using ScottPlot;
 using ScottPlot.WPF;
 using ScottPlot.Plottables;
-using System.Collections.Generic;
 
 namespace Sandbox.WPF;
 
@@ -13,13 +12,18 @@ public partial class MainWindow : Window
     SignalXY? PlottableBeingDragged = null;
     DataPoint StartingDragPosition = DataPoint.None;
     double StartingDragOffset = 0;
+    double ScaleFactor => PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice.M11;
 
     public MainWindow()
     {
         InitializeComponent();
-        double[] xAxis = new List<double>(Enumerable.Range(0, 100).Select(x => (double)x)).ToArray();
-        WpfPlot1.Plot.Add.SignalXY(xAxis,Generate.Sin(100));
-        WpfPlot1.Plot.Add.SignalXY(xAxis,Generate.Cos(100));
+
+        double[] xs = Generate.Consecutive(100);
+        double[] ys1 = Generate.Sin(100);
+        double[] ys2 = Generate.Cos(100);
+
+        WpfPlot1.Plot.Add.SignalXY(xs, ys1);
+        WpfPlot1.Plot.Add.SignalXY(xs, ys2);
 
         WpfPlot1.MouseDown += FormsPlot1_MouseDown;
         WpfPlot1.MouseUp += FormsPlot1_MouseUp;
@@ -28,39 +32,42 @@ public partial class MainWindow : Window
 
     private void FormsPlot1_MouseDown(object sender, MouseButtonEventArgs e)
     {
-        var plot = (WpfPlot)sender;
-        if (plot != null)
-        {
-            double factor = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice.M11;
-            double x = e.GetPosition(plot).X * factor;
-            double y = e.GetPosition(plot).Y * factor;
-            DataPoint dataPoint;
-            var signalUnderMouse = GetSignalUnderMouse(plot, x, y, out dataPoint);
-            if (signalUnderMouse != null)
-            {
-                PlottableBeingDragged = signalUnderMouse;
-                StartingDragPosition = dataPoint;
-                StartingDragOffset = signalUnderMouse.Data.XOffset;
-                plot.Interaction.Disable(); // disable panning while dragging
-            }
-        }
+        WpfPlot plot = (WpfPlot)sender;
+        if (plot is null)
+            return;
+
+        double x = e.GetPosition(plot).X * ScaleFactor;
+        double y = e.GetPosition(plot).Y * ScaleFactor;
+
+        (var signalUnderMouse, DataPoint dataPoint) = GetSignalXYUnderMouse(plot, x, y);
+        if (signalUnderMouse is null)
+            return;
+
+        PlottableBeingDragged = signalUnderMouse;
+        StartingDragPosition = dataPoint;
+        StartingDragOffset = signalUnderMouse.Data.XOffset;
+        plot.Interaction.Disable(); // disable panning while dragging
     }
+
     private void FormsPlot1_MouseUp(object sender, MouseButtonEventArgs e)
     {
         var plot = (WpfPlot)sender;
-        if (plot != null)
-        {
-            PlottableBeingDragged = null;
-            StartingDragPosition = DataPoint.None;
-            plot.Interaction.Enable(); // enable panning again
-            plot.Refresh();
-        }
+        if (plot is null)
+            return;
+
+        PlottableBeingDragged = null;
+        StartingDragPosition = DataPoint.None;
+        plot.Interaction.Enable(); // enable panning again
+        plot.Refresh();
     }
 
-    private SignalXY GetSignalUnderMouse(WpfPlot plot, double x, double y, out DataPoint datapoint)
+    /// <summary>
+    /// Returns the SignalXY object and data point beneath the mouse,
+    /// or null if nothing is beneath the mouse.
+    /// </summary>
+    private static (SignalXY? signalXY, DataPoint point) GetSignalXYUnderMouse(WpfPlot plot, double x, double y)
     {
         Pixel mousePixel = new(x, y);
-        datapoint = DataPoint.None;
 
         Coordinates mouseLocation = plot.Plot.GetCoordinates(mousePixel);
 
@@ -69,39 +76,37 @@ public partial class MainWindow : Window
             DataPoint nearest = signal.Data.GetNearest(mouseLocation, plot.Plot.LastRender);
             if (nearest.IsReal)
             {
-                datapoint = nearest;
-                return signal;
+                return (signal, nearest);
             }
         }
 
-        return null;
+        return (null, DataPoint.None);
     }
 
     private void FormsPlot1_MouseMove(object sender, MouseEventArgs e)
     {
-        var plot = (WpfPlot)sender;
-        if (plot != null)
+        var plotControl = (WpfPlot)sender;
+        if (plotControl is null)
+            return;
+
+        // this rectangle is the area around the mouse in coordinate units
+        float x = (float)(e.GetPosition(plotControl).X * ScaleFactor);
+        float y = (float)(e.GetPosition(plotControl).Y * ScaleFactor);
+        CoordinateRect rect = plotControl.Plot.GetCoordinateRect(x, y, radius: 5);
+
+        // update the cursor to reflect what is beneath it
+        if (PlottableBeingDragged is null)
         {
-            double factor = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice.M11;
+            (var signalUnderMouse, _) = GetSignalXYUnderMouse(plotControl, x, y);
+            Cursor = signalUnderMouse is null ? Cursors.Arrow : Cursors.SizeWE;
+            return;
+        }
 
-            // this rectangle is the area around the mouse in coordinate units
-            CoordinateRect rect = plot.Plot.GetCoordinateRect((float)(e.GetPosition(plot).X * factor), (float)(e.GetPosition(plot).Y * factor), radius: 5);
-
-            if (PlottableBeingDragged is null)
-            {
-                var signalUnderMouse = GetSignalUnderMouse(plot, e.GetPosition(plot).X * factor, e.GetPosition(plot).Y * factor, out _);
-                if (signalUnderMouse is null) Cursor = Cursors.Arrow;
-                else Cursor = Cursors.SizeWE;
-            }
-            else
-            {
-                // update the position of the plottable being dragged
-                if (PlottableBeingDragged is SignalXY vl)
-                {
-                    vl.Data.XOffset = rect.HorizontalCenter - StartingDragPosition.X + StartingDragOffset;
-                }
-                plot.Refresh();
-            }
+        // update the position of the plottable being dragged
+        if (PlottableBeingDragged is SignalXY sigXY)
+        {
+            sigXY.Data.XOffset = rect.HorizontalCenter - StartingDragPosition.X + StartingDragOffset;
+            plotControl.Refresh();
         }
     }
 }
