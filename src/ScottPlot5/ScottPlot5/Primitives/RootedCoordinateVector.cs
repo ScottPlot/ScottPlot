c4573da266f6369b1ae825534402ee5833dfd156
Benjamin Lehmann <bclehmann@yahoo.ca>
2024-04-08 18:47:49 -0600

    Rooted Vector Primitives

---
+++ src/ScottPlot5/ScottPlot5/Primitives/RootedCoordinateVector.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace ScottPlot.Primitives;
+
+public struct RootedCoordinateVector(Coordinates tail, Vector2 vector)
+{
+    public Coordinates Tail { get; set; } = tail;
+    public Vector2 Vector { get; set; } = vector;
+
+    public readonly float Direction => (float)Math.Atan2(Vector.Y, Vector.X); // Note that this is properly defined for x = 0, unlike Math.Atan(y / x)
+    public readonly float DistanceSquared => Vector.LengthSquared();
+    public readonly float Distance => Vector.Length();
+}
