51de841307c714bc58091dcaf0e31555b457384f
Benjamin Lehmann <bclehmann@yahoo.ca>
2024-04-08 18:48:40 -0600

    Basic VectorField plottable

---
+++ src/ScottPlot5/ScottPlot5/DataSources/VectorFieldDataSourceCoordinatesList.cs
@@ -0,0 +1,75 @@
+﻿using ScottPlot.Interfaces;
+using ScottPlot.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlot.DataSources;
+
+public class VectorFieldDataSourceCoordinatesList(IList<RootedCoordinateVector> rootedVectors) : IVectorFieldSource
+{
+    private readonly IList<RootedCoordinateVector> RootedVectors = rootedVectors;
+
+    public int MinRenderIndex { get; set; } = 0;
+    public int MaxRenderIndex { get; set; } = int.MaxValue;
+    private int RenderIndexCount => Math.Min(RootedVectors.Count - 1, MaxRenderIndex) - MinRenderIndex + 1;
+
+
+    public IReadOnlyList<RootedCoordinateVector> GetRootedVectors()
+    {
+        return RootedVectors
+            .Skip(MinRenderIndex)
+            .Take(RenderIndexCount)
+            .ToList();
+    }
+
+    public AxisLimits GetLimits()
+    {
+        ExpandingAxisLimits limits = new();
+        limits.Expand(RootedVectors.Select(v => v.Tail).Skip(MinRenderIndex).Take(RenderIndexCount));
+        return limits.AxisLimits;
+    }
+
+    public CoordinateRange GetLimitsX()
+    {
+        return GetLimits().Rect.XRange;
+    }
+
+    public CoordinateRange GetLimitsY()
+    {
+        return GetLimits().Rect.YRange;
+    }
+
+    // TODO: This matches to the closest tail, this might not be desirable depending if the arrows are anchored to their tail or their midpoint
+    public DataPoint GetNearest(Coordinates mouseLocation, RenderDetails renderInfo, float maxDistance = 15)
+    {
+        double maxDistanceSquared = maxDistance * maxDistance;
+        double closestDistanceSquared = double.PositiveInfinity;
+
+        int closestIndex = 0;
+        double closestX = double.PositiveInfinity;
+        double closestY = double.PositiveInfinity;
+
+        for (int i2 = 0; i2 < RenderIndexCount; i2++)
+        {
+            int i = MinRenderIndex + i2;
+            double dX = (RootedVectors[i].Tail.X - mouseLocation.X) * renderInfo.PxPerUnitX;
+            double dY = (RootedVectors[i].Tail.Y - mouseLocation.Y) * renderInfo.PxPerUnitY;
+            double distanceSquared = dX * dX + dY * dY;
+
+            if (distanceSquared <= closestDistanceSquared)
+            {
+                closestDistanceSquared = distanceSquared;
+                closestX = RootedVectors[i].Tail.X;
+                closestY = RootedVectors[i].Tail.Y;
+                closestIndex = i;
+            }
+        }
+
+        return closestDistanceSquared <= maxDistanceSquared
+            ? new DataPoint(closestX, closestY, closestIndex)
+            : DataPoint.None;
+    }
+}
---
+++ src/ScottPlot5/ScottPlot5/Plottables/VectorField.cs
@@ -0,0 +1,62 @@
+﻿using ScottPlot.Interfaces;
+using ScottPlot.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlot.Plottables;
+
+public class VectorField(IVectorFieldSource source) : IPlottable
+{
+    public bool IsVisible { get; set; } = true;
+    public IAxes Axes { get; set; } = new Axes();
+    public string? Label { get; set; }
+    public LineStyle LineStyle { get; set; } = new(); // TODO: Maybe this should be a field within an ArrowStyle?
+
+    public IEnumerable<LegendItem> LegendItems => EnumerableExtensions.One(
+        new LegendItem
+        {
+            Label = Label,
+            Marker = MarkerStyle.None, // TODO: Should there be an arrow-style marker?
+            Line = LineStyle,
+        });
+
+    IVectorFieldSource Source { get; set; } = source;
+
+    public AxisLimits GetAxisLimits() => Source.GetLimits();
+
+    public void Render(RenderPack rp)
+    {
+        const float maxLength = 25;
+
+        // TODO: Filter out those that are off-screen? This is subtle, an arrow may be fully off-screen except for its arrowhead, if the blades are long enough.
+        var vectors = Source.GetRootedVectors().Select(v => new RootedPixelVector(Axes.GetPixel(v.Tail), v.Vector)).ToArray();
+        if (vectors.Length == 0)
+            return;
+
+        var minMagnitudeSquared = double.PositiveInfinity;
+        var maxMagnitudeSquared = double.NegativeInfinity;
+        foreach (var v in vectors)
+        {
+            var magSquared = v.DistanceSquared;
+            minMagnitudeSquared = Math.Min(minMagnitudeSquared, magSquared);
+            maxMagnitudeSquared = Math.Max(maxMagnitudeSquared, magSquared);
+        }
+
+        var range = new Range(Math.Sqrt(minMagnitudeSquared), Math.Sqrt(maxMagnitudeSquared));
+
+        for (int i = 0; i < vectors.Length; i++)
+        {
+            var oldMagnitude = vectors[i].Distance;
+            var newMagnitude = range.Normalize(oldMagnitude) * maxLength;
+            var direction = vectors[i].Direction;
+
+            vectors[i].Vector = new((float)(Math.Cos(direction) * newMagnitude), (float)(Math.Sin(direction) * newMagnitude));
+        }
+
+        using SKPaint paint = new();
+        Drawing.DrawArrows(rp.Canvas, paint, vectors, LineStyle);
+    }
+}
