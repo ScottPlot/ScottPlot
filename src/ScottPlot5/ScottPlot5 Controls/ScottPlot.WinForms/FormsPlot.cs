5bb1749080d94bfdf052a995993fb121673af213
Scott W Harden <swharden@gmail.com>
2024-04-12 17:36:43 -0400

    FormsPlot: refactor SKControl setup and teardown

--- src/ScottPlot5/ScottPlot5 Controls/ScottPlot.WinForms/FormsPlot.cs
+++ src/ScottPlot5/ScottPlot5 Controls/ScottPlot.WinForms/FormsPlot.cs
@@ -13,72 +13,63 @@ namespace ScottPlot.WinForms;
 #endif
 public class FormsPlot : FormsPlotBase
 {
-    private SKControl SKElement;
+    private SKControl? SKControl;
 
     public override GRContext GRContext => null!;
 
     public FormsPlot()
     {
-        SKElement = new() { Dock = DockStyle.Fill };
-        BindSKElement();
-        Controls.Add( SKElement );
-
-        HandleCreated += (s, e) =>
-        {
-            if ( SKElement != null && !SKElement.IsDisposed)
-            {
-                DisposeSKElement();
-            }
-            SKElement = new() { Dock = DockStyle.Fill };
-            BindSKElement();
-            Controls.Add( SKElement );
-        };
-
-        HandleDestroyed += (s, e) =>
-        {
-            DisposeSKElement();
-        };
+        HandleCreated += (s, e) => SetupSKControl();
+        HandleDestroyed += (s, e) => TeardownSKControl();
+        SetupSKControl();
     }
 
-    private void DisposeSKElement()
+    private void SetupSKControl()
     {
-        UnbindSKElement();
-        Controls.Remove(SKElement);
-        SKElement.Dispose();
+        TeardownSKControl();
+
+        SKControl = new() { Dock = DockStyle.Fill };
+
+        SKControl.PaintSurface += SKElement_PaintSurface;
+        SKControl.MouseDown += SKElement_MouseDown;
+        SKControl.MouseUp += SKElement_MouseUp;
+        SKControl.MouseMove += SKElement_MouseMove;
+        SKControl.DoubleClick += SKElement_DoubleClick;
+        SKControl.MouseWheel += SKElement_MouseWheel;
+        SKControl.KeyDown += SKElement_KeyDown;
+        SKControl.KeyUp += SKElement_KeyUp;
+
+        Controls.Add(SKControl);
     }
 
-    private void BindSKElement()
+    private void TeardownSKControl()
     {
-        SKElement.PaintSurface += SKElement_PaintSurface;
-        SKElement.MouseDown += SKElement_MouseDown;
-        SKElement.MouseUp += SKElement_MouseUp;
-        SKElement.MouseMove += SKElement_MouseMove;
-        SKElement.DoubleClick += SKElement_DoubleClick;
-        SKElement.MouseWheel += SKElement_MouseWheel;
-        SKElement.KeyDown += SKElement_KeyDown;
-        SKElement.KeyUp += SKElement_KeyUp;
-    }
+        if (SKControl is null)
+            return;
 
-    private void UnbindSKElement()
-    {
-        SKElement.PaintSurface -= SKElement_PaintSurface; ;
-        SKElement.MouseDown -= SKElement_MouseDown;
-        SKElement.MouseUp -= SKElement_MouseUp;
-        SKElement.MouseMove -= SKElement_MouseMove;
-        SKElement.DoubleClick -= SKElement_DoubleClick;
-        SKElement.MouseWheel -= SKElement_MouseWheel;
-        SKElement.KeyDown -= SKElement_KeyDown;
-        SKElement.KeyUp -= SKElement_KeyUp;
+        SKControl.PaintSurface -= SKElement_PaintSurface; ;
+        SKControl.MouseDown -= SKElement_MouseDown;
+        SKControl.MouseUp -= SKElement_MouseUp;
+        SKControl.MouseMove -= SKElement_MouseMove;
+        SKControl.DoubleClick -= SKElement_DoubleClick;
+        SKControl.MouseWheel -= SKElement_MouseWheel;
+        SKControl.KeyDown -= SKElement_KeyDown;
+        SKControl.KeyUp -= SKElement_KeyUp;
+
+        Controls.Remove(SKControl);
+
+        if (!SKControl.IsDisposed)
+            SKControl.Dispose();
     }
 
     private void SKElement_PaintSurface(object? sender, SKPaintSurfaceEventArgs e)
     {
-        Plot.Render(e.Surface.Canvas, (int)e.Surface.Canvas.LocalClipBounds.Width, (int)e.Surface.Canvas.LocalClipBounds.Height);
+        Plot.Render(e.Surface);
     }
 
     public override void Refresh()
     {
-        SKElement.Invalidate();
+        SKControl?.Invalidate();
         base.Refresh();
     }
 }
