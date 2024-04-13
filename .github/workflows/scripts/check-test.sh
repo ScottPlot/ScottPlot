336cb10c91feca50e4dac580910e3190550da7f5
Scott W Harden <swharden@gmail.com>
2024-04-13 09:51:44 -0400

    CICD: store paths in shell script variables

--- .github/workflows/scripts/check-test.sh
+++ .github/workflows/scripts/check-test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-while IFS=$'\n' read -r line; do
-    echo "Testing: $line"
-    dotnet test "$line" --configuration Release --no-build --verbosity minimal
-done <".github/workflows/scripts/check-projects.txt"
+test_project_files=(
+    "src/ScottPlot5/ScottPlot5 Tests/ScottPlot Tests.csproj"
+    "src/ScottPlot5/ScottPlot5 Cookbook/ScottPlot Cookbook.csproj"
+    "src/ScottPlot4/ScottPlot/ScottPlot.csproj"
+)
+
+for test_project_file in "${project_files[@]}"; do
+    echo "Testing: $test_project_file"
+    dotnet restore "$test_project_file"
+    dotnet build "$test_project_file" --configuration Release
+done
