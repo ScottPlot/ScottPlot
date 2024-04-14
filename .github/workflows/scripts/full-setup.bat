b8b5fa376e638bdbe9c16a51d349ad9075cb0c7d
Scott W Harden <swharden@gmail.com>
2024-04-14 16:18:28 -0400

    CICD: run tests from individual workflow steps (not a single shell script)

--- .github/workflows/scripts/full-setup.bat
+++
@@ -1,3 +0,0 @@
-dotnet workload install maui-android maui-ios maccatalyst wasm-tools
-dotnet workload restore "src\ScottPlot5\ScottPlot5.sln"
-dotnet workload restore "src\ScottPlot4\ScottPlot4.sln"
