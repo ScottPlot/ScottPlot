082120c8b9746318a95b035aada300b66b1baed5
Scott W Harden <swharden@gmail.com>
2024-04-15 08:25:35 -0400

    LabelStyleProperties

---
+++ src/ScottPlot5/ScottPlot5/StyleProperties/LabelStyleProperties.cs
@@ -0,0 +1,33 @@
+﻿namespace ScottPlot.StyleProperties;
+
+/// <summary>
+/// Classes with a <see cref="LabelStyle"/> can inherit this
+/// to include shortcuts to its most commonly used properties.
+/// </summary>
+public abstract class LabelStyleProperties
+{
+    public abstract Label LabelStyle { get; }
+
+    public float LabelOffsetX { get => LabelStyle.OffsetX; set => LabelStyle.OffsetX = value; }
+    public float LabelOffsetY { get => LabelStyle.OffsetY; set => LabelStyle.OffsetY = value; }
+    public float LabelRotation { get => LabelStyle.Rotation; set => LabelStyle.Rotation = value; }
+    public float LabelPadding { get => LabelStyle.Padding; set => LabelStyle.Padding = value; }
+    public PixelRect LabelLastRenderPixelRect => LabelStyle.LastRenderPixelRect;
+
+    public string LabelText { get => LabelStyle.Text; set => LabelStyle.Text = value; }
+    public Alignment LabelAlignment { get => LabelStyle.Alignment; set => LabelStyle.Alignment = value; }
+
+    public string LabelFontName { get => LabelStyle.FontName; set => LabelStyle.FontName = value; }
+    public float LabelFontSize { get => LabelStyle.FontSize; set => LabelStyle.FontSize = value; }
+    public bool LabelItalic { get => LabelStyle.Italic; set => LabelStyle.Italic = value; }
+    public bool LabelBold { get => LabelStyle.Bold; set => LabelStyle.Bold = value; }
+
+    public Color LabelFontColor { get => LabelStyle.ForeColor; set => LabelStyle.ForeColor = value; }
+    public Color LabelBackgroundColor { get => LabelStyle.BackgroundColor; set => LabelStyle.BackgroundColor = value; }
+
+    public float LabelBorderWidth { get => LabelStyle.BorderWidth; set => LabelStyle.BorderWidth = value; }
+    public Color LabelBorderColor { get => LabelStyle.BorderColor; set => LabelStyle.BorderColor = value; }
+
+    public Color LabelShadowColor { get => LabelStyle.ShadowColor; set => LabelStyle.ShadowColor = value; }
+    public PixelOffset LabelShadowOffset { get => LabelStyle.ShadowOffset; set => LabelStyle.ShadowOffset = value; }
+}
