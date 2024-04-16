3ad048f5b4dddd26446bcc037aa47af5310b5566
Scott W Harden <swharden@gmail.com>
2024-04-15 23:30:06 -0400

    Rectangle: implement IHasFill properties

--- src/ScottPlot5/ScottPlot5/Plottables/Rectangle.cs
+++ src/ScottPlot5/ScottPlot5/Plottables/Rectangle.cs
@@ -11,7 +11,7 @@ public class Rectangle : IPlottable, IHasLine, IHasFill
     public LinePattern LinePattern { get => LineStyle.Pattern; set => LineStyle.Pattern = value; }
     public Color LineColor { get => LineStyle.Color; set => LineStyle.Color = value; }
 
-    public FillStyle FillStyle { get => FillStyle; }
+    public FillStyle FillStyle { get; } = new();
     public Color FillColor { get => FillStyle.Color; set => FillStyle.Color = value; }
     public Color FillHatchColor { get => FillStyle.HatchColor; set => FillStyle.HatchColor = value; }
     public IHatch? FillHatch { get => FillStyle.Hatch; set => FillStyle.Hatch = value; }
