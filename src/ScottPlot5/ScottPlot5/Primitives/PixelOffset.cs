88200ecf414a673de174874530777aa007598d88
Scott W Harden <swharden@gmail.com>
2024-04-18 09:05:03 -0400

    PixelOffset: Zero and NaN

--- src/ScottPlot5/ScottPlot5/Primitives/PixelOffset.cs
+++ src/ScottPlot5/ScottPlot5/Primitives/PixelOffset.cs
@@ -5,4 +5,8 @@
 /// Increasing X offset moves a pixel to the right.
 /// Increasing Y offset moves a pixel downward.
 /// </summary>
-public record struct PixelOffset(float X, float Y);
+public record struct PixelOffset(float X, float Y)
+{
+    public static PixelOffset Zero => new(0, 0);
+    public static PixelOffset NaN => new(float.NaN, float.NaN);
+}
