24db923c3eb378d24d366dccb8f7161eb2fe966f
Fabrice Mousset <3978902+KroMignon@users.noreply.github.com>
2024-04-19 12:22:28 +0200

    update layout to be closer to Blazor standards
    - add service to hold recipes
    - minor code cleanup

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Blazor.WebAssembly/Program.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Blazor.WebAssembly/Program.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sandbox.Blazor.WebAssembly;
+using Sandbox.Blazor.WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+builder.Services.AddScoped(sp => httpClient);
+var recipies = new RecipesService(httpClient);
+builder.Services.AddSingleton<IRecipesService>(recipies);
+var app = builder.Build();
 
-await builder.Build().RunAsync();
+await recipies.GetRecipesAsync();
+await app.RunAsync();
---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Blazor.WebAssembly/Services/RecipesService.cs
@@ -0,0 +1,88 @@
+﻿using ScottPlot.Blazor;
+using ScottPlotCookbook;
+using System.Collections.ObjectModel;
+using static System.Net.WebRequestMethods;
+using System.Text.Json;
+
+namespace Sandbox.Blazor.WebAssembly.Services
+{
+    public class RecipesService : IRecipesService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly Dictionary<ICategory, IEnumerable<IRecipe>> _recipesByCategory = Query.GetRecipesByCategory();
+        private JsonDocument? JsonRecipes = null;
+
+        public ReadOnlyDictionary<ICategory, IEnumerable<IRecipe>> RecipesByCategory => _recipesByCategory.AsReadOnly();
+
+        public event Action? RecipeChanged;
+        public IRecipe? Recipe
+        {
+            get => _recipe;
+            set
+            {
+                if (value == _recipe)
+                    return;
+                SetRecipe(value);
+                RecipeChanged?.Invoke();
+            }
+        }
+        private IRecipe? _recipe = null;
+
+        public bool HasRecipe => _recipe != null;
+        public string RecipeName => _recipe?.Name ?? string.Empty;
+        public string RecipeDescription => _recipe?.Description ?? string.Empty;
+        public bool HasSourceCode => !string.IsNullOrEmpty(SourceCode);
+        public string SourceCode { get; private set; } = string.Empty;
+        public async Task GetRecipesAsync()
+        {
+            if (JsonRecipes is null)
+            {
+                string json = await _httpClient.GetStringAsync("sample-data/recipes.json");
+                JsonRecipes = JsonDocument.Parse(json);
+            }
+        }
+        private void SetRecipe(IRecipe? recipe)
+        {
+            _recipe = recipe;
+            string source = "// source code not found";
+            if (JsonRecipes != null && HasRecipe)
+            {
+                foreach (JsonElement recipeElement in JsonRecipes.RootElement.GetProperty("recipes").EnumerateArray())
+                {
+                    string name = recipeElement.GetProperty("name").GetString() ?? string.Empty;
+                    string desc = recipeElement.GetProperty("description").GetString() ?? string.Empty;
+                    if (name == Recipe!.Name && desc == Recipe!.Description)
+                    {
+                        source = recipeElement.GetProperty("source").GetString() ?? string.Empty;
+                        break;
+                    }
+                }
+            }
+            SourceCode = source;
+        }
+        public bool IsSelected(IRecipe? recipe)
+        {
+            return recipe?.Name == _recipe?.Name && recipe?.Description == _recipe?.Description;
+        }
+
+        public bool FindRecipe(string name)
+        {
+            foreach (var category in RecipesByCategory)
+            {
+                foreach (var recipe in category.Value)
+                {
+                    if (string.Compare(recipe.Name, name, true) == 0 || string.Compare(string.Join(string.Empty, recipe.Name.Split()), name, true) == 0)
+                    {
+                        SetRecipe(recipe);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public RecipesService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
