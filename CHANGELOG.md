4614501c0e23073fd39b8f3020e7f36baec788c8
Scott W Harden <swharden@gmail.com>
2024-04-26 20:50:17 -0400

    DataGenerators.RandomWalk2D

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@ _Not yet on NuGet..._
 * Bar: Set default line width to 1 so error bars are visible without requiring line customization (#3741) @Kareltje1980
 * Controls: Added a `Interaction.ChangeOpposingAxesTogether` flag to enable mouse actions to one axis to be applied to all axes with the same orientation (#3729) @rubenslkirchner
 * DataLogger: Remove requirement for new data points to contain ascending X values (#3737) @TenebrosFR
+* RandomWalk2D: Created `ScottPlot.DataGenerators.RandomWalk2D` for easily generating 2D random data with randomly changing velocity
 
 ## ScottPlot 5.0.29
 _Published on [NuGet](https://www.nuget.org/profiles/ScottPlot) on 2024-04-26_
--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
@@ -1,9 +1,11 @@
-﻿namespace Sandbox.WinForms;
+﻿using ScottPlot;
+
+namespace Sandbox.WinForms;
 
 public partial class Form1 : Form
 {
     System.Windows.Forms.Timer Timer = new() { Interval = 10 };
-
+    ScottPlot.DataGenerators.RandomWalker2D Walker = new();
     public Form1()
     {
         InitializeComponent();
@@ -12,18 +14,10 @@ public partial class Form1 : Form
         logger.Add(0, 0);
         logger.LineWidth = 2;
 
-        double xMult = 1;
-        double yMult = 1;
-
         Timer.Tick += (s, e) =>
         {
-            if (Random.Shared.Next(100) == 1) xMult = -xMult;
-            if (Random.Shared.Next(100) == 1) yMult = -yMult;
-
-            double x = logger.Data.Coordinates.Last().X + Random.Shared.NextDouble() * xMult;
-            double y = logger.Data.Coordinates.Last().Y + Random.Shared.NextDouble() * yMult;
-            logger.Add(x, y);
-
+            Coordinates pt = Walker.Next();
+            logger.Add(pt);
             formsPlot1.Refresh();
         };
 
