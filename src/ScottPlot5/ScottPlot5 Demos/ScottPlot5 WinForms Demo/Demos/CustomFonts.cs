757b2ab844865d8c13448f569f01e8e8f5752d77
Scott W Harden <swharden@gmail.com>
2024-05-04 01:02:52 -0400

    Demo: custom fonts
    #3722

---
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/CustomFonts.cs
@@ -0,0 +1,37 @@
+﻿using ScottPlot;
+
+namespace WinForms_Demo.Demos;
+
+public partial class CustomFonts : Form, IDemoWindow
+{
+    public string Title => "Custom Fonts";
+
+    public string Description => "Demonstrates how to create plots that " +
+        "render text using fonts defined in an external TTF file.";
+
+    public CustomFonts()
+    {
+        InitializeComponent();
+
+        formsPlot1.Plot.Add.Signal(Generate.Sin());
+        formsPlot1.Plot.Add.Signal(Generate.Cos());
+
+        string fontPath = "Fonts/ShadowsIntoLight/ShadowsIntoLight-Regular.ttf";
+
+        formsPlot1.Plot.Title("Plot Using Custom Fonts");
+        formsPlot1.Plot.Axes.Title.Label.FontFile = fontPath;
+        formsPlot1.Plot.Axes.Title.Label.FontSize = 36;
+
+        formsPlot1.Plot.XLabel("Horizontal Axis");
+        formsPlot1.Plot.Axes.Bottom.Label.FontFile = fontPath;
+        formsPlot1.Plot.Axes.Bottom.Label.FontSize = 24;
+        formsPlot1.Plot.Axes.Bottom.TickLabelStyle.FontFile = fontPath;
+        formsPlot1.Plot.Axes.Bottom.TickLabelStyle.FontSize = 18;
+
+        formsPlot1.Plot.YLabel("Vertical Axis");
+        formsPlot1.Plot.Axes.Left.Label.FontFile = fontPath;
+        formsPlot1.Plot.Axes.Left.Label.FontSize = 24;
+        formsPlot1.Plot.Axes.Left.TickLabelStyle.FontFile = fontPath;
+        formsPlot1.Plot.Axes.Left.TickLabelStyle.FontSize = 18;
+    }
+}
