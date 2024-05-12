a97afcb21fa4964a122e95c7342026ef4bd1816a
BrianAtZetica <brian@zetica.com>
2024-05-12 05:02:33 +0100

    reinstate snaptotick logic to ensure user intention is honoured

--- src/ScottPlot5/ScottPlot5/AxisRules/SnapToTicksX.cs
+++ src/ScottPlot5/ScottPlot5/AxisRules/SnapToTicksX.cs
@@ -13,12 +13,82 @@ public class SnapToTicksX(IXAxis xAxis) : IAxisRule
         if (rp.Plot.RenderManager.RenderCount > 0 && rp.Plot.RenderManager.LastRender.DataRect != rp.DataRect)
             return;
 
+        bool leftIsLocked = false;
+        bool rightIsLocked = false;
+        // a locked axis wont be snapped (locking rules take priority)
+        foreach (var rule in rp.Plot.Axes.Rules)
+        {
+            if (rule is LockedHorizontal lockedHorizontalRule)
+            {
+                if (lockedHorizontalRule.XAxis == XAxis)
+                {
+                    // the requested axis already has a horizontal lock
+                    return;
+                }
+            }
+
+            if (rule is LockedLeft lockedLeftRule)
+            {
+                if (lockedLeftRule.XAxis == XAxis)
+                {
+                    // the requested axis already has a left lock
+                    leftIsLocked = true;
+                }
+            }
+
+            if (rule is LockedRight lockedRightRule)
+            {
+                if (lockedRightRule.XAxis == XAxis)
+                {
+                    // the requested axis already has a right lock
+                    rightIsLocked = true;
+                }
+            }
+        }
+
+        var newLimits = XAxis.Range;
+        double newRight = newLimits.Max;
+        double newLeft = newLimits.Min;
+
         XAxis.RegenerateTicks(new PixelLength(rp.FigureRect.Width));
         var ticks = XAxis.TickGenerator.Ticks.Where(tick => tick.IsMajor).Select(x => x.Position);
         if (ticks.Count() < 2)
             return;
 
         double tickDelta = ticks.Skip(1).First() - ticks.First();
-        XAxis.Range.Set(ticks.Min() - tickDelta, ticks.Max() + tickDelta);
+
+        if (!rightIsLocked)
+        {
+            var oldRight = rp.Plot.LastRender.AxisLimitsByAxis[XAxis].Max;
+            var proposedNewRight = Math.Ceiling(newLimits.Max / tickDelta) * tickDelta;
+
+            if (newLimits.Max >= oldRight || proposedNewRight < oldRight)
+            { // we'll snap outwards if we can 
+                newRight = proposedNewRight;
+            }
+            else
+            { // but if the snapped limit expands the range when a user requested to reduce the range we'll snap inwards
+                newRight = Math.Floor(newLimits.Max / tickDelta) * tickDelta;
+            }
+        }
+
+        if (!leftIsLocked)
+        {
+            var oldLeft = rp.Plot.LastRender.AxisLimitsByAxis[XAxis].Min;
+            var proposedNewLeft = Math.Floor(newLimits.Min / tickDelta) * tickDelta;
+
+            if (newLimits.Min <= oldLeft || proposedNewLeft > oldLeft)
+            { // we'll snap outwards if we can 
+                newLeft = proposedNewLeft;
+            }
+            else
+            { // but if the snapped limit expands the range when a user requested to reduce the range we'll snap inwards
+                newLeft = Math.Ceiling(newLimits.Min / tickDelta) * tickDelta;
+            }
+        }
+        if (newLeft != newRight) XAxis.Range.Set(newLeft, newRight);
+        
     }
+
+
 }
--- src/ScottPlot5/ScottPlot5/AxisRules/SnapToTicksY.cs
+++ src/ScottPlot5/ScottPlot5/AxisRules/SnapToTicksY.cs
@@ -13,12 +13,80 @@ public class SnapToTicksY(IYAxis yAxis) : IAxisRule
         if (rp.Plot.RenderManager.RenderCount > 0 && rp.Plot.RenderManager.LastRender.DataRect != rp.DataRect)
             return;
 
+        bool topIsLocked = false;
+        bool bottomIsLocked = false;
+        // a locked axis wont be snapped (locking rules take priority)
+        foreach (var rule in rp.Plot.Axes.Rules)
+        {
+            if (rule is LockedVertical lockedVerticalRule)
+            {
+                if (lockedVerticalRule.YAxis == YAxis)
+                {
+                    // the requested axis already has a vertical lock
+                    return;
+                }
+            }
+
+            if (rule is LockedTop lockedTopRule)
+            {
+                if (lockedTopRule.YAxis == YAxis)
+                {
+                    // the requested axis already has a top lock
+                    topIsLocked = true;
+                }
+            }
+
+            if (rule is LockedBottom lockedBottomRule)
+            {
+                if (lockedBottomRule.YAxis == YAxis)
+                {
+                    // the requested axis already has a bottom lock
+                    bottomIsLocked = true;
+                }
+            }
+        }
+
+        var newLimits = YAxis.Range;
+        double newTop = newLimits.Max;
+        double newBottom = newLimits.Min;
+
         YAxis.RegenerateTicks(new PixelLength(rp.FigureRect.Height));
         var ticks = YAxis.TickGenerator.Ticks.Where(tick => tick.IsMajor).Select(x => x.Position);
         if (ticks.Count() < 2)
             return;
 
         double tickDelta = ticks.Skip(1).First() - ticks.First();
-        YAxis.Range.Set(ticks.Min() - tickDelta, ticks.Max() + tickDelta);
+
+        if (!topIsLocked)
+        {
+            var oldTop = rp.Plot.LastRender.AxisLimitsByAxis[YAxis].Max;
+            var proposedNewTop = Math.Ceiling(newLimits.Max / tickDelta) * tickDelta;
+
+            if (newLimits.Max >= oldTop || proposedNewTop < oldTop)
+            { // we'll snap outwards if we can 
+                newTop = proposedNewTop;
+            }
+            else
+            { // but if the snapped limit expands the range when a user requested to reduce the range we'll snap inwards
+                newTop = Math.Floor(newLimits.Max / tickDelta) * tickDelta;
+            }
+        }
+
+        if (!bottomIsLocked)
+        {
+            var oldBottom = rp.Plot.LastRender.AxisLimitsByAxis[YAxis].Min;
+            var proposedNewBottom = Math.Floor(newLimits.Min / tickDelta) * tickDelta;
+
+            if (newLimits.Min <= oldBottom || proposedNewBottom > oldBottom)
+            { // we'll snap outwards if we can 
+                newBottom = proposedNewBottom;
+            }
+            else
+            { // but if the snapped limit expands the range when a user requested to reduce the range we'll snap inwards
+                newBottom = Math.Ceiling(newLimits.Min / tickDelta) * tickDelta;
+            }
+        }
+
+        if (newTop != newBottom) YAxis.Range.Set(newBottom, newTop);
     }
 }
