8fb19878b87d4eef7dd2bb94bd9f072dbf581e58
Scott W Harden <swharden@gmail.com>
2024-06-05 09:14:38 -0400

    ImageMarker: remove verbose docstring and whitespace

--- src/ScottPlot5/ScottPlot5/Plottables/ImageMarker.cs
+++ src/ScottPlot5/ScottPlot5/Plottables/ImageMarker.cs
@@ -1,23 +1,14 @@
 ﻿namespace ScottPlot.Plottables;
 
-/// <summary>
-/// It is recommended to use images that are correctly dimensioned during creation.
-/// A scaling property is available for convenience, but produces undesirable results on scaling up.
-/// This object retains the initial reference image used and is restored when resetting the scale if
-/// the displayed marker becomes distorted.
-/// </summary>
 public class ImageMarker : IPlottable
 {
     public bool IsVisible { get; set; } = true;
     public IAxes Axes { get; set; } = new Axes();
-
     public ScottPlot.Image? Image { get; set; } = null;
     public Coordinates Location { get; set; }
     public float Scale { get; set; } = 1;
     public bool AntiAlias { get; set; } = true;
-
     public IEnumerable<LegendItem> LegendItems => LegendItem.None;
-
     public AxisLimits GetAxisLimits() => AxisLimits.FromPoint(Location);
 
     public virtual void Render(RenderPack rp)
