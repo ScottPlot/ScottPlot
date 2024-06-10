480c67e2e7041e2e3a95fc5de187819331a74ed6
Scott W Harden <swharden@gmail.com>
2024-06-10 18:58:26 -0400

    Sandbox: simplify app

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
@@ -8,22 +8,7 @@ public partial class Form1 : Form
     {
         InitializeComponent();
 
-        double[] ys = Generate.SquareWave(5, 3);
-        double[] xs = Generate.Consecutive(ys.Length);
-
-        var sig1 = formsPlot1.Plot.Add.SignalXY(xs, ys);
-        sig1.MarkerSize = 5;
-
-        var sig2 = formsPlot1.Plot.Add.SignalXY(xs, ys);
-        sig2.Data.YOffset = 2;
-        sig2.Data.XOffset = .2;
-        sig2.ConnectStyle = ConnectStyle.StepHorizontal;
-        sig2.MarkerSize = 5;
-
-        var sig3 = formsPlot1.Plot.Add.SignalXY(xs, ys);
-        sig3.Data.YOffset = 4;
-        sig3.Data.XOffset = .4;
-        sig3.ConnectStyle = ConnectStyle.StepVertical;
-        sig3.MarkerSize = 5;
+        formsPlot1.Plot.Add.Signal(Generate.Sin());
+        formsPlot1.Plot.Add.Signal(Generate.Cos());
     }
 }
