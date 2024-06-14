e76f6530deef6a13a4bccb996a273c110db0bfdc
Scott W Harden <swharden@gmail.com>
2024-06-13 21:30:48 -0400

    Statistics.Tests.UnpairedTTest

--- src/ScottPlot5/ScottPlot5 Tests/Unit Tests/UnitTests/PopulationTests.cs
+++ src/ScottPlot5/ScottPlot5 Tests/Unit Tests/UnitTests/PopulationTests.cs
@@ -13,4 +13,19 @@ internal class PopulationTests
         pop.StandardError.Should().BeApproximately(16.082835, precision: 1e-5);
         pop.Median.Should().Be(231);
     }
+
+    [Test]
+    public void Test_Statistics_UnpairedTTest()
+    {
+        double[] arr1 = { 10, 20, 30, 40, 50 };
+        double[] arr2 = { 1, 29, 46, 78, 99 };
+
+        Population pop1 = new(arr1);
+        Population pop2 = new(arr2);
+
+        ScottPlot.Statistics.Tests.UnpairedTTest utt = new(pop1, pop2);
+        utt.DF.Should().Be(8);
+        utt.T.Should().BeApproximately(-1.09789, precision: 1e-5);
+        utt.P.Should().BeApproximately(0.3042, precision: 1e-4);
+    }
 }
