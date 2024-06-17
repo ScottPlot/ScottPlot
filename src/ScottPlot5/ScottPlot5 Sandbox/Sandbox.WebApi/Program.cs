79f6f560d87db67e774c5f4d4cc62a570cd250d7
Alfonso Espitia <Alfonso.Espitia@fairviewinvest.com>
2024-06-17 14:17:28 -0400

    new sandbox project

---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WebApi/Program.cs
@@ -0,0 +1,30 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => GetSampleImage());
+
+app.Run();
+
+
+string GetSampleImage() {
+
+
+    ScottPlot.Plot myPlot = new();
+
+    double[] dataX = { 1, 2, 3, 4, 5 };
+    double[] dataY = { 1, 4, 9, 16, 25 };
+    myPlot.Add.Scatter(dataX, dataY);
+
+
+    var imgBytes =  myPlot.GetImageBytes(400,300, ScottPlot.ImageFormat.Png);
+
+    var base64String = Convert.ToBase64String(imgBytes);
+    //return base64String;
+
+    //If trying to create a data URI, you can do something like this:
+    var dataUri =  $"data:image/png;base64,{base64String}";
+
+    //you can use this in an img tag in html like this:
+    // <img src="data:image/png;base64,base64String" alt="ScottPlot Image" />        
+    return dataUri;
+}
