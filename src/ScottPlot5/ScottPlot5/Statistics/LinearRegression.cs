acbd3a54151de3ed954617c9940a32eb0f896fc2
Angadjeet Singh <angadjeet22071@iiitd.ac.in>
2024-06-23 02:55:05 +0530

    Added a constructor that accepts List<Coordinates>

--- src/ScottPlot5/ScottPlot5/Statistics/LinearRegression.cs
+++ src/ScottPlot5/ScottPlot5/Statistics/LinearRegression.cs
@@ -24,6 +24,22 @@ public readonly struct LinearRegression
     }
 
     /// <summary>
+    /// Calculate the linear regression from a collection of X/Y coordinates
+    /// Accepts a List of Coordinates.
+    /// </summary>     
+    public LinearRegression(List<Coordinates> coordinates)
+    {
+        if (coordinates == null || coordinates.Count < 2)
+        {
+            throw new ArgumentException($"{nameof(coordinates)} must have at least 2 points");
+        }
+
+        double[] xs = coordinates.Select(c => c.X).ToArray();
+        double[] ys = coordinates.Select(c => c.Y).ToArray();
+        (Slope, Offset, Rsquared) = GetCoefficients(xs, ys);
+    }
+    
+    /// <summary>
     /// Calculate the linear regression a paired collection of X and Y points
     /// </summary>
     public LinearRegression(double[] xs, double[] ys)
