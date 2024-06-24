8b44122f84806f6e0b6dbbb659e8e2fcbff058d7
Fabrice Mousset <3978902+KroMignon@users.noreply.github.com>
2024-06-24 16:06:57 +0200

    Fix Navigation bar  issues
    - hide navigation bar on menu item selection for small devices (eg. Smartphone)
    - add "iconized" mode for large screens

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Blazor.WebAssembly/Services/IResizeService.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Blazor.WebAssembly/Services/IResizeService.cs
@@ -8,6 +8,10 @@ namespace Sandbox.Blazor.WebAssembly.Services
 
         public float PixelWidth { get; }
         public float PixelHeight { get; }
+        public bool IsLandscape { get; }
+        public bool IsPortrait { get; }
+        public bool IsLargeScreen { get; }
+        public bool IsSquare { get; }
         public void SetSize(float width, float height);
     }
 }
--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Blazor.WebAssembly/Services/ResizeService.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Blazor.WebAssembly/Services/ResizeService.cs
@@ -9,6 +9,11 @@ namespace Sandbox.Blazor.WebAssembly.Services
 
         public float PixelWidth { get; private set; } = 0;
         public float PixelHeight { get; private set; } = 0;
+        public bool IsLandscape => PixelWidth > PixelHeight;
+        public bool IsPortrait => PixelHeight > PixelWidth;
+        public bool IsLargeScreen => PixelWidth >= 641;
+        public bool IsSquare => PixelWidth > 0 && PixelHeight > 0 ?
+            ((IsLandscape ? PixelHeight / PixelWidth : PixelWidth / PixelHeight) > .97) : false;
         public void SetSize(float width, float height)
         {
             bool hasChanged = false;
