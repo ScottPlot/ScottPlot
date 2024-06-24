2febec9850429d4693d39fe4b398c267d07e47a9
Scott W Harden <swharden@gmail.com>
2024-06-24 19:58:47 -0400

    Sandbox: extend minimal API
    #3959

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WebApi/Program.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WebApi/Program.cs
@@ -22,7 +22,7 @@ app.MapGet("/random.png", async context =>
     await context.Response.Body.WriteAsync(imageBytes, 0, imageBytes.Length);
 });
 
-app.MapGet("/svg", async context =>
+app.MapGet("/inlineSVG", async context =>
 {
     // create a plot and fill it with sample data
     ScottPlot.Plot myPlot = new();
@@ -33,6 +33,23 @@ app.MapGet("/svg", async context =>
     // render the plot as a SVG string and serve it inside HTML
     string svg = myPlot.GetSvgXml(600, 400);
     string html = $"<html><body>{svg}</body></html>";
+    context.Response.ContentType = "text/html";
+    await context.Response.WriteAsync(html);
+});
+
+app.MapGet("/inlinePNG", async context =>
+{
+    // create a plot and fill it with sample data
+    ScottPlot.Plot myPlot = new();
+    double[] dataX = ScottPlot.Generate.Consecutive(100);
+    double[] dataY = ScottPlot.Generate.RandomWalk(100);
+    myPlot.Add.Scatter(dataX, dataY);
+
+    // render the plot as a PNG and encode its bytes in HTML
+    byte[] imgBytes = myPlot.GetImageBytes(600, 400, ScottPlot.ImageFormat.Png);
+    string b64 = Convert.ToBase64String(imgBytes);
+    string png = $"<img src='data:image/png;base64,{b64}'>";
+    string html = $"<html><body>{png}</body></html>";
     context.Response.ContentType = "text/html";
     await context.Response.WriteAsync(html);
 });
