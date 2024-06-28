0dea9874fe0bc1790ae5bd4eb9d5cf57a66013bf
Scott W Harden <swharden@gmail.com>
2024-06-28 06:08:19 -0400

    Sandbox: create .NET Maui app

---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Sandbox.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace Sandbox.Maui;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
