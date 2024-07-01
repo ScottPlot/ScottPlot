b28bf1c8d133499265fa9e25848a2f7232d9051c
Scott W Harden <swharden@gmail.com>
2024-07-01 08:37:46 -0400

    Tests: show XML docs as HTML

---
+++ src/ScottPlot5/ScottPlot5 Tests/Unit Tests/ApiDocs/ApiDocGeneration.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace ScottPlotTests.ApiDocs;
+
+internal class ApiDocGeneration
+{
+    [Test]
+    public void Test_ApiDoc_Generate()
+    {
+        string xmlFilePath = Path.Combine(Paths.RepoFolder, @"src/ScottPlot5/ScottPlot5/bin/Debug/net8.0/ScottPlot.xml");
+        if (!File.Exists(xmlFilePath))
+            throw new FileNotFoundException(xmlFilePath);
+
+        StringBuilder sb = new();
+
+        string xml = File.ReadAllText(xmlFilePath);
+        XDocument doc = XDocument.Parse(xml);
+
+        string ASSEMBLY_NAME = doc.Element("doc")!.Element("assembly")!.Element("name")!.Value;
+        sb.AppendLine($"<h1>{ASSEMBLY_NAME}</h1>");
+
+        foreach (XElement member in doc.Element("doc")!.Element("members")!.Elements("member")!)
+        {
+            string name = member.Attribute("name")!.Value;
+
+            if (!name.Contains("ScottPlot.AxisManager."))
+                continue;
+
+            sb.AppendLine($"<h3>{name}</h3>");
+
+            string summary = member.Element("summary")!.Value
+                .Replace("<", "&lt;")
+                .Replace(">", "&gt;")
+                .Replace("\n", " ")
+                .Trim();
+            sb.AppendLine($"<p>{summary}</p>");
+        }
+
+        string saveAs = Path.GetFullPath("api.html");
+        File.WriteAllText(saveAs, sb.ToString());
+        Console.WriteLine(saveAs);
+    }
+}
--- src/ScottPlot5/ScottPlot5 Tests/Unit Tests/Paths.cs
+++ src/ScottPlot5/ScottPlot5 Tests/Unit Tests/Paths.cs
@@ -1,5 +1,31 @@
 ﻿namespace ScottPlotTests;
+
 public static class Paths
 {
-    public static string OutputFolder => Path.Combine(TestContext.CurrentContext.TestDirectory, "test-images");
+    public readonly static string OutputFolder = GetOutputFolder();
+    public readonly static string RepoFolder = GetRepoFolder();
+
+    private static string GetOutputFolder()
+    {
+        return Path.Combine(TestContext.CurrentContext.TestDirectory, "test-images");
+    }
+
+    private static string GetRepoFolder()
+    {
+        string defaultFolder = Path.GetFullPath(TestContext.CurrentContext.TestDirectory); ;
+        string? repoFolder = defaultFolder;
+        while (repoFolder is not null)
+        {
+            if (File.Exists(Path.Combine(repoFolder, "LICENSE")))
+            {
+                return repoFolder;
+            }
+            else
+            {
+                repoFolder = Path.GetDirectoryName(repoFolder);
+            }
+        }
+
+        throw new InvalidOperationException($"repository folder not found in any folder above {defaultFolder}");
+    }
 }
