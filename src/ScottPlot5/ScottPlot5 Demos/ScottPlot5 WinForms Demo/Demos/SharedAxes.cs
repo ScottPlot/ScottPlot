9c3d73c08522a3e776766981036b72036d7d15eb
Scott W Harden <swharden@gmail.com>
2024-07-02 08:09:11 -0400

    demo: use fixed layout for shared axes

--- src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/SharedAxes.cs
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/SharedAxes.cs
@@ -12,9 +12,16 @@ public partial class SharedAxes : Form, IDemoWindow
     {
         InitializeComponent();
 
+        // add data to both plots
         formsPlot1.Plot.Add.Signal(Generate.Sin());
         formsPlot2.Plot.Add.Signal(Generate.Cos());
 
+        // use fixed layout so plots remain perfectly aligned
+        PixelPadding padding = new(50, 20, 30, 5);
+        formsPlot1.Plot.Layout.Fixed(padding);
+        formsPlot2.Plot.Layout.Fixed(padding);
+
+        // update linking options
         checkShareX.CheckedChanged += (s, e) => UpdateLinkedPlots();
         checkShareY.CheckedChanged += (s, e) => UpdateLinkedPlots();
         UpdateLinkedPlots();
