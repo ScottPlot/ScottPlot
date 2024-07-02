cb2e28e13fe5ab573fc48f12d1dd5209abc2540d
Scott W Harden <swharden@gmail.com>
2024-07-02 08:19:45 -0400

    Cookbook: add Layout category

--- src/ScottPlot5/ScottPlot5 Cookbook/Query.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Query.cs
@@ -31,6 +31,7 @@ public static class Query
         {
             "Introduction",
             "Axis",
+            "Layout",
             "Plot Types",
             "Statistics",
             "Miscellaneous"
