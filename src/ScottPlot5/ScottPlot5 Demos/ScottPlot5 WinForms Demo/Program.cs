8dcdf9e93dd576b367ad8856e065dc6286b6d58d
Scott W Harden <swharden@gmail.com>
2024-07-01 20:19:56 -0400

    Demo: simplify shared axis demo

--- src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Program.cs
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Program.cs
@@ -11,7 +11,7 @@ static class Program
         Application.EnableVisualStyles();
 
         // CTRL+D opens this window (useful for testing in development)
-        Type testingFormType = typeof(Demos.ShowValueOnHover);
+        Type testingFormType = typeof(Demos.SharedAxes);
 
         Application.Run(new MainMenuForm(testingFormType));
     }
