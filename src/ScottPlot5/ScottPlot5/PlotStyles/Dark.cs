1649dd0d0df7fd2c20e3b6fabff1d1e2676a64b0
Evgenii Karachevtsev <kebox7@gmail.com>
2024-07-03 18:07:37 +0300

    PlotStyle: properties naming

--- src/ScottPlot5/ScottPlot5/PlotStyles/Dark.cs
+++ src/ScottPlot5/ScottPlot5/PlotStyles/Dark.cs
@@ -5,12 +5,12 @@ public class Dark : PlotStyle
     public Dark()
     {
         Palette = new Palettes.Penumbra();
-        Axes = Color.FromHex("#d7d7d7");
-        GridMajorLine = Color.FromHex("#404040");
-        FigureBackground = Color.FromHex("#181818");
-        DataBackGround = Color.FromHex("#1f1f1f");
-        LegendBackground = Color.FromHex("#404040");
-        LegendFont = Color.FromHex("#d7d7d7");
-        LegendOutline = Color.FromHex("#d7d7d7");
+        AxisColor = Color.FromHex("#d7d7d7");
+        GridMajorLineColor = Color.FromHex("#404040");
+        FigureBackgroundColor = Color.FromHex("#181818");
+        DataBackgroundColor = Color.FromHex("#1f1f1f");
+        LegendBackgroundColor = Color.FromHex("#404040");
+        LegendFontColor = Color.FromHex("#d7d7d7");
+        LegendOutlineColor = Color.FromHex("#d7d7d7");
     }
 }
