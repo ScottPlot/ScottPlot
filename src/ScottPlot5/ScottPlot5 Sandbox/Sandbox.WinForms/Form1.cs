f8fccef260a49eadd3fed18865ff62c9ad9f82c4
Scott W Harden <swharden@gmail.com>
2024-07-06 08:56:11 -0400

    WinForms Sandbox: use single plot example

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
@@ -8,27 +8,7 @@ public partial class Form1 : Form
     {
         InitializeComponent();
 
-        SetupTab1();
-        SetupTab2();
-
-        tabControl1.SelectedIndex = 1;
-    }
-
-    private void SetupTab1()
-    {
-        // add sample data
         formsPlot1.Plot.Add.Signal(Generate.Sin());
         formsPlot1.Plot.Add.Signal(Generate.Cos());
-    }
-
-    private void SetupTab2()
-    {
-        // add sample data
-        formsPlot2.Plot.Add.Signal(Generate.Sin());
-        formsPlot3.Plot.Add.Signal(Generate.Cos());
-
-        // link axes of two controls together bidirectionally
-        formsPlot2.Plot.Axes.Link(formsPlot3);
-        formsPlot3.Plot.Axes.Link(formsPlot2);
     }
 }
