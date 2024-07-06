2a36a1e4180f5d0692fb021879beaee0d9bb8f9f
Scott W Harden <swharden@gmail.com>
2024-07-06 17:06:10 -0400

    Create Interactivity Inspector project
    to facilitate experimentation related to alternate control interaction systems #3186

---
+++ src/ScottPlot5/ScottPlot5 Tests/InteractivityInspector/Form1.cs
@@ -0,0 +1,12 @@
+﻿namespace Interactivity_Inspector;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+
+        formsPlot1.Plot.Add.Signal(ScottPlot.Generate.Sin());
+        formsPlot1.Plot.Add.Signal(ScottPlot.Generate.Cos());
+    }
+}
