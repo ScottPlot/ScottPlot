22053b3a2bb20b1ee7fbc4bf2f7ba74af5ffb7cc
Scott W Harden <swharden@gmail.com>
2024-07-11 08:05:44 -0400

    Tests: right-click context menu

---
+++ src/ScottPlot5/ScottPlot5 Tests/Unit Tests/InteractivityTests/UserInputActionTests/RightClickContextMenuTests.cs
@@ -0,0 +1,47 @@
+﻿using ScottPlot.Interactivity;
+using ScottPlot.Interactivity.UserInputs;
+
+namespace ScottPlotTests.InteractivityTests.UserInputActionTests;
+
+internal class RightClickContextMenuTests
+{
+    const int FIGURE_WIDTH = 400;
+    const int FIGURE_HEIGHT = 300;
+    Pixel FIGURE_CENTER => new(FIGURE_WIDTH / 2, FIGURE_HEIGHT / 2);
+
+    [Test]
+    public void Test_RightClickContextMenu_LaunchesMenu()
+    {
+        MockPlotControl control = new();
+        control.Refresh();
+        control.ContextMenuLaunchCount.Should().Be(0);
+
+        UserInputProcessor proc = new(control.Plot);
+
+        proc.Process(new RightMouseDown(FIGURE_CENTER));
+        proc.Process(new RightMouseUp(FIGURE_CENTER));
+        control.ContextMenuLaunchCount.Should().Be(1);
+
+        proc.Process(new RightMouseDown(FIGURE_CENTER));
+        proc.Process(new RightMouseUp(FIGURE_CENTER));
+        control.ContextMenuLaunchCount.Should().Be(2);
+    }
+
+    [Test]
+    public void Test_RightClickDrag_DoesNotLaunchMenu()
+    {
+        MockPlotControl control = new();
+        control.Refresh();
+        control.ContextMenuLaunchCount.Should().Be(0);
+
+        UserInputProcessor proc = new(control.Plot);
+
+        proc.Process(new RightMouseDown(FIGURE_CENTER));
+        proc.Process(new RightMouseUp(FIGURE_CENTER.WithOffset(10, 10)));
+        control.ContextMenuLaunchCount.Should().Be(0);
+
+        proc.Process(new RightMouseDown(FIGURE_CENTER));
+        proc.Process(new RightMouseUp(FIGURE_CENTER));
+        control.ContextMenuLaunchCount.Should().Be(1);
+    }
+}
