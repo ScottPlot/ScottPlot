fbb559ae3012042f079c25d3e1c0e5feea501c2c
Scott W Harden <swharden@gmail.com>
2024-07-25 18:40:40 -0400

    Cookbook: move discrete phaser plot to advanced axis category

--- src/ScottPlot5/ScottPlot5 Cookbook/Recipes/PlotTypes/Phasor.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/PlotTypes/Phasor.cs
@@ -30,36 +30,4 @@ public class Phasor : ICategory
             myPlot.Add.PhasorLines(points);
         }
     }
-
-    public class PolarArrowQuickstart : RecipeBase
-    {
-        public override string Name => "Phasor Plot with Arrows";
-        public override string Description => "A phasor plot may be achieved by placing arrows";
-
-        [Test]
-        public override void Execute()
-        {
-            // these are the points we will display
-            PolarCoordinates[] points = [
-                new (10, Angle.FromDegrees(15)),
-                new (20, Angle.FromDegrees(120)),
-                new (30, Angle.FromDegrees(240)),
-            ];
-
-            // start by adding a polar axis to the plot
-            var polarAxis = myPlot.Add.PolarAxis(30);
-            polarAxis.LinePattern = LinePattern.Dotted;
-
-            // then place arrows representing each polar point
-            IPalette palette = new ScottPlot.Palettes.Category10();
-            Coordinates center = polarAxis.GetCoordinates(0, 0);
-            for (int i = 0; i < points.Length; i++)
-            {
-                Coordinates tip = polarAxis.GetCoordinates(points[i]);
-                var arrow = myPlot.Add.Arrow(center, tip);
-                arrow.ArrowLineWidth = 0;
-                arrow.ArrowFillColor = palette.GetColor(i).WithAlpha(.7);
-            }
-        }
-    }
 }
