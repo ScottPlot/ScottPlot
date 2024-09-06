6eaaf7b6156284decb3216695070c67b6ef067d1
Scott W Harden <swharden@gmail.com>
2024-09-05 21:08:26 -0400

    Cookbook: improve pie label demo

--- src/ScottPlot5/ScottPlot5 Cookbook/Recipes/PlotTypes/Pie.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/PlotTypes/Pie.cs
@@ -94,11 +94,12 @@ public class Pie : ICategory
         [Test]
         public override void Execute()
         {
-            PieSlice slice1 = new() { Value = 5, FillColor = Colors.Red, Label = "Red" };
-            PieSlice slice2 = new() { Value = 2, FillColor = Colors.Orange, Label = "Orange" };
-            PieSlice slice3 = new() { Value = 8, FillColor = Colors.Gold, Label = "Yellow" };
-            PieSlice slice4 = new() { Value = 4, FillColor = Colors.Green, Label = "Green" };
-            PieSlice slice5 = new() { Value = 8, FillColor = Colors.Blue, Label = "Blue" };
+            PieSlice slice1 = new() { Value = 5, FillColor = Colors.Red, Label = "alpha" };
+            PieSlice slice2 = new() { Value = 2, FillColor = Colors.Orange, Label = "beta" };
+            PieSlice slice3 = new() { Value = 8, FillColor = Colors.Gold, Label = "gamma" };
+            PieSlice slice4 = new() { Value = 4, FillColor = Colors.Green, Label = "delta" };
+            PieSlice slice5 = new() { Value = 8, FillColor = Colors.Blue, Label = "epsilon" };
+
             List<PieSlice> slices = new() { slice1, slice2, slice3, slice4, slice5 };
 
             // setup the pie to display slice labels
@@ -107,10 +108,13 @@ public class Pie : ICategory
             pie.ShowSliceLabels = true;
             pie.SliceLabelDistance = 1.3;
 
+            // color each label's text to match the slice
+            slices.ForEach(x => x.LabelFontColor = x.FillColor);
+
             // styling can be customized for individual slices
             slice5.LabelStyle.FontSize = 22;
-            slice5.LabelStyle.ForeColor = Colors.Magenta;
             slice5.LabelStyle.Bold = true;
+            slice5.LabelStyle.Italic = true;
         }
     }
 }
