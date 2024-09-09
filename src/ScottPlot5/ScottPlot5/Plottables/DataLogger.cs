f0bef97cb66c15eda47d5d25d6bfdfacb6f9b929
Andrij Abyzov <aabyzov@slb.com>
2024-09-09 22:45:03 +0200

    Bring back Add method overloads to DataLogger

--- src/ScottPlot5/ScottPlot5/Plottables/DataLogger.cs
+++ src/ScottPlot5/ScottPlot5/Plottables/DataLogger.cs
@@ -181,6 +181,31 @@ public class DataLogger : IPlottable, IManagesAxisLimits, IHasLine, IHasMarker, 
             Add(y);
         }
     }
+    
+    public void Add(double[] xs, double[] ys)
+    {
+        if (xs is null || ys is null)
+            throw new ArgumentException($"{nameof(xs)} and {nameof(ys)} must not be null");
+
+        if (xs.Length != ys.Length)
+            throw new ArgumentException($"{nameof(xs).Length} and {nameof(ys).Length} must have equal length");
+
+        for (int i = 0; i < xs.Length; i++)
+        {
+            Add(xs[i], ys[i]);
+        }
+    }
+
+    public void Add(Coordinates[] coordinates)
+    {
+        if (coordinates is null)
+            throw new ArgumentException($"{coordinates} must not be null");
+
+        for (int i = 0; i < coordinates.Length; i++)
+        {
+            Add(coordinates[i]);
+        }
+    }    
 
     public void UpdateAxisLimits(Plot plot)
     {
