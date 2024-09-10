7180b091039bbca111f939a5c30d0709c8aeea36
Scott W Harden <swharden@gmail.com>
2024-09-10 09:08:53 -0400

    WPF Demo: multi-threading

---
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WPF Demo/DemoWindows/MultiThreading.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace WPF_Demo.DemoWindows;
+
+public partial class MultiThreading : Window, IDemoWindow
+{
+    public string DemoTitle => "WPF Multi-Threading";
+    public string Description => "Demonstrate how to safely change data while rendering asynchronously.";
+
+    public MultiThreading()
+    {
+        InitializeComponent();
+    }
+}
