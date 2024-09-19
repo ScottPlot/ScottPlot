d7be919fb46e1a7f549ed6a1c1fc6996d6ca7eb2
Alfonso Espitia <Alfonso.Espitia@fairviewinvest.com>
2024-09-19 11:18:32 -0400

    new getter and setter for coordinateLine

---
+++ src/ScottPlot5/ScottPlot5 Tests/Unit Tests/RenderTests/Plottable/LinePlotTests.cs
@@ -0,0 +1,23 @@
+﻿using ScottPlot.Plottables;
+
+namespace ScottPlotTests.RenderTests.Plottable;
+
+internal class LinePlotTests
+{
+    [Test]
+    public void Test_LinePlotGetSet()
+    {
+        Coordinates start = new Coordinates(0, 0);
+        Coordinates end = new Coordinates(5, 6);
+
+        LinePlot lp = new();
+
+        lp.Line = new CoordinateLine(start, end);
+
+        var newStart = lp.Line.Start;
+        var newEnd = lp.Line.End;
+
+        newStart.Should().Be(start);
+        newEnd.Should().Be(end);
+    }
+}
