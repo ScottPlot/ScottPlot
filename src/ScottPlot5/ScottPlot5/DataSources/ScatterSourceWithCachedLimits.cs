3eed4e51d75d249fb153b00641a823faaab0c398
Robert Brenckman <20431767+RFBomb@users.noreply.github.com>
2024-10-09 14:27:09 -0400

    Refactor data sources to implement `IDataSource` and include `GetNearest` functionality (#4270)
    * Resolve Issue 3807,  Add IGetNearest interface
    
    - Introduces the 'IGetNearest' interface for plottables that have the `GetNearest` and `GetNearestX` methods
    - Applies the IGetNearest interface to all Scatter data sources, as well as SignalXY and VectorFieldDataSource
    - Implement GetNearestX for all scatter data sources
    
    * Add IGetNearest interface
    
    * Update ScatterSourceWithCachedLimits.cs
    
    - Adds missing using for Interfaces.IGetNearest
    
    * Fix IGetNearest namespace
    
    * Introduce IDataSource Interface and DataSourceUtilities
    
    IDataSource Interface
    - Interface applied to DataSources to use with the DataSourceUtilities static class
    - Implemented on Scatter and Signal sources where able
    - Implemented on Scatter and Signal + SignalXY plottables
    
    DataSourceUtilities
    - Extracted code from various GetNearest() functions and normalized them into GetNearest and GetNearestX in this static class
    - Any rotation or scaling necessary for the calculation is expected to be performed via the IDataSource interface.
    
    * Implement BinarySearch
    
    - Moves the Binary Search Extensions into DataSourceUtilities
    - Adds several overloads to common scenarios ( double[] )
    - Renames the default comparer and extends it for more IComparer interfaces
    - Create the generic comparer
    - Adds a new helper class  `CoordinateDataSource` for scenarios (specifically Scatter plot) that should reduce expensive interface calls  where the IDataSource does not own the data collection
    
    * Fixes
    
    * Update DataSourceUtilities.cs
    
    * Add Unit Tests
    
    Add Unit Tests for IGetNearest and IDataSource
    
    * Cache IsSorted values
    
    * More Unit Tests
    
    * Various Fixes & Optimizations
    
    GenericDoubleComparer => GenericComparer
    - Now should use Comparer<T>.Default in almost all scenarios, which significantly improves throughput.
    
    Tweaks to Scatter to reduce amount of casting and allow usage of the GetNearestFast
    
    * Update DataSourceUtilities.cs
    
    - Fix broken demo for SignalXY Interaction
    
    * Fix classes and Unit Tests
    
    * Update DataSourceUtilities.cs
    
    - disable the erroneous warning, as it shouldn't be a concern at runtime
    
    * Changes per Review
    
    - Updates `CoordinateDataSource` to use properties over fields, and allows settings of min/max render index.
    - Updates `IDataSource.IsSorted { get; }` to `IDataSource.IsSorted()`
    - Updates Scatter Sources to prefer the `DataSourceUtilities.GetRenderIndexCount` function over a private property
    - Adds `IndexRange.None` to represent an index range for an empty collection
    
    DataSourceUtilties
    - Improves `IsAscending` and handles enumerator disposal
    - `GetClosestIndex` now uses `BinarySearch` function
    - Better validation on `GetRenderIndexCount` and `GetRenderIndexRange`
    - added documentation, fixed formatting
    
    * Rename IDataSourceTests file
    
    - Rename the file and class to better describe what it is testing (not just the interface, but the utilty class itself)
    
    * Update DataSourceUtilitiesTests.cs
    
    - Corrected name to not compete with class it tests
    
    * Improvements to GenericComparer
    
    The generic comparer does not need to implement IComparer, it just needs to provide one.
    
    * Update DataSourceUtilitiesTests.cs
    
    - Fix erroneous warning preventing github compile
    
    * Make GenericComparer<T> Static
    
    - Make GenericComparer<T> static
    - Rename 'Instance' to 'Default' to more closesly mimick the Comparer<T>.Default api
    
    * Add ArgumentNull checks to DataSourceUtilities
    
    - also renames BinarySearch parameter' count' to 'length' to pick up the inherited docs description
    
    * Prefer GetRenderIndexRange()
    
    DataSourceUtilities
    - Ensure `GetRenderIndexRange().Length` == `GetRenderIndexCount()`
    
    Scatter Sources
    - Prefer GetRenderIndexRange() over direct instantiation of IndexRange()
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Scott W Harden <swharden@gmail.com>

--- src/ScottPlot5/ScottPlot5/DataSources/ScatterSourceWithCachedLimits.cs
+++ src/ScottPlot5/ScottPlot5/DataSources/ScatterSourceWithCachedLimits.cs
@@ -1,6 +1,6 @@
 ﻿namespace ScottPlot.DataSources;
 
-public class CacheScatterLimitsDecorator(IScatterSource source) : IScatterSource
+public class CacheScatterLimitsDecorator(IScatterSource source) : IScatterSource, IGetNearest
 {
     private readonly IScatterSource _source = source;
 
@@ -49,17 +49,13 @@ public class CacheScatterLimitsDecorator(IScatterSource source) : IScatterSource
         return _source.GetNearest(mouseLocation, renderInfo, maxDistance);
     }
 
+    public DataPoint GetNearestX(Coordinates mouseLocation, RenderDetails renderInfo, float maxDistance = 15)
+    {
+        return _source.GetNearestX(mouseLocation, renderInfo, maxDistance);
+    }
+
     public IReadOnlyList<Coordinates> GetScatterPoints()
     {
         return _source.GetScatterPoints();
-    }
-
-    public DataPoint GetNearestX(Coordinates mouseLocation, RenderDetails renderInfo, float maxDistance = 15)
-    {
-        // TODO: Implement GetNearestX() in this DataSource
-        // Code can be copied from ScatterSourceDoubleArray.GetNearestX() and modified as needed
-        // Contributions are welcome!
-        // https://github.com/ScottPlot/ScottPlot/issues/3807
-        throw new NotImplementedException();
     }
 }
