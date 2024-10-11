d59d676663be78e72235488889291e2410e84eb7
Scott W Harden <swharden@gmail.com>
2024-10-11 18:45:50 -0400

    Cookbook: add contour line recipes

--- src/ScottPlot5/ScottPlot5 Cookbook/Recipes/PlotTypes/Contour.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/PlotTypes/Contour.cs
@@ -10,8 +10,9 @@ public class Contour : ICategory
 
     public class ContourQuickstart : RecipeBase
     {
-        public override string Name => "Contour Plot Quickstart";
-        public override string Description => "A contour plot can be created from a collection of 2D data points.";
+        public override string Name => "Irregular Contour Plot";
+        public override string Description => "A contour plot can be created from " +
+            "a collection of 3D data points placed arbitrarily in X/Y plane.";
 
         [Test]
         public override void Execute()
@@ -24,10 +25,62 @@ public class Contour : ICategory
                 double z = Math.Sin(x) + Math.Cos(y);
                 cs[i] = new(x, y, z);
             }
+            
+            myPlot.Add.ContourLines(cs);
 
-            ScottPlot.Plottables.TINPlot contour = new(cs);
-            myPlot.PlottableList.Add(contour);
+            myPlot.Axes.TightMargins();
+            myPlot.HideGrid();
+        }
+    }
 
+    public class ContourGrid : RecipeBase
+    {
+        public override string Name => "Rectangular Contour Plot";
+        public override string Description => "A rectangular contour plot with evenly spaced points " +
+            "can be created from a 2D array of 3D points.";
+
+        [Test]
+        public override void Execute()
+        {
+            Coordinates3d[,] cs = new Coordinates3d[50, 50];
+            for (int y = 0; y < cs.GetLength(0); y++)
+            {
+                for (int x = 0; x < cs.GetLength(1); x++)
+                {
+                    double z = Math.Sin(x * .1) + Math.Cos(y * .1);
+                    cs[y, x] = new(x, y, z);
+                }
+            }
+
+            myPlot.Add.ContourLines(cs);
+
+            myPlot.Axes.TightMargins();
+            myPlot.HideGrid();
+        }
+    }
+
+    public class ContourHeatmap : RecipeBase
+    {
+        public override string Name => "Contour Lines with Heatmap";
+        public override string Description => "Contour lines may be placed on top of heatmaps.";
+
+        [Test]
+        public override void Execute()
+        {
+            Coordinates3d[,] cs = new Coordinates3d[50, 50];
+            for (int y = 0; y < cs.GetLength(0); y++)
+            {
+                for (int x = 0; x < cs.GetLength(1); x++)
+                {
+                    double z = Math.Sin(x * .1) + Math.Cos(y * .1);
+                    cs[y, x] = new(x, y, z);
+                }
+            }
+
+            myPlot.Add.Heatmap(cs);
+            myPlot.Add.ContourLines(cs);
+
+            myPlot.Axes.TightMargins();
             myPlot.HideGrid();
         }
     }
