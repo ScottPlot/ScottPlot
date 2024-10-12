e5fc8930e0bf1696928dde5702fa159b1f465fd6
Kosmos Werner Heisenberg <114314821+KosmosWerner@users.noreply.github.com>
2024-10-12 11:21:13 -0500

    Maui support (#4320)
    * csproj: added WindowsSdkPackageVersion
    
    * MauiPlot: changed to SKCanvasView, added pan and zoom
    
    * MauiPlotMenu: MenuFlyout available
    
    * Register: builder.UseScottPlot()
    
    * MauiProgram: UseScottPlot()
    
    * Update CHANGELOG.md
    
    * Maui Sandbox: ignore strong name warning
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Scott W Harden <swharden@gmail.com>

---
+++ src/ScottPlot5/ScottPlot5 Controls/ScottPlot.Maui/Register.cs
@@ -0,0 +1,18 @@
+﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottPlot.Maui
+{
+    public static class Register
+    {
+        public static MauiAppBuilder UseScottPlot(this MauiAppBuilder builder)
+        {
+            builder.UseSkiaSharp();
+            return builder;
+        }
+    }
+}
