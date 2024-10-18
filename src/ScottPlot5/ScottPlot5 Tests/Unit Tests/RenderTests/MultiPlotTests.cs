46f5c907f5c5696d509d93d00b0d02260add9cc0
Scott W Harden <swharden@gmail.com>
2024-10-18 18:55:37 -0400

    Tests: add Multiplot support

--- src/ScottPlot5/ScottPlot5 Tests/Unit Tests/RenderTests/MultiPlotTests.cs
+++ src/ScottPlot5/ScottPlot5 Tests/Unit Tests/RenderTests/MultiPlotTests.cs
@@ -2,38 +2,20 @@
 
 class MultiPlotTests
 {
-    private static Plot MakePlot(string label)
-    {
-        Plot plot = new();
-        plot.Add.Signal(Generate.Sin());
-        plot.Add.Signal(Generate.Cos());
-        var an = plot.Add.Annotation(label, Alignment.MiddleCenter);
-        an.LabelFontSize = 100;
-        an.LabelBackgroundColor = Colors.Transparent;
-        an.LabelShadowColor = Colors.Transparent;
-        an.LabelBorderColor = Colors.Transparent;
-        an.LabelBold = true;
-        an.LabelFontColor = Colors.Black.WithAlpha(.5);
-        return plot;
-    }
-
-
     [Test]
-    public void Test_Multiplot_Basic()
+    public void Test_Multiplot_Default()
     {
-        Plot plot1 = new();
-        var sig1 = plot1.Add.Signal(Generate.Sin());
-        sig1.Color = Colors.Blue;
-        sig1.LineWidth = 5;
-
-        Plot plot2 = new();
-        var sig2 = plot2.Add.Signal(Generate.Cos());
-        sig2.Color = Colors.Red;
-        sig2.LineWidth = 5;
-
         Multiplot mp = new();
-        mp.AddSubplot(plot1, 0, 2, 0, 1);
-        mp.AddSubplot(plot2, 1, 2, 0, 1);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Plot plot = new();
+            var sig = plot.Add.Signal(Generate.Sin());
+            sig.Color = Colormap.Default.GetColor(i, 3);
+            sig.LineWidth = 5;
+            mp.AddPlot(plot);
+        }
+
         mp.SaveTestImage();
     }
 }
