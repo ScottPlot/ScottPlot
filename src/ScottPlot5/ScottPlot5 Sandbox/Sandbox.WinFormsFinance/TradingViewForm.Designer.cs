fd3b1592261bdb228d9dd48824f5c983a75f7812
Scott W Harden <swharden@gmail.com>
2024-10-24 20:36:41 -0400

    Sandbox: start finance demo (#4388)
    * Sandbox: finance demo
    
    * Sandbox: add line to finance demo

---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinFormsFinance/TradingViewForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Sandbox.WinFormsFinance;
+
+partial class TradingViewForm
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        comboBoxSymbol = new ComboBox();
+        comboBoxInterval = new ComboBox();
+        formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+        label1 = new Label();
+        label2 = new Label();
+        buttonClearAll = new Button();
+        checkBoxAddLine = new CheckBox();
+        SuspendLayout();
+        // 
+        // comboBoxSymbol
+        // 
+        comboBoxSymbol.DropDownStyle = ComboBoxStyle.DropDownList;
+        comboBoxSymbol.FormattingEnabled = true;
+        comboBoxSymbol.Location = new Point(12, 30);
+        comboBoxSymbol.Name = "comboBoxSymbol";
+        comboBoxSymbol.Size = new Size(121, 23);
+        comboBoxSymbol.TabIndex = 0;
+        // 
+        // comboBoxInterval
+        // 
+        comboBoxInterval.DropDownStyle = ComboBoxStyle.DropDownList;
+        comboBoxInterval.FormattingEnabled = true;
+        comboBoxInterval.Location = new Point(12, 86);
+        comboBoxInterval.Name = "comboBoxInterval";
+        comboBoxInterval.Size = new Size(121, 23);
+        comboBoxInterval.TabIndex = 3;
+        // 
+        // formsPlot1
+        // 
+        formsPlot1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        formsPlot1.DisplayScale = 1F;
+        formsPlot1.Location = new Point(139, 12);
+        formsPlot1.Name = "formsPlot1";
+        formsPlot1.Size = new Size(649, 426);
+        formsPlot1.TabIndex = 7;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(12, 12);
+        label1.Name = "label1";
+        label1.Size = new Size(50, 15);
+        label1.TabIndex = 9;
+        label1.Text = "Symbol:";
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(12, 68);
+        label2.Name = "label2";
+        label2.Size = new Size(49, 15);
+        label2.TabIndex = 10;
+        label2.Text = "Interval:";
+        // 
+        // buttonClearAll
+        // 
+        buttonClearAll.Location = new Point(12, 168);
+        buttonClearAll.Name = "buttonClearAll";
+        buttonClearAll.Size = new Size(75, 31);
+        buttonClearAll.TabIndex = 12;
+        buttonClearAll.Text = "Clear All";
+        buttonClearAll.UseVisualStyleBackColor = true;
+        // 
+        // checkBoxAddLine
+        // 
+        checkBoxAddLine.Appearance = Appearance.Button;
+        checkBoxAddLine.Location = new Point(12, 131);
+        checkBoxAddLine.Name = "checkBoxAddLine";
+        checkBoxAddLine.Size = new Size(75, 31);
+        checkBoxAddLine.TabIndex = 14;
+        checkBoxAddLine.Text = "Add Line";
+        checkBoxAddLine.TextAlign = ContentAlignment.MiddleCenter;
+        checkBoxAddLine.UseVisualStyleBackColor = true;
+        // 
+        // TradingViewForm
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(checkBoxAddLine);
+        Controls.Add(buttonClearAll);
+        Controls.Add(label2);
+        Controls.Add(label1);
+        Controls.Add(formsPlot1);
+        Controls.Add(comboBoxInterval);
+        Controls.Add(comboBoxSymbol);
+        Name = "TradingViewForm";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "ScottPlot Financial Charting Sandbox (TradingView)";
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private ComboBox comboBoxSymbol;
+    private ComboBox comboBoxInterval;
+    private ScottPlot.WinForms.FormsPlot formsPlot1;
+    private Label label1;
+    private Label label2;
+    private Button buttonClearAll;
+    private CheckBox checkBoxAddLine;
+}
---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinFormsFinance/TradingViewForm.cs
@@ -0,0 +1,121 @@
+﻿using ScottPlot;
+
+namespace Sandbox.WinFormsFinance;
+
+public partial class TradingViewForm : Form
+{
+    // TODO: make an abstraction for click-drag placement of new technical indicators
+    ScottPlot.Plottables.LinePlot? LineBeingAdded = null;
+
+    public TradingViewForm()
+    {
+        InitializeComponent();
+        InitializeSymbolComboBox();
+        InitializeIntervalComboBox();
+        InitializePlot();
+
+        buttonClearAll.Click += ButtonClearAll_Click;
+
+        formsPlot1.MouseDown += FormsPlot1_MouseDown;
+        formsPlot1.MouseMove += FormsPlot1_MouseMove;
+        formsPlot1.MouseUp += FormsPlot1_MouseUp;
+    }
+
+    void InitializeSymbolComboBox()
+    {
+        comboBoxSymbol.Items.Add("AAPL");
+        comboBoxSymbol.Items.Add("AMZN");
+        comboBoxSymbol.Items.Add("GOOGL");
+        comboBoxSymbol.Items.Add("META");
+        comboBoxSymbol.Items.Add("MSFT");
+        comboBoxSymbol.Items.Add("NVDA");
+        comboBoxSymbol.SelectedIndex = 0;
+    }
+
+    void InitializeIntervalComboBox()
+    {
+        comboBoxInterval.Items.Add("1 second");
+        comboBoxInterval.Items.Add("5 seconds");
+        comboBoxInterval.Items.Add("10 seconds");
+        comboBoxInterval.Items.Add("30 seconds");
+        comboBoxInterval.Items.Add("1 minute");
+        comboBoxInterval.Items.Add("5 minutes");
+        comboBoxInterval.Items.Add("10 minutes");
+        comboBoxInterval.Items.Add("30 minutes");
+        comboBoxInterval.Items.Add("1 hour");
+        comboBoxInterval.Items.Add("2 hours");
+        comboBoxInterval.Items.Add("4 hours");
+        comboBoxInterval.Items.Add("1 day");
+        comboBoxInterval.Items.Add("1 week");
+        comboBoxInterval.Items.Add("1 month");
+        comboBoxInterval.Items.Add("1 year");
+        comboBoxInterval.SelectedIndex = 5;
+    }
+
+    void InitializePlot()
+    {
+        // generate random walk data
+        List<OHLC> ohlcs = Generate.RandomOHLCs(100);
+
+        // use dates according to the selected interval
+        DateTime start = new(2024, 10, 24);
+        TimeSpan interval = TimeSpan.FromSeconds(10); // TODO: read value from combo box
+        for (int i = 0; i < ohlcs.Count; i++)
+        {
+            ohlcs[i] = ohlcs[i]
+                .WithDate(start + interval * i)
+                .WithTimeSpan(interval);
+        }
+
+        formsPlot1.Plot.Clear();
+        formsPlot1.Plot.Axes.DateTimeTicksBottom();
+        formsPlot1.Plot.Add.Candlestick(ohlcs);
+        formsPlot1.Refresh();
+    }
+
+    private void ButtonClearAll_Click(object? sender, EventArgs e)
+    {
+        formsPlot1.Plot.Remove<ScottPlot.Plottables.LinePlot>();
+        formsPlot1.Refresh();
+    }
+
+    private void FormsPlot1_MouseDown(object? sender, MouseEventArgs e)
+    {
+        if (!checkBoxAddLine.Checked)
+            return;
+
+        // disable mouse pan and zoom while click-dragging to add a new indicator
+        formsPlot1.UserInputProcessor.Disable();
+
+        Coordinates cs = formsPlot1.Plot.GetCoordinates(e.X, e.Y);
+
+        // TODO: make a custom plot type and interface for technical indicators
+        LineBeingAdded = formsPlot1.Plot.Add.Line(cs, cs);
+        LineBeingAdded.LineWidth = 3;
+        LineBeingAdded.LinePattern = LinePattern.DenselyDashed;
+        LineBeingAdded.LineColor = Colors.Black;
+        LineBeingAdded.MarkerShape = MarkerShape.FilledCircle;
+        LineBeingAdded.MarkerFillColor = Colors.Black;
+        LineBeingAdded.MarkerSize = 8;
+        formsPlot1.Refresh();
+    }
+
+    private void FormsPlot1_MouseMove(object? sender, MouseEventArgs e)
+    {
+        if (LineBeingAdded is null)
+            return;
+
+        Coordinates cs = formsPlot1.Plot.GetCoordinates(e.X, e.Y);
+        LineBeingAdded.End = cs;
+
+        formsPlot1.Refresh();
+    }
+
+    private void FormsPlot1_MouseUp(object? sender, MouseEventArgs e)
+    {
+        LineBeingAdded = null;
+        formsPlot1.Refresh();
+        formsPlot1.UserInputProcessor.Reset();
+        formsPlot1.UserInputProcessor.Enable();
+    }
+}
