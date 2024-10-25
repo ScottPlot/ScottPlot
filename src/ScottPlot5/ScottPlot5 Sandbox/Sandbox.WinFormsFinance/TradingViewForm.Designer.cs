30f53815b37c558c4e3db38fd9a3e2e85ef7b873
Scott W Harden <swharden@gmail.com>
2024-10-24 20:05:25 -0400

    Sandbox: finance demo

---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinFormsFinance/TradingViewForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Sandbox.WinFormsFinance;
+
+partial class TradingViewForm
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        comboBoxSymbol = new ComboBox();
+        comboBoxInterval = new ComboBox();
+        formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+        button1 = new Button();
+        label1 = new Label();
+        label2 = new Label();
+        label3 = new Label();
+        button2 = new Button();
+        SuspendLayout();
+        // 
+        // comboBoxSymbol
+        // 
+        comboBoxSymbol.DropDownStyle = ComboBoxStyle.DropDownList;
+        comboBoxSymbol.FormattingEnabled = true;
+        comboBoxSymbol.Location = new Point(12, 30);
+        comboBoxSymbol.Name = "comboBoxSymbol";
+        comboBoxSymbol.Size = new Size(121, 23);
+        comboBoxSymbol.TabIndex = 0;
+        // 
+        // comboBoxInterval
+        // 
+        comboBoxInterval.DropDownStyle = ComboBoxStyle.DropDownList;
+        comboBoxInterval.FormattingEnabled = true;
+        comboBoxInterval.Location = new Point(12, 86);
+        comboBoxInterval.Name = "comboBoxInterval";
+        comboBoxInterval.Size = new Size(121, 23);
+        comboBoxInterval.TabIndex = 3;
+        // 
+        // formsPlot1
+        // 
+        formsPlot1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        formsPlot1.DisplayScale = 1F;
+        formsPlot1.Location = new Point(139, 12);
+        formsPlot1.Name = "formsPlot1";
+        formsPlot1.Size = new Size(649, 426);
+        formsPlot1.TabIndex = 7;
+        // 
+        // button1
+        // 
+        button1.Location = new Point(12, 150);
+        button1.Name = "button1";
+        button1.Size = new Size(45, 23);
+        button1.TabIndex = 8;
+        button1.Text = "Line";
+        button1.UseVisualStyleBackColor = true;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(12, 12);
+        label1.Name = "label1";
+        label1.Size = new Size(50, 15);
+        label1.TabIndex = 9;
+        label1.Text = "Symbol:";
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(12, 68);
+        label2.Name = "label2";
+        label2.Size = new Size(49, 15);
+        label2.TabIndex = 10;
+        label2.Text = "Interval:";
+        // 
+        // label3
+        // 
+        label3.AutoSize = true;
+        label3.Location = new Point(12, 132);
+        label3.Name = "label3";
+        label3.Size = new Size(32, 15);
+        label3.TabIndex = 11;
+        label3.Text = "Add:";
+        // 
+        // button2
+        // 
+        button2.Enabled = false;
+        button2.Location = new Point(63, 150);
+        button2.Name = "button2";
+        button2.Size = new Size(40, 23);
+        button2.TabIndex = 12;
+        button2.Text = "Ray";
+        button2.UseVisualStyleBackColor = true;
+        // 
+        // TradingViewForm
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(button2);
+        Controls.Add(label3);
+        Controls.Add(label2);
+        Controls.Add(label1);
+        Controls.Add(button1);
+        Controls.Add(formsPlot1);
+        Controls.Add(comboBoxInterval);
+        Controls.Add(comboBoxSymbol);
+        Name = "TradingViewForm";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "ScottPlot Financial Charting Sandbox (TradingView)";
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private ComboBox comboBoxSymbol;
+    private ComboBox comboBoxInterval;
+    private ScottPlot.WinForms.FormsPlot formsPlot1;
+    private Button button1;
+    private Label label1;
+    private Label label2;
+    private Label label3;
+    private Button button2;
+}
---
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinFormsFinance/TradingViewForm.cs
@@ -0,0 +1,67 @@
+﻿using ScottPlot;
+
+namespace Sandbox.WinFormsFinance;
+
+public partial class TradingViewForm : Form
+{
+    public TradingViewForm()
+    {
+
+        InitializeComponent();
+        InitializeSymbolComboBox();
+        InitializeIntervalComboBox();
+        InitializePlot();
+    }
+
+    void InitializePlot()
+    {
+        // generate random walk data
+        List<OHLC> ohlcs = Generate.RandomOHLCs(100);
+
+        // use dates according to the selected interval
+        DateTime start = new(2024, 10, 24);
+        TimeSpan interval = TimeSpan.FromSeconds(10); // TODO: read value from combo box
+        for (int i = 0; i < ohlcs.Count; i++)
+        {
+            ohlcs[i] = ohlcs[i]
+                .WithDate(start + interval * i)
+                .WithTimeSpan(interval);
+        }
+
+        formsPlot1.Plot.Clear();
+        formsPlot1.Plot.Axes.DateTimeTicksBottom();
+        formsPlot1.Plot.Add.Candlestick(ohlcs);
+        formsPlot1.Refresh();
+    }
+
+    void InitializeSymbolComboBox()
+    {
+        comboBoxSymbol.Items.Add("AAPL");
+        comboBoxSymbol.Items.Add("AMZN");
+        comboBoxSymbol.Items.Add("GOOGL");
+        comboBoxSymbol.Items.Add("META");
+        comboBoxSymbol.Items.Add("MSFT");
+        comboBoxSymbol.Items.Add("NVDA");
+        comboBoxSymbol.SelectedIndex = 0;
+    }
+
+    void InitializeIntervalComboBox()
+    {
+        comboBoxInterval.Items.Add("1 second");
+        comboBoxInterval.Items.Add("5 seconds");
+        comboBoxInterval.Items.Add("10 seconds");
+        comboBoxInterval.Items.Add("30 seconds");
+        comboBoxInterval.Items.Add("1 minute");
+        comboBoxInterval.Items.Add("5 minutes");
+        comboBoxInterval.Items.Add("10 minutes");
+        comboBoxInterval.Items.Add("30 minutes");
+        comboBoxInterval.Items.Add("1 hour");
+        comboBoxInterval.Items.Add("2 hours");
+        comboBoxInterval.Items.Add("4 hours");
+        comboBoxInterval.Items.Add("1 day");
+        comboBoxInterval.Items.Add("1 week");
+        comboBoxInterval.Items.Add("1 month");
+        comboBoxInterval.Items.Add("1 year");
+        comboBoxInterval.SelectedIndex = 5;
+    }
+}
