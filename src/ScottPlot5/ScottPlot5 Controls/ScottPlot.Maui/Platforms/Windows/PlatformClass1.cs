d07e5829c6e85deccff386bbd64e99025a969e33
Kosmos Werner <kosmoswerner@gmail.com>
2024-11-01 04:16:51 -0500

    PlatformClass1: Comments removed

--- src/ScottPlot5/ScottPlot5 Controls/ScottPlot.Maui/Platforms/Windows/PlatformClass1.cs
+++ src/ScottPlot5/ScottPlot5 Controls/ScottPlot.Maui/Platforms/Windows/PlatformClass1.cs
@@ -5,37 +5,5 @@ namespace ScottPlot.Maui;
 
 // All the code in this file is only included on Windows.
 public class PlatformClass1
-//internal static partial class MauiPlotExtensions
 {
-//    static partial void ProcessKeyModifiersPressed(UserInputProcessor processor, MauiPlot plot, PointerEventArgs e)
-//    {
-//        var args = e.PlatformArgs?.PointerRoutedEventArgs;
-//        if (args is null) return;
-//        var keyModifiers = args.KeyModifiers;
-
-
-//        if (keyModifiers is null) return;
-
-//        bool control = ((uint)keyModifiers & (uint)VirtualKeyModifiers.Control) > 0;
-//        bool shift = ((int)keyModifiers & (int)VirtualKeyModifiers.Shift) > 0;
-//        bool alt = ((int)keyModifiers & (int)VirtualKeyModifiers.Menu) > 0;
-
-
-//        IUserAction actionShift = shift
-//            ? new Interactivity.UserActions.KeyDown(StandardKeys.Shift)
-//            : new Interactivity.UserActions.KeyUp(StandardKeys.Shift);
-//        processor.Process(actionShift);
-
-//        IUserAction actionControl = control
-//            ? new Interactivity.UserActions.KeyDown(StandardKeys.Control)
-//            : new Interactivity.UserActions.KeyUp(StandardKeys.Control);
-//        processor.Process(actionControl);
-
-//        IUserAction actionAlt = alt
-//            ? new Interactivity.UserActions.KeyDown(StandardKeys.Alt)
-//            : new Interactivity.UserActions.KeyUp(StandardKeys.Alt);
-//        processor.Process(actionAlt);
-
-//        Trace.WriteLine($"dsa{args}");
-//    }
 }
