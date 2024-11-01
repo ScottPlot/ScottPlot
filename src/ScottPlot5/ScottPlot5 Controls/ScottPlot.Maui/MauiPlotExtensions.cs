edc88b3219f44efd48541c53dd6eff8d097b3847
Kosmos Werner <kosmoswerner@gmail.com>
2024-11-01 04:26:47 -0500

    MauiPlot: Added pan and zoom per platform, pan glitch removed

--- src/ScottPlot5/ScottPlot5 Controls/ScottPlot.Maui/MauiPlotExtensions.cs
+++ src/ScottPlot5/ScottPlot5 Controls/ScottPlot.Maui/MauiPlotExtensions.cs
@@ -1,37 +1,26 @@
-﻿namespace ScottPlot.Maui;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Maui;
+using ScottPlot.Interactivity;
+
+namespace ScottPlot.Maui;
 
 internal static class MauiPlotExtensions
 {
-    internal static Pixel ToPixel(this PointerEventArgs e, MauiPlot? plot)
+    internal static Pixel ToPixel(this PanUpdatedEventArgs e) => new Pixel(e.TotalX, e.TotalY);
+
+    internal static Pixel ToPixel(this Point e) => new Pixel(e.X, e.Y);
+
+    internal static Pixel ToPixelScaled(this Point e, float width, float height) => new Pixel(e.X * width, e.Y * height);
+
+    internal static Pixel ToPixel(this SKPoint e) => new Pixel(e.X, e.Y);
+
+    internal static void ProcessPanUpdated(this UserInputProcessor processor, MauiPlot plot, PanUpdatedEventArgs e)
     {
-        Point? position = e.GetPosition(plot);
-
-        if (position is null) return Pixel.NaN;
-        if (plot is null) return new(position.Value.X, position.Value.Y);
-        return new(position.Value.X * plot.DisplayScale, position.Value.Y * plot.DisplayScale);
-    }
-
-    internal static Pixel ToPixel(this TappedEventArgs e, MauiPlot? plot)
-    {
-        Point? position = e.GetPosition(plot);
-
-        if (position is null) return Pixel.NaN;
-        if (plot is null) return new(position.Value.X, position.Value.Y);
-        return new(position.Value.X * plot.DisplayScale, position.Value.Y * plot.DisplayScale);
-    }
-
-    internal static Pixel ToPixel(this PanUpdatedEventArgs e)
-    {
-        return new Pixel(e.TotalX, e.TotalY);
-    }
-
-    internal static void ProcessPanUpdated(this Interactivity.UserInputProcessor processor, MauiPlot plot, PanUpdatedEventArgs e)
-    {
-        Interactivity.IUserAction action = e.StatusType switch
+        IUserAction action = e.StatusType switch
         {
             GestureStatus.Started => new Interactivity.UserActions.LeftMouseDown(Pixel.Zero),
             GestureStatus.Running => new Interactivity.UserActions.MouseMove(e.ToPixel()),
-            GestureStatus.Completed => new Interactivity.UserActions.LeftMouseUp(plot.LastPixel), //When StatusType == Completed, e.ToPixel returns (0,0)
+            GestureStatus.Completed => new Interactivity.UserActions.LeftMouseUp(plot.LastPixel),
             _ => new Interactivity.UserActions.Unknown(),
         };
         plot.LastPixel = e.ToPixel();
@@ -39,59 +28,64 @@ internal static class MauiPlotExtensions
         processor.Process(action);
     }
 
-    internal static void ProcessPinchUpdated(this Interactivity.UserInputProcessor processor, MauiPlot plot, PinchGestureUpdatedEventArgs e)
+    internal static void ProcessPinchUpdated(this UserInputProcessor processor, MauiPlot plot, PinchGestureUpdatedEventArgs e, float width, float height)
     {
-        if (e.Status == GestureStatus.Running && e.Scale != 1.0)
+        if (e.Status == GestureStatus.Running)
         {
-            Interactivity.IUserAction action = e.Scale > 1
-                ? new ScottPlot.Interactivity.UserActions.MouseWheelUp(new Pixel(plot.Width / 2, plot.Height / 2))
-                : new ScottPlot.Interactivity.UserActions.MouseWheelDown(new Pixel(plot.Width / 2, plot.Height / 2));
+            Pixel pixel = e.ScaleOrigin.ToPixelScaled(width, height);
+
+            IUserAction action = e.Scale > 1
+                ? new ScottPlot.Interactivity.UserActions.MouseWheelUp(pixel)
+                : new ScottPlot.Interactivity.UserActions.MouseWheelDown(pixel);
 
             processor.Process(action);
         }
     }
 
-    internal static void ProcessPointerPressed(this Interactivity.UserInputProcessor processor, MauiPlot plot, PointerEventArgs e)
+    internal static void ProcessMouseDown(this UserInputProcessor processor, MauiPlot plot, SKTouchEventArgs e)
     {
-        Pixel pixel = e.ToPixel(plot);
+        Pixel pixel = e.Location.ToPixel();
 
-        //ProcessKeyModifiersPressed(processor, plot, e);
-        Interactivity.IUserAction action = new Interactivity.UserActions.LeftMouseDown(pixel);
+        IUserAction action = e.MouseButton switch
+        {
+            SKMouseButton.Left => new Interactivity.UserActions.LeftMouseDown(pixel),
+            SKMouseButton.Middle => new Interactivity.UserActions.MiddleMouseDown(pixel),
+            SKMouseButton.Right => new Interactivity.UserActions.RightMouseDown(pixel),
+            _ => new Interactivity.UserActions.Unknown()
+        };
         processor.Process(action);
     }
 
-    internal static void ProcessPointerMoved(this Interactivity.UserInputProcessor processor, MauiPlot plot, PointerEventArgs e)
+    internal static void ProcessMouseUp(this UserInputProcessor processor, MauiPlot plot, SKTouchEventArgs e)
     {
-        Pixel pixel = e.ToPixel(plot);
+        Pixel pixel = e.Location.ToPixel();
 
-        Interactivity.IUserAction action = new Interactivity.UserActions.MouseMove(pixel);
+        IUserAction action = e.MouseButton switch
+        {
+            SKMouseButton.Left => new Interactivity.UserActions.LeftMouseUp(pixel),
+            SKMouseButton.Middle => new Interactivity.UserActions.MiddleMouseUp(pixel),
+            SKMouseButton.Right => new Interactivity.UserActions.RightMouseUp(pixel),
+            _ => new Interactivity.UserActions.Unknown()
+        };
         processor.Process(action);
     }
 
-    internal static void ProcessPointerReleased(this Interactivity.UserInputProcessor processor, MauiPlot plot, PointerEventArgs e)
+    internal static void ProcessMouseMove(this UserInputProcessor processor, MauiPlot plot, SKTouchEventArgs e)
     {
-        Pixel pixel = e.ToPixel(plot);
+        Pixel pixel = e.Location.ToPixel();
 
-        Interactivity.IUserAction action = new Interactivity.UserActions.LeftMouseUp(pixel);
+        IUserAction action = new Interactivity.UserActions.MouseMove(pixel);
         processor.Process(action);
     }
 
-    internal static void ProcessContext(this Interactivity.UserInputProcessor processor, MauiPlot plot, TappedEventArgs e)
+    internal static void ProcessWheelChanged(this UserInputProcessor processor, MauiPlot plot, SKTouchEventArgs e)
     {
-        Pixel pixel = e.ToPixel(plot);
+        Pixel pixel = e.Location.ToPixel();
 
-        Interactivity.IUserAction action = new Interactivity.UserActions.RightMouseDown(pixel);
-        processor.Process(action);
-        action = new Interactivity.UserActions.RightMouseUp(pixel);
-        processor.Process(action);
-    }
+        IUserAction action = e.WheelDelta > 0
+          ? new ScottPlot.Interactivity.UserActions.MouseWheelUp(pixel)
+          : new ScottPlot.Interactivity.UserActions.MouseWheelDown(pixel);
 
-    internal static void ProcessZoomAll(this Interactivity.UserInputProcessor processor, MauiPlot plot, TappedEventArgs e)
-    {
-        Pixel pixel = e.ToPixel(plot);
-
-        Interactivity.IUserAction action = new Interactivity.UserActions.MouseWheelUp(pixel);
         processor.Process(action);
     }
 }
-
