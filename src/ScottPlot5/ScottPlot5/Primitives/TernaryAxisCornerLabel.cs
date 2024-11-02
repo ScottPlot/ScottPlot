2386049d3d5b54c0d309d1610a02f361f7c6fee8
Mamdouh <naruto_mysterious@yahoo.com>
2024-11-02 17:40:27 +0400

    Update TernaryAxisCornerLabel.cs
    removed public properties of ticklablecorner, now the ticklabel corner take their properties from the labelStyle

--- src/ScottPlot5/ScottPlot5/Primitives/TernaryAxisCornerLabel.cs
+++ src/ScottPlot5/ScottPlot5/Primitives/TernaryAxisCornerLabel.cs
@@ -6,22 +6,13 @@ namespace ScottPlot;
 public class TernaryAxisCornerLabel
 {
     public Coordinates Position { get; set; }
-    public string? LabelText { get; set; } = null;
-
-    // Customizable offset and alignment properties
-    public float OffsetX { get; set; } = 0;
-    public float OffsetY { get; set; } = 0;
-    public Alignment Alignment { get; set; } = Alignment.MiddleCenter;
 
     public LabelStyle LabelStyle { get; } = new()
     {
         Alignment = Alignment.MiddleCenter,
     };
-
-    // Allow the user to set text size and color
-    public float TextSize { get; set; } = 16f; // Default text size
-    public Color TextColor { get; set; } = Colors.Black; // Default text color
-    public bool IsBold { get; set; } = false; // Default to non-bold text
+    
+    public string? LabelText { get; set; } = null;
 
     public TernaryAxisCornerLabel(Coordinates position, string labelText)
     {
