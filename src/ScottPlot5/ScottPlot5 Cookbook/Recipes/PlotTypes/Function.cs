e2214b9ffe4cfa06b539e1457c6268a58e7c235c
Scott W Harden <swharden@gmail.com>
2024-11-03 13:32:17 -0500

    Cookbook: dynamic function plotting
    resolves #3522

--- src/ScottPlot5/ScottPlot5 Cookbook/Recipes/PlotTypes/Function.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/PlotTypes/Function.cs
@@ -38,13 +38,51 @@ public class Function : ICategory
         [Test]
         public override void Execute()
         {
-            var func = new Func<double, double>((x) => Math.Sin(x) * Math.Sin(x / 2));
+            static double func1(double x) => (Math.Sin(x) * Math.Sin(x / 2));
 
-            var f = myPlot.Add.Function(func);
+            var f = myPlot.Add.Function(func1);
             f.MinX = -3;
             f.MaxX = 3;
 
             myPlot.Axes.SetLimits(-5, 5, -.2, 1.0);
+        }
+    }
+
+    public class FunctionDynamic : RecipeBase
+    {
+        public override string Name => "Dynamically Generated Functions";
+        public override string Description => "When a function cannot be represented " +
+            "as a static method (e.g., one that requires custom parameters) it can be " +
+            "represented as variable of type Func<double, double> and plotted accordingly.";
+
+        [Test]
+        public override void Execute()
+        {
+            static double LogNormalDist(double x, double a, double b)
+            {
+                double expNum = Math.Log(x / a);
+                double exp = Math.Exp(-(expNum * expNum) / (2 * b * b));
+                double y = Math.Sqrt(2 * Math.PI) * b * x * exp;
+                return double.IsNaN(y) ? 0 : y;
+            }
+
+            double[] testValues = Generate.Range(0.8, 1.2, 0.05);
+            Color[] colors = new ScottPlot.Colormaps.MellowRainbow().GetColors(testValues.Length);
+            for (int i = 0; i < testValues.Length; i++)
+            {
+                double testValue = testValues[i];
+                var myFunc = new Func<double, double>((x) => LogNormalDist(x, testValue, 0.5));
+                var funcPlot = myPlot.Add.Function(myFunc);
+                funcPlot.LegendText = $"{testValue:0.00}";
+                funcPlot.LineWidth = 2;
+                funcPlot.LineColor = colors[i];
+            }
+
+            myPlot.ShowLegend();
+            myPlot.Legend.Orientation = Orientation.Horizontal;
+
+            myPlot.Axes.SetLimitsX(-0.5, 4);
+            myPlot.Axes.SetLimitsY(-0.8, 2);
         }
     }
 }
