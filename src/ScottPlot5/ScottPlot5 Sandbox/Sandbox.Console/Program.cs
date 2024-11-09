76873e5cc343123598b5b5821b6bea41026ffe86
Scott W Harden <swharden@gmail.com>
2024-11-08 20:00:25 -0500

    Sandbox: simplify console project

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Console/Program.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.Console/Program.cs
@@ -1,20 +1,7 @@
 ﻿using ScottPlot;
 
-Plot plt = new();
-plt.Add.Signal(Generate.Sin()).LineStyle.HandDrawn = true;
-plt.Add.Signal(Generate.Cos()).LineStyle.HandDrawn = true;
+Plot myPlot = new();
+myPlot.Add.Signal(Generate.Sin());
+myPlot.Add.Signal(Generate.Cos());
 
-foreach (var axis in plt.Axes.GetAxes())
-{
-    axis.FrameLineStyle.HandDrawn = true;
-}
-
-plt.Axes.DefaultGrid.XAxisStyle.MajorLineStyle.HandDrawn = true;
-plt.Axes.DefaultGrid.XAxisStyle.MinorLineStyle.HandDrawn = true;
-plt.Axes.DefaultGrid.YAxisStyle.MajorLineStyle.HandDrawn = true;
-plt.Axes.DefaultGrid.YAxisStyle.MajorLineStyle.HandDrawn = true;
-
-
-plt.SavePng("test.png", 600, 300).LaunchFile();
-plt.SavePng("test.png", 600, 300).LaunchInBrowser();
-plt.SavePng("test.png", 600, 300).ConsoleWritePath();
+myPlot.SavePng("test.png", 400, 300).LaunchFile();
