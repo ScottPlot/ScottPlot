cee2b7bf1f9f086f0b8ffbeee5d7e24e0f0147fa
Scott W Harden <swharden@gmail.com>
2024-11-15 15:17:59 -0500

    Cookbook: axis with image labels

--- src/ScottPlot5/ScottPlot5 Cookbook/Recipes/General/AxisRecipes.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/General/AxisRecipes.cs
@@ -365,4 +365,34 @@ public class AxisAndTicks : ICategory
             myPlot.Grid.LinePattern = LinePattern.Dotted;
         }
     }
+
+    public class ImageAxisLabel : RecipeBase
+    {
+        public override string Name => "Image Axis Label";
+        public override string Description => "For cases where axis label font styling " +
+            "does not provide the desired level of customization, a bitmap image may be " +
+            "displayed as an axis label. This strategy allows rich text to be realized " +
+            "using any third party tool that can render that text as a bitmap. It also " +
+            "enables users to place icons or images in their axis labels.";
+
+        [Test]
+        public override void Execute()
+        {
+            myPlot.Add.Signal(Generate.Sin(51));
+            myPlot.Add.Signal(Generate.Cos(51));
+
+            // This array holds the bytes of a bitmap. Here it's generated,
+            // but it could be a byte array read from a bitmap file on disk.
+            byte[] bytes1 = SampleImages.NoisyText("Horiz", 150, 50).GetImageBytes();
+            byte[] bytes2 = SampleImages.NoisyText("Vert", 150, 50).GetImageBytes();
+
+            // Create a ScottPlot.Image from the bitmap bytes
+            ScottPlot.Image img1 = new(bytes1);
+            ScottPlot.Image img2 = new(bytes2);
+
+            // Display the image for the bottom axis label
+            myPlot.Axes.Bottom.Label.Image = img1;
+            myPlot.Axes.Left.Label.Image = img2;
+        }
+    }
 }
