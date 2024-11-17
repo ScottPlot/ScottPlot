3aea525626eca8960b87bb86cb0eb338399a2982
BambOoxX <42067365+BambOoxX@users.noreply.github.com>
2024-11-17 19:45:15 +0100

    Revert "Add detached legend option"
    This reverts commit de4bbefbd7785a2b9cc6f352010fcc89f68aa26d.

--- src/ScottPlot5/ScottPlot5 Controls/ScottPlot.WinForms/FormsPlotMenu.cs
+++ src/ScottPlot5/ScottPlot5 Controls/ScottPlot.WinForms/FormsPlotMenu.cs
@@ -45,19 +45,12 @@ public class FormsPlotMenu : IPlotMenu
             OnInvoke = OpenInNewWindow,
         };
 
-         ContextMenuItem detachLegend = new()
-        {
-            Label = "Detach Legend",
-            OnInvoke = DetachLegend,
-        };
-
         return new List<ContextMenuItem>()
         {
             saveImage,
             copyImage,
             autoscale,
             newWindow,
-            detachLegend,
         };
     }
 
@@ -71,12 +64,6 @@ public class FormsPlotMenu : IPlotMenu
     public void OpenInNewWindow(IPlotControl plotControl)
     {
         FormsPlotViewer.Launch(plotControl.Plot, "Interactive Plot");
-        plotControl.Refresh();
-    }
-
-    public void DetachLegend(IPlotControl plotControl)
-    {
-        FormsPlotLegendViewer fplv = new FormsPlotLegendViewer(plotControl, "Detached Legend");
         plotControl.Refresh();
     }
 
