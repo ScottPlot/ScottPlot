f3b6749c057f3922ad6681901ece1eed0e76a959
Scott W Harden <swharden@gmail.com>
2024-11-17 16:18:50 -0500

    SignalConst: resolve warnings in deprecated class

--- src/ScottPlot5/ScottPlot5/Plottables/SignalConst.cs
+++ src/ScottPlot5/ScottPlot5/Plottables/SignalConst.cs
@@ -5,30 +5,8 @@ namespace ScottPlot.Plottables;
 [Obsolete("SignalConst has been deprecated, " +
     "but its functionality may be achieved by creating a Signal plot with a SignalConstSource data source. " +
     "See the Add.SignalConst() method for reference.", true)]
-public class SignalConst<T> : Signal, IPlottable, IHasLine, IHasMarker, IHasLegendText
+public class SignalConst<T>(SignalConstSource<T> data) : Signal(data), IPlottable, IHasLine, IHasMarker, IHasLegendText
     where T : struct, IComparable
 {
-    public SignalConst(SignalConstSource<T> data) : base(data)
-    {
-    }
-
-    /// <summary>
-    /// Setting this flag causes lines to be drawn between every visible point
-    /// (similar to scatter plots) to improve anti-aliasing in static images.
-    /// Setting this will decrease performance for large datasets 
-    /// and is not recommended for interactive environments.
-    /// </summary>
-    public bool AlwaysUseLowDensityMode { get; set; } = false;
-
-    public bool IsVisible { get; set; } = true;
-    public IAxes Axes { get; set; } = ScottPlot.Axes.Default;
-
-    public IEnumerable<LegendItem> LegendItems => LegendItem.Single(this,LegendText, MarkerStyle, LineStyle);
-
-    public AxisLimits GetAxisLimits() => Data.GetAxisLimits();
-
-    public virtual void Render(RenderPack rp)
-    {
-    }
 }
 
