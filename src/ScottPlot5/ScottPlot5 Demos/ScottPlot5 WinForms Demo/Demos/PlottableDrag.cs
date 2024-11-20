b7a2b537f11434c6d2c983cc18f76e0d27ee95d0
StendProg <53831487+StendProg@users.noreply.github.com>
2024-11-20 17:00:18 +0300

    Add pixel-based hit detection and drag support for plottables (#4531)
    * Implement DraggablePlottableDecorator
    Implement HitablePlottableDecorator
    Add Draggable Plottable Demo
    
    * Fix warning.
    
    * Make Render() virtual to pass the test
    
    * Fix wrong Plottable type request
    
    * Convert if to switch
    
    * Fix typo in clip range
    
    * Add lock for bitmap dispose vs read.
    
    * Move hitmap calculation to a separate thread, for best performance.
    
    * Simplify nesting for IsHit.
    
    * PlottableDrag Demo: update description
    
    * HitablePlottableDecorator.IsHit() accept Pixel instead of two floats
    
    * DragablePlottableDecorator: improve support for dragging arrows
    
    * Dragable → Draggable & Hitable → Hittable
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Scott W Harden <swharden@gmail.com>

---
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/PlottableDrag.cs
@@ -0,0 +1,121 @@
+﻿using ScottPlot;
+using ScottPlot.Plottables;
+
+namespace WinForms_Demo.Demos;
+
+public partial class PlottableDrag : Form, IDemoWindow
+{
+    public string Title => "Draggable Plottables";
+
+    public string Description => "Demonstrates how to create Plottables " +
+        "which can be dragged with the mouse";
+
+    DraggablePlottableDecorator? PlottableBeingDragged = null;
+
+    public PlottableDrag()
+    {
+        InitializeComponent();
+
+        double[] xs = Generate.Consecutive(100);
+        double[] ys1 = Generate.Sin(100);
+        double[] ys2 = Generate.Cos(100);
+
+        double[] xsLarge = Generate.Consecutive(1_000_000, 0.0001);
+        double[] ysLarge = Generate.RandomWalk(1_000_000, 0.01);
+
+        var signal1 = formsPlot1.Plot.Add.Signal(ys1);
+        formsPlot1.Plot.Remove(signal1);
+        formsPlot1.Plot.Add.Plottable(new DraggablePlottableDecorator(signal1));
+
+        var signal2 = formsPlot1.Plot.Add.Signal(ys2);
+        formsPlot1.Plot.Remove(signal2);
+        formsPlot1.Plot.Add.Plottable(new DraggablePlottableDecorator(signal2));
+
+        var signalXY = formsPlot1.Plot.Add.SignalXY(xsLarge, ysLarge);
+        formsPlot1.Plot.Remove(signalXY);
+        formsPlot1.Plot.Add.Plottable(new DraggablePlottableDecorator(signalXY));
+
+        var scatter = formsPlot1.Plot.Add.Scatter(xs, ys2);
+        formsPlot1.Plot.Remove(scatter);
+        formsPlot1.Plot.Add.Plottable(new DraggablePlottableDecorator(scatter));
+
+        var ellipse = formsPlot1.Plot.Add.Ellipse(25, 2, 5, 3, 30);
+        formsPlot1.Plot.Remove(ellipse);
+        formsPlot1.Plot.Add.Plottable(new DraggablePlottableDecorator(ellipse));
+
+        var arrow = formsPlot1.Plot.Add.Arrow(20, 1, 0, 0);
+        formsPlot1.Plot.Remove(arrow);
+        formsPlot1.Plot.Add.Plottable(new DraggablePlottableDecorator(arrow));
+
+        var imageMarker = formsPlot1.Plot.Add.ImageMarker(new Coordinates(20, 1), SampleImages.ScottPlotLogo(), 0.1f);
+        formsPlot1.Plot.Remove(imageMarker);
+        formsPlot1.Plot.Add.Plottable(new DraggablePlottableDecorator(imageMarker));
+
+        var marker = formsPlot1.Plot.Add.Marker(30, -1);
+        formsPlot1.Plot.Remove(marker);
+        formsPlot1.Plot.Add.Plottable(new DraggablePlottableDecorator(marker));
+
+        var text = formsPlot1.Plot.Add.Text("Text", 12, 1);
+        text.LabelFontSize = 50;
+        formsPlot1.Plot.Remove(text);
+        formsPlot1.Plot.Add.Plottable(new DraggablePlottableDecorator(text));
+
+        formsPlot1.MouseDown += FormsPlot1_MouseDown;
+        formsPlot1.MouseUp += FormsPlot1_MouseUp;
+        formsPlot1.MouseMove += FormsPlot1_MouseMove;
+    }
+
+    private void FormsPlot1_MouseDown(object? sender, MouseEventArgs e)
+    {
+        Pixel mousePixel = new(e.X, e.Y);
+
+        foreach (DraggablePlottableDecorator dp in formsPlot1.Plot.GetPlottables<DraggablePlottableDecorator>().Reverse())
+        {
+            if (dp.IsHit(mousePixel, 10))
+            {
+                var PlottableCoordinates = formsPlot1.Plot.GetCoordinates(mousePixel, dp.Axes.XAxis, dp.Axes.YAxis);
+                dp.StartDrag(PlottableCoordinates);
+                PlottableBeingDragged = dp;
+                formsPlot1.UserInputProcessor.Disable(); // disable panning while dragging
+                return;
+            }
+        }
+    }
+
+    private void FormsPlot1_MouseUp(object? sender, MouseEventArgs e)
+    {
+        PlottableBeingDragged = null;
+        formsPlot1.UserInputProcessor.Enable(); // enable panning again
+        formsPlot1.Refresh();
+    }
+
+    private void FormsPlot1_MouseMove(object? sender, MouseEventArgs e)
+    {
+        Pixel mousePixel = new(e.X, e.Y);
+
+        if (PlottableBeingDragged is not null)
+        {
+            // update the position of the plottable being dragged
+            Coordinates mouseCoordinates = formsPlot1.Plot.GetCoordinates(
+                pixel: mousePixel,
+                xAxis: PlottableBeingDragged.Axes.XAxis,
+                yAxis: PlottableBeingDragged.Axes.YAxis);
+
+            PlottableBeingDragged.DragTo(mouseCoordinates);
+            formsPlot1.Refresh();
+            return;
+        }
+
+        // update the cursor to reflect what is underneath it
+        foreach (DraggablePlottableDecorator dp in formsPlot1.Plot.GetPlottables<DraggablePlottableDecorator>())
+        {
+            if (dp.IsHit(mousePixel, 10))
+            {
+                Cursor = Cursors.Hand;
+                return;
+            }
+        }
+
+        Cursor = Cursors.Arrow;
+    }
+}
---
+++ src/ScottPlot5/ScottPlot5/Plottables/HittablePlottableDecorator.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Tasks;
+
+namespace ScottPlot.Plottables
+{
+    /// <summary>
+    /// This class wraps an <see cref="IPlottable"/> to give it a 
+    /// pixel-based hit detection map that can be used for mouse interaction
+    /// </summary>
+    public class HittablePlottableDecorator : IPlottable, IDisposable
+    {
+        private readonly object _lock = new();
+        private SKBitmap? _bitmap;
+        public IPlottable Source { get; }
+        public bool IsVisible { get => Source.IsVisible; set => Source.IsVisible = value; }
+        public IAxes Axes { get => Source.Axes; set => Source.Axes = value; }
+
+        public IEnumerable<LegendItem> LegendItems => Source.LegendItems;
+
+        public AxisLimits GetAxisLimits() => Source.GetAxisLimits();
+
+        public HittablePlottableDecorator(IPlottable plottable)
+        {
+            Source = plottable;
+        }
+
+        /// <summary>
+        /// Returns true if the plottable is within <paramref name="radius"/> 
+        /// pixels of the given <paramref name="pixel"/>
+        /// </summary>
+        public bool IsHit(Pixel pixel, float radius)
+        {
+            if (_bitmap == null)
+                return false;
+
+            lock (_lock)
+            {
+                int fromX = Math.Max(0, (int)(pixel.X - radius / 2));
+                int toX = Math.Min(_bitmap.Width, (int)(pixel.X + radius / 2));
+                int fromY = Math.Max(0, (int)(pixel.Y - radius / 2));
+                int toY = Math.Min(_bitmap.Height, (int)(pixel.Y + radius / 2));
+                for (int i = fromX; i < toX; i++)
+                {
+                    for (int j = fromY; j < toY; j++)
+                    {
+                        if (_bitmap.GetPixel(i, j).Alpha != 0)
+                            return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public virtual void Render(RenderPack rp)
+        {
+            Source.Render(rp);
+            Task.Run(() =>
+            {
+                SKBitmap bitmapBuf = new SKBitmap((int)rp.FigureRect.Width, (int)rp.FigureRect.Height, SKColorType.Rgba8888, SKAlphaType.Premul);
+                using (var hitCanvas = new SKCanvas(bitmapBuf))
+                {
+                    hitCanvas.Clear(new SKColor(255, 255, 255, 0));
+                    using (RenderPack rpHitable = new RenderPack(rp.Plot, rp.FigureRect, hitCanvas))
+                    {
+                        rpHitable.CalculateLayout();
+                        rpHitable.CanvasState.Clip(rpHitable.DataRect);
+                        Source.Render(rpHitable);
+                        rpHitable.CanvasState.DisableClipping();
+                    }
+                }
+                lock (_lock)
+                {
+                    _bitmap?.Dispose();
+                    _bitmap = bitmapBuf;
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            _bitmap?.Dispose();
+            _bitmap = null;
+        }
+    }
+}
