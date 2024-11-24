e0b4e71ca1434a9903f7d8441c30b3fcc56da6ee
Scott W Harden <swharden@gmail.com>
2024-11-24 15:40:39 -0500

    HistogramBars: new plot type (#4561)
    resolves 4557

--- src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/LiveHistogram.cs
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/LiveHistogram.cs
@@ -1,5 +1,6 @@
 ﻿using ScottPlot;
 using ScottPlot.Plottables;
+using ScottPlot.Statistics;
 
 namespace WinForms_Demo.Demos;
 
@@ -11,41 +12,24 @@ public partial class LiveHistogram : Form, IDemoWindow
 
     readonly System.Windows.Forms.Timer Timer = new() { Enabled = true, Interval = 20 };
 
-    int Total = 0;
-
     public LiveHistogram()
     {
         InitializeComponent();
 
-        // create a histogram with 100 bins
-        int binCount = 100;
-        double[] initialHeights = Generate.Zeros(binCount);
-        BarPlot bp = formsPlot1.Plot.Add.Bars(initialHeights);
-
-        // style bars so there are no gaps between them
-        foreach (var bar in bp.Bars)
-        {
-            bar.LineWidth = 0;
-            bar.Size = 1;
-            bar.FillStyle.AntiAlias = false;
-        }
-
-        // prevent AutoScale() from adding space below the bars
-        formsPlot1.Plot.Axes.Margins(bottom: 0);
+        var histogram = Histogram.WithBinCount(count: 50, minValue: 0, maxValue: 100);
+        var histogramPlot = formsPlot1.Plot.Add.Histogram(histogram);
 
         // simulate live incoming data
         Timer.Tick += (s, e) =>
         {
-            int newValueCount = Generate.RandomInteger(50, 100);
-            for (int i = 0; i < newValueCount; i++)
+            int numberOfNewValues = Generate.RandomInteger(50, 100);
+            for (int i = 0; i < numberOfNewValues; i++)
             {
                 double randomValue = Generate.RandomNormalNumber(mean: 50, stdDev: 10);
-                int binIndex = (int)NumericConversion.Clamp(randomValue, 0, binCount - 1);
-                bp.Bars[binIndex].Value += 1;
-                Total += 1;
+                histogram.Add(randomValue);
             }
 
-            formsPlot1.Plot.Title($"Total: {Total:N0}");
+            formsPlot1.Plot.Title($"Total: {histogram.Counts.Sum():N0}");
             formsPlot1.Plot.Axes.AutoScale();
             formsPlot1.Refresh();
         };
