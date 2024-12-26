61f6702bfce2b600154595dedf496e07af69e79e
Scott W Harden <swharden@gmail.com>
2024-12-26 13:23:09 -0500

    Demo: collapsed financial multiplot with right axis
    #4600

--- src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/MultiplotCollapsed.cs
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/MultiplotCollapsed.cs
@@ -18,28 +18,31 @@ public partial class MultiplotCollapsed : Form, IDemoWindow
 
         // add sample price data to the first plot
         Plot pricePlot = formsPlot1.Multiplot.GetPlot(0);
-        pricePlot.YLabel("Price");
+        pricePlot.Axes.Right.Label.Text = "Price";
         List<OHLC> ohlcs = Generate.RandomOHLCs(50);
         var candlestick = pricePlot.Add.Candlestick(ohlcs);
+        candlestick.Axes.YAxis = pricePlot.Axes.Right;
         candlestick.Sequential = true;
 
         // add sample RSI data to the second plot
         Plot rsiPlot = formsPlot1.Multiplot.GetPlot(1);
-        rsiPlot.YLabel("RSI");
+        rsiPlot.Axes.Right.Label.Text = "RSI";
         double[] rsiValues = Generate.RandomWalk(ohlcs.Count);
         var rsiSig = rsiPlot.Add.Signal(rsiValues);
+        rsiSig.Axes.YAxis = rsiPlot.Axes.Right;
         rsiSig.LineWidth = 2;
 
         // add sample RSI data to the second plot
         Plot volumePlot = formsPlot1.Multiplot.GetPlot(2);
-        volumePlot.YLabel("Volume");
+        volumePlot.Axes.Right.Label.Text = "Volume";
         double[] volumes = Generate.RandomSample(50, 10, 90);
-        volumePlot.Add.Bars(volumes);
+        var bars = volumePlot.Add.Bars(volumes);
+        bars.Axes.YAxis = volumePlot.Axes.Right;
         volumePlot.Axes.Margins(bottom: 0);
 
         // give all plots identical left and right padding to ensure alignment
-        float padLeft = 60;
-        float padRight = 20;
+        float padLeft = 20;
+        float padRight = 65;
 
         // set padding so there is no space between the middle and adjacent plots
         pricePlot.Layout.Fixed(new PixelPadding(padLeft, padRight, bottom: 0, top: 25));
@@ -54,6 +57,7 @@ public partial class MultiplotCollapsed : Form, IDemoWindow
         // link horizontal axes across all plots
         formsPlot1.Multiplot.ShareX([pricePlot, rsiPlot, volumePlot]);
     }
+
     class ExpandingTopRow(int rowIndex) : ISubplotPosition
     {
         public float MiddlePlotHeight = 100;
