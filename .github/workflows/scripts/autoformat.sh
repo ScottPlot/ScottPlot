a5b6acffa6dab157a3d9f20fcf4ee5a8c142b95d
Scott W Harden <swharden@gmail.com>
2025-01-08 18:54:49 -0500

    CICD: remove ScottPlot4 jobs

--- .github/workflows/scripts/autoformat.sh
+++ .github/workflows/scripts/autoformat.sh
@@ -6,7 +6,6 @@ project_files=(
     "src/ScottPlot5/ScottPlot5 Cookbook/ScottPlot Cookbook.csproj"
     "src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/WinForms Demo.csproj"
     "src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WPF Demo/WPF Demo.csproj"
-    "src/ScottPlot4/ScottPlot/ScottPlot.csproj"
 )
 
 for project_file in "${project_files[@]}"; do
