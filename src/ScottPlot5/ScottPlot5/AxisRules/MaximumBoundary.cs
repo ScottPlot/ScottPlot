1a360f870ae77cee2fe4552ee7d0b47317254f38
Evgenii Karachevtsev <46753156+kebox7@users.noreply.github.com>
2025-01-10 23:51:44 +0300

    Axis Rules: Improve support for boundaries on plots with inverted axes (#4686)
    * Fix MinimumBoundary
    
    * Fix MaximumBoundary
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Scott W Harden <swharden@gmail.com>

--- src/ScottPlot5/ScottPlot5/AxisRules/MaximumBoundary.cs
+++ src/ScottPlot5/ScottPlot5/AxisRules/MaximumBoundary.cs
@@ -8,16 +8,21 @@ public class MaximumBoundary(IXAxis xAxis, IYAxis yAxis, AxisLimits limits) : IA
 
     public void Apply(RenderPack rp, bool beforeLayout)
     {
+        double horizontalSpan = Math.Min(Math.Abs(XAxis.Range.Span), Limits.XRange.Span);
+        double verticalSpan = Math.Min(Math.Abs(YAxis.Range.Span), Limits.YRange.Span);
+
         if (XAxis.IsInverted())
         {
             if (XAxis.Range.Min > Limits.XRange.Max)
             {
                 XAxis.Range.Min = Limits.XRange.Max;
+                XAxis.Range.Max = Limits.XRange.Max - horizontalSpan;
             }
 
             if (XAxis.Range.Max < Limits.XRange.Min)
             {
                 XAxis.Range.Max = Limits.XRange.Min;
+                XAxis.Range.Min = Limits.XRange.Min + horizontalSpan;
             }
         }
         else
@@ -25,11 +30,13 @@ public class MaximumBoundary(IXAxis xAxis, IYAxis yAxis, AxisLimits limits) : IA
             if (XAxis.Range.Max > Limits.XRange.Max)
             {
                 XAxis.Range.Max = Limits.XRange.Max;
+                XAxis.Range.Min = Limits.XRange.Max - horizontalSpan;
             }
 
             if (XAxis.Range.Min < Limits.XRange.Min)
             {
                 XAxis.Range.Min = Limits.XRange.Min;
+                XAxis.Range.Max = Limits.XRange.Min + horizontalSpan;
             }
         }
 
@@ -38,11 +45,13 @@ public class MaximumBoundary(IXAxis xAxis, IYAxis yAxis, AxisLimits limits) : IA
             if (YAxis.Range.Min > Limits.YRange.Max)
             {
                 YAxis.Range.Min = Limits.YRange.Max;
+                YAxis.Range.Max = Limits.YRange.Max - verticalSpan;
             }
 
             if (YAxis.Range.Max < Limits.YRange.Min)
             {
                 YAxis.Range.Max = Limits.YRange.Min;
+                YAxis.Range.Min = Limits.YRange.Min + verticalSpan;
             }
         }
         else
@@ -50,11 +59,13 @@ public class MaximumBoundary(IXAxis xAxis, IYAxis yAxis, AxisLimits limits) : IA
             if (YAxis.Range.Max > Limits.YRange.Max)
             {
                 YAxis.Range.Max = Limits.YRange.Max;
+                YAxis.Range.Min = Limits.YRange.Max - verticalSpan;
             }
 
             if (YAxis.Range.Min < Limits.YRange.Min)
             {
                 YAxis.Range.Min = Limits.YRange.Min;
+                YAxis.Range.Max = Limits.YRange.Min + verticalSpan;
             }
         }
     }
