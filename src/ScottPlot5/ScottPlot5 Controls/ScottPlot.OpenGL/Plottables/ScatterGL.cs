2a74711c4427297d2c3e5bb6797367258e7dcb7d
onur-akaydin <78170309+onur-akaydin@users.noreply.github.com>
2025-01-17 16:30:47 +0300

    Update src/ScottPlot5/ScottPlot5 Controls/ScottPlot.OpenGL/Plottables/ScatterGL.cs
    Co-authored-by: StendProg <53831487+StendProg@users.noreply.github.com>

--- src/ScottPlot5/ScottPlot5 Controls/ScottPlot.OpenGL/Plottables/ScatterGL.cs
+++ src/ScottPlot5/ScottPlot5 Controls/ScottPlot.OpenGL/Plottables/ScatterGL.cs
@@ -206,6 +206,9 @@ public class ScatterGL : Scatter, IPlottableGL, IDisposable
             GLHasBeenInitialized = false;
         }
     }
-
+    ~ScatterGL()
+    {
+        Dispose(false);
+    }
     #endregion
 }
