afece893fc22a9990e7d581dd3b9199fd8dd1133
Scott W Harden <swharden@gmail.com>
2025-01-25 10:58:01 -0500

    WinForms demo: custom multiplot

---
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WinForms Demo/Demos/MultiplotCustom.cs
@@ -0,0 +1,15 @@
+﻿namespace WinForms_Demo.Demos;
+public partial class MultiplotCustom : Form, IDemoWindow
+{
+    public string Title => "Custom Multiplot System";
+
+    public string Description => "Although the default Multiplot is suitable for most applications, " +
+        "advanced users may achieve extreme control by creating a class that implements IMultiplot.";
+
+    public MultiplotCustom()
+    {
+        InitializeComponent();
+
+        //formsPlot1.Multiplot = new CustomMultiplot();
+    }
+}
