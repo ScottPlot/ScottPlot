52060f9db31131da7e37536dd63e887005111c0b
Scott W Harden <swharden@gmail.com>
2025-02-26 07:10:40 -0500

    Website: list all demo windows (#4823)
    * winforms demo: generate json and html output
    
    * WPF demo: generate html output

--- dev/ScottPlot5/generate-cookbook.bat
+++ dev/ScottPlot5/generate-cookbook.bat
@@ -1,5 +1,13 @@
-:: this is where the cookbook will be created
+:: Folder the cookbook will be generated in when the test project runs
 SET CookbookSource=..\www\cookbook\5.0
+
+:: Folder where the website repository is located
+SET ShortcodeFolder=..\..\..\ScottPlot.NET\layouts\shortcodes
+SET CookbookDest=%ShortcodeFolder%\content\cookbook\5.0
+
+:: generate the demo app shortcode
+dotnet run --project "..\..\src\ScottPlot5\ScottPlot5 Demos\ScottPlot5 WinForms Demo" "%ShortcodeFolder%\demos-winforms.html"
+dotnet run --project "..\..\src\ScottPlot5\ScottPlot5 Demos\ScottPlot5 WPF Demo" "%ShortcodeFolder%\demos-wpf.html"
 
 :: delete the old cookbook if it exists
 rmdir /q/s "%CookbookSource%"
@@ -11,7 +19,6 @@ dotnet test "..\..\src\ScottPlot5\ScottPlot5 Tests\Unit Tests"
 dotnet test "..\..\src\ScottPlot5\ScottPlot5 Cookbook"
 
 :: copy all files (except HTML files) from the ScottPlot repo's cookbook output folder to the ScottPlot.NET repo's cookbook folder
-SET CookbookDest=..\..\..\ScottPlot.NET\content\cookbook\5.0
 del /f/q/s "%CookbookDest%" > nul
 rmdir /q/s "%CookbookDest%"
 robocopy "%CookbookSource%" "%CookbookDest%" /E /Z /R:5 /W:5 /TBD /NP /V
