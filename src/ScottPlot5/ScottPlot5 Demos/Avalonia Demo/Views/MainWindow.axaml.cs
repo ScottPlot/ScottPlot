20e016af180659d558dbf056669172b3315202d5
heartacker <heartacker@outlook.com>
2025-03-02 16:41:53 +0800

    auto size and dynamic cols while windows size changed
    (cherry picked from commit 71f9d5155dee6a1d24aa58028e83c474422fae69)

--- src/ScottPlot5/ScottPlot5 Demos/Avalonia Demo/Views/MainWindow.axaml.cs
+++ src/ScottPlot5/ScottPlot5 Demos/Avalonia Demo/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia_Demo.ViewModels;
 
 namespace Avalonia_Demo.Views
 {
@@ -7,6 +8,19 @@ namespace Avalonia_Demo.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        MainWindowViewModel vm => (MainWindowViewModel)DataContext;
+        double margin = 6;
+        int div_width = 400;
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs args)
+        {
+            double winwidth = args.NewSize.Width;
+            double winheight = args.NewSize.Height;
+            int div_n = (int)(winwidth / div_width + 0.5);
+            //int div_res =((int)(winheight)) % div_width;
+            vm.DemoWidth = (winwidth - margin) / (div_n);
         }
     }
 }
