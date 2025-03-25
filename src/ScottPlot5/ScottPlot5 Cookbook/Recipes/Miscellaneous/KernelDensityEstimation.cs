398461dbe762e9194728790bbd60bde3ff333ed7
Benjamin Lehmann <bclehmann@yahoo.ca>
2025-03-24 20:12:18 -0600

    KDE Demo: fix scaling

--- src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Miscellaneous/KernelDensityEstimation.cs
+++ src/ScottPlot5/ScottPlot5 Cookbook/Recipes/Miscellaneous/KernelDensityEstimation.cs
@@ -28,7 +28,7 @@ public class KernelDensityEstimation : ICategory
             histPlot.BarWidthFraction = 0.8;
 
             var densityEstimate = hist.Bins.Select((x, i) => KernelDensity.Estimate(x, ys)).ToArray();
-            double scale = ys.Max() / densityEstimate.Sum();
+            double scale = ys.Length;
 
             var rescaledDensityEstimate = densityEstimate.Select(x => x * scale).ToArray();
 
@@ -59,7 +59,7 @@ public class KernelDensityEstimation : ICategory
             foreach(var kernel in Enum.GetValues<KdeKernel>())
             {
                 var densityEstimate = hist.Bins.Select((x, i) => KernelDensity.Estimate(x, ys, kernel)).ToArray();
-                double scale = ys.Max() / densityEstimate.Sum();
+                double scale = ys.Length;
 
                 var rescaledDensityEstimate = densityEstimate.Select(x => x * scale).ToArray();
 
