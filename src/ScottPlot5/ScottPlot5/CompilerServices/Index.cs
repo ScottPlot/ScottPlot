fc3a9ccc63c6bc2991fb2a675cb82ca41af5871e
Scott W Harden <swharden@gmail.com>
2025-08-26 23:34:07 -0400

    Add notice markers around all code comment licensees

--- src/ScottPlot5/ScottPlot5/CompilerServices/Index.cs
+++ src/ScottPlot5/ScottPlot5/CompilerServices/Index.cs
@@ -3,7 +3,35 @@
 //
 // Obtained from dotnet runtime repository on 2024-04-10
 // https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Index.cs
+/*
+--- NOTICE BEGIN ---
 
+The MIT License (MIT)
+
+Copyright (c) .NET Foundation and Contributors
+
+All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+--- NOTICE END ---
+*/
 #if !NETSTANDARD2_1_OR_GREATER && !NET6_0_OR_GREATER
 
 using System.Runtime.CompilerServices;
