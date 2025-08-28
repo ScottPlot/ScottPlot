49e96454429bfd39e9419c3e30cf117b560f2a00
Benjamin Lehmann <bclehmann@yahoo.ca>
2025-08-27 20:04:12 -0600

    Use real Turbo colormap

--- src/ScottPlot5/ScottPlot5/Colormaps/Turbo.cs
+++ src/ScottPlot5/ScottPlot5/Colormaps/Turbo.cs
@@ -1,6 +1,210 @@
-// This colormap was created by Scott Harden on 2020-06-16
-// It was designed to mimic Turbo, but is not a copy of or derived from Turbo source code.
-// https://ai.googleblog.com/2019/08/turbo-improved-rainbow-colormap-for.html
+// Adapted from https://gist.github.com/mikhailov-work/6a308c20e494d9e0ccc29036b28faa7a
+/*
+--- NOTICE BEGIN ---
+
+                                 Apache License
+                           Version 2.0, January 2004
+                        http://www.apache.org/licenses/
+
+   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+   1. Definitions.
+
+      "License" shall mean the terms and conditions for use, reproduction,
+      and distribution as defined by Sections 1 through 9 of this document.
+
+      "Licensor" shall mean the copyright owner or entity authorized by
+      the copyright owner that is granting the License.
+
+      "Legal Entity" shall mean the union of the acting entity and all
+      other entities that control, are controlled by, or are under common
+      control with that entity. For the purposes of this definition,
+      "control" means (i) the power, direct or indirect, to cause the
+      direction or management of such entity, whether by contract or
+      otherwise, or (ii) ownership of fifty percent (50%) or more of the
+      outstanding shares, or (iii) beneficial ownership of such entity.
+
+      "You" (or "Your") shall mean an individual or Legal Entity
+      exercising permissions granted by this License.
+
+      "Source" form shall mean the preferred form for making modifications,
+      including but not limited to software source code, documentation
+      source, and configuration files.
+
+      "Object" form shall mean any form resulting from mechanical
+      transformation or translation of a Source form, including but
+      not limited to compiled object code, generated documentation,
+      and conversions to other media types.
+
+      "Work" shall mean the work of authorship, whether in Source or
+      Object form, made available under the License, as indicated by a
+      copyright notice that is included in or attached to the work
+      (an example is provided in the Appendix below).
+
+      "Derivative Works" shall mean any work, whether in Source or Object
+      form, that is based on (or derived from) the Work and for which the
+      editorial revisions, annotations, elaborations, or other modifications
+      represent, as a whole, an original work of authorship. For the purposes
+      of this License, Derivative Works shall not include works that remain
+      separable from, or merely link (or bind by name) to the interfaces of,
+      the Work and Derivative Works thereof.
+
+      "Contribution" shall mean any work of authorship, including
+      the original version of the Work and any modifications or additions
+      to that Work or Derivative Works thereof, that is intentionally
+      submitted to Licensor for inclusion in the Work by the copyright owner
+      or by an individual or Legal Entity authorized to submit on behalf of
+      the copyright owner. For the purposes of this definition, "submitted"
+      means any form of electronic, verbal, or written communication sent
+      to the Licensor or its representatives, including but not limited to
+      communication on electronic mailing lists, source code control systems,
+      and issue tracking systems that are managed by, or on behalf of, the
+      Licensor for the purpose of discussing and improving the Work, but
+      excluding communication that is conspicuously marked or otherwise
+      designated in writing by the copyright owner as "Not a Contribution."
+
+      "Contributor" shall mean Licensor and any individual or Legal Entity
+      on behalf of whom a Contribution has been received by Licensor and
+      subsequently incorporated within the Work.
+
+   2. Grant of Copyright License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      copyright license to reproduce, prepare Derivative Works of,
+      publicly display, publicly perform, sublicense, and distribute the
+      Work and such Derivative Works in Source or Object form.
+
+   3. Grant of Patent License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      (except as stated in this section) patent license to make, have made,
+      use, offer to sell, sell, import, and otherwise transfer the Work,
+      where such license applies only to those patent claims licensable
+      by such Contributor that are necessarily infringed by their
+      Contribution(s) alone or by combination of their Contribution(s)
+      with the Work to which such Contribution(s) was submitted. If You
+      institute patent litigation against any entity (including a
+      cross-claim or counterclaim in a lawsuit) alleging that the Work
+      or a Contribution incorporated within the Work constitutes direct
+      or contributory patent infringement, then any patent licenses
+      granted to You under this License for that Work shall terminate
+      as of the date such litigation is filed.
+
+   4. Redistribution. You may reproduce and distribute copies of the
+      Work or Derivative Works thereof in any medium, with or without
+      modifications, and in Source or Object form, provided that You
+      meet the following conditions:
+
+      (a) You must give any other recipients of the Work or
+          Derivative Works a copy of this License; and
+
+      (b) You must cause any modified files to carry prominent notices
+          stating that You changed the files; and
+
+      (c) You must retain, in the Source form of any Derivative Works
+          that You distribute, all copyright, patent, trademark, and
+          attribution notices from the Source form of the Work,
+          excluding those notices that do not pertain to any part of
+          the Derivative Works; and
+
+      (d) If the Work includes a "NOTICE" text file as part of its
+          distribution, then any Derivative Works that You distribute must
+          include a readable copy of the attribution notices contained
+          within such NOTICE file, excluding those notices that do not
+          pertain to any part of the Derivative Works, in at least one
+          of the following places: within a NOTICE text file distributed
+          as part of the Derivative Works; within the Source form or
+          documentation, if provided along with the Derivative Works; or,
+          within a display generated by the Derivative Works, if and
+          wherever such third-party notices normally appear. The contents
+          of the NOTICE file are for informational purposes only and
+          do not modify the License. You may add Your own attribution
+          notices within Derivative Works that You distribute, alongside
+          or as an addendum to the NOTICE text from the Work, provided
+          that such additional attribution notices cannot be construed
+          as modifying the License.
+
+      You may add Your own copyright statement to Your modifications and
+      may provide additional or different license terms and conditions
+      for use, reproduction, or distribution of Your modifications, or
+      for any such Derivative Works as a whole, provided Your use,
+      reproduction, and distribution of the Work otherwise complies with
+      the conditions stated in this License.
+
+   5. Submission of Contributions. Unless You explicitly state otherwise,
+      any Contribution intentionally submitted for inclusion in the Work
+      by You to the Licensor shall be under the terms and conditions of
+      this License, without any additional terms or conditions.
+      Notwithstanding the above, nothing herein shall supersede or modify
+      the terms of any separate license agreement you may have executed
+      with Licensor regarding such Contributions.
+
+   6. Trademarks. This License does not grant permission to use the trade
+      names, trademarks, service marks, or product names of the Licensor,
+      except as required for reasonable and customary use in describing the
+      origin of the Work and reproducing the content of the NOTICE file.
+
+   7. Disclaimer of Warranty. Unless required by applicable law or
+      agreed to in writing, Licensor provides the Work (and each
+      Contributor provides its Contributions) on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+      implied, including, without limitation, any warranties or conditions
+      of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+      PARTICULAR PURPOSE. You are solely responsible for determining the
+      appropriateness of using or redistributing the Work and assume any
+      risks associated with Your exercise of permissions under this License.
+
+   8. Limitation of Liability. In no event and under no legal theory,
+      whether in tort (including negligence), contract, or otherwise,
+      unless required by applicable law (such as deliberate and grossly
+      negligent acts) or agreed to in writing, shall any Contributor be
+      liable to You for damages, including any direct, indirect, special,
+      incidental, or consequential damages of any character arising as a
+      result of this License or out of the use or inability to use the
+      Work (including but not limited to damages for loss of goodwill,
+      work stoppage, computer failure or malfunction, or any and all
+      other commercial damages or losses), even if such Contributor
+      has been advised of the possibility of such damages.
+
+   9. Accepting Warranty or Additional Liability. While redistributing
+      the Work or Derivative Works thereof, You may choose to offer,
+      and charge a fee for, acceptance of support, warranty, indemnity,
+      or other liability obligations and/or rights consistent with this
+      License. However, in accepting such obligations, You may act only
+      on Your own behalf and on Your sole responsibility, not on behalf
+      of any other Contributor, and only if You agree to indemnify,
+      defend, and hold each Contributor harmless for any liability
+      incurred by, or claims asserted against, such Contributor by reason
+      of your accepting any such warranty or additional liability.
+
+   END OF TERMS AND CONDITIONS
+
+   APPENDIX: How to apply the Apache License to your work.
+
+      To apply the Apache License to your work, attach the following
+      boilerplate notice, with the fields enclosed by brackets "[]"
+      replaced with your own identifying information. (Don't include
+      the brackets!)  The text should be enclosed in the appropriate
+      comment syntax for the file format. We also recommend that a
+      file or class name and description of purpose be included on the
+      same "printed page" as the copyright notice for easier
+      identification within third-party archives.
+
+   Copyright 2019 Google LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+--- NOTICE END ---
+*/
 
 
 namespace ScottPlot.Colormaps;
@@ -13,43 +217,43 @@ public class Turbo : IColormap
 
     public Turbo()
     {
-        uint[] rgbColors = new int[]
+        uint[] argbs = new uint[256]
         {
-            -13559489, -13493436, -13427382, -13361328, -13295018, -13228964, -13162911, -13096857,
-            -13030547, -12964493, -12898440, -12832130, -12766077, -12700023, -12633970, -12567660,
-            -12501607, -12435554, -12369245, -12303192, -12237139, -12171086, -12170313, -12104260,
-            -12038208, -12037436, -11971383, -11905331, -11904559, -11838507, -11837991, -11771940,
-            -11771168, -11770653, -11770138, -11703831, -11703316, -11702801, -11702287, -11701517,
-            -11701003, -11700489, -11765255, -11764742, -11764228, -11828995, -11828482, -11893506,
-            -11892737, -11957761, -12022785, -12022017, -12087042, -12152067, -12217092, -12347397,
-            -12412423, -12477448, -12542218, -12672780, -12737806, -12802577, -12933139, -12998166,
-            -13128729, -13193500, -13324063, -13389091, -13519654, -13584682, -13714989, -13780017,
-            -13910581, -13975609, -14106173, -14171201, -14301765, -14366793, -14431822, -14562386,
-            -14627414, -14692442, -14757471, -14822499, -14887527, -14952556, -14952048, -15017332,
-            -15082361, -15081853, -15147137, -15146629, -15146121, -15145869, -15145361, -15145109,
-            -15079065, -15078812, -15013024, -15012515, -14946726, -14880938, -14749356, -14683567,
-            -14617778, -14486453, -14355127, -14223801, -14092475, -13961405, -13764543, -13633217,
-            -13436355, -13239748, -13108422, -12911559, -12714952, -12518089, -12255946, -12059339,
-            -11862476, -11600333, -11403725, -11141582, -10879182, -10682575, -10420431, -10158287,
-            -09896143, -09633999, -09372111, -09175503, -08913359, -08651215, -08389327, -08127183,
-            -07865294, -07603150, -07341262, -07079117, -06817229, -06555341, -06293452, -06031308,
-            -05834955, -05573067, -05311178, -05114826, -04852937, -04656585, -04394952, -04198600,
-            -04002247, -03740358, -03544262, -03347910, -03217349, -03020997, -02824900, -02628804,
-            -02497987, -02301891, -02171331, -02040770, -01910210, -01779394, -01648834, -01518273,
-            -01387713, -01257153, -01126849, -01061824, -00931264, -00866240, -00801216, -00670656,
-            -00605888, -00540864, -00475840, -00411072, -00346048, -00346560, -00281792, -00282304,
-            -00217536, -00218048, -00153280, -00153793, -00154561, -00155073, -00155841, -00156354,
-            -00157122, -00157634, -00158403, -00224451, -00225219, -00291524, -00292036, -00358341,
-            -00424389, -00490694, -00491206, -00557511, -00623559, -00755400, -00821705, -00887754,
-            -00954058, -01085643, -01151948, -01283533, -01349837, -01481422, -01613263, -01679312,
-            -01811153, -01942738, -02074579, -02206164, -02337749, -02469590, -02601175, -02733016,
-            -02930137, -03061978, -03193563, -03390940, -03522526, -03654111, -03851488, -03983073,
-            -04180450, -04377572, -04509157, -04706534, -04838119, -05035497, -05232618, -05429739,
-            -05561580, -05758702, -05956079, -06153200, -06350322, -06482163, -06679284, -06876662,
-            -07073783, -07270904, -07468282, -07665403, -07862524, -08059902, -08257023, -08388608,
-        }.Select(argb => (uint)argb).ToArray();
-
-        Color[] colors = rgbColors.Select(Color.FromARGB).ToArray();
+            4281340475, 4281472323, 4281538634, 4281604945, 4281671256, 4281737567, 4281803878, 4281870189,
+            4281936499, 4282002809, 4282068864, 4282135174, 4282201483, 4282267793, 4282334103, 4282334876,
+            4282400930, 4282467239, 4282468012, 4282534321, 4282534837, 4282601146, 4282667455, 4282668227,
+            4282668743, 4282735051, 4282735823, 4282736339, 4282802646, 4282803418, 4282803933, 4282804704,
+            4282805219, 4282871526, 4282872297, 4282872811, 4282873582, 4282874096, 4282874866, 4282809844,
+            4282810614, 4282811128, 4282811898, 4282812411, 4282747644, 4282748157, 4282683390, 4282618366,
+            4282553599, 4282488575, 4282423807, 4282293246, 4282228478, 4282097917, 4282033148, 4281902587,
+            4281837818, 4281707512, 4281576951, 4281446389, 4281316084, 4281251058, 4281120752, 4280990190,
+            4280859883, 4280794857, 4280664295, 4280533988, 4280468962, 4280338399, 4280273373, 4280208346,
+            4280077784, 4280013013, 4279947986, 4279948496, 4279883213, 4279818186, 4279818696, 4279819205,
+            4279819714, 4279819968, 4279820477, 4279886523, 4279886777, 4279952566, 4280084148, 4280149938,
+            4280281519, 4280347308, 4280478634, 4280675495, 4280807076, 4281003937, 4281135262, 4281332123,
+            4281528984, 4281725844, 4281922705, 4282185102, 4282381962, 4282644359, 4282841220, 4283103360,
+            4283365757, 4283628154, 4283824758, 4284087155, 4284349551, 4284611692, 4284874089, 4285136230,
+            4285398626, 4285660767, 4285922908, 4286185049, 4286447446, 4286644051, 4286906193, 4287168334,
+            4287364939, 4287627081, 4287823687, 4288085572, 4288282178, 4288478784, 4288675135, 4288806205,
+            4289002556, 4289199162, 4289329977, 4289526584, 4289722935, 4289853750, 4290050102, 4290246453,
+            4290377269, 4290573620, 4290704436, 4290900788, 4291031348, 4291227700, 4291358516, 4291554612,
+            4291685428, 4291881524, 4292012341, 4292142901, 4292338997, 4292469814, 4292600374, 4292730935,
+            4292861751, 4292992311, 4293122872, 4293253432, 4293383993, 4293514553, 4293645113, 4293710138,
+            4293840698, 4293905722, 4294036282, 4294101306, 4294231866, 4294296890, 4294361914, 4294426938,
+            4294491705, 4294556729, 4294621753, 4294686776, 4294686263, 4294751030, 4294750518, 4294815285,
+            4294814772, 4294879539, 4294879026, 4294878257, 4294877488, 4294876719, 4294875949, 4294875436,
+            4294874667, 4294873898, 4294873129, 4294806823, 4294806054, 4294739749, 4294738979, 4294672674,
+            4294671905, 4294605599, 4294539294, 4294538525, 4294472220, 4294405914, 4294339609, 4294273304,
+            4294206999, 4294140693, 4294074388, 4294008083, 4293942034, 4293875729, 4293743888, 4293677839,
+            4293611534, 4293545485, 4293413644, 4293347596, 4293216011, 4293149962, 4293018378, 4292952329,
+            4292820744, 4292689160, 4292623111, 4292491527, 4292359942, 4292228358, 4292096773, 4291965189,
+            4291833605, 4291702020, 4291570436, 4291439108, 4291307523, 4291110403, 4290979075, 4290847490,
+            4290650370, 4290519042, 4290321922, 4290190594, 4289993473, 4289862145, 4289665025, 4289468161,
+            4289271297, 4289139713, 4288942849, 4288745985, 4288548865, 4288352001, 4288155137, 4287958273,
+            4287761153, 4287498753, 4287301890, 4287105026, 4286908162, 4286645762, 4286448898, 4286186499,
+        };
+        
+        Color[] colors = argbs.Select(Color.FromARGB).ToArray();
 
         Colormap = new CustomPalette(colors);
     }
