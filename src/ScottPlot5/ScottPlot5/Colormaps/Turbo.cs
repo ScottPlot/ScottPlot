740f9f57ddb16a47ec55acb6df940dd117655472
Benjamin Lehmann <bclehmann@yahoo.ca>
2025-08-28 18:48:09 -0600

    Don't confuse Apache 2 license and notice (they're separate)

--- src/ScottPlot5/ScottPlot5/Colormaps/Turbo.cs
+++ src/ScottPlot5/ScottPlot5/Colormaps/Turbo.cs
@@ -1,6 +1,19 @@
 // Adapted from https://gist.github.com/mikhailov-work/6a308c20e494d9e0ccc29036b28faa7a
 /*
 --- NOTICE BEGIN ---
+   Copyright 2019 Google LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 
                                  Apache License
                            Version 2.0, January 2004
@@ -190,7 +203,7 @@
       same "printed page" as the copyright notice for easier
       identification within third-party archives.
 
-   Copyright 2019 Google LLC
+   Copyright [yyyy] [name of copyright owner]
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
