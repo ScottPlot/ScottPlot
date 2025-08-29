e8548e12b32a3c01f0842d4c3a9d59a7832c5e72
Benjamin Lehmann <bclehmann@yahoo.ca>
2025-08-28 18:37:01 -0600

    emit one big THIRD-PARTY-NOTICES.txt file

--- src/ScottPlot5/ScottPlot5 Tests/Unit Tests/CodeTests/ThirdPartyLicenseTests.cs
+++ src/ScottPlot5/ScottPlot5 Tests/Unit Tests/CodeTests/ThirdPartyLicenseTests.cs
@@ -9,7 +9,7 @@ internal class ThirdPartyLicenseTests
     [Test]
     public void Test_Identify_SourceFilesWithLicenses()
     {
-        HashSet<string> savedNotices = [];
+        Dictionary<string, List<string>> licenseToFiles = [];
 
         // ensure no old licenses stick around
         if (Directory.Exists(NoticeFolder))
@@ -60,16 +60,36 @@ internal class ThirdPartyLicenseTests
             Assert.That(text, Contains.Substring("--- NOTICE END ---"), errorMessage);
             string notice = ExtractNotice(text, relPath);
 
-            if (savedNotices.Contains(notice))
-                continue;
-
-            // The same license may be in different files, so name it according to the license content itself.
-            string noticeFilename = ScottPlot.Testing.MD5Hasher.GetHash(notice)[..7] + ".txt";
-            string noticeFile = Path.Combine(NoticeFolder, noticeFilename);
-            File.WriteAllText(noticeFile, notice);
-            Console.WriteLine(new Uri(noticeFile).AbsoluteUri);
-            savedNotices.Add(notice);
+            if (!licenseToFiles.ContainsKey(notice))
+                licenseToFiles[notice] = [];
+            
+            licenseToFiles[notice].Add(relPath);
         }
+        
+        var orderedNotices = licenseToFiles.Select(item => (item.Key, item.Value.Order().ToArray()))
+                                          .OrderBy(item => item.Item2[0])
+                                          .ToArray();
+        StringBuilder output = new();
+        output.AppendLine("ScottPlot includes third-party content from the projects listed below, alongside their licenses.");
+        output.AppendLine("");
+        
+        foreach (var (license, files) in orderedNotices)
+        {
+            var divider = new string('#', 80);
+            var header = $"# Used in: {String.Join(", ", files.Order())}";
+
+            output.AppendLine(divider);
+            output.AppendLine(header);
+            output.AppendLine(divider);
+            output.AppendLine("");
+            output.AppendLine(license);
+            output.AppendLine(divider);
+            output.AppendLine("");
+        }
+        
+        var noticesPath = Path.Combine(NoticeFolder, "THIRD-PARTY-NOTICES.txt");
+        File.WriteAllText(noticesPath, output.ToString());
+        Console.WriteLine(new Uri(noticesPath).AbsoluteUri);
     }
 
     public string ExtractNotice(string text, string relPath)
