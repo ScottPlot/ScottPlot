f1ca7e0ace6fbd207c63dd6dcf226d1d14a55d5c
Bill Tarbell <btarb24@gmail.com>
2026-01-16 14:25:49 -0500

    xxWithLineXX marker shapes

---
+++ src/ScottPlot5/ScottPlot5/Markers/TriangleWithLineLeft.cs
@@ -0,0 +1,40 @@
+namespace ScottPlot.Markers;
+
+internal class TriangleWithLineLeft : IMarker
+{
+    public void Render(SKCanvas canvas, Paint paint, Pixel center, float size, MarkerStyle markerStyle)
+    {
+        float halfWidth = size / 2;
+
+        //Horizontal line
+        SKPath line = new();
+        line.MoveTo(center.X + halfWidth, center.Y);
+        line.LineTo(center.X - halfWidth, center.Y);
+        Drawing.DrawPath(canvas, paint, line, markerStyle.LineStyle);
+
+        //Triangle
+        var triangleHeight = (float)(size * .8);
+        var triangleWidth = (float)(size * .5);
+        var centerOfTriangle = center.MovedLeft(halfWidth - (triangleWidth / 2));
+        
+        var left = centerOfTriangle.X - (triangleWidth / 2);
+        var right = centerOfTriangle.X + (triangleWidth / 2);
+        var top = centerOfTriangle.Y - (triangleHeight / 2);
+        var bottom = centerOfTriangle.Y + (triangleHeight / 2);
+
+        SKPoint[] pointsList =
+        {
+            new (left, top),
+            new (right, centerOfTriangle.Y),
+            new (left, bottom),
+        };
+
+        SKPath path = new();
+        path.AddPoly(pointsList);
+
+        PixelRect rect = new(left, right, bottom, top);
+        Drawing.FillPath(canvas, paint, path, markerStyle.FillStyle, rect);
+        Drawing.DrawPath(canvas, paint, path, markerStyle.OutlineStyle);
+
+    }
+}
